92db0fdbb01607c4d1d4c1e5c274e04f1e60fde2
naol86 <naol.kasinet@a2sv.org>
2024-08-14 00:14:25 +0300

    set up side bar colors

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/NavBar/NavBar.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/NavBar/NavBar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function NavBar() {
   return (
-    <div className='w-full p-5 bg-blue-50 h-[80px]'>
+    <div className='w-full p-5 bg-white h-[80px]'>
       <p className=''>
         pls test your components and check if it is responsive too to check your components
         <span className='hidden sm:inline'>
---
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/SideBarItems/SideBarItems.tsx
@@ -0,0 +1,6 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function SideBarItems() {
+  return <div>image</div>;
+}

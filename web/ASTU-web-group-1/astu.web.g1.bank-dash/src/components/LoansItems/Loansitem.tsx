7035ef76d52bc1b787d05f9253281ccf31acaba8
Your GitHub Username <your-email@example.com>
2024-08-14 08:34:45 -0900

    Loan Items cat added

---
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/LoansItems/Loansitem.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import LoanCard from "./LoanCard";
+
+const Loansitem = () => {
+  return (
+    <div className="flex overflow-x-auto justify-around overflow-clip whitespace-nowrap w-full">
+      <LoanCard
+        image={"/assets/icons/personal.svg"}
+        name={"Personal Loans"}
+        amount={`$${50000}`}
+      />
+      <LoanCard
+        image={"/assets/icons/bag.svg"}
+        name={"Corporate Loans"}
+        amount={`$${100000}`}
+      />
+      <LoanCard
+        image={"/assets/icons/Businesstrack.svg"}
+        name={"Business Loans"}
+        amount={`$${500000}`}
+      />
+      <LoanCard
+        image={"/assets/icons/customLoan.svg"}
+        name={"Custom Loans"}
+        amount={`Choose Money`}
+      />
+    </div>
+  );
+};
+
+export default Loansitem;

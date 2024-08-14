68f42544a3d87e01c340a72772381a0c4a4c93b6
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-08-14 16:38:53 +0300

    first commit

--- web/AASTU-web-group-2/a2sv-banking-system/app/dashboard/page.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import ImageComponent from '../components/QuickTransfer'
 const page = () => {
   return (
     <div>
       Dashboard
+      <ImageComponent></ImageComponent>
     </div>
   )
 }

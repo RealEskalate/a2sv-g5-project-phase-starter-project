320bdd36258eed8acfb38ad3d8f6908ed0500318
naol86 <naol.kasinet@a2sv.org>
2024-08-14 11:47:35 +0300

    edited side bar

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/SideBarItems/SideBarItems.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/SideBarItems/SideBarItems.tsx
@@ -11,11 +11,7 @@ export default function SideBarItems({
   active: boolean;
 }) {
   return (
-    <div
-      className={`${
-        active ? 'text-blue' : 'text-gray-light'
-      } flex font-semibold items-center my-6 text-16px`}
-    >
+    <div className={`${active ? 'text-blue' : 'text-gray-light'} flex items-center my-6 text-16px`}>
       {active && <div className='w-1.5 h-10 bg-blue rounded-r-[10px] absolute'></div>}
       <Icon className='w-[21px] h-[21px] mx-5 ml-6 object-cover fon' />
       <p className='font-inter'>{title}</p>

fe83886dd5c399df6f0ecc6e3073870b287fcb24
naol86 <naol.kasinet@a2sv.org>
2024-08-14 17:49:00 +0300

    pull avater from chalas branch

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/NavBar/NavBar.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/NavBar/NavBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Hamburger from '../../../public/assets/icons/hamburger-icon.svg';
+import Settings from '../../../public/assets/icons/setting-icon.svg';
+import Notification from '../../../public/assets/icons/notification-icon.svg';
 
 export default function NavBar() {
   return (
@@ -7,8 +9,15 @@ export default function NavBar() {
       <button type='button' className='items-center rounded-lg sm:hidden font-bold'>
         <Hamburger className='w-7 h-7 object-cover' />
       </button>
-      <p className=''>url</p>
-      <p>pp</p>
+      <p className=''>active</p>
+      <div className='flex space-x-2'>
+        <div className='w-11 h-11 bg-slate-50 rounded-full flex justify-center items-center text-blue-steel'>
+          <Settings className='w-6 h-6' />
+        </div>
+        <div className='w-11 h-11 bg-slate-50 rounded-full flex justify-center items-center text-red-400'>
+          <Notification className='w-6 h-6' />
+        </div>
+      </div>
     </div>
   );
 }

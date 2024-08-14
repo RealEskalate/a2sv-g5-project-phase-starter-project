bcfe08bcb2cb4ddd6f368ba2a20c36eef14896b6
Kuma Telila <kumatelila26@gmail.com>
2024-08-14 14:07:36 +0300

    Sidebar component

---
+++ web/ASTU-web-group-2/banking-dashboard/app/components/sidebar/Sidebar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { sidebarListItems } from "./sidebarListItems";
+import Link from "next/link";
+import Image from "next/image";
+
+const Sidebar = () => {
+  return (
+    <div className="flex flex-col pt-[114px] w-[179px] h-[420px] gap-2">
+      {sidebarListItems.map((item, index) => (
+        <div className="flex gap-6">
+          <Link className="flex gap-5 " key={index} href={item.path}>
+            {index === 0 ? (
+              <div className="h-[50px] w-[5px] bg-[#2D60FF] rounded-r-lg"></div>
+            ) : (
+              <div className="h-[50px] w-[5px] active:bg-[#2D60FF] rounded-r-lg"></div>
+            )}
+            <div className="flex gap-5 pt-3">
+              <div>
+                <Image src={`${item.icon}`} alt="icon" width={20} height={20} />
+              </div>
+              {index === 0 ? (
+                <h1 className="font-medium text-[16px] text-[#2D60FF]  "> {item.name}</h1>
+              ):(
+                <h1 className="font-medium text-[16px]  text-[#B1B1B1] active:text-[#2D60FF] ">
+                  {item.name}
+                </h1>
+              )}
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;

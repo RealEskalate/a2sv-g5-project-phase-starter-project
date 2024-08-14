0d8bcd74768ede1d5549dc08e31cffb4d86924f0
kiya kebe <kiyakebe799@gmail.com>
2024-08-14 03:00:23 +0300

    styled: made input Group responsive

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/InputGroup.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/InputGroup.tsx
@@ -8,7 +8,7 @@ const InputGroup = ({
   placeholder,
 }: InputGroupType) => {
   return (
-    <div className="w-6/12 space-y-3 my-3">
+    <div className=" w-full lg:w-6/12 space-y-3 my-3">
       <label htmlFor={id} className="gray-dark text-16px">
         {label} <br />
       </label>
@@ -16,7 +16,7 @@ const InputGroup = ({
         type={inputType}
         id={id}
         placeholder={placeholder}
-        {...register(registerName)}
+        // {...register(registerName)}
         className="w-full border-2 border-[#DFEAF2] p-5 py-3 rounded-xl placeholder:text-blue-steel focus:border-blue-steel outline-none"
       />
     </div>

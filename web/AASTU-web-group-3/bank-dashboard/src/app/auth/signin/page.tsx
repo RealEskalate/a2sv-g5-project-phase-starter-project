a2bb87114ddddc45d36e35d4d24a03cc801199b7
Amanuel Mandefrow <amanuelmandefro3@gmail.com>
2024-08-14 18:05:28 +0300

    create login

---
+++ web/AASTU-web-group-3/bank-dashboard/src/app/auth/signin/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './component/Form'
+
+const SigninPage = () => {
+  return (
+    <div>
+      <Form/>
+    </div>
+  )
+}
+
+export default SigninPage
+
+
+

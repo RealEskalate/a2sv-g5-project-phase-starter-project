68a0f1efd13e9bda15ef698b807a466f090d9870
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-08-14 15:58:33 +0300

    Implement bottom part of the component

--- web/ASTU-web-group-2/banking-dashboard/tailwind.config.ts
+++ web/ASTU-web-group-2/banking-dashboard/tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      boxShadow: {
+        'custom-shadow': '4px 4px 18px -2px rgba(231, 228, 232, 0.8)',
+      },
     },
   },
   plugins: [],

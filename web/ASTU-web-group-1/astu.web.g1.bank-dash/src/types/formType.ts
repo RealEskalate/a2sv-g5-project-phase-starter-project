42d127ba2245ab7f9e7275c6d1fa24546cfa844a
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-08-14 03:30:04 +0300

    created: InputGroup (#16)
    * created: InputGroup
    
    * styled: made input Group responsive

---
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/types/formType.ts
@@ -0,0 +1,9 @@
+// Form Types - Input Group ( Label and INput Fiels )
+export interface InputGroupType {
+  id: string;
+  label: string;
+  inputType: string;
+  registerName: string;
+  register: any;
+  placeholder: string;
+}

4812a2f3e61cad62f7f6bf633ca711bb96db8ee5
Your GitHub Username <your-email@example.com>
2024-08-14 09:11:17 -0900

    Loan Items Added

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
@@ -1,5 +1,10 @@
+import Loansitem from "@/components/LoansItems/Loansitem";
 import MyCard from "@/components/MyCard/MyCard";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Loansitem />
+    </>
+  );
 }

222fced71732fe9b3c149757c2b490d7dfdef4ab
kiya kebe <kiyakebe799@gmail.com>
2024-08-14 17:41:10 +0300

    finished the security section and put it all together

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/ToggleInput.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/ToggleInput.tsx
@@ -20,10 +20,11 @@ const ToggleInput = ({
           // {...register(registerName)}
           className="sr-only peer"
         />
-        <div className={` ${currentState && 'bg-[#16dbcc]'} relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-[#16dbcc] rounded-full peer 
+        <div className={`${currentState ? 'bg-[#16dbcc]' : 'bg-gray-200'} relative w-11 h-6 peer-focus:outline-none peer-focus:ring-2 peer-focus:ring-[#16dbcc] rounded-full peer 
         peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] 
         after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full 
-        after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600`}></div>
+        after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600`}></div>
+
         <span className="ms-3 text-sm font-medium text-gray-900">
           {label}
         </span>

4777d8cec8db2c37f638c28975dc49687fa68f8f
dipherent1 <Bini.Blue.1996@gmail.com>
2024-08-15 14:30:54 +0300

    create close db function and run router

--- backend/AASTU-backend-group-7/Delivery/routers/routers.go
+++ backend/AASTU-backend-group-7/Delivery/routers/routers.go
@@ -3,7 +3,7 @@ package routers
 import (
 	config "blogapp/Config"
 	"blogapp/Domain"
-	"fmt"
+	"context"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -11,18 +11,27 @@ import (
 
 var router *gin.Engine
 
-func Setuprouter() {
+func Setuprouter() *gin.Engine {
+	// Initialize the database
 	client := config.ConnectDB()
 	log.Println(client)
-
 	DataBase := client.Database("Blog")
+
+	// Defer the closing of the database
+	defer func() {
+		if err := client.Disconnect(context.TODO()); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	// Initialize the collections
 	BlogCollections := Domain.Collections{
 
 		Users: DataBase.Collection("Users"),
 		Blogs: DataBase.Collection("Blogs"),
 	}
-	
-	fmt.Println(BlogCollections)
 
+	// Initialize the router
 	router = gin.Default()
+	return router
 }

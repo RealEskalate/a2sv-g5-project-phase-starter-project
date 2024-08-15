9938048718fe9bb7cadcab70663ed105b679d8a7
kika1s1 <tamiratkebede120@gmail.com>
2024-08-15 21:29:04 +0300

    findbyemail method is implemted is added

--- backend/ASTU-backend-group-5/internal/repository/mongodb/user_repo.go
+++ backend/ASTU-backend-group-5/internal/repository/mongodb/user_repo.go
@@ -1,1 +1,26 @@
 package mongodb
+
+import (
+	"blogApp/internal/domain"
+	"context"
+	"errors"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type UserRepositoryMongo struct {
+	Collection *mongo.Collection
+}
+
+
+
+func (r *UserRepositoryMongo) FindByEmail(ctx context.Context, email string) (*domain.User, error) {
+	user := &domain.User{}
+	err := r.Collection.FindOne(ctx, bson.M{"email": email}).Decode(user)
+	if err == mongo.ErrNoDocuments {
+		return nil, errors.New("user not found")
+	}
+	return user, err
+}
+

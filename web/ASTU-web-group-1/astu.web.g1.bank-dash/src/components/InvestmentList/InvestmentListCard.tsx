679c0b2a0ca7de3572b5c5eae5dc2663ce6bd24a
Mubarak Adem <mubarakadem@gmail.com>
2024-08-15 21:41:46 +0300

    page cards added

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/InvestmentList/InvestmentListCard.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/InvestmentList/InvestmentListCard.tsx
@@ -19,7 +19,7 @@ const InvestmentListCard: React.FC<InvestmentListCardProps> = ({
 }) => {
   return (
     <div className="flex  w-full bg-white rounded-[20px] p-2 ">
-      <div className="flex-shrink-0 mr-4 ">
+      <div className="flex flex-shrink-0 mr-4 items-center">
         <img
           className="lg:w-[55px] lg:h-[55px] w-[40px] h-[40px] sm:rounded-[20px]"
           src={imageUrl}
@@ -27,19 +27,19 @@ const InvestmentListCard: React.FC<InvestmentListCardProps> = ({
         />
       </div>
       <div className="flex-1 min-w-0 py-2 ">
-        <p className="text-sm text-[#232323]">{companyName}</p>
-        <p className="text-xs text-[#718EBF]">E-commerce, Marketplace</p>
+        <p className="text-sm text-[#232323] truncate">{companyName}</p>
+        <p className="text-xs text-[#718EBF] truncate ...">E-commerce, Marketplace</p>
       </div>
       <div className="hidden  md:block flex-1 min-w-0 py-2">
-        <p className="text-sm text-[#232323]">${amount}</p>
-        <p className="text-xs text-[#718EBF]">Envestment Value</p>
+        <p className="text-sm text-[#232323] truncate">${amount}</p>
+        <p className="text-xs text-[#718EBF] truncate ...">Envestment Value</p>
       </div>
       <div className=" min-w-0 py-2">
-        <p className="text-sm text-[#232323]" style={{ color }}>
+        <p className="text-sm text-[#232323] truncate" style={{ color }}>
           {sign}
           {returnValue}
         </p>
-        <p className="text-xs text-[#718EBF]">Return Value</p>
+        <p className="text-xs text-[#718EBF] truncate">Return Value</p>
       </div>
     </div>
   );

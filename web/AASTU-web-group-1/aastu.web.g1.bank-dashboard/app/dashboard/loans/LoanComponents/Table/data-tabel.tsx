300d70fc782fd03af23bf678facb52ecec697151
manuel-heav <manuelheav32@gmail.com>
2024-08-15 16:57:05 +0300

    Done with the settings page

---
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/loans/LoanComponents/Table/data-tabel.tsx
@@ -0,0 +1,74 @@
+export const Invoices = [
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+    {
+      SLNo: "INV001",
+      LoanMoney:"100,000",
+      LeftToPay: "40,000",
+      Duration: "8 Months",
+      InterestRate: "10%",
+      Installment:"$2,000/month",
+      Repay:""
+    },
+  ]

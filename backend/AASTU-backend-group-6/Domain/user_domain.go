d2d2661bb053b609991790c5570db19fbc458b41
yordanos legesse <yordanoslegesse15@gmail.com>
2024-08-15 13:33:50 +0300

    user repository

--- backend/AASTU-backend-group-6/Domain/user_domain.go
+++ backend/AASTU-backend-group-6/Domain/user_domain.go
@@ -16,3 +16,26 @@ type User struct {
 	Role   string           `json:"roles"`
 	
 }
+
+
+type UserRepository interface {
+	CreateUser(user User) (User, error)
+	FindUserByEmail(email string) (User, error)
+	FindUserByUsername(username string) (User, error)
+	FindUserByID(id string) (User, error)
+	UpdateUser(user User) (User, error)
+	DeleteUser(id string) error
+	ForgotPassword(email string, token string) error
+}
+
+type UserUseCase interface { 
+	CreateUser(user User) interface{}
+	FindUserByEmail(email string) interface{}
+	FindUserByUsername(username string) interface{}
+	FindUserByID(id string) interface{}
+	UpdateUser(user User) interface{}
+	DeleteUser(id string) interface{}
+	ForgotPassword(email string, token string) interface{}
+	
+}
+

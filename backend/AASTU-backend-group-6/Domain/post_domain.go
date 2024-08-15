c96190a391927b73b50ed376fe6f2147b0b8ee8f
Daniel Asfaw <asfawdaniel779@gmail.com>
2024-08-15 13:51:58 +0300

    update to domain files

--- backend/AASTU-backend-group-6/Domain/post_domain.go
+++ backend/AASTU-backend-group-6/Domain/post_domain.go
@@ -7,21 +7,22 @@ import (
 )
 
 type Post struct {
-	ID          primitive.ObjectID `bson:"_id,omitempity" json:"id" `
-	Author      string             `json:"author"`
-	Title       string             `json:"title"`
-	Content	 string             `json:"content"`
-	Tags	 []string           `json:"tags"`
-	UserID	  string             `json:"userID"`
-	CreatedAt  time.Time		  `json:"createdAt"`
-	UpdatedAt  time.Time		  `json:"updatedAt"`
-	Comments   []Comment         `json:"comments"`
-
+	ID         primitive.ObjectID `bson:"_id,omitempity" json:"id" `
+	Author     string             `json:"author"`
+	Title      string             `json:"title"`
+	Content    string             `json:"content"`
+	Tags       []string           `json:"tags"`
+	Creater_id primitive.ObjectID `json:"creater_id"`
+	CreatedAt  time.Time          `json:"createdAt"`
+	UpdatedAt  time.Time          `json:"updatedAt"`
+	Comments   []Comment          `json:"comments"`
+	Post_image string             `json:"post_image"`
 }
 
 type Comment struct {
-	ID          primitive.ObjectID `bson:"_id,omitempity" json:"id" `
-	Commentor      string             `json:"Commentor"`
-	Content	 string             `json:"content"`
+	ID                 primitive.ObjectID `bson:"_id,omitempity" json:"id" `
+	Post_ID            primitive.ObjectID `json:"post_id"`
+	Commentor_ID       primitive.ObjectID `json:"commentor_id"`
+	Commentor_username string             `json:"commentor_username"`
+	Content            string             `json:"content"`
 }
-
--- backend/AASTU-backend-group-6/Domain/user_domain.go
+++ backend/AASTU-backend-group-6/Domain/user_domain.go
@@ -3,16 +3,17 @@ package domain
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `bson:"_id,omitempity" json:"id" `
-	Email    string             `json:"email"`
-	Username string             `json:"username"`
-	Password string             `json:"password"`
-	Profile  string             `json:"profile"`
-	GoogleID string             `json:"googleId"`
-	Posts	[]Post           `json:"posts"`
-	ResetToken string           `json:"resetToken"`
-	Contact  string             `json:"contact"`	
-	Bio	  string             `json:"bio"`		
-	Role   string           `json:"roles"`
-	
+	ID                primitive.ObjectID `bson:"_id,omitempity" json:"id" `
+	Email             string             `json:"email"`
+	Username          string             `json:"username"`
+	Password          string             `json:"password"`
+	Profile_image_url string             `json:"profile_image"`
+	GoogleID          string             `json:"googleId"`
+	Posts             []Post             `json:"posts"`
+	RefreshToken      string             `json:"refreshToken"`
+	AccessToken       string             `json:"accessToken"`
+	Contact           string             `json:"contact"`
+	Bio               string             `json:"bio"`
+	Role              string             `json:"roles"`
+	Comments          []Comment          `json:"comments"`
 }

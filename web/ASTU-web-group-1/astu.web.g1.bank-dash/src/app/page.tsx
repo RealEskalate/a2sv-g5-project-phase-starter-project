0401f0ccd054b288c680341c713fa5caec9afd33
kiya kebe <kiyakebe799@gmail.com>
2024-08-15 23:58:48 +0300

    finished: YearlyTotalInvestment and MonthlyRevenue Charts

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
@@ -1,5 +1,7 @@
 import DepateAndCredit from "@/components/Charts/DepateAndCredit";
+import MonthlyRevenue from "@/components/Charts/MonthlyRevenue";
 import MyExpence from "@/components/Charts/MyExpence";
+import YearlyTotalInvestment from "@/components/Charts/YearlyTotalInvestment";
 import dynamic from "next/dynamic";
 const BalanceHistory = dynamic(
   () => import("@/components/Charts/BalanceHistory"),
@@ -9,7 +11,8 @@ const BalanceHistory = dynamic(
 export default function Home() {
   return (
     <div className="md:flex md:space-x-6">
-      <DepateAndCredit />
+      <YearlyTotalInvestment />
+      <MonthlyRevenue />
     </div>
   );
 }

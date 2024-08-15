8c3893ca1663276e912a15efdbe8f871f2c6e36f
nanati45 <nansiasamnew@gmail.com>
2024-08-15 23:20:34 +0300

    Credit card updated

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/credit-card/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/credit-card/page.tsx
@@ -1,5 +1,20 @@
+import AddNewCard from "@/components/AddNewCard/AddNewCard";
+import CardSettings from "@/components/CardSettings/CardSettings";
+import MyCard from "@/components/MyCard/MyCard";
 import React from "react";
 
 export default function page() {
-  return <div>credit card</div>;
+  return (
+    <div>
+      <div className="flex overflow-x-scroll space-x-2 scroll whitespace-nowrap scroll-smooth lg:flex lg:space-x-4  ">
+        <MyCard />
+        <MyCard />
+        <MyCard />
+      </div>
+      <div className="md:flex md:gap-6">
+        <AddNewCard />
+        <CardSettings />
+      </div>
+    </div>
+  );
 }

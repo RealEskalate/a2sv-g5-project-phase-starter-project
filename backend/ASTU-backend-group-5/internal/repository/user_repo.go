5a2cb3ba0de83d4b916fb2da9970122b51fa508d
kika1s1 <tamiratkebede120@gmail.com>
2024-08-15 21:28:20 +0300

    user interfece, with findbyemail method is added

--- backend/ASTU-backend-group-5/internal/repository/user_repo.go
+++ backend/ASTU-backend-group-5/internal/repository/user_repo.go
@@ -1,1 +1,17 @@
 package repository
+
+import (
+	"blogApp/internal/domain"
+	"context"
+)
+
+// UserRepository is an interface that defines the methods a user repository should implement
+// You can add more methods if needed
+// FindByUserName(ctx context.Context, username string) (*domain.User, error)
+// Update(ctx context.Context, user *domain.User) error
+// Delete(ctx context.Context, id string) error
+// CreateUser(ctx context.Context, user *domain.User) error
+type UserRepository interface {
+	FindByEmail(ctx context.Context, email string) (*domain.User, error)
+	
+}

9f48527733f2f7547c71c8cebf0804feb21de931
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-15 11:04:46 -0700

    emil sending made background process

--- backend/ASTU-backend-group-5/cmd/app/main.go
+++ backend/ASTU-backend-group-5/cmd/app/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"blogApp/internal/http/routes"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	route := gin.Default()
+	routes.RegisterVerificationRoutes(route)
+
+	route.Run()
 }

67efafe4b106b3f0ecefc9143b391af64e2a0005
dura12 <duranu104@gmail.com>
2024-08-15 10:00:32 +0300

    recent transaction and invoices sent finished

--- web/ASTU-web-group-2/banking-dashboard/app/components/recent-transaction/RecentTransaction.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/recent-transaction/RecentTransaction.tsx
@@ -1,38 +1,50 @@
 import React from 'react';
+const recentlistitems = [
+  {   
+      transactionName: "Deposit from my",
+      date: "28 January 2021", 
+      amount: "-$880",
+      isDeposited: false,
+      icons: "/assets/recentTransaction/icon1.svg"
+  },
+  {
+      transactionName: "Deposit Paypal",
+      date: "28 January 2021",
+      amount: "+$2500",
+      isDeposited: true,
+      icons: "/assets/recentTransaction/icon2.svg"
+  },
+  {
+      transactionName: "Jemi Wilson",
+      date: "28 January 2021",
+      amount: "+$5,400",
+      isDeposited: true,
+      icons: "/assets/recentTransaction/icon3.svg"
+  }
+];
 
 const RecentTransaction = () => {
   return (
-    <div className='w-[350px] h-[282px]'>
-      <h1 className='mb-[40px] text-[22px] text-[#343C6A] font-semibold'>Recent Transaction</h1>
-      <div className='bg-white drop-shadow-xl font-medium rounded-[25px] p-[25px]'>
-        <div className='flex flex-col flex-wrap gap-3'>
-          <div className='flex items-center gap-2'>
-            <img src='/assets/recentTransaction/p1.png' alt='Icon 1' className='w-[55px] h-[55px]' />
-            <div className='flex flex-col basis-1/2 g-1 ml-2'>
-              <p className='text-[16px] leading-[19.36px]'>Deposit from my</p>
-              <p className='text-[#718EBF] text-[15px] leading-[18.15px]'>28 January 2021</p>
+    <div className='max-w-[350px] mx-auto h-auto'>
+      <h1 className='mb-[20px] text-[22px] text-[#343C6A] font-semibold'>Recent Transactions</h1>
+      <div className='flex  flex-col flex-initial flex-wrap gap-[10px] bg-white drop-shadow-xl font-medium rounded-[25px] p-[25px]'>
+          {recentlistitems.map((value, index) => (
+            <div key={index} className='flex items-center gap-3'>
+              <img src={value.icons} alt='Icon'  />
+              <div className='flex flex-col  gap-1'>
+                <p className='text-[16px] text-[#232323]  leading-[19.36px]'>
+                  {value.transactionName}
+                </p>
+                <p className='text-[15px] leading-[18.36px] text-[#718EBF]'>{value.date}</p>
+              </div>
+              <p className='text-lg ml-auto' style={{ color: value.isDeposited ? '#41D4A8' : '#FF4B4A' }}>
+                {value.amount}
+              </p>
             </div>
-            <p className='text-[#FF4B4A] ml-auto '>-$850</p>
-          </div>
-          <div className='flex items-center gap-2 '>
-            <img src='/assets/recentTransaction/p2.png' alt='Icon 2' className='w-[55px] h-[55px]' />
-            <div className='flex flex-col flex-wrap basis-1/2  gap-1 ml-2'>
-              <p className='text-[16px] leading-[19.36px]'>Deposit Paypal</p>
-              <p className='text-[#718EBF] text-[15px] leading-[18.15px]'>28 January 2021</p>
-            </div>
-            <p className=' text-[#41D4A8] ml-auto'>+$2,5000</p>
-          </div>
-          <div className='flex items-center gap-2'>
-            <img src='/assets/recentTransaction/p3.png' alt='Icon 3' className='w-[55px] h-[55px]' />
-            <div className='flex flex-col  flex-wrap basis-1/2  gap-1 ml-2'>
-              <p className='text-[16px] leading-[19.36px]'>Jemi Wilson</p>
-              <p className='text-[#718EBF] text-[15px] leading-[18.15px]'>28 January 2021</p>
-            </div>
-            <p className='flex flex-wrap  text-[#41D4A8] ml-auto'>+$5,4000</p>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
+
   );
 }
 

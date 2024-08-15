9df21d80177ebd7e1ce9ed46da30b724984f3d97
urjilaa <urji53210@gmail.com>
2024-08-15 06:37:23 -0700

    urji updates

--- web/AASTU-web-group-4/next-bank/app/(root)/accounts/page.tsx
+++ web/AASTU-web-group-4/next-bank/app/(root)/accounts/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import BalanceCard from '@/components/AccountSmallCard'
+import App from '@/components/LastTransactionCard'
 
 const Accounts = () => {
   return (
-    <div>Accounts</div>
+    <div>
+      <div>
+        <h1>Accounts</h1>
+        <div><BalanceCard/></div>
+      </div>
+      <div>
+        <h1>Last Transaction</h1>
+        <div><App/></div>
+      </div>
+    </div>
   )
 }
 

3388dca2529b1968c0500c768be1bd6c1054b2bb
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-15 07:07:44 -0700

    email sending and password reset logic done and not integrated with others work

--- backend/ASTU-backend-group-5/cmd/app/main.go
+++ backend/ASTU-backend-group-5/cmd/app/main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"blogApp/internal/http/routes"
-	"fmt"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	router := gin.Default()
 
-	routes.RegisterVerificationRoutes(router)
-
-	router.Run(fmt.Sprintf(":%d", 8080))
 }

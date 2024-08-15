7303384d606fa7315ada82ab2ed190ea7aa9d021
kaleb wondimu <kalebwondimu33@gmail.com>
2024-08-15 15:25:19 +0300

    CreditCard component done

--- web/ASTU-web-group-2/banking-dashboard/app/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import CreditCard from "./components/creditCard/CreditCard";
 const page = () => {
   return (
-    <div></div>
+    <div>
+      <CreditCard
+        balance={1250}
+        cardHolder="John Doe"
+        expiryDate="12/24"
+        cardNumber="1234 5678 9012 3456"
+        cardType="tertiary" // Can be "primary", "secondary", or "tertiary"
+      />
+    </div>
+  );
+};
 
-  )
-}
-
-export default page
+export default page;
--- web/ASTU-web-group-2/banking-dashboard/tailwind.config.ts
+++ web/ASTU-web-group-2/banking-dashboard/tailwind.config.ts
@@ -12,9 +12,13 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "credit-card-gradient":
+          "linear-gradient(107.38deg, #4C49ED 2.61%, #0A06F4 101.2%)",
+        "secondary-credit-card":
+          "linear-gradient(107.38deg, #2D60FF 2.61%, #539BFF 101.2%)",
       },
       boxShadow: {
-        'custom-shadow': '4px 4px 18px -2px rgba(231, 228, 232, 0.8)',
+        "custom-shadow": "4px 4px 18px -2px rgba(231, 228, 232, 0.8)",
       },
     },
   },

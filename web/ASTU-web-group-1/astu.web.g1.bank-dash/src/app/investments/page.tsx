aab10af9206c8c4b36d0a096f198a89eb6dead2a
Mubarak Adem <mubarakadem@gmail.com>
2024-08-15 21:41:46 +0300

    page cards added

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/investments/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/investments/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import InvestmentItem from '@/components/InvestmentItems/InvestmentItem';
+import InvestmentList from '@/components/InvestmentList/InvestmentList';
+import TrendingList from '@/components/TrendingStock/trendingList';
+
 export default function page() {
-  return <div>
-    <InvestmentItem/>
-   
-  </div>;
+    return (
+    <div>
+      <InvestmentItem/>
+      <div className='border h-[282px]'>
+
+      </div>
+      <div className="flex flex-col md:flex-row">
+      <InvestmentList />
+      <TrendingList />
+      </div>
+  </div>
+  );
 }

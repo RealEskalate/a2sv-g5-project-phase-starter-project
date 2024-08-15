5dc7f574be2ef514c422d577336aa24deac9b0b8
chalaa <chalaolanigeleta@gmail.com>
2024-08-15 22:51:22 +0300

    Update transaction page

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/transactions/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/transactions/page.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import MyCard from '@/components/MyCard/MyCard';
-import RecentTransactionTable from '@/components/RecentTransactionTable/RecentTransactionTable';
+import React from "react";
+import MyCard from "@/components/MyCard/MyCard";
+import RecentTransactionTable from "@/components/RecentTransactionTable/RecentTransactionTable";
 export default function page() {
   return (
     <div className="flex flex-col gap-5">
-      <div className="flex flex-row gap-6">
-        <div className="flex flex-col w-2/3 gap-4">
-          <div className='flex justify-between'>
+      <div className="flex flex-col lg:flex-row gap-6">
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
             <p>MyCard</p>
             <p>+ Add card</p>
           </div>
-          <div className='flex flex-row justify-between'>
-          <MyCard  />
-          <MyCard />
+          <div className="hidden lg:flex flex-row gap-6">
+            <MyCard />
+            <MyCard />
+          </div>
+          <div className="flex lg:hidden gap-6 overflow-x-auto">
+            <div className="flex flex-row gap-6 whitespace-nowrap">
+              <MyCard />
+              <MyCard />
+            </div>
           </div>
         </div>
-        <div className="flex flex-col w-1/3 gap-4">
-          <div className='flex justify-between'>
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-between">
             <p>My Expense</p>
           </div>
-          <div className='flex'>
-          <MyCard  />
+          <div className="flex">
+            <MyCard />
           </div>
         </div>
       </div>

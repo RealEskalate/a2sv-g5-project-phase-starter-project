86281c9fc1165b2a60e1e8fa001b63a2c9e4e7b5
try1 <abdulkerimseid0966@gmail.com>
2024-08-15 17:34:24 +0300

    first progress in interfeces

--- backend/AASTU-backend-group-1/domain/user.go
+++ backend/AASTU-backend-group-1/domain/user.go
@@ -1,1 +1,24 @@
 package domain
+
+import "time"
+
+type User struct {
+	FirstName  string    `json:"firstname"`
+	LastName   string    `json:"lastname"`
+	Bio        string    `json:"bio"`
+	Avatar     string    `json:"avatar"`
+	UserName   string    `json:"username" binding:"required"`
+	Password   string    `json:"password" binding:"required"`
+	Email      string    `json:"email" binding:"required"`
+	Role       string    `json:"role" `
+	Address    string    `json:"address"`
+	JoinedDate time.Time `json:"joinedDate"`
+}
+
+type UserRepository interface {
+	CheckUsernameAndEmail(username, email string) ( error)
+	Register(user *User) error
+	GetUserByUsernameorEmail(usernameoremail string) (*User, error)
+	UpdateProfile(usernameoremail string, user *User) error
+	Resetpassword(usernameoremail string, password string) error
+}

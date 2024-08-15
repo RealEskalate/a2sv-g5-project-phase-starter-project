721dbad182ed6deeb5bddd84ff5590327f4968fb
chalaa <chalaolanigeleta@gmail.com>
2024-08-15 15:46:36 +0300

    Add Transaction page

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/transactions/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/transactions/page.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
-
+import MyCard from '@/components/MyCard/MyCard';
+import RecentTransactionTable from '@/components/RecentTransactionTable/RecentTransactionTable';
 export default function page() {
-  return <div>transaction</div>;
+  return (
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-row gap-6">
+        <div className="flex flex-col w-2/3 gap-4">
+          <div className='flex justify-between'>
+            <p>MyCard</p>
+            <p>+ Add card</p>
+          </div>
+          <div className='flex flex-row justify-between'>
+          <MyCard  />
+          <MyCard />
+          </div>
+        </div>
+        <div className="flex flex-col w-1/3 gap-4">
+          <div className='flex justify-between'>
+            <p>My Expense</p>
+          </div>
+          <div className='flex'>
+          <MyCard  />
+          </div>
+        </div>
+      </div>
+      <div>
+        <RecentTransactionTable />
+      </div>
+    </div>
+  );
 }

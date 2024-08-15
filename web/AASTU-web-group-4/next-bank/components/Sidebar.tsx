5c45c6632bc739e4e637bef273068e8eeb9742a1
Mistire <mdwithgod@gmail.com>
2024-08-15 09:07:50 +0300

    Implemented the Navbar and Sudebar

--- web/AASTU-web-group-4/next-bank/components/Sidebar.tsx
+++ web/AASTU-web-group-4/next-bank/components/Sidebar.tsx
@@ -2,7 +2,12 @@ import { FC } from 'react';
 import Link from 'next/link';
 import { sidebarLinks, user } from "@/constants";
 
-export const Sidebar: FC = () => {
+type SidebarProps = {
+  isOpen: boolean;
+  toggleSidebar: () => void;
+};
+
+export const Sidebar: React.FC<SidebarProps> = () => {
   return (
     <div className="hidden md:flex flex-col w-64 h-full bg-white shadow-lg p-4 fixed left-0 top-0">
       <div className="space-y-4">

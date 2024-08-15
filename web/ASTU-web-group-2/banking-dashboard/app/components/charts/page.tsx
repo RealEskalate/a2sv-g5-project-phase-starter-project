4028c7bbcab440a46e628ff1936e211332ff62f3
wondmD <wondmenehdereje@gmail.com>
2024-08-15 05:10:07 -0700

    added my-expense chart

---
+++ web/ASTU-web-group-2/banking-dashboard/app/components/charts/page.tsx
@@ -0,0 +1,13 @@
+// import DebitCreditOverviewChart from './DebitCreditOverviewChart'
+import MyExpenseChart from './MyExpenseChart'
+
+const page = () => {
+    return (
+        <div className=''>
+            {/* <DebitCreditOverviewChart /> */}
+            <MyExpenseChart />
+        </div>
+    )
+}
+
+export default page

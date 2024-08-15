d2bd270df74675bc32d50fa944cf9341d8ab69fe
dipherent1 <Bini.Blue.1996@gmail.com>
2024-08-15 17:50:54 +0300

    add registeration

--- backend/AASTU-backend-group-7/Delivery/controllers/auth_controller.go
+++ backend/AASTU-backend-group-7/Delivery/controllers/auth_controller.go
@@ -2,14 +2,18 @@ package controllers
 
 import (
 	"blogapp/Domain"
+	"fmt"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 )
 
 type authController struct {
 	AuthUseCase Domain.AuthUseCase
 }
-func NewAuthController(usecase Domain.AuthUseCase) (*authController) {
+
+func NewAuthController(usecase Domain.AuthUseCase) *authController {
 
 	return &authController{
 		AuthUseCase: usecase,
@@ -19,11 +23,32 @@ func NewAuthController(usecase Domain.AuthUseCase) (*authController) {
 // login
 func (ac *authController) Login(c *gin.Context) {
 	// return error
-	 
+
 }
 
 // register
 func (ac *authController) Register(c *gin.Context) {
 	// return error
- 
+	var newUser Domain.User
+	v := validator.New()
+	if err := c.ShouldBindJSON(&newUser); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid data", "error": err.Error()})
+		return
+	}
+
+	if err := v.Struct(newUser); err != nil {
+		fmt.Printf(err.Error())
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid or missing data", "error": err.Error()})
+		return
+	}
+
+	createdUser, err, statusCode := ac.AuthUseCase.Register(c, &newUser)
+
+	if err != nil {
+		c.IndentedJSON(statusCode, gin.H{"error": err.Error()})
+	} else {
+		//success
+		c.IndentedJSON(http.StatusCreated, gin.H{"message": "User created successfully", "user": createdUser})
+	}
+
 }
--- backend/AASTU-backend-group-7/go.mod
+++ backend/AASTU-backend-group-7/go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect

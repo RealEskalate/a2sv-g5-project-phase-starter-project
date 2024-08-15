061f314c51cc50d7100b94b797d5273bc8f7b611
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-08-15 11:09:34 +0300

    folder structure init

---
+++ backend/AASTU-backend-group-7/Config/env.go
@@ -0,0 +1,27 @@
+package Config_test
+
+import (
+	"log"
+	"os"
+)
+
+// Global variable to store the JWT secret
+var JwtSecret = []byte("your_jwt_secret")
+var MONGO_CONNECTION_STRING string
+
+func Envinit() {
+	JwtSecretKey := os.Getenv("JWT_SECRETE_KEY")
+	if JwtSecretKey != "" {
+		JwtSecret = []byte(JwtSecretKey)
+	} else {
+		JwtSecret = []byte("JwtSecretKey")
+		log.Fatal("JWT secret key not configured")
+	}
+	// Read MONGO_CONNECTION_STRING from environment
+	MONGO_CONNECTION_STRING = os.Getenv("MONGO_CONNECTION_STRING")
+	if MONGO_CONNECTION_STRING == "" {
+		MONGO_CONNECTION_STRING = "tst"
+		log.Fatal("MONGO_CONNECTION_STRING is not set")
+	}
+
+}
---
+++ backend/AASTU-backend-group-7/run.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("air", "--build.cmd", "go run Delivery/main.go", "--build.bin", "./bin/api")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}

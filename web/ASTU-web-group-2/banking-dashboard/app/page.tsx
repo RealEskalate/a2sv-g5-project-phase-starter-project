41a2cbe8c14658362526dffd42d38ad64ba88eca
kaleb wondimu <kalebwondimu33@gmail.com>
2024-08-15 15:26:56 +0300

    modification

--- web/ASTU-web-group-2/banking-dashboard/app/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/page.tsx
@@ -1,17 +1,6 @@
 import React from "react";
-import CreditCard from "./components/creditCard/CreditCard";
 const page = () => {
-  return (
-    <div>
-      <CreditCard
-        balance={1250}
-        cardHolder="John Doe"
-        expiryDate="12/24"
-        cardNumber="1234 5678 9012 3456"
-        cardType="tertiary" // Can be "primary", "secondary", or "tertiary"
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default page;

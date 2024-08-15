dbed377885086a83c10bc9c2eb685fcb2b31500f
naol86 <naol.kasinet@a2sv.org>
2024-08-15 15:34:21 +0300

    edited mycards for eph

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
@@ -1,5 +1,11 @@
+import MyCard from '@/components/MyCard/MyCard';
 
 export default function Home() {
-
-  return <div></div>;
+  return (
+    <div className='flex justify-between'>
+      <MyCard />
+      <MyCard />
+      <MyCard />
+    </div>
+  );
 }

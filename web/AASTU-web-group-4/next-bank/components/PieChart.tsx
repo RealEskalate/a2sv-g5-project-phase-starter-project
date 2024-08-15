d780f9651c7c0e89ed98a5e44f24c90d50cb6fe4
Natnael Worku <natnael.worku@a2sv.org>
2024-08-15 12:45:33 +0300

    added imports that were not present at first

--- web/AASTU-web-group-4/next-bank/components/PieChart.tsx
+++ web/AASTU-web-group-4/next-bank/components/PieChart.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { Pie } from 'react-chartjs-2';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import React from 'react';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 

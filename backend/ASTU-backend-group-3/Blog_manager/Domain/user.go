5898c6a36a548ea7830203f034dd337fca3189fa
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-08-15 16:49:15 +0300

    chore: Add RegisterInput, LoginInput, UpdateUserInput, ForgetPasswordInput, and ResetPassworInput models

--- backend/ASTU-backend-group-3/Blog_manager/Domain/user.go
+++ backend/ASTU-backend-group-3/Blog_manager/Domain/user.go
@@ -15,3 +15,37 @@ type User struct{
 	Adress string `json:"adress" bson:"adress"`
 }
 
+
+
+type  RegisterInput struct{
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+	Email string `json:"email" bson:"email"`
+	ProfilePicture string `json:"profile_picture" bson:"profile_picture"`
+	Bio string `json:"bio" bson:"bio"`
+	Gender string `json:"gender" bson:"gender"`
+	Adress string `json:"adress" bson:"adress"`
+}
+
+type LoginInput struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+	
+}
+
+type UpdateUserInput struct {
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+	ProfilePicture string `json:"profile_picture" bson:"profile_picture"`
+	Bio string `json:"bio" bson:"bio"`
+	Adress string `json:"adress" bson:"adress"`
+}
+
+
+type ForgetPasswordInput struct {
+	Email string `json:"email" bson:"email"`
+}
+
+type ResetPassworInput struct {
+	NewPassword string `json:"password" bson:"password"`
+}

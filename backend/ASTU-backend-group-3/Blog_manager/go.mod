5712600021ada5e32c1265552cc92dc39269fef2
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-08-15 17:34:28 +0300

    chore: Remove is_admin field from User struct

--- backend/ASTU-backend-group-3/Blog_manager/go.mod
+++ backend/ASTU-backend-group-3/Blog_manager/go.mod
@@ -1,3 +1,8 @@
 module ASTU-backend-group-3/Blog_manager
 
 go 1.22.5
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
---
+++ backend/ASTU-backend-group-3/Blog_manager/infrastructure/jwt_services.go
@@ -0,0 +1,77 @@
+package utils
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/joho/godotenv"
+)
+
+
+type Claims struct {
+	Username string `json:"username"`
+	Role string `json:"role"`
+	jwt.StandardClaims
+}
+// Generate a token based on username and Role
+func GenerateToken(username , role string ) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	
+	claims := &Claims{
+		Username: username,
+		Role: role,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
+	
+}
+
+
+
+// Parse the token and return the claims
+func ParseUsernameToken(tokenString string) (username string , err error){
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var jwtKey = []byte(os.Getenv("JWT_SECRET"))
+
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		return "", err
+	}
+
+	return claims.Username, nil
+}
+
+
+

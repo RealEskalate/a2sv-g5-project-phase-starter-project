0da23d79ddbe5ca65ed7ad7ce8d7e350483c8b81
kika1s1 <tamiratkebede120@gmail.com>
2024-08-15 21:36:46 +0300

    token and user model is settedup

--- backend/ASTU-backend-group-5/internal/domain/token.go
+++ backend/ASTU-backend-group-5/internal/domain/token.go
@@ -1,1 +1,19 @@
 package domain
+
+import "github.com/golang-jwt/jwt/v5"
+
+// Claims struct that will be encoded to a JWT.
+// This can contain any information you want to include in your JWT.
+type Claims struct {
+	UserID   string `json:"user_id"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Role	 string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+// Token struct that will be returned to the user
+type Token struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}

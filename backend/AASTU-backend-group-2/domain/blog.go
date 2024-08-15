8d34a8f7cdde28be572603a1546c35ea31f815f9
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-15 11:29:57 +0300

    [Feat]: necessary tools and imports done

---
+++ backend/AASTU-backend-group-2/domain/blog.go
@@ -0,0 +1,35 @@
+package domain
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Blog struct {
+	ID      primitive.ObjectID `bson:"_id,omitempty" json:"-"`
+	Title   string             `bson:"title,omitempty" json:"title,omitempty"`
+	Content string             `bson:"content,omitempty" json:"content,omitempty"`
+	UserID  primitive.ObjectID `bson:"userid,omitempty" json:"-"`
+	Tags    []string           `bson:"tags,omitempty" json:"tags,omitempty"`
+	Date    time.Time          `bson:"due_date,omitempty" json:"due_date,omitempty"`
+}
+
+type BlogUsecase interface {
+	CreateBlog(c context.Context, blog Blog) error
+	RetrieveBlog(c context.Context) ([]Blog, error)
+	UpdateBlog(c context.Context, updatedblog Blog) error
+	DeleteBlog(c context.Context, blogID primitive.ObjectID) error
+	SearchBlog(c context.Context) ([]Blog, error)
+	FilterBlog(c context.Context) ([]Blog, error)
+}
+
+type BlogRepository interface {
+	CreateBlog(blog Blog) error
+	RetrieveBlog(pgnum int) ([]Blog, error)
+	UpdateBlog(updatedblog Blog) error
+	DeleteBlog(blogID primitive.ObjectID) error
+	SearchBlog(postName string, authorName string) ([]Blog, error)
+	FilterBlog(tag string, date time.Time) ([]Blog, error)
+}
--- backend/AASTU-backend-group-2/repositories/blog_repository.go
+++ backend/AASTU-backend-group-2/repositories/blog_repository.go
@@ -1,1 +1,48 @@
 package repositories
+
+import (
+	"blog_g2/domain"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type BlogRepository struct {
+	client     *mongo.Client
+	database   *mongo.Database
+	collection *mongo.Collection
+}
+
+func NewBlogRepository(mongoClient *mongo.Client) domain.BlogRepository {
+	return &BlogRepository{
+		client:     mongoClient,
+		database:   mongoClient.Database("Blog-manager"),
+		collection: mongoClient.Database("Blog-manager").Collection("Blogs"),
+	}
+
+}
+
+func (br *BlogRepository) CreateBlog(blog domain.Blog) error {
+	return nil
+}
+
+func (br *BlogRepository) RetrieveBlog(pgnum int) ([]domain.Blog, error) {
+	return []domain.Blog{}, nil
+}
+
+func (br *BlogRepository) UpdateBlog(updatedblog domain.Blog) error {
+	return nil
+}
+
+func (br *BlogRepository) DeleteBlog(blogID primitive.ObjectID) error {
+	return nil
+}
+
+func (br *BlogRepository) SearchBlog(postName string, authorName string) ([]domain.Blog, error) {
+	return []domain.Blog{}, nil
+}
+
+func (br *BlogRepository) FilterBlog(tag string, date time.Time) ([]domain.Blog, error) {
+	return []domain.Blog{}, nil
+}

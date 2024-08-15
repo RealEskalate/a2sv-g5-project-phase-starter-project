b46289aea394854cd8a1464d4d2ae2cf149cdf03
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-15 17:27:00 +0300

    [Feat]: added blog search and filter functionality

--- backend/AASTU-backend-group-2/repositories/blog_repository.go
+++ backend/AASTU-backend-group-2/repositories/blog_repository.go
@@ -2,8 +2,10 @@ package repositories
 
 import (
 	"blog_g2/domain"
+	"context"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -40,9 +42,46 @@ func (br *BlogRepository) DeleteBlog(blogID primitive.ObjectID) error {
 }
 
 func (br *BlogRepository) SearchBlog(postName string, authorName string) ([]domain.Blog, error) {
-	return []domain.Blog{}, nil
+	var results []domain.Blog
+
+	filter := bson.M{
+		"$or": []bson.M{
+			{"title": bson.M{"$regex": primitive.Regex{Pattern: postName, Options: "i"}}},
+		},
+	}
+
+	cursor, err := br.collection.Find(context.Background(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = cursor.All(context.Background(), &results); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 }
 
-func (br *BlogRepository) FilterBlog(tag string, date time.Time) ([]domain.Blog, error) {
-	return []domain.Blog{}, nil
+func (br *BlogRepository) FilterBlog(tag []string, date time.Time) ([]domain.Blog, error) {
+	var results []domain.Blog
+
+	filter := bson.M{
+		"$or": []bson.M{
+			{
+				"date": bson.M{"$gt": date},
+				"tags": bson.M{"$all": tag},
+			},
+		},
+	}
+
+	cursor, err := br.collection.Find(context.Background(), filter)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = cursor.All(context.Background(), &results); err != nil {
+		return nil, err
+	}
+
+	return results, nil
 }

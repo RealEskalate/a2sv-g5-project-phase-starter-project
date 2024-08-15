3c2f3c4cb562e5982db02136d9fadd5e964213fb
Kuma Telila <kumatelila26@gmail.com>
2024-08-15 22:02:00 +0300

    Infobox for Investment page done

--- web/ASTU-web-group-2/banking-dashboard/app/investments/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/investments/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import InfoboxForInvestementPage from "../components/infobox/InfoboxForInvestementPage";
+import MyExpenseChart from "../components/charts/MyExpenseChart";
+import Card from "../components/card/Card";
 
 const InvestmentsPage = () => {
   return (
-    <div>Investments Page</div>
-  )
-}
+    <div className="flex flex-col gap-2">
+      <InfoboxForInvestementPage />
+      <div className="flex max-sm:flex-col gap-[30px]">
+        <Card
+          title="My Expense"
+          className="flex flex-col lg:w-[730px] lg:h-[300px] md:w-[487px] md:h-[299px] h-[254]"
+        >
+          <MyExpenseChart />
+        </Card>
+      </div>
+    </div>
+  );
+};
 
-export default InvestmentsPage
+export default InvestmentsPage;

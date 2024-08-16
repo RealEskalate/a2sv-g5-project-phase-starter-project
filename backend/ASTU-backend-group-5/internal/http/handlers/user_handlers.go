a3a13edac7444338662c2f97e0dcf62f80aea7c4
samuel <samuelfikadesilassie@gmail.com>
2024-08-16 05:29:15 -0700

    implementing handler

--- backend/ASTU-backend-group-5/internal/http/handlers/user_handlers.go
+++ backend/ASTU-backend-group-5/internal/http/handlers/user_handlers.go
@@ -62,3 +62,50 @@ func (h *UserHandler) Register(c *gin.Context) {
 	})
 
 }
+
+func (h *UserHandler) GetUser(c *gin.Context){
+	user, ok := c.Request.Context().Value("user").(*domain.User)
+
+	if user == nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+		return
+	}
+	
+	c.JSON(http.StatusOK, user)
+}
+
+
+func (h *UserHandler) DeleteUser(c *gin.Context){
+	userID, exists := c.Request.Context().Value("id").(string)
+	if !exists || userID == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "User ID not found in context"})
+		return
+	}
+
+		
+	if err := h.UserUsecase.DeleteUser(userID); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
+}
+
+func (h *UserHandler) UpdateUser(c * gin.Context){
+	var user domain.User
+    if err := c.ShouldBindJSON(&user); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid input data"})
+        return
+    }
+
+    err := h.UserUsecase.UpdateUser(&user)
+
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+        return
+    }
+
+    c.JSON(http.StatusOK, gin.H{"message": "User updated successfully"})
+}
+
+

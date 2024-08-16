00125f4035484f7b76ebffe165ef4cf456ec7e34
Hiwot2127 <hiwotbkm25@gmail.com>
2024-08-16 10:42:17 +0300

    :fire: Authentication

--- backend/AASTU-backend-group-9/delivery/middleware/auth_middleware.go
+++ backend/AASTU-backend-group-9/delivery/middleware/auth_middleware.go
@@ -1,1 +1,25 @@
 package middleware
+
+import (
+    "net/http"
+	"blog/internal/tokenutil"
+    "github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(authService *tokenutil.AuthService) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        token := c.GetHeader("Authorization")
+        if token == "" {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing access token"})
+            return
+        }
+
+        _, err := authService.ValidateAccessToken(token)
+        if err != nil {
+            c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired access token"})
+            return
+        }
+
+        c.Next()
+    }
+}

835606ef40cf69113e63a64801cb909dee99c9c0
Your GitHub Username <your-email@example.com>
2024-08-15 23:18:33 -0900

    charts integrated

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/investments/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/investments/page.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import InvestmentItem from "@/components/InvestmentItems/InvestmentItem";
 import InvestmentList from "@/components/InvestmentList/InvestmentList";
 import TrendingList from "@/components/TrendingStock/trendingList";
+import YearlyTotalInvestment from "@/components/Charts/YearlyTotalInvestment";
+import MonthlyRevenue from "@/components/Charts/MonthlyRevenue";
 
 export default function page() {
   return (
     <div>
       <InvestmentItem />
-      <div className="border h-[282px]"></div>
+      <div className="md:flex space-x-2">
+        <YearlyTotalInvestment />
+        <MonthlyRevenue />
+      </div>
       <div className="flex flex-col md:flex-row ">
         <InvestmentList />
         <TrendingList />

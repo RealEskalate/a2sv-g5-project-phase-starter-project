85c762e99364defb19dc304211cdec3443033e02
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-16 11:58:34 +0300

    fix: user interface setup done

--- backend/AASTU-backend-group-2/deliveries/controllers/user_controller.go
+++ backend/AASTU-backend-group-2/deliveries/controllers/user_controller.go
@@ -1,1 +1,44 @@
 package controllers
+
+import (
+	"blog_g2/domain"
+
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	Userusecase domain.UserUsecase
+}
+
+// Blog-controller constructor
+func NewUserController(Usermgr domain.UserUsecase) *UserController {
+	return &UserController{
+		Userusecase: Usermgr,
+	}
+
+}
+
+// RegisterUser is a controller method to register a user
+func (uc *UserController) RegisterUser(c *gin.Context) {
+
+}
+
+// LoginUser is a controller method to login a user
+func (uc *UserController) LoginUser(c *gin.Context) {
+
+}
+
+// ForgotPassword is a controller method to reset a user's password
+func (uc *UserController) ForgotPassword(c *gin.Context) {
+
+}
+
+// LogoutUser is a controller method to logout a user
+func (uc *UserController) LogoutUser(c *gin.Context) {
+
+}
+
+// PromoteDemoteUser is a controller method to promote or demote a user
+func (uc *UserController) PromoteDemoteUser(c *gin.Context) {
+
+}
--- backend/AASTU-backend-group-2/usecase/user_usecase.go
+++ backend/AASTU-backend-group-2/usecase/user_usecase.go
@@ -1,1 +1,41 @@
 package usecase
+
+import (
+	"blog_g2/domain"
+	"context"
+
+	"time"
+)
+
+type UserUsecase struct {
+	UserRepo       domain.UserRepository
+	contextTimeout time.Duration
+}
+
+func NewUserUsecase(Userrepo domain.UserRepository, timeout time.Duration) domain.UserUsecase {
+	return &UserUsecase{
+		UserRepo:       Userrepo,
+		contextTimeout: timeout,
+	}
+
+}
+
+func (uuse *UserUsecase) RegisterUser(c context.Context, user domain.User) error {
+	return nil
+}
+
+func (uuse *UserUsecase) LoginUser(c context.Context, user domain.User) (string, error) {
+	return "", nil
+}
+
+func (uuse *UserUsecase) ForgotPassword(c context.Context, email string) error {
+	return nil
+}
+
+func (uuse *UserUsecase) LogoutUser(c context.Context) error {
+	return nil
+}
+
+func (uuse *UserUsecase) PromoteDemoteUser(c context.Context, userid string) error {
+	return nil
+}

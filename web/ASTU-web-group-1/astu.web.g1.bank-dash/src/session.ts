17a3a190f49986076ad0dc6f50c64a7638a2d214
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-08-16 15:15:33 +0300

    Astu.web.g1.340 naol.redux init (#424)
    * working on redux
    
    * working on redux

---
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/session.ts
@@ -0,0 +1,2 @@
+export const session =
+  'eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJuYW9sa2EiLCJpYXQiOjE3MjM4MDcyOTMsImV4cCI6MTcyMzg5MzY5M30.LKg_jlh39JPsbX3owh9fFEIBwx68NBo7J6Ldg6IOooMbP-hyzDomw-BZFwCfX9rB';

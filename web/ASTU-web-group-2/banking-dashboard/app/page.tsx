8a8f3082a167202948e601568c6a1aa109940bdc
Kuma Telila <kumatelila26@gmail.com>
2024-08-16 13:31:14 +0300

    Conflict from github on components

--- web/ASTU-web-group-2/banking-dashboard/app/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/page.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import WeeklyActivityChart from "./components/charts/WeeklyActivityChart";
+import Card from "./components/card/Card";
+import CardForCreditCards from "./components/card/CardForCreditCards";
 import CreditCard from "./components/creditCard/CreditCard";
 import Page from "./components/TrendingStock/Page";
 const page = () => {
-
   return <div></div>;
 };
 

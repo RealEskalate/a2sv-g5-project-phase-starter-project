d7eaf0d5f3680cbfaafe5542f985f02347ac1d4e
fasill <fasilhawultie19@gmail.com>
2024-08-16 17:08:33 +0300

    signup functionality added

---
+++ web/ASTU-web-group-2/banking-dashboard/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from '../components/auth/signup/Signup'
+
+const page = () => {
+  return (
+    <div>
+        <Signup/>
+    </div>
+  )
+}
+
+export default page

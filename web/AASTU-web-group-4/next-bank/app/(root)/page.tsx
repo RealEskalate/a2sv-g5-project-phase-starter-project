99d47db25091e6a73cfc6218e30963be64db2991
selome <selomegech@gmail.com>
2024-08-16 09:36:18 +0300

    new changes

--- web/AASTU-web-group-4/next-bank/app/(root)/page.tsx
+++ web/AASTU-web-group-4/next-bank/app/(root)/page.tsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { Color } from 'chart.js'
+import { colors  , logo} from '@/constants'
+import DesktopCreditCart from '@/components/DesktopCreditCard'
+import RecentTransaction from '@/components/Recent Transaction'
+import { icons } from 'lucide-react'
+import { text } from 'stream/consumers'
+import BarChart from '@/components/BarChart'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='px-6 '>
+      <div className='flex flex-col  justify-between md:flex-row'>
+        <div>
+          <div className={`${colors.navbartext} flex justify-between`}>
+            <h1 className='font-bold text-2xl'>My Cards</h1>
+            <p className='my-2'> See All</p>
+          </div>
+          <div className='flex gap-3 overflow-x-auto md:overflow-x-hidden max-w-lg'>
+            <div className='flex py-3'> <DesktopCreditCart/></div>
+            <div className='flex py-3'> <DesktopCreditCart/></div>
+          </div>
+        </div>
+        <div>
+          <div className={`${colors.navbartext} flex justify-between py-4`}>
+            <h1 className='font-bold text-2xl'>Recent Transaction</h1>
+          </div>
+          <div className=''>
+            <RecentTransaction icon={logo.RT1} color={colors.lightorange} colortext={colors.textred} name="deposit from my" date = "23 january 2023" amount='-850'/>
+            <RecentTransaction icon={logo.RT2} color={colors.lightblue} colortext={colors.textgreen} name="deposit from my" date = "23 january 2023" amount='+2500'/>
+            <RecentTransaction icon={logo.RT3} color={colors.lightgreen} colortext={colors.textgreen} name="deposit from my" date = "23 january 2023" amount='+5400'/>
+          </div>
+        </div>
+      </div>
+      <div className={`${colors.navbartext} flex justify-between py-4`}>
+        <h1 className='font-bold text-2xl'>Weekly Activity</h1>
+      </div>
+      <div><BarChart/></div>
+    </div>
   )
 }
 
---
+++ web/AASTU-web-group-4/next-bank/components/Recent Transaction.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { colors , logo } from '@/constants';
+import Image from 'next/image';
+const RecentTransaction = ({icon , color ,colortext , name , date ,amount}:{
+    icon:string;
+    color:string;
+    colortext : string;
+    name:string;
+    date:string;
+    amount:string
+}) => {
+  return (
+    <div className='my-2'>
+        <div className=''>
+          <div className='flex  p-2 justify-between '>
+            <div className=' flex gap-4'>
+                <div className={`${color} rounded-full flex items-center justify-center p-4 h-20 w-20  `}>
+                    <Image
+                  src={icon}
+                  alt="next logo"
+                  width={150}
+                  height={50}
+                  className="object-contain"
+                />
+                </div>
+                <div className='my-2'>
+                    <p className='text-lg font-medium'> {name}</p>
+                    <p className={`${colors.textgray} text-sm text-start`}> {date}</p>
+                </div>
+            </div>
+            <div className='mx-9 my-5'>
+                <p className={`${colortext}`}> ${amount}</p>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default RecentTransaction

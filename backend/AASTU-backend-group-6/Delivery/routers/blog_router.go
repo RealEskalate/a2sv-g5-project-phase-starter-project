1763229cd64b000cb620f94274854bd3f54e5d65
Bz14 <bezueyerusalem@gmail.com>
2024-08-16 10:07:00 +0300

    Create comment endpoint added.

---
+++ backend/AASTU-backend-group-6/Delivery/routers/blog_router.go
@@ -0,0 +1,27 @@
+package routers
+
+import (
+	infrastructure "blogs/Infrastructure"
+	"blogs/mongo"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewBlogrouter(config *infrastructure.Config, DB mongo.Database, blogRouter *gin.RouterGroup) {
+	// unprotected
+	blogRouter.GET("/")
+	blogRouter.GET("/:id")
+
+	blogRouter.GET("/search/:title&:author")
+	blogRouter.GET("/tag/:tag")
+
+	// protected
+	blogRouter.GET("/my")
+	blogRouter.GET("/my/:id")
+
+	blogRouter.POST("/create")
+	blogRouter.PUT("/update/:id")
+	blogRouter.DELETE("/delete/:id")
+
+	blogRouter.POST("/comment/create")
+}

5fd35cb40770ccdc12cb8cc021abd53494ac0a99
yordanos legesse <yordanoslegesse15@gmail.com>
2024-08-16 15:55:19 +0300

    routeConfigured

---
+++ backend/AASTU-backend-group-6/Delivery/routers/route.go
@@ -0,0 +1,21 @@
+package routers
+
+import (
+	infrastructure "blogs/Infrastructure"
+	"blogs/mongo"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Router(server *gin.Engine, config *infrastructure.Config, DB mongo.Database) {
+
+	blog_repo := repositories.NewBlogRepository(DB.Collection(config.BlogCollection), config)
+	blog_usecase := usecases.NewBlogUsecase(blog_repo)
+	blog_controller := controllers.NewBlogController(blog_usecase)
+	blogRouter := server.Group("blogs")
+	NewBlogrouter(blogRouter, blog_controller)
+	userRouter := server.Group("")
+	NewUserrouter(config, DB, userRouter)
+	NewSignupRoute(config, DB, userRouter)
+
+}

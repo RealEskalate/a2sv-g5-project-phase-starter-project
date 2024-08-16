c34072716c65aa019091f132b87a3f926f5369ff
melkemk <melkemk503@gmail.com>
2024-08-16 23:11:15 +0300

    idn

--- web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/CreditCards/page.tsx
+++ web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/CreditCards/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import CreditCardComponent from "./CreditCardComponent";
 
-const page = () => {
+const Page = () => {
 	return (
 		<div>
 			<CreditCardComponent />
@@ -9,4 +9,4 @@ const page = () => {
 	);
 };
 
-export default page;
+export default Page;
--- web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/components/common/sideBar.tsx
+++ web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/components/common/sideBar.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { FaTimes } from "react-icons/fa";
 
 import creditCard from "/public/assets/icons/credit-card 1.svg";
@@ -30,9 +30,9 @@ const primary_3 = 'rgba(45, 96, 255, 1)';
 const sidecolor = '#B1B1B1';
 
 const SideBar = ({ isSidebarVisible, toggleSidebar }: { isSidebarVisible: boolean, toggleSidebar: () => void }) => {
-    const [enabled, setEnabled] = useState<string>("home");
+    const [enabled, setEnabled] = useState<string>(usePathname().substring(1,));
+    console.log(enabled,111)
     const router = useRouter();
-
     const handleIconClick = (option: string, path: string) => {
         setEnabled(option);
         router.push(path);
@@ -49,7 +49,6 @@ const SideBar = ({ isSidebarVisible, toggleSidebar }: { isSidebarVisible: boolea
                 </button>
             </div>
 
-
             <div className="flex flex-col gap-[42px]">
                 <div className="flex gap-[23px]" onClick={() => handleIconClick("home", "/home")}>
                     <Image src={enabled === "home" ? enabledHome : home} alt="Home Icon" className="h-[25px] w-[25px]" />
@@ -63,12 +62,12 @@ const SideBar = ({ isSidebarVisible, toggleSidebar }: { isSidebarVisible: boolea
                     <Image src={enabled === "user" ? enabledUser : user} alt="User Icon" className="h-[25px] w-[25px]" />
                     <div> Accounts</div>
                 </div>
-                <div className="flex gap-[23px]" onClick={() => handleIconClick("economicInvestment", "/investments")}>
-                    <Image src={enabled === "economicInvestment" ? enabledEconomicInvestment : economicInvestment} alt="Investments Icon" className="h-[25px] w-[25px]" />
+                <div className="flex gap-[23px]" onClick={() => handleIconClick("investments", "/investments")}>
+                    <Image src={enabled === "investments" ? enabledEconomicInvestment : economicInvestment} alt="Investments Icon" className="h-[25px] w-[25px]" />
                     <div> Investments</div>
                 </div>
-                <div className="flex gap-[23px]" onClick={() => handleIconClick("creditCard", "/credit-cards")}>
-                    <Image src={enabled === "creditCard" ? enabledCreditCard : creditCard} alt="Credit Card Icon" className="h-[25px] w-[25px]" />
+                <div className="flex gap-[23px]" onClick={() => handleIconClick("CreditCards", "/CreditCards")}>
+                    <Image src={enabled === "CreditCards" ? enabledCreditCard : creditCard} alt="Credit Card Icon" className="h-[25px] w-[25px]" />
                     <div> Credit Cards</div>
                 </div>
                 <div className="flex gap-[23px]" onClick={() => handleIconClick("loan", "/loans")}>
@@ -90,7 +89,6 @@ const SideBar = ({ isSidebarVisible, toggleSidebar }: { isSidebarVisible: boolea
             </div>
         </div>
     );
-
-};
+}; 
 
 export default SideBar;

6387b0406e70d7b7c602a3c7303240b7e0b562f3
rebumatadele <118250996+rebumatadele@users.noreply.github.com>
2024-08-16 11:44:55 +0300

    made changes to the account setting

--- web/AASTU-web-group-2/a2sv-banking-system/app/accounts/page.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/accounts/page.tsx
@@ -23,7 +23,7 @@ type Column = {
 
 const Page = () => {
   // Example data for the first ListCard
-  const column1: Column = {
+  const ReusableCard: Column = {
     icon: MdHome,
     iconStyle: "text-[#FFBB38] bg-[#FFF5D9]",
     data: [
@@ -37,37 +37,37 @@ const Page = () => {
   };
 
   // Example data for the second ListCard
-  const column2: Column = {
+  const card1: Column = {
     icon: MdAttachMoney, // Updating the icon
     iconStyle: "text-[#396AFF] bg-[#E7EDFF]", // Updating the iconStyle
-    data: column1.data.map((item) => ({
+    data: ReusableCard.data.map((item) => ({
       ...item,
       heading: "Income", // Updating the heading
     })),
   };
 
   // Example data for the third ListCard
-  const column3: Column = {
+  const card2: Column = {
     icon: MdSettings, // Updating the icon
     iconStyle: "text-[#FF82AC] bg-[#FFE0EB]", // Updating the iconStyle
-    data: column1.data.map((item) => ({
+    data: ReusableCard.data.map((item) => ({
       ...item,
       heading: "Expense", // Updating the heading
     })),
   };
 
   // Example data for the fourth ListCard
-  const column4: Column = {
+  const card3: Column = {
     icon: MdAccountBalance, // Updating the icon
     iconStyle: "text-[#16DBCC] bg-[#DCFAF8]", // Updating the iconStyle
-    data: column1.data.map((item) => ({
+    data: ReusableCard.data.map((item) => ({
       ...item,
       heading: "Total Savings", // Updating the heading
     })),
   };
 
   // First column with multiple data items
-  const column5: Column = {
+  const ReusableLastTransaction: Column = {
     icon: MdHome,
     iconStyle: "text-[#FFBB38] bg-[#FFF5D9]",
     data: [
@@ -85,24 +85,55 @@ const Page = () => {
       },
     ],
   };
+
+  // First transaction example
+  const transaction1: Column = {
+    icon: MdAccountBalance, // Different icon
+    iconStyle: "text-[#16DBCC] bg-[#DCFAF8]", // Different iconStyle
+    data: ReusableLastTransaction.data.map((item, index) => ({
+      ...item,
+      heading: index === 0 ? "Mobile Services" : item.heading, // Custom heading for the first item
+    })),
+  };
+
+  const transaction2: Column = {
+    icon: MdAttachMoney, // Updating the icon
+    iconStyle: "text-[#16DBCC] bg-[#DCFAF8]", // Updating the iconStyle
+    data: ReusableLastTransaction.data.map((item, index) => ({
+      ...item,
+      heading: index === 0 ? "Emilly Wilson " : "+$780",
+      headingStyle:
+        index === 0 ? item.headingStyle : "text-xs font-bold text-[#16DBAA]",
+    })),
+  };
+
   return (
     <>
-      <div className="flex flex-col h-svh bg-[#F5F7FA] px-3 py-3 gap-5">
+      <div className="flex flex-col h-full bg-[#F5F7FA] px-3 py-3 gap-5">
         <div>
           <div className="flex flex-wrap gap-2">
-            <ListCard column={column1} width={"w-[48%] md:w-[24.3%]"} />
-            <ListCard column={column2} width={"w-[48%] md:w-[24.3%]"} />
-            <ListCard column={column3} width={"w-[48%] md:w-[24.3%]"} />
-            <ListCard column={column4} width={"w-[48%] md:w-[24.3%]"} />
+            <ListCard column={ReusableCard} width={"w-[48%] md:w-[24.3%]"} />
+            <ListCard column={card1} width={"w-[48%] md:w-[24.3%]"} />
+            <ListCard column={card2} width={"w-[48%] md:w-[24.3%]"} />
+            <ListCard column={card3} width={"w-[48%] md:w-[24.3%]"} />
           </div>
         </div>
         <div className="flex flex-col gap-5">
           <span>Last Transaction</span>
           <div className="bg-white flex flex-col justify-between rounded-2xl">
-            <ListCard column={column5} width={"w-full"} />
-            <ListCard column={column5} width={"w-full"} />
-            <ListCard column={column5} width={"w-full"} />
-            <ListCard column={column5} width={"w-full"} />
+            <ListCard column={ReusableLastTransaction} width={"w-full"} />
+            <ListCard column={transaction1} width={"w-full"} />
+            <ListCard column={transaction2} width={"w-full"} />
+          </div>
+        </div>
+        <div>Card Goes Here</div>
+        <div>Chart Goes here</div>
+        <div className="flex flex-col gap-5">
+          <span>Invoice Sent</span>
+          <div className="bg-white flex flex-col justify-between rounded-2xl">
+            <ListCard column={ReusableLastTransaction} width={"w-full"} />
+            <ListCard column={transaction1} width={"w-full"} />
+            <ListCard column={transaction2} width={"w-full"} />
           </div>
         </div>
       </div>

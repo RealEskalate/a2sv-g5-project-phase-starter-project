9eb44f6166b3ab3e646cf52808e20df88e5f6c0a
Hundera Awoke <hunderaweke@gmail.com>
2024-08-16 15:16:01 +0300

    added: database constructor function

--- backend/ASTU-backend-group-1/database/mongo.go
+++ backend/ASTU-backend-group-1/database/mongo.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"astu-backend-g1/config"
+	"context"
+
+	"github.com/sv-tools/mongoifc"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func NewMongoDatabase(config config.Config) (mongoifc.Database, error) {
+	uri := config.Database.Uri
+	opts := options.Client().ApplyURI(uri)
+	clnt, err := mongo.Connect(context.Background(), opts)
+	if err != nil {
+		return mongoifc.WrapDatabase(&mongo.Database{}), err
+	}
+	db := clnt.Database(config.Database.Name)
+	return mongoifc.WrapDatabase(db), nil
+}

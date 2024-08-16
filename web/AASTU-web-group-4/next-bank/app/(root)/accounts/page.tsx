1ee3e6e3203d1eb99ee8000846b07d161bc9c6c6
urjilaa <urji53210@gmail.com>
2024-08-15 22:12:49 -0700

    signup page

--- web/AASTU-web-group-4/next-bank/app/(root)/accounts/page.tsx
+++ web/AASTU-web-group-4/next-bank/app/(root)/accounts/page.tsx
@@ -3,15 +3,8 @@ import React from 'react'
 import BalanceCard from '@/components/AccountSmallCard'
 import App from '@/components/LastTransactionCard'
 import DesktopCreditCart from '@/components/DesktopCreditCard';
-import BarChart from '@/components/BarChartAccount';
 
-const Accounts: React.FC = () => {
-  const labels = ["January", "February", "March", "April", "May", "June", "July"];
-  const data = [65, 59, 80, 81, 56, 55, 40];
-  const title = "Sales Over Months";
-
-
-// const Accounts = () => {
+const Accounts = () => {
   return (
     <div>
       <div>
@@ -22,16 +15,12 @@ const Accounts: React.FC = () => {
         <h1>Last Transaction</h1>
         <App/>
       </div>
-        <div>
+        {/* <div>
           <h1>My Card</h1>
           <DesktopCreditCart/>
-          <DesktopCreditCart/>
-          <DesktopCreditCart/>
-          <DesktopCreditCart/>
-        </div>
+        </div> */}
         {/* <div>
-          <h1>Debit & Credit Overview</h1>
-          <BarChart labels={labels} title={title}/>
+          <h1>Debit & Credit Overview</h1>   
         </div> */}
     </div>
   )

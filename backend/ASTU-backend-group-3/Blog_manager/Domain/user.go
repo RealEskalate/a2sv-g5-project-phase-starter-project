61ef5e3338fab0583a45a41594d1b6f1427a9941
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-08-16 17:20:10 +0300

    edit the user.go

--- backend/ASTU-backend-group-3/Blog_manager/Domain/user.go
+++ backend/ASTU-backend-group-3/Blog_manager/Domain/user.go
@@ -1,21 +1,25 @@
 package Domain
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type User struct {
-	Id             string   `json:"id" bson:"id"`
-	Username       string   `json:"username" bson:"username"`
-	Password       string   `json:"password" bson:"password"`
-	Email          string   `json:"email" bson:"email"`
-	PostsIDs       []string `json:"posts_id" bson:"posts_id"`
-	ProfilePicture string   `json:"profile_picture" bson:"profile_picture"`
-	Bio            string   `json:"bio" bson:"bio"`
-	Gender         string   `json:"gender" bson:"gender"`
-	Role           string   `json:"role" bson:"role"`
-	IsAdmin        bool     `json:"is_admin" bson:"is_admin"`
-	IsActive       bool     `json:"is_active" bson:"is_active"`
-	Address        string   `json:"address" bson:"address"`
+	Id             primitive.ObjectID `json:"id" bson:"id"`
+	Name           string             `json:"name" bson:"name"`
+	Username       string             `json:"username" bson:"username"`
+	Password       string             `json:"password" bson:"password"`
+	Email          string             `json:"email" bson:"email"`
+	PostsIDs       []string           `json:"posts_id" bson:"posts_id"`
+	ProfilePicture string             `json:"profile_picture" bson:"profile_picture"`
+	Bio            string             `json:"bio" bson:"bio"`
+	Gender         string             `json:"gender" bson:"gender"`
+	Role           string             `json:"role" bson:"role"`
+	IsAdmin        bool               `json:"is_admin" bson:"is_admin"`
+	IsActive       bool               `json:"is_active" bson:"is_active"`
+	Address        string             `json:"address" bson:"address"`
 }
 
 type RegisterInput struct {
+	Name           string `json:"name" bson:"name"`
 	Username       string `json:"username" bson:"username"`
 	Password       string `json:"password" bson:"password"`
 	Email          string `json:"email" bson:"email"`

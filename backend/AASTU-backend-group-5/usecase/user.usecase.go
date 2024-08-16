a6d91d5e04f064de6362983a94520b0436a871e5
ermi9s <ermiayele1122334477@gmail.com>
2024-08-16 15:46:37 +0300

    [update]:user usecase added

--- backend/AASTU-backend-group-5/usecase/user.usecase.go
+++ backend/AASTU-backend-group-5/usecase/user.usecase.go
@@ -1,1 +1,73 @@
 package usecase
+
+import "github.com/RealEskalate/blogpost/domain"
+
+type UserUseCase struct {
+	UserRepo domain.User_Registery_interface
+}
+
+func (usecase *UserUseCase) GetOneUser(id string) (domain.ResponseUser, error) {
+	user,err := usecase.UserRepo.GetUserDocumentByID(id)
+	if err != nil {
+		return domain.ResponseUser{},err
+	}
+	response_user := domain.CreateResponseUser(user)
+	return response_user,nil
+}
+
+func (usecase *UserUseCase) GetUsers() ([]domain.ResponseUser, error) {
+	users,err := usecase.UserRepo.GetUserDocuments()
+	if err != nil {
+		return []domain.ResponseUser{},err
+	}
+	responses_users:= []domain.ResponseUser{}
+
+	for _,user := range users {
+		responses_users = append(responses_users , domain.CreateResponseUser(user))
+	}
+	return responses_users,nil
+}
+
+func (usecase *UserUseCase) UpdateUser(id string, user domain.UpdateUser) (domain.ResponseUser, error) {
+	new_user,err := usecase.UserRepo.UpdateUserDocument(id, user)
+	if err != nil {
+		return domain.ResponseUser{},err
+	}
+	return domain.CreateResponseUser(new_user),nil
+}
+
+
+func (usecase *UserUseCase) DeleteUser(id string) error {
+	return usecase.UserRepo.DeleteUserDocument(id)
+}
+
+func (usecase *UserUseCase) LogIn(user domain.User) (domain.ResponseUser, error) {
+	user,err := usecase.UserRepo.LogIn(user)
+	if err != nil {
+		return domain.ResponseUser{},err
+	}
+	return domain.CreateResponseUser(user),nil
+}
+
+func (usecase *UserUseCase) Register(user domain.RegisterUser) (domain.ResponseUser, error) {
+	new_user,err := usecase.UserRepo.Register(user)
+	if err != nil {
+		return domain.ResponseUser{},err
+	}
+	return domain.CreateResponseUser(new_user),nil
+}
+
+func (usecase *UserUseCase) FilterUser(filter map[string]string) ([]domain.ResponseUser, error) {
+	users,err := usecase.UserRepo.FilterUserDocument(filter)
+	if err != nil {
+		return []domain.ResponseUser{},err
+	}
+
+	response_users := []domain.ResponseUser{}
+
+	for _,user := range users {
+		response_users = append(response_users , domain.CreateResponseUser(user))
+	}
+
+	return response_users,nil
+}

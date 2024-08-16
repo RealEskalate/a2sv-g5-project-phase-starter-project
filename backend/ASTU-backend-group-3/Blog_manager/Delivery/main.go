e56960d8e780ab98a1205cb72bc2001b9701150c
Nebiyu Musbah <nebiyumusbah378@gmail.com>
2024-08-16 14:56:55 +0300

    correct user registration and solve mongodb and .env related problems

--- backend/ASTU-backend-group-3/Blog_manager/Delivery/main.go
+++ backend/ASTU-backend-group-3/Blog_manager/Delivery/main.go
@@ -11,7 +11,6 @@ import (
 	"context"
 	"log"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,27 +18,19 @@ import (
 )
 
 func main() {
-	// Load environment variables from .env file
-	if err := godotenv.Load(".env"); err != nil {
+	err := godotenv.Load()
+	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-
-	// Create a new MongoDB client and connect to the server
-	clientOptions := options.Client().ApplyURI(os.Getenv("MONGO_URI"))
+	mongoURI := os.Getenv("MONGO_URL")
+	clientOptions := options.Client().ApplyURI(mongoURI)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
-		log.Fatal("Failed to create MongoDB client:", err)
+		log.Fatal(err)
 	}
+	defer client.Disconnect(context.TODO())
 
-	// Check the connection
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	if err := client.Ping(ctx, nil); err != nil {
-		log.Fatal("Failed to ping MongoDB:", err)
-	}
-
-	userDatabase := client.Database("Blog management")
+	userDatabase := client.Database("Blog_management")
 
 	userCollection := userDatabase.Collection("User")
 	userRepository := Repository.NewUserRepository(userCollection)
--- backend/ASTU-backend-group-3/Blog_manager/Delivery/router/router.go
+++ backend/ASTU-backend-group-3/Blog_manager/Delivery/router/router.go
@@ -2,7 +2,7 @@ package router
 
 import (
 	"ASTU-backend-group-3/Blog_manager/Delivery/controller"
-	"ASTU-backend-group-3/Blog_manager/infrastructure"
+	// "ASTU-backend-group-3/Blog_manager/infrastructure"
 
 	// "ASTU-backend-group-3/Blog_manager/utils"
 
@@ -14,16 +14,14 @@ func SetupRouter(userController *controller.UserController) *gin.Engine {
 
 	router.POST("/register", userController.Register)
 	router.POST("/login", userController.Login)
-	
-	usersRoute := router.Group("/user")
-	usersRoute.Use(infrastructure.AuthMiddleware()) // make sure to add Auth_User in the middleware
-	usersRoute.PUT("/update/:username", userController.UpdateUser)
 
-	protected := usersRoute.Group("/")
-	protected.Use(infrastructure.RoleMiddleware("admin")) // make sure to add Auth_User in the middleware
-	protected.DELETE("/delete/:username", userController.DeleteUser)
+	// usersRoute := router.Group("/user")
+	// usersRoute.Use(infrastructure.AuthMiddleware()) // make sure to add Auth_User in the middleware
+	// usersRoute.PUT("/update/:username", userController.UpdateUser)
 
-	
+	// protected := usersRoute.Group("/")
+	// protected.Use(infrastructure.RoleMiddleware("admin")) // make sure to add Auth_User in the middleware
+	// protected.DELETE("/delete/:username", userController.DeleteUser)
 
 	return router
 }

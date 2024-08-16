5d46f1a29e60c4c3687f12c0ea7057ef4a3ebc80
Lij Naol <135800478+Naol86@users.noreply.github.com>
2024-08-16 17:13:49 +0300

    Astu.web.g1.340 naol.redux init (#459)
    * working on redux
    
    * working on redux
    
    * edit side bar

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/SideBar/Sidebar.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/SideBar/Sidebar.tsx
@@ -11,9 +11,12 @@ import Services from '../../../public/assets/icons/services-icon.svg';
 import MyPrivileges from '../../../public/assets/icons/myPrivileges-icon.svg';
 import Setting from '../../../public/assets/icons/setting-icon.svg';
 import Logo from '../../../public/assets/icons/logo-icon.svg';
+import Hamburger from '../../../public/assets/icons/hamburger-icon.svg';
 import Link from 'next/link';
 import { useParams, usePathname } from 'next/navigation';
 import { useRouter } from 'next/navigation';
+import { useAppDispatch, useAppSelector } from '@/hooks/hoooks';
+import { toggleHamburgerMenu } from '@/lib/redux/slices/uiSlice';
 
 const data = [
   {
@@ -74,14 +77,33 @@ const data = [
 
 export default function Sidebar() {
   const asPath = usePathname();
-  console.log(asPath, 'as');
+  const hamburgerMenu = useAppSelector((state) => state.ui.hamburgerMenu);
+  const dispatch = useAppDispatch();
+  console.log('here i am');
+  const handleClick = () => {
+    dispatch(toggleHamburgerMenu());
+  };
+
   return (
     <>
-      <aside className='fixed sm:relative top-0 left-0 z-40 w-[215px] transition-transition -translate-x-full sm:translate-x-0 duration-300 '>
+      <aside
+        className={`fixed ${
+          hamburgerMenu ? '-translate-x-full' : ''
+        } sm:relative top-0 left-0 z-40 w-[215px] transition-transition  sm:translate-x-0 duration-300`}
+      >
         <div className='min-h-screen py-4 overflow-y-auto bg-white '>
-          <p className='flex items-center px-3 text-xl font-bold text-navy mb-10 my-3'>
-            <Logo className='w-[28px] h-[25px] mx-2' /> BankDash.
-          </p>
+          <div className='flex sm:block items-center mb-8'>
+            <p className='flex items-center px-3 text-xl font-bold text-navy  my-3'>
+              <Logo className='w-[28px] h-[25px] mx-2' /> BankDash.
+            </p>
+            <button
+              type='button'
+              className='items-center rounded-lg sm:hidden font-bold'
+              onClick={handleClick}
+            >
+              <Hamburger className='w-6 h-6 object-cover' />
+            </button>
+          </div>
           {data.map((ele) => (
             <Link href={ele.url} key={ele.title}>
               <SideBarItems Icon={ele.icon} title={ele.title} active={ele.url === asPath} />

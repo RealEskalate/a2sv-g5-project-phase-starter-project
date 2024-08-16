dfda684a13d9196655ca43fff1c2cde32669dc48
Kuma Telila <kumatelila26@gmail.com>
2024-08-16 14:49:31 +0300

    my investement page elements are puted on done

--- web/ASTU-web-group-2/banking-dashboard/app/investments/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/investments/page.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import InfoboxForInvestementPage from "../components/infobox/InfoboxForInvestementPage";
 import MyExpenseChart from "../components/charts/MyExpenseChart";
 import Card from "../components/card/Card";
+import YearlyTotalInvestment from "../components/charts/YearlyTotalInvestment";
+import MonthlyRevenueChart from "../components/charts/MonthlyRevenueChart";
+import MyInvestment from "../components/myInvestment/myInvestment";
+import TrendingStock from "../components/TrendingStock/TrendingStock";
 
 const InvestmentsPage = () => {
   return (
@@ -9,10 +13,30 @@ const InvestmentsPage = () => {
       <InfoboxForInvestementPage />
       <div className="flex max-sm:flex-col gap-[30px]">
         <Card
-          title="My Expense"
-          className="flex flex-col lg:w-[730px] lg:h-[300px] md:w-[487px] md:h-[299px] h-[254]"
+          title="Yearly Total Investment"
+          className="w-[540px] h-[329px] "
         >
-          <MyExpenseChart />
+          <YearlyTotalInvestment />
+        </Card>
+        <Card
+          title="Monthly Revenue"
+          className="w-[540px] h-[329px]"
+        >
+          <MonthlyRevenueChart />
+        </Card>
+      </div>
+      <div className="flex max-sm:flex-col gap-[30px]">
+        <Card
+          title="My Investment"
+          className=""
+        >
+          <MyInvestment />
+        </Card>
+        <Card
+          title="Trending Stock"
+          className=""
+        >
+          <TrendingStock />
         </Card>
       </div>
     </div>

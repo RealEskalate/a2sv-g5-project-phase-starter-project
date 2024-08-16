ced48d887995994e85844ea2e26b1856b74d7d5e
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-16 00:42:27 -0700

    interfaces added for user related crud operations

--- backend/ASTU-backend-group-5/internal/repository/user_repo.go
+++ backend/ASTU-backend-group-5/internal/repository/user_repo.go
@@ -13,13 +13,12 @@ import (
 // CreateUser(ctx context.Context, user *domain.User) error
 type UserRepository interface {
 	FindUserByEmail(ctx context.Context, email string) (*domain.User, error)
-	// FindUserByUserName(ctx context.Context, username string) (*domain.User, error)
+	FindUserByUserName(ctx context.Context, username string) (*domain.User, error)
+	FindUserById(ctx context.Context, id string) (*domain.User, error)
 	CreateUser(ctx context.Context, user *domain.User) error
-	UpdatePassword(ctx context.Context, email string, password string) error
 	UpdateUser(ctx context.Context, user *domain.User) error
-	// UpdateUser(ctx context.Context, user *domain.User) error
-	// DeleteUser(ctx context.Context, id string) error
-	// UpdateUserPassword(ctx context.Context, id string, password string) error
-	// UpdateUserRole(ctx context.Context, id string, role string) error
-	// UpdateUserEmail(ctx context.Context, id string, email string) error
+	DeleteUser(ctx context.Context, id string) error
+
+	GetAllUsers(ctx context.Context) ([]*domain.User, error)
+	FilterUsers(ctx context.Context, filter map[string]interface{}) ([]*domain.User, error)
 }

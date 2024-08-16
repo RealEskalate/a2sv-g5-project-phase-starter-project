52973aae0cd7e905c37161858804de4309e5ac89
Abenezer Mulugeta <abenezer.woldesenbet@gmail.com>
2024-08-16 18:03:13 +0300

    fix unused imports

--- web/AASTU-web-group-3/bank-dashboard/src/app/page.tsx
+++ web/AASTU-web-group-3/bank-dashboard/src/app/page.tsx
@@ -1,11 +1,3 @@
-import Image from "next/image";
-import CreditCardPage from "./creditcardpage/page";
-import DoughnutChart from "./components/charts/Doughnut";
-import CardListPage from "./components/cardList/CardList";
-import CardBox from "./components/cardBox";
-
 export default function Home() {
-  return <div>
-    
-  </div>;
+  return <div></div>;
 }

42a3ccd9df5b3bbd190d88eac715a991f97372a8
Kalkidan Amare <kalkidanamare11a@gmail.com>
2024-08-16 15:02:05 +0300

    update controllers and other setups

---
+++ backend/AASTU-backend-group-8/infrastructure/auth_middlware.go
@@ -0,0 +1,49 @@
+package infrastructure
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// AdminMiddleware checks if the user is an admin
+func AdminMiddleware(jwtService JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+		token, claims, err := jwtService.ValidateToken(tokenString)
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		isAdmin := claims.Role
+
+		if isAdmin != "is_admin" {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+// AuthMiddleware checks if the user is authenticated
+func AuthMiddleware(jwtService JWTService) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		token, claims, err := jwtService.ValidateToken(tokenString)
+		if err != nil || !token.Valid {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		// You can use the claims, e.g., setting them in the context for later use
+		c.Set("userID", claims.ID)
+		c.Set("username", claims.Username)
+		c.Set("role", claims.Role)
+
+		c.Next()
+	}
+}

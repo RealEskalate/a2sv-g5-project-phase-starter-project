8d8aa83717fb35d03e5fe20b21e1263666828106
fasill <fasilhawultie19@gmail.com>
2024-08-16 11:17:13 +0300

    next auth setted up

---
+++ web/ASTU-web-group-2/node_modules/next-auth/utils/detect-origin.d.ts
@@ -0,0 +1,3 @@
+/** Extract the origin from the environment */
+export declare function detectOrigin(forwardedHost: any, protocol: any): string | undefined;
+//# sourceMappingURL=detect-origin.d.ts.map

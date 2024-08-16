690490f7ca60de902fc5e3183c0b7bd57f8b9cdd
Hiwot2127 <hiwotbkm25@gmail.com>
2024-08-16 13:33:26 +0300

    :fire: Authentication updated

--- backend/AASTU-backend-group-9/repository/token_repository.go
+++ backend/AASTU-backend-group-9/repository/token_repository.go
@@ -2,17 +2,18 @@ package repository
 
 import (
     "context"
+	"errors"
     "blog/domain"
+    "blog/database"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
 )
 
 type MongoTokenRepository struct {
-    db *mongo.Collection
+    db database.Collection
 }
 
-func NewMongoTokenRepository(db *mongo.Collection) domain.TokenRepository {
+func NewMongoTokenRepository(db database.Collection) domain.TokenRepository {
     return &MongoTokenRepository{db: db}
 }
 
@@ -23,17 +24,29 @@ func (repo *MongoTokenRepository) SaveToken(ctx context.Context, token *domain.T
 
 func (repo *MongoTokenRepository) FindTokenByAccessToken(ctx context.Context, accessToken string) (*domain.Token, error) {
     var token domain.Token
-    err := repo.db.FindOne(ctx, bson.M{"access_token": accessToken}).Decode(&token)
-    return &token, err
+    result := repo.db.FindOne(ctx, bson.M{"access_token": accessToken})
+    if err := result.Decode(&token); err != nil {
+        return nil, err
+    }
+    return &token, nil
 }
 
 func (repo *MongoTokenRepository) FindTokenByRefreshToken(ctx context.Context, refreshToken string) (*domain.Token, error) {
     var token domain.Token
-    err := repo.db.FindOne(ctx, bson.M{"refresh_token": refreshToken}).Decode(&token)
-    return &token, err
+    result := repo.db.FindOne(ctx, bson.M{"refresh_token": refreshToken})
+    if err := result.Decode(&token); err != nil {
+        return nil, err
+    }
+    return &token, nil
 }
 
 func (repo *MongoTokenRepository) DeleteToken(ctx context.Context, tokenID primitive.ObjectID) error {
-    _, err := repo.db.DeleteOne(ctx, bson.M{"_id": tokenID})
-    return err
+    deletedCount, err := repo.db.DeleteOne(ctx, bson.M{"_id": tokenID})
+    if err != nil {
+        return err
+    }
+    if deletedCount == 0 {
+        return errors.New("no token found to delete")
+    }
+    return nil
 }

9abfdd6f6f4e0222d05a158af3a61f98c4771281
Olana Kelbesa <olanakelbesa@gmail.com>
2024-08-16 10:26:16 +0300

    Credit card update

--- web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/page.tsx
+++ web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/page.tsx
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-import React from "react";
-import Card from "@/app/components/common/card";
-import CreditCardComponent from "./CreditCards/CreditCardComponent";
-=======
 import Image from "next/image";
 export default function Home() {
-return(
-  
-  <>  </>
-);
+	return <> </>;
 }
->>>>>>> 141e3cb161eadb389b2b8b976e45414f9d329403
-
-const Home = () => {
-	return (
-		<>
-			<CreditCardComponent />{" "}
-		</>
-	);
-};
-
-export default Home;

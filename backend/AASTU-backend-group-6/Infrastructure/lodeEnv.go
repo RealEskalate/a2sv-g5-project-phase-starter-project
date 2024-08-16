13d9c48e94018c3a384aca8a477200ab7ea848ee
Daniel Asfaw <asfawdaniel779@gmail.com>
2024-08-16 17:06:33 +0300

    working

--- backend/AASTU-backend-group-6/Infrastructure/lodeEnv.go
+++ backend/AASTU-backend-group-6/Infrastructure/lodeEnv.go
@@ -9,72 +9,72 @@ import (
 )
 
 type Config struct {
-    DatabaseUrl            string
-    Port                   int
-    DbName                 string
-    UserCollection         string
-    BlogCollection         string
-    ContextTimeout         int
-    AccessTokenExpiryHour  int
-    RefreshTokenExpiryHour int
-    AccessTokenSecret      string
-    RefreshTokenSecret     string
+	DatabaseUrl            string
+	Port                   int
+	DbName                 string
+	UserCollection         string
+	BlogCollection         string
+	ContextTimeout         int
+	AccessTokenExpiryHour  int
+	RefreshTokenExpiryHour int
+	AccessTokenSecret      string
+	RefreshTokenSecret     string
 }
 
 func LoadEnv() (*Config, error) {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-        return nil, err
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		return nil, err
+	}
 
-    dbURL := os.Getenv("DATABASE_URL")
-    portStr := os.Getenv("PORT")
-    dbname := os.Getenv("DB_NAME")
-    usercoll := os.Getenv("user_collection")
-    blogcoll := os.Getenv("blog_collection")
-    contextTimeoutStr := os.Getenv("CONTEXT_TIMEOUT")
-    accessTokenExpiryHourStr := os.Getenv("ACCESS_TOKEN_EXPIRY_HOUR")
-    refreshTokenExpiryHourStr := os.Getenv("REFRESH_TOKEN_EXPIRY_HOUR")
-    accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
-    refreshTokenSecret := os.Getenv("REFRESH_TOKEN_SECRET")
+	dbURL := os.Getenv("DATABASE_URL")
+	portStr := os.Getenv("PORT")
+	dbname := os.Getenv("DB_NAME")
+	usercoll := os.Getenv("user_collection")
+	blogcoll := os.Getenv("blog_collection")
+	contextTimeoutStr := os.Getenv("CONTEXT_TIMEOUT")
+	accessTokenExpiryHourStr := os.Getenv("ACCESS_TOKEN_EXPIRY_HOUR")
+	refreshTokenExpiryHourStr := os.Getenv("REFRESH_TOKEN_EXPIRY_HOUR")
+	accessTokenSecret := os.Getenv("ACCESS_TOKEN_SECRET")
+	refreshTokenSecret := os.Getenv("REFRESH_TOKEN_SECRET")
 
-    port, err := strconv.Atoi(portStr)
-    if err != nil {
-        log.Fatal("Invalid PORT value")
-        return nil, err
-    }
+	port, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatal("Invalid PORT value")
+		return nil, err
+	}
 
-    contextTimeout, err := strconv.Atoi(contextTimeoutStr)
-    if err != nil {
-        log.Fatal("Invalid CONTEXT_TIMEOUT value")
-        return nil, err
-    }
+	contextTimeout, err := strconv.Atoi(contextTimeoutStr)
+	if err != nil {
+		log.Fatal("Invalid CONTEXT_TIMEOUT value")
+		return nil, err
+	}
 
-    accessTokenExpiryHour, err := strconv.Atoi(accessTokenExpiryHourStr)
-    if err != nil {
-        log.Fatal("Invalid ACCESS_TOKEN_EXPIRY_HOUR value")
-        return nil, err
-    }
+	accessTokenExpiryHour, err := strconv.Atoi(accessTokenExpiryHourStr)
+	if err != nil {
+		log.Fatal("Invalid ACCESS_TOKEN_EXPIRY_HOUR value")
+		return nil, err
+	}
 
-    refreshTokenExpiryHour, err := strconv.Atoi(refreshTokenExpiryHourStr)
-    if err != nil {
-        log.Fatal("Invalid REFRESH_TOKEN_EXPIRY_HOUR value")
-        return nil, err
-    }
+	refreshTokenExpiryHour, err := strconv.Atoi(refreshTokenExpiryHourStr)
+	if err != nil {
+		log.Fatal("Invalid REFRESH_TOKEN_EXPIRY_HOUR value")
+		return nil, err
+	}
 
-    config := &Config{
-        DatabaseUrl:            dbURL,
-        Port:                   port,
-        DbName:                 dbname,
-        UserCollection:         usercoll,
-        BlogCollection:         blogcoll,
-        ContextTimeout:         contextTimeout,
-        AccessTokenExpiryHour:  accessTokenExpiryHour,
-        RefreshTokenExpiryHour: refreshTokenExpiryHour,
-        AccessTokenSecret:      accessTokenSecret,
-        RefreshTokenSecret:     refreshTokenSecret,
-    }
+	config := &Config{
+		DatabaseUrl:            dbURL,
+		Port:                   port,
+		DbName:                 dbname,
+		UserCollection:         usercoll,
+		BlogCollection:         blogcoll,
+		ContextTimeout:         contextTimeout,
+		AccessTokenExpiryHour:  accessTokenExpiryHour,
+		RefreshTokenExpiryHour: refreshTokenExpiryHour,
+		AccessTokenSecret:      accessTokenSecret,
+		RefreshTokenSecret:     refreshTokenSecret,
+	}
 
-    return config, nil
+	return config, nil
 }

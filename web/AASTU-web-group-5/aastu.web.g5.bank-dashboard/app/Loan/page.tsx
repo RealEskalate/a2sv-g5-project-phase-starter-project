f5ea1a6621de016802d028f9c4d3024d5930bd29
Olana Kelbesa <olanakelbesa@gmail.com>
2024-08-16 15:17:53 +0300

    loan page

--- web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/Loan/page.tsx
+++ web/AASTU-web-group-5/aastu.web.g5.bank-dashboard/app/Loan/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import LoanCards from "./LoanCards";
+import LoanTable from "./LoanTable";
 
 function page() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div className="px-10 py-8">
+			<LoanCards />
+			<LoanTable />
+		</div>
+	);
 }
 
-export default page
+export default page;

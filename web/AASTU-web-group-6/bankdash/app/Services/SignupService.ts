dad34485cd5ade0fee423f8f6ae96a18ac14e052
naodmulu <naodmulugeta1621@gmail.com>
2024-08-16 13:00:31 +0300

    Fix: Deploy Conflict Fix

--- web/AASTU-web-group-6/bankdash/app/Services/SignupService.ts
+++ web/AASTU-web-group-6/bankdash/app/Services/SignupService.ts
@@ -1,11 +1,12 @@
 import axios, { isAxiosError } from 'axios';
 import UserValue from '@/types/UserValue';
 import ResponseValue from '@/types/ResponseValue';
+import SignupResponseValue from '@/types/SignupResponseValue';
 
 
 const SignupService = async (formData: UserValue): Promise<ResponseValue> => {
     try {
-        const response = await axios.post(
+        const response:SignupResponseValue = await axios.post(
             "https://bank-dashboard-6acc.onrender.com/auth/signup",
             formData,
             {

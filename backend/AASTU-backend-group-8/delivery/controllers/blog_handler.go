8893b93adbbfaf4fc7535d4ad213c28142298d71
Kalkidan Amare <kalkidanamare11a@gmail.com>
2024-08-16 15:01:17 +0300

    add controllers

---
+++ backend/AASTU-backend-group-8/delivery/controllers/blog_handler.go
@@ -0,0 +1,117 @@
+package controllers
+
+import(
+	"meleket/domain"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type BlogController struct {
+	blogUsecase domain.BlogUsecaseInterface
+}
+
+func NewBlogController(blogUsecase domain.BlogUsecaseInterface) *BlogController {
+	return &BlogController{blogUsecase: blogUsecase}
+}
+
+func (bc *BlogController) CreateBlogPost(c *gin.Context){
+	var blog domain.BlogPost
+	if err := c.ShouldBindJSON(&blog); err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	autherID := c.MustGet("userID").(primitive.ObjectID)
+	blog.AuthorID = autherID
+
+	createdBlog,err := bc.blogUsecase.CreateBlogPost(&blog)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "Blog post created successfully", "blog": createdBlog})
+}
+
+func (bc *BlogController) GetAllBlogPosts(c *gin.Context){
+	blogs, err := bc.blogUsecase.GetAllBlogPosts()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, blogs)
+}
+
+func (bc *BlogController) GetBlogByID(c *gin.Context){
+	id , err := primitive.ObjectIDFromHex(c.Param("id")) // should be updated
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+		return
+	}
+
+	blog, err := bc.blogUsecase.GetBlogByID(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Blog post not found"})
+		return
+	}
+
+	c.JSON(http.StatusOK, blog)
+
+}
+
+func (bc *BlogController) UpdateBlogPost(c *gin.Context) {
+	id , err := primitive.ObjectIDFromHex(c.Param("id")) // should be updated
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+		return
+	}
+
+	var blog domain.BlogPost
+	if err := c.ShouldBindJSON(&blog); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	updatedBlog, err := bc.blogUsecase.UpdateBlogPost(id, &blog)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, updatedBlog)
+}
+
+func (bc *BlogController) SearchBlogPost(c *gin.Context) {
+	var search domain.SearchBlogPost
+
+	if err := c.ShouldBindJSON(&search); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	blogs, err := bc.blogUsecase.SearchBlogPosts(&search)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, blogs)
+}
+
+func (bc *BlogController) DeleteBlogPost(c *gin.Context) {
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
+		return
+	}
+
+	if err := bc.blogUsecase.DeleteBlogPost(id); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Blog post deleted successfully"})
+}
---
+++ backend/AASTU-backend-group-8/delivery/controllers/user_handler.go
@@ -0,0 +1,153 @@
+package controllers
+
+import(
+	"meleket/domain"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive" // should be updated
+)
+
+type UserController struct {
+	userUsecase domain.UserUsecaseInterface
+}
+
+func NewUserController(usercase domain.UserUsecaseInterface) *UserController {
+	return &UserController{userUsecase: usercase}
+}
+
+func (uc *UserController) Register(c *gin.Context){
+	var user domain.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	if err := uc.userUsecase.Register(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user": user})
+}
+
+func (uc *UserController) Login(c *gin.Context){
+	var user domain.AuthUser
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	token, refreshToken, err := uc.userUsecase.Login(&user); 
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged in successfully", "token": token, "refresh_token": refreshToken})
+}
+
+func (uc *UserController) Logout(c *gin.Context) {
+	userID := c.MustGet("userID")
+	objectID, ok := userID.(primitive.ObjectID)
+	//the below might not be necessary
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Invalid userID type"})
+		return
+	}
+	
+	err := uc.userUsecase.DeleteRefeshToken(objectID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User logged out successfully"})
+}
+
+func (uc *UserController) RefreshToken(c *gin.Context){
+	var token domain.RefreshToken
+	if err := c.ShouldBindJSON(&token); err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	newToken, err := uc.userUsecase.RefreshToken(&token)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Token refreshed successfully", "token": newToken})
+}
+
+func (uc *UserController) ForgotPassword(c *gin.Context){
+	var email domain.Email
+	if err := c.ShouldBindJSON(&email); err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	err := uc.userUsecase.ForgotPassword(&email.Email)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Password reset link sent to email"})
+}
+
+func (uc *UserController) GetProfile(c *gin.Context){
+	userID := c.MustGet("userID").(primitive.ObjectID)
+	profile, err := uc.userUsecase.GetProfile(userID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, profile)
+}
+
+func (uc *UserController) UpdateProfile(c *gin.Context){  //gonna include change password here in update profile
+	userID := c.MustGet("userID").(primitive.ObjectID)
+	var profile domain.Profile
+	if err := c.ShouldBindJSON(&profile); err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{"error":err.Error()})
+		return
+	}
+
+	updatedProfile,err := uc.userUsecase.UpdateProfile(userID, &profile)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Profile updated successfully", "updatedProfile": updatedProfile})
+}
+
+
+func (uc *UserController) GetAllUsers(c *gin.Context){
+	users, err := uc.userUsecase.GetAllUsers()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, users)
+}
+
+func (uc *UserController) DeleteUser(c *gin.Context) {
+	id, err := primitive.ObjectIDFromHex(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	err = uc.userUsecase.DeleteUser(id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User deleted successfully"})
+}

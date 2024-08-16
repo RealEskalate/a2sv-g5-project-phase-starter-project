f09b3ca035ec97834333dd449a0932cb6763f7a4
Samuel Keno <sgere195@gmail.com>
2024-08-16 11:16:07 +0300

    setting up  rtk and redux

--- web/ASTU-web-group-2/banking-dashboard/app/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/page.tsx
@@ -1,29 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import WeeklyActivityChart from "./components/charts/WeeklyActivityChart";
-import Card from "./components/card/Card";
-import CardForCreditCards from "./components/card/CardForCreditCards";
+const page = () => {
 
-const page = () => {
-  return (
-<<<<<<< HEAD
-    <div className="grid grid-cols-2">
-      <CardForCreditCards title="Weekly Activity" button="See All" link="/report">
-        <WeeklyActivityChart />
-      </CardForCreditCards>
-    </div>
-  );
-};
-=======
-    <div>page </div>
->>>>>>> 6b39585 (updated the recent transaction and finished the list card components)
-=======
-=======
->>>>>>> 607423e (fixed conflict)
-const page = () => {
   return <div></div>;
 };
->>>>>>> 0412ffb (Active Loans Over view completed)
 
 export default page;

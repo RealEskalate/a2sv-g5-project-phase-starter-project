2c39df56be9dea9cae31e466ba79566d52d62075
selome <selomegech@gmail.com>
2024-08-16 15:01:57 +0300

    newchanges

--- web/AASTU-web-group-4/next-bank/app/(root)/page.tsx
+++ web/AASTU-web-group-4/next-bank/app/(root)/page.tsx
@@ -5,10 +5,12 @@ import { colors  , logo} from '@/constants'
 import DesktopCreditCart from '@/components/DesktopCreditCard'
 import RecentTransaction from '@/components/Recent Transaction'
 import ExpensesChart from '@/components/ExpensesCart'
-import { icons } from 'lucide-react'
+import { icons, Import } from 'lucide-react'
 import { text } from 'stream/consumers'
 import BarChart from '@/components/BarChart'
 import  PieChart  from '@/components/PieChart'  
+import QuickTransfer from '@/components/QuickTransfer'
+import LineChart from '@/components/Linechart'
 
 const page = () => {
   return (
@@ -19,7 +21,7 @@ const page = () => {
             <h1 className='font-bold text-2xl'>My Cards</h1>
             <p className='my-2'> See All</p>
           </div>
-          <div className='flex justify-between gap-3 overflow-x-auto md: max-w-4xl'>
+          <div className='flex justify- gap-3 overflow-x-auto md: max-w-4xl'>
             <div className='flex py-3 '> <DesktopCreditCart bgColor={colors.blue} textColor={colors.textwhite}/></div>
             <div className='flex py-3'> <DesktopCreditCart bgColor={colors.white} textColor={colors.textblack}/></div>
           </div>
@@ -44,11 +46,36 @@ const page = () => {
         </div>
         <div className='  md:w-2/5 py-5'>
           <div className={`${colors.navbartext} flex justify-between py-4`}>
-            <h1 className='font-bold text-2xl'>Expense Statstics</h1>
+            <h1 className='font-bold text-2xl'>Quick Transfer</h1>
           </div>
           <div><PieChart/></div>
         </div>
+
             </div>
+           
+           
+           
+           
+            <div className='flex flex-col justify-between w-screen  md:flex-row max-w-screen-2xl gap-10 '>
+        <div className=' md:w-2/5 py-4 '>
+          <div className={`${colors.navbartext} flex justify-between `}>
+            <h1 className='font-bold text-2xl'>Quick Transfer</h1>
+            
+          </div>
+          <div className='flex justify- gap-3 overflow-x-auto md: max-w-4xl'>
+            <div className='flex py-3 '> <QuickTransfer/></div>
+           
+          </div>
+        </div>
+        <div className='w-3/5'>
+          <div className={`${colors.navbartext} flex justify-between  py-4`}>
+            <h1 className='font-bold text-2xl'>Balance History</h1>
+          </div>
+          <div className=''>
+           <LineChart/>
+          </div>
+        </div>
+      </div>
       </div>
     
   )

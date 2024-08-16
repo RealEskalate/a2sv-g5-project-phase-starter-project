80ece57bca5e9483fde4c5374373b2af5886c795
nanati45 <nansiasamnew@gmail.com>
2024-08-16 11:46:15 +0300

    credit card with static data done

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/credit-card/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/credit-card/page.tsx
@@ -1,7 +1,9 @@
 import AddNewCard from "@/components/AddNewCard/AddNewCard";
 import CardList from "@/components/CardList/CardList";
 import CardSettings from "@/components/CardSettings/CardSettings";
+import CardExpenceStatistics from "@/components/Charts/CardExpenceStatistics";
 import MyCard from "@/components/MyCard/MyCard";
+import { Plus } from "lucide-react";
 import React from "react";
 
 export default function page() {
@@ -9,29 +11,26 @@ export default function page() {
     <div className="space-y-5 ">
       <div>
         <h1 className="text-[#333B69] py-2 font-semibold">My Cards</h1>
+
         <div className="flex overflow-x-scroll space-x-2 scroll whitespace-nowrap scroll-smooth lg:flex lg:space-x-4  ">
           <MyCard />
           <MyCard />
           <MyCard />
+          <div className="w-[295px] h-[175px] bg-gray-200 rounded-3xl justify-center items-center flex flex-shrink-0">
+            <Plus size={32} />
+          </div>
         </div>
       </div>
 
-      <div className="space-y-5 md:flex md:gap-6">
-        <div className=" md:w-1/3 ">
-          <p className="text-[#333B69] pb-2 font-semibold">
-            Card Expense Statistics
-          </p>
-          <div className="bg-white rounded-3xl ">
-            shvshs dfhbsfs hhhhhhhhhhhhhhhhhhfds hhhhhhhhhhh hsjdffff
-            ffffffffffajcvdjhbc havvvvvvvabervhdrvb b chxvjzbvhvchv vh
-            dfahvbhvash v dshvshvshddsh
-          </div>
+      <div className="space-y-5 lg:space-y-0 lg:flex lg:gap-6">
+        <div className=" w-full lg:w-4/12">
+          <CardExpenceStatistics />
         </div>
-        <div className="w-full md:w-2/3 ">
+        <div className="w-full lg:w-2/3  ">
           <CardList />
         </div>
       </div>
-      <div className="space-y-5 md:flex md:gap-6">
+      <div className="space-y-5 md:space-y-0 md:flex md:gap-6">
         <AddNewCard />
         <CardSettings />
       </div>

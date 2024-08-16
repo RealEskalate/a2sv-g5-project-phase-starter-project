60a374f8e2063442f14500a5d618eaa0c42e948a
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-16 01:55:52 -0700

    user repo implemented

--- backend/ASTU-backend-group-5/internal/repository/mongodb/user_repo.go
+++ backend/ASTU-backend-group-5/internal/repository/mongodb/user_repo.go
@@ -13,6 +13,12 @@ type UserRepositoryMongo struct {
 	Collection *mongo.Collection
 }
 
+func NewUserRepositoryMongo(collection *mongo.Collection) *UserRepositoryMongo {
+	return &UserRepositoryMongo{
+		Collection: collection,
+	}
+}
+
 func (r *UserRepositoryMongo) CreateUser(ctx context.Context, user *domain.User) error {
 	user.ID = primitive.NewObjectID()
 	_, err := r.Collection.InsertOne(ctx, user)
@@ -22,21 +28,76 @@ func (r *UserRepositoryMongo) CreateUser(ctx context.Context, user *domain.User)
 func (r *UserRepositoryMongo) FindUserByEmail(ctx context.Context, email string) (*domain.User, error) {
 	user := &domain.User{}
 	err := r.Collection.FindOne(ctx, bson.M{"email": email}).Decode(user)
-	if err == mongo.ErrNoDocuments {
-		return nil, nil //expected if no user is found
-	}
-	if err != nil { //something went wrong
+	if err != nil {
 		return nil, err
 	}
 	return user, err //user exists
 }
 
-func (r *UserRepositoryMongo) UpdatePassword(ctx context.Context, email string, password string) error {
-	_, err := r.Collection.UpdateOne(ctx, bson.M{"email": email}, bson.M{"$set": bson.M{"password": password}})
-	return err
+func (r *UserRepositoryMongo) FindUserById(ctx context.Context, id string) (*domain.User, error) {
+	user := &domain.User{}
+	err := r.Collection.FindOne(ctx, bson.M{"_id": id}).Decode(user)
+	if err != nil {
+		return nil, err
+	}
+	return user, err
+}
+
+func (r *UserRepositoryMongo) FindUserByUserName(ctx context.Context, username string) (*domain.User, error) {
+	user := &domain.User{}
+	err := r.Collection.FindOne(ctx, bson.M{"username": username}).Decode(user)
+	if err != nil {
+		return nil, err
+	}
+	return user, err //user exists
 }
 
 func (r *UserRepositoryMongo) UpdateUser(ctx context.Context, user *domain.User) error {
 	_, err := r.Collection.UpdateOne(ctx, bson.M{"_id": user.ID}, bson.M{"$set": user})
 	return err
+}
+
+func (r *UserRepositoryMongo) DeleteUser(ctx context.Context, id string) error {
+	_, err := r.Collection.DeleteOne(ctx, bson.M{"_id": id})
+	return err
+}
+
+func (r *UserRepositoryMongo) GetAllUsers(ctx context.Context) ([]*domain.User, error) {
+	var users []*domain.User
+
+	cursor, err := r.Collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	for cursor.Next(ctx) {
+		var user domain.User
+		if err := cursor.Decode(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+
+	return users, nil
+}
+
+func (r *UserRepositoryMongo) FilterUsers(ctx context.Context, filter map[string]interface{}) ([]*domain.User, error) {
+
+	var users []*domain.User
+
+	cursor, err := r.Collection.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	for cursor.Next(ctx) {
+		var user domain.User
+		if err := cursor.Decode(&user); err != nil {
+			return nil, err
+		}
+		users = append(users, &user)
+	}
+
+	return users, nil
+
 }
--- backend/ASTU-backend-group-5/internal/usecase/user/profile_update.go
+++ backend/ASTU-backend-group-5/internal/usecase/user/profile_update.go
@@ -1,1 +1,20 @@
 package user
+
+import (
+	"blogApp/internal/domain"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (u *UserUsecase) UpdateProfile(user *domain.User) error {
+	userId := user.ID
+	dbUser, err := u.repo.FindUserByEmail(context.Background(), userId.Hex())
+	if err != nil {
+		return err
+	}
+	dbUser.Profile = user.Profile
+	dbUser.Updated = primitive.NewDateTimeFromTime(time.Now())
+	return u.repo.UpdateUser(context.Background(), dbUser)
+}

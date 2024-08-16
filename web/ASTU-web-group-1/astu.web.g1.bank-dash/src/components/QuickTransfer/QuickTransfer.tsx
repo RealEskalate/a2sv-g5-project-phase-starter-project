f2e478d5e065c1faf1ae490700f8e6fa7ac09af0
naol86 <naol.kasinet@a2sv.org>
2024-08-16 07:48:45 +0300

    pull charts from origin

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/QuickTransfer/QuickTransfer.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/QuickTransfer/QuickTransfer.tsx
@@ -11,8 +11,8 @@ import {
 
 const QuickTransfer = () => {
   return (
-    <div className='flex flex-col gap-[12px] md:gap-[16px] lg:gap-[20px]'>
-      <h1 className='text-navy text-18px lg:text-22px'>Quick Transfer</h1>
+    <div className='flex flex-col '>
+      <p className='text-[#333B69] font-semibold mt-5 mb-3'>Quick Transfer</p>
       <div className='flex flex-col lg:gap-5 rounded-3xl  md:w-[320px]  w-[325px] py-5 px-6 bg-white'>
         <div className='px-10 md:px-6'>
           <Carousel className='w-full max-w-sm'>
@@ -32,17 +32,17 @@ const QuickTransfer = () => {
             <CarouselNext />
           </Carousel>
         </div>
-        <div className='flex flex-row gap-[12px] lg:gap-[35px] w-full text-12px lg:text-16px'>
-          <p className='text-blue-steel flex whitespace-nowrap items-center'>Write amount</p>
+        <div className='flex flex-row  w-full text-15px'>
+          <p className='text-blue-steel flex whitespace-nowrap items-center mr-2'>Write amount</p>
           <div className='flex relative flex-row items-center h-10 rounded-full'>
             <div className='relative flex-1 h-full'>
               <input
                 type='number'
                 placeholder='552.50'
-                className='bg-[#EDF1F7] rounded-full pl-[10px] pr-[100px] lg:pl-[20px] lg:pr-[120px] w-full border-none h-full'
+                className='bg-[#EDF1F7] rounded-full pl-[10px] pr-[100px] w-full border-none h-full outline-none'
               />
             </div>
-            <button className='bg-blue-bright absolute right-0 rounded-full px-[21px] lg:px-[24px] gap-[9px] lg:gap-[15px] h-full text-white flex items-center justify-center'>
+            <button className='bg-blue-bright absolute right-0 rounded-full px-5  h-full text-white flex items-center justify-center '>
               <p className='block'>send</p>
               <Telegram />
             </button>

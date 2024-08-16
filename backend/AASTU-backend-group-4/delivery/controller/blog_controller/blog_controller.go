1c3abbfefd0c32372993f5c94b7053aab0b72b1f
Nahom Zewdu <nahiyo86@gmail.com>
2024-08-16 04:26:00 +0300

    Defined struct field for all layers

--- backend/AASTU-backend-group-4/delivery/controller/blog_controller/blog_controller.go
+++ backend/AASTU-backend-group-4/delivery/controller/blog_controller/blog_controller.go
@@ -1,1 +1,9 @@
 package blog_controller
+
+import (
+	"blog-api/domain/blog"
+)
+
+type blogController struct {
+	usecase blog.BlogUsecase
+}
--- backend/AASTU-backend-group-4/usecase/blog_usecase/blog_usecase.go
+++ backend/AASTU-backend-group-4/usecase/blog_usecase/blog_usecase.go
@@ -1,1 +1,19 @@
 package blog_usecase
+
+import (
+	"time"
+
+	"blog-api/domain/blog"
+)
+
+type BlogUsecase struct {
+	repo           blog.BlogRepository
+	contextTimeout time.Duration
+}
+
+func NewBlogUsecase(BlogRepository blog.BlogRepository, timeout time.Duration) blog.BlogUsecase {
+	return &BlogUsecase{
+		repo:           BlogRepository,
+		contextTimeout: timeout,
+	}
+}

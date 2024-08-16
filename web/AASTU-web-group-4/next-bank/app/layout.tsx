71eb115d02abf1a1a564db6883e280f93a9d4995
matiyasjosh <torbaw457@gmail.com>
2024-08-16 23:29:26 +0300

    The credit card page

--- web/AASTU-web-group-4/next-bank/app/layout.tsx
+++ web/AASTU-web-group-4/next-bank/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`inter.className bg-[#e6e9ef]`}>
         <ReduxProvider>{children}</ReduxProvider></body>
     </html>
   );
---
+++ web/AASTU-web-group-4/next-bank/components/CardSettingLine.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { colors } from '@/constants';
+import Image from 'next/image';
+
+const CardSettingLine = ({icon, title, description, background}:{icon:string, title:string, description:string, background:string}) => {
+  return (
+    <div className='flex p-0.5 justify-between'>
+        <div className=' flex gap-4'>
+            <div className={`rounded-full flex items-center justify-center p-4 h-20 w-20`}>
+                <Image
+            src={icon}
+            alt="go"
+            width={120}
+            height={120}
+            className={`${background} p-2 rounded-xl items-center`}
+            />
+            </div>
+            <div className='my-2'>
+            <p className='md:text-[16px] sm:text-[13px] font-medium pb-1'>{title}</p>
+            <p className={`${colors.textgray} md:text-[13px] sm:text-[11px] text-start`}>{ description }</p>
+            </div>
+        </div>
+        </div>
+  )
+}
+
+export default CardSettingLine

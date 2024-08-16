9f8dd76c64597923c18cbf375ebe58d673168a4e
Hiwot2127 <hiwotbkm25@gmail.com>
2024-08-16 17:50:27 +0300

    :fire: Authentication, collection added

--- backend/AASTU-backend-group-9/repository/token_repository.go
+++ backend/AASTU-backend-group-9/repository/token_repository.go
@@ -1,54 +1,56 @@
 package repository
 
 import (
-    "context"
+	"context"
 	"errors"
-    "blog/domain"
-    "blog/database"
+	"blog/database"
 	"blog/domain"
 
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type MongoTokenRepository struct {
-    db database.Collection
+	db         database.Database
+	collection string
 }
 
-func NewMongoTokenRepository(db database.Collection) domain.TokenRepository {
-    return &MongoTokenRepository{db: db}
+func NewMongoTokenRepository(db database.Database) domain.TokenRepository {
+	return &MongoTokenRepository{
+		db:         db,
+		collection: domain.TokenCollection,
+	}
 }
 
 func (repo *MongoTokenRepository) SaveToken(ctx context.Context, token *domain.Token) error {
-    _, err := repo.db.InsertOne(ctx, token)
-    return err
+	collection := repo.db.Collection(repo.collection)
+	_, err := collection.InsertOne(ctx, token)
+	return err
 }
 
+
 func (repo *MongoTokenRepository) FindTokenByAccessToken(ctx context.Context, accessToken string) (*domain.Token, error) {
-    var token domain.Token
-    result := repo.db.FindOne(ctx, bson.M{"access_token": accessToken})
-    if err := result.Decode(&token); err != nil {
-        return nil, err
-    }
-    return &token, nil
+	var token domain.Token
+	collection := repo.db.Collection(repo.collection)
+	err := collection.FindOne(ctx, bson.M{"access_token": accessToken}).Decode(&token)
+	return &token, err
 }
 
 func (repo *MongoTokenRepository) FindTokenByRefreshToken(ctx context.Context, refreshToken string) (*domain.Token, error) {
-    var token domain.Token
-    result := repo.db.FindOne(ctx, bson.M{"refresh_token": refreshToken})
-    if err := result.Decode(&token); err != nil {
-        return nil, err
-    }
-    return &token, nil
+	var token domain.Token
+	collection := repo.db.Collection(repo.collection)
+	err := collection.FindOne(ctx, bson.M{"refresh_token": refreshToken}).Decode(&token)
+	return &token, err
 }
 
 func (repo *MongoTokenRepository) DeleteToken(ctx context.Context, tokenID primitive.ObjectID) error {
-    deletedCount, err := repo.db.DeleteOne(ctx, bson.M{"_id": tokenID})
-    if err != nil {
-        return err
-    }
-    if deletedCount == 0 {
-        return errors.New("no token found to delete")
-    }
-    return nil
+	collection := repo.db.Collection(repo.collection)
+	deletedCount, err := collection.DeleteOne(ctx, bson.M{"_id": tokenID})
+	if err != nil {
+		return err
+	}
+	if deletedCount == 0 {
+		return errors.New("no token found to delete")
+	}
+	return nil
 }

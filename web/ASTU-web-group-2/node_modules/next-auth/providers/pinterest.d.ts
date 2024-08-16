1bd4dfeef0b3ccbf27db601710a4701a8c8733f4
fasill <fasilhawultie19@gmail.com>
2024-08-16 11:17:13 +0300

    next auth setted up

---
+++ web/ASTU-web-group-2/node_modules/next-auth/providers/pinterest.d.ts
@@ -0,0 +1,9 @@
+import { OAuthConfig, OAuthUserConfig } from ".";
+export interface PinterestProfile extends Record<string, any> {
+    account_type: "BUSINESS" | "PINNER";
+    profile_image: string;
+    website_url: string;
+    username: string;
+}
+export default function PinterestProvider<P extends PinterestProfile>(options: OAuthUserConfig<P>): OAuthConfig<P>;
+//# sourceMappingURL=pinterest.d.ts.map

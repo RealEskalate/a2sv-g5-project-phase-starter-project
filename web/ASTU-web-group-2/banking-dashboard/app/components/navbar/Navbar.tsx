49f2857306a6bfe64919909b137d12a76ef7a2f8
Kuma Telila <kumatelila26@gmail.com>
2024-08-16 17:49:35 +0300

    Sidebar responsive done

--- web/ASTU-web-group-2/banking-dashboard/app/components/navbar/Navbar.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/navbar/Navbar.tsx
@@ -1,49 +1,58 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "../sidebar/Sidebar"; // Import Sidebar component
 
-interface titleProp {
+interface TitleProp {
   title: string;
 }
 
-const Navbar = ({ title }: titleProp) => {
-  return (
-    <header className="flex flex-col justify-center items-center">
-      <div className="flex w-full">
-        <div className="logo flex items-center justify-between sm:mr-14 pl-[27px]">
-          <Image
-            src={"assets/navbar/hamburger.svg"}
-            width={25}
-            height={25}
-            alt="hamburger"
-            className="mr-3 sm:hidden block"
-          />
-          <Image
-            src={"/assets/navbar/credit-card.svg"}
-            width={36}
-            height={36}
-            alt="bankDash logo"
-            className="mr-3 sm:block hidden"
-          />
-          <p className="font-black text-[25px] text-[#343C6A] sm:flex hidden">
-            BankDash.
-          </p>
-        </div>
+const Navbar = ({ title }: TitleProp) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
-        <div className="w-full m-5 flex sm:justify-between items-center">
-          <div className="flex w-full justify-center sm:w-auto">
-            <p className="font-semibold text-[20px] sm:text-[25px] text-[#343C6A] sm:ml-7">
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <>
+      <header className="fixed top-0 left-0 right-0 bg-white shadow-md z-40">
+        <div className="flex w-full items-center justify-between px-4 py-2">
+          <div className="flex items-center">
+            <Image
+              src={"/assets/navbar/hamburger.svg"}
+              width={25}
+              height={25}
+              alt="hamburger"
+              className="sm:hidden block cursor-pointer"
+              onClick={toggleSidebar}
+            />
+            <Image
+              src={"/assets/navbar/credit-card.svg"}
+              width={36}
+              height={36}
+              alt="bankDash logo"
+              className="mr-3 sm:block hidden"
+            />
+            <p className="font-black text-[25px] text-[#343C6A] sm:flex hidden">
+              BankDash.
+            </p>
+          </div>
+
+          <div className="flex-grow flex items-center justify-center">
+            <p className="font-semibold text-[20px] sm:text-[25px] text-[#343C6A]">
               {title}
             </p>
           </div>
 
-          <div className="sm:flex position:absolute right-0 gap-5">
-            <div className="search-div hidden sm:flex bg-[#F5F7FA] items-center rounded-full h-[50px] pl-5 pr-5 pt-3 pb-3">
+          <div className="hidden sm:flex gap-5 items-center">
+            <div className="search-div bg-[#F5F7FA] items-center rounded-full h-[50px] pl-5 pr-5 pt-3 pb-3">
               <Image
                 src={"/assets/navbar/magnifying-glass.svg"}
                 width={20}
                 height={20}
                 alt="magnifying-glass"
-                className="mr-5 "
+                className="mr-5"
               />
               <input
                 type="text"
@@ -52,7 +61,7 @@ const Navbar = ({ title }: titleProp) => {
               />
             </div>
 
-            <div className="bg-[#F5F7FA] hidden sm:flex justify-center rounded-full items-center">
+            <div className="bg-[#F5F7FA] rounded-full flex justify-center items-center">
               <Image
                 src={"/assets/navbar/settings.svg"}
                 width={50}
@@ -62,7 +71,7 @@ const Navbar = ({ title }: titleProp) => {
               />
             </div>
 
-            <div className="bg-[#F5F7FA] hidden sm:flex justify-center rounded-full items-center">
+            <div className="bg-[#F5F7FA] rounded-full flex justify-center items-center">
               <Image
                 src={"/assets/navbar/notification.svg"}
                 width={50}
@@ -77,27 +86,29 @@ const Navbar = ({ title }: titleProp) => {
               width={50}
               height={50}
               alt="profile-picture"
-              className="object-fill rounded-full "
+              className="object-fill rounded-full"
             />
           </div>
         </div>
-      </div>
 
-      <div className="search-div flex w-4/5 sm:hidden bg-[#F5F7FA] items-center rounded-full pl-5 pr-5 pt-3 pb-3">
-        <Image
-          src={"/assets/navbar/magnifying-glass.svg"}
-          width={20}
-          height={20}
-          alt="magnifying-glass"
-          className="mr-5"
-        />
-        <input
-          type="text"
-          placeholder="Search for something"
-          className="text-md bg-[#F5F7FA] w-full"
-        />
-      </div>
-    </header>
+        <div className="search-div flex w-4/5 sm:hidden bg-[#F5F7FA] items-center rounded-full pl-5 pr-5 pt-3 pb-3 mt-2 mx-auto">
+          <Image
+            src={"/assets/navbar/magnifying-glass.svg"}
+            width={20}
+            height={20}
+            alt="magnifying-glass"
+            className="mr-5"
+          />
+          <input
+            type="text"
+            placeholder="Search for something"
+            className="text-md bg-[#F5F7FA] w-full"
+          />
+        </div>
+      </header>
+
+      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+    </>
   );
 };
 

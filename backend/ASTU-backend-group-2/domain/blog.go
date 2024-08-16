f7830e19d453baab472f5e5e0e4ed9aed58bf484
dagota12 <chernetdagim@gmail.com>
2024-08-16 17:26:27 +0300

    defining initial entities user and blog models are defined

--- backend/ASTU-backend-group-2/domain/blog.go
+++ backend/ASTU-backend-group-2/domain/blog.go
@@ -1,1 +1,48 @@
 package domain
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// this structure will be used when responding for blog request
+type Blog struct {
+	ID           primitive.ObjectID `json:"id" bson:"_id"`
+	AuthorID     primitive.ObjectID `json:"author_id" bson:"author_id"`
+	Title        string             `json:"title" bson:"title" binding:"required"`
+	Tags         []string           `json:"tags" bson:"tags"`
+	Content      string             `json:"content" bson:"content" binding:"required"`
+	ViewCount    int                `json:"view_count" bson:"view_count"`
+	LikeCount    int                `json:"like_count" bson:"like_count"`
+	DislikeCount int                `json:"dislike_count" bson:"dislike_count"`
+	Comments     []Comment          `json:"comments" bson:"comments"`
+	Likes        []UserAction       `json:"likes" bson:"likes"`
+	Dislikes     []UserAction       `json:"dislikes" bson:"dislikes"`
+	CreatedAt    time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt    time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+// defines the structure for the blogs that will be  received from the request when creating and updating
+type BlogIn struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	AuthorID  primitive.ObjectID `json:"author_id" bson:"author_id"`
+	Title     string             `json:"title" bson:"title" binding:"required"`
+	Tags      []string           `json:"tags" bson:"tags"`
+	Content   string             `json:"content" bson:"content" binding:"required"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+// this structure when creating and updating comments
+type Comment struct {
+	ID        primitive.ObjectID `json:"id" bson:"_id"`
+	UserID    primitive.ObjectID `json:"user_id" bson:"user_id"`
+	BlogID    primitive.ObjectID `json:"blog_id" bson:"blog_id"`
+	Content   string             `json:"content" bson:"content" binding:"required"`
+	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
+}
+type UserAction struct {
+	UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Date   time.Time          `json:"date" bson:"date"`
+}
--- backend/ASTU-backend-group-2/domain/user.go
+++ backend/ASTU-backend-group-2/domain/user.go
@@ -1,1 +1,20 @@
 package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID         primitive.ObjectID `json:"id" bson:"_id"`
+	FirstName  string             `json:"first_name" bson:"first_name" binding:"required,min=3,max=30"`
+	LastName   string             `json:"last_name" bson:"last_name" binding:"max=30"`
+	Email      string             `json:"email" bson:"email" binding:"required,email"`
+	Bio        string             `json:"bio" bson:"bio"`
+	ProfileImg string             `json:"profile_img" bson:"profile_img"`
+	Password   string             `json:"password" bson:"password"`
+	IsOwner    bool               `json:"is_owner" bson:"is_owner"`
+	Role       string             `json:"role" bson:"role"` //may make only tobe admin or user
+	Tokens     []string           `json:"tokens" bson:"tokens"`
+}
+
+// this structure defined to take data when user registers
+type UserIn struct {
+}

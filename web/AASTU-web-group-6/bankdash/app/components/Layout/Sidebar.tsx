cbf7e3519552d9af0117730332ca15188ebe2c9c
Nahom <nahomt2419@gmail.com>
2024-08-16 10:51:29 +0300

    Update: Update sidebar

--- web/AASTU-web-group-6/bankdash/app/components/Layout/Sidebar.tsx
+++ web/AASTU-web-group-6/bankdash/app/components/Layout/Sidebar.tsx
@@ -18,109 +18,132 @@ const Sidebar = () => {
 
       {/* Menu */}
       <div className="px-8 flex flex-col gap-y-8 font-medium">
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image src="/assets/home 2.svg" alt="logo" width={25} height={25}/>
-          <h2 className="text-basex font-medium ">Dashboard</h2>
-        </div>
+        <Link href="/">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image src="/assets/home 2.svg" alt="logo" width={25} height={25} />
+            <h2 className="text-basex  ">Dashboard</h2>
+          </div>
+        </Link>
+        <Link href="/transaction">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/transaction") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/transfer 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base   ">Transactions</h2>
+          </div>
+        </Link>
+        <Link href="/">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/Accounts") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/user 3 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base   ">Accounts</h2>
+          </div>
+        </Link>
+        <Link href="/investment">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/Investments") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/economic-investment 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base   ">Investments</h2>
+          </div>
+        </Link>
+        <Link href="/CreditCards">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/credit-cards") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/credit-card 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base   ">Credit Cards</h2>
+          </div>
+        </Link>
+        <Link href="/loan">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/loan") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image src="/assets/loan 1.svg" alt="logo" width={25} height={25} />
+            <h2 className="text-base">Loans</h2>
+          </div>
+        </Link>
+        <Link href="/Services">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/Services") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/service 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base">Services</h2>
+          </div>
+        </Link>
+        <Link href="/">
+          <div
+            className={`flex items-center gap-x-6  ${
+              isActive("/privilages") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/econometrics 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base">My Privileges</h2>
+          </div>
+        </Link>
 
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/transaction") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image
-            src="/assets/transfer 1.svg"
-            alt="logo"
-            width={25}
-            height={25}
-          />
-          <h2 className="text-base   ">Transactions</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/Accounts") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image src="/assets/user 3 1.svg" alt="logo" width={25} height={25} />
-          <h2 className="text-base   ">Accounts</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/Investments") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image
-            src="/assets/economic-investment 1.svg"
-            alt="logo"
-            width={25}
-            height={25}
-          />
-          <h2 className="text-base   ">Investments</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/credit-cards") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image
-            src="/assets/credit-card 1.svg"
-            alt="logo"
-            width={25}
-            height={25}
-          />
-          <h2 className="text-base   ">Credit Cards</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/loan") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image src="/assets/loan 1.svg" alt="logo" width={25} height={25} />
-          <h2 className="text-base">Loans</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/service") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image
-            src="/assets/service 1.svg"
-            alt="logo"
-            width={25}
-            height={25}
-          />
-          <h2 className="text-base">Services</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/privilages") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image
-            src="/assets/econometrics 1.svg"
-            alt="logo"
-            width={25}
-            height={25}
-          />
-          <h2 className="text-base">My Privileges</h2>
-        </div>
-        <div
-          className={`flex items-center gap-x-6  ${
-            isActive("/setting") ? "text-[#1814F3]" : "text-[#b1b1b1]"
-          }`}
-        >
-          <Image
-            src="/assets/settings solid 1.svg"
-            alt="logo"
-            width={25}
-            height={25}
-          />
-          <h2 className="text-base   ">Setting</h2>
-        </div>
+        <Link href="/setting">
+          <div
+            className={`flex items-center gap-x-6 ${
+              isActive("/setting") ? "text-[#1814F3]" : "text-[#b1b1b1]"
+            }`}
+          >
+            <Image
+              src="/assets/settings solid 1.svg"
+              alt="logo"
+              width={25}
+              height={25}
+            />
+            <h2 className="text-base   ">Setting</h2>
+          </div>
+        </Link>
       </div>
     </div>
   );

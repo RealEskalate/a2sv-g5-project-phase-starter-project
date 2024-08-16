afb23575f48865929a087e2878a12d9ed00a5f25
Kuma Telila <kumatelila26@gmail.com>
2024-08-16 15:10:54 +0300

    Loans page done

--- web/ASTU-web-group-2/banking-dashboard/app/credit-cards/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/credit-cards/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Card from "../components/card/Card";
 import CreditCard from "../components/creditCard/CreditCard";
+import ExpenseStatisticsChart from "../components/charts/ExpenseStatisticsChart";
+import CardList from "../components/cardList/cardList";
+import CardExpenseStatisticsChart from "../components/charts/CardExpenseStatisticsChart";
+import AddNewCard from "../components/addNewCard/AddNewCard";
+import CardSetting from "../components/CardSetting/CardSetting";
 
 const CreditCardsPage = () => {
   return (
@@ -39,6 +44,34 @@ const CreditCardsPage = () => {
               />
             </div>
           </div>
+        </Card>
+      </div>
+      <div className="flex max-sm:flex-col gap-[30px]">
+        <Card
+          title="Card Expense Statistics"
+          className="flex flex-col max-w-[350px] lg:mx-auto h-auto"
+        >
+          <CardExpenseStatisticsChart />
+        </Card>
+        <Card
+          title="Card List"
+          className="flex flex-col max-w-[730px] lg:mx-auto h-auto"
+        >
+          <CardList />
+        </Card>
+      </div>
+      <div className="flex max-sm:flex-col gap-[30px]">
+        <Card
+          title="Add New Card"
+          className="flex flex-col min-w-[730px] lg:mx-auto h-auto"
+        >
+          <AddNewCard />
+        </Card>
+        <Card
+          title="Card Setting"
+          className="flex flex-col max-w-[350px] lg:mx-auto h-auto"
+        >
+          <CardSetting />
         </Card>
       </div>
     </div>

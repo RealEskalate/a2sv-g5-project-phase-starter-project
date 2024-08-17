b5d381f1adcb60df54bcf81215bbaa47b34c1f7b
Ayenewtarekegn18 <ayenewtarekegn18@gmail.com>
2024-08-17 15:16:47 +0300

    updates committed

--- web/AASTU-web-group-4/next-bank/app/(root)/page.tsx
+++ web/AASTU-web-group-4/next-bank/app/(root)/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-// import CardList from "../../components/CardList"
+import CardList from "../../components/CardList"
+import DoughnutChart from '@/components/DoughnutChart'
 
 const page = () => {
   return (
-    <div>page</div>
+
+    <div className="flex flex-col md:flex-row">
+    <DoughnutChart />
+    <CardList />
+  </div>
+  
   )
 }
 

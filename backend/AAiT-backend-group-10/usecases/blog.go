8a8219fbc2015093edd0e47015b24f02cbf82e01
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-08-17 16:26:48 +0300

    Added basic blog usecases

--- backend/AAiT-backend-group-10/usecases/blog.go
+++ backend/AAiT-backend-group-10/usecases/blog.go
@@ -1,1 +1,41 @@
 package usecases
+
+import (
+	"time"
+
+	"aait.backend.g10/domain"
+	"aait.backend.g10/usecases/interfaces"
+	"github.com/google/uuid"
+)
+
+type BlogUseCase struct {
+	BlogRepo interfaces.BlogRepoInterface
+}
+
+func (b *BlogUseCase) CreateBlog(blog *domain.Blog) (*domain.Blog, error) {
+	blog.ID = uuid.New()
+	blog.CreatedAt = time.Now()
+	blog.UpdatedAt = time.Now()
+	return blog, b.BlogRepo.Create(blog)
+}
+
+func (b *BlogUseCase) GetAllBlogs() ([]domain.Blog, error) {
+	return b.BlogRepo.FindAll()
+}
+
+func (b *BlogUseCase) GetBlogByID(id uuid.UUID) (*domain.Blog, error) {
+	return b.BlogRepo.FindByID(id)
+}
+
+func (b *BlogUseCase) UpdateBlog(blog *domain.Blog) error {
+	blog.UpdatedAt = time.Now()
+	return b.BlogRepo.Update(blog)
+}
+
+func (b *BlogUseCase) DeleteBlog(id uuid.UUID) error {
+	return b.BlogRepo.Delete(id)
+}
+
+func (b *BlogUseCase) AddView(id uuid.UUID) error {
+	return b.BlogRepo.AddView(id)
+}
--- backend/AAiT-backend-group-10/usecases/interfaces/blog_repo_interface.go
+++ backend/AAiT-backend-group-10/usecases/interfaces/blog_repo_interface.go
@@ -1,11 +1,15 @@
 package interfaces
 
-import "aait.backend.g10/domain"
+import (
+	"aait.backend.g10/domain"
+	"github.com/google/uuid"
+)
 
 type BlogRepoInterface interface {
 	Create(blog *domain.Blog) error
 	FindAll() ([]domain.Blog, error)
-	FindByID(id string) (*domain.Blog, error)
+	FindByID(id uuid.UUID) (*domain.Blog, error)
 	Update(blog *domain.Blog) error
-	Delete(id string) error
+	Delete(id uuid.UUID) error
+	AddView(id uuid.UUID) error
 }

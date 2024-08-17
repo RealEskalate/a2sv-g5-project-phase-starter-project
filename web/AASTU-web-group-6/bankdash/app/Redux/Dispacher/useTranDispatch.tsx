46ea8c9f61df1c460a4df0e204e3715c06e7cda1
Natnael Meseret <natnaelmeseret5@gmail.com>
2024-08-17 16:17:30 +0300

    Preparing transaction sliced final

--- web/AASTU-web-group-6/bankdash/app/Redux/Dispacher/useTranDispatch.tsx
+++ web/AASTU-web-group-6/bankdash/app/Redux/Dispacher/useTranDispatch.tsx
@@ -1,34 +1,34 @@
-"use client";
-import { useEffect } from "react";
-import { useAppDispatch } from "@/app/Redux/store/store";
-import { setCards, setStatus, setError } from "@/app/Redux/slices/cardSlice";
-import CardService from "@/app/Services/api/CardService";
-import React from "react";
+// "use client";
+// import { useEffect } from "react";
+// import { useAppDispatch } from "@/app/Redux/store/store";
+// import { setCards, setStatus, setError } from "@/app/Redux/slices/cardSlice";
+// import CardService from "@/app/Services/api/CardService";
+// import React from "react";
 
-const useTranDispatch = () => {
-  return <div>useTranDispatch</div>;
-};
+// const useTranDispatch = () => {
+//   return <div>useTranDispatch</div>;
+// };
 
-const useCardDispatch = (accessToken: string) => {
-  const dispatch = useAppDispatch();
+// const useCardDispatch = (accessToken: string) => {
+//   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    const fetchInitialCards = async () => {
-      try {
-        dispatch(setStatus("loading"));
-        const res: any = await CardService.getAllCards(accessToken);
-        if (res) {
-          dispatch(setCards(res));
-          dispatch(setStatus("succeeded"));
-        }
-      } catch (error) {
-        dispatch(setError("Failed to fetch cards"));
-        dispatch(setStatus("failed"));
-      }
-    };
+//   useEffect(() => {
+//     const fetchInitialCards = async () => {
+//       try {
+//         dispatch(setStatus("loading"));
+//         const res: any = await CardService.getAllCards(accessToken);
+//         if (res) {
+//           dispatch(setCards(res));
+//           dispatch(setStatus("succeeded"));
+//         }
+//       } catch (error) {
+//         dispatch(setError("Failed to fetch cards"));
+//         dispatch(setStatus("failed"));
+//       }
+//     };
 
-    fetchInitialCards();
-  }, [dispatch, accessToken]);
-};
+//     fetchInitialCards();
+//   }, [dispatch, accessToken]);
+// };
 
-export default useTranDispatch;
+// export default useTranDispatch;

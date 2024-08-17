97dbbf728449009a3ee9f1678158aef227610eb5
Natnael Meseret <natnaelmeseret5@gmail.com>
2024-08-17 16:17:30 +0300

    Preparing transaction sliced final

--- web/AASTU-web-group-6/bankdash/app/Redux/Dispacher/useCardDispatch.tsx
+++ web/AASTU-web-group-6/bankdash/app/Redux/Dispacher/useCardDispatch.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { useEffect } from "react";
 import { useAppDispatch } from "@/app/Redux/store/store";
-// import {
-//   setTran,
-//   setStatus,
-//   setError,
-// } from "@/app/Redux/slices/transactionSlice";
+import { setCards, setStatus, setError } from "@/app/Redux/slices/cardSlice";
+import CardService from "@/app/Services/api/CardService";
 
 const useCardDispatch = (accessToken: string) => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     const fetchInitialCards = async () => {
-      // for transaction
+      try {
+        dispatch(setStatus("loading"));
+        const res: any = await CardService.getAllCards(accessToken);
+        if (res) {
+          dispatch(setCards(res));
+          dispatch(setStatus("succeeded"));
+        }
+      } catch (error) {
+        dispatch(setError("Failed to fetch cards"));
+        dispatch(setStatus("failed"));
+      }
     };
 
     fetchInitialCards();

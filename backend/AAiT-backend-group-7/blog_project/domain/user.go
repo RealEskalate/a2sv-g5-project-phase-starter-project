af8493881e19ee4c314c0279da840de92b11e369
Tamiru Alemnew <tamirualemnew33@gmail.com>
2024-08-17 15:48:15 +0300

    add (naming convention) : naming conventions and method contacts

--- backend/AAiT-backend-group-7/blog_project/domain/user.go
+++ backend/AAiT-backend-group-7/blog_project/domain/user.go
@@ -1,6 +1,10 @@
 package domain
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+)
 
 type User struct {
 	ID           int    `json:"id"`
@@ -8,39 +12,38 @@ type User struct {
 	Password     string `json:"password"`
 	Email        string `json:"email"`
 	Blogs        []int  `json:"blogs"`
-	RefreshToken string `json:"refresh_token"`
 	Role         string `json:"role"`
 	Bio          string `json:"bio"`
 	Phone        string `json:"phone"`
 	ProfilePic   string `json:"profile_pic"`
 }
 
-type IUser_Repository interface {
-	GetAllUsers() ([]User, error)
-	GetUserByID(id int) (User, error)
-	CreateUser(user User) (User, error)
-	UpdateUser(id int, user User) (User, error)
-	DeleteUser(id int) error
-	SearchByUsername(username string) (User, error)
-	SearchByEmail(email string) (User, error)
-	AddBlog(user_id int, blog_id int) (User, error)
-	RefreshToken(username string, token string) (User, error)
-	CreateToken(username string, token string) (User, error)
+type IUserRepository interface {
+	GetAllUsers(ctx context.Context) ([]User, error)
+	GetUserByID(ctx context.Context, id int) (User, error)
+	CreateUser(ctx context.Context, user User) (User, error)
+	UpdateUser(ctx context.Context, id int, user User) (User, error)
+	DeleteUser(ctx context.Context, id int) error
+	SearchByUsername(ctx context.Context, username string) (User, error)
+	SearchByEmail(ctx context.Context, email string) (User, error)
+	AddBlog(ctx context.Context, userID int, blogID int) (User, error)
+	RefreshToken(ctx context.Context, username string, token string) (User, error)
+	CreateToken(ctx context.Context, username string, token string) (User, error)
 }
 
-type IUser_Usecases interface {
-	GetAllUsers() ([]User, error)
-	GetUserByID(id int) (User, error)
-	CreateUser(user User) (User, error)
-	UpdateUser(id int, user User) (User, error)
-	DeleteUser(id int) error
-	AddBlog(user_id int, blog_id int) (User, error)
-	Login(username string, password string) (User, error)
-	ForgetPassword(email string) (User, error)
-	ResetPassword(username string, password string) (User, error)
+type IUserUsecase interface {
+	GetAllUsers(ctx context.Context) ([]User, error)
+	GetUserByID(ctx context.Context, id int) (User, error)
+	CreateUser(ctx context.Context, user User) (User, error)
+	UpdateUser(ctx context.Context, id int, user User) (User, error)
+	DeleteUser(ctx context.Context, id int) error
+	AddBlog(ctx context.Context, userID int, blogID int) (User, error)
+	Login(ctx context.Context, username, password string) (User, error)
+	ForgetPassword(ctx context.Context, email string) error
+	ResetPassword(ctx context.Context, username, password string) error
 }
 
-type IUser_Controller interface {
+type IUserController interface {
 	GetAllUsers(c *gin.Context)
 	GetUserByID(c *gin.Context)
 	CreateUser(c *gin.Context)

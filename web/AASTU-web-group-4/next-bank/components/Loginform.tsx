1ec24d4f52ab709051e0f61c49ca41c496d06d85
Ayenewtarekegn18 <ayenewtarekegn18@gmail.com>
2024-08-17 16:06:53 +0300

    redux removed doing normak fetch

--- web/AASTU-web-group-4/next-bank/components/Loginform.tsx
+++ web/AASTU-web-group-4/next-bank/components/Loginform.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
 import Image from "next/image";
@@ -9,11 +9,8 @@ import {
   ExclamationCircleIcon,
   ArrowPathIcon,
 } from "@heroicons/react/24/outline";
-import { useLoginMutation } from "@/lib/redux/api/authapi";
 import { creditcardstyles, colors ,logo } from "../constants/index";
-import { useDispatch } from "react-redux";
-import { setCredentials } from "@/lib/redux/features/authSlice";
-
+import Cookies from 'js-cookie';
 
 const LoginForm: React.FC = () => {
   const {
@@ -21,52 +18,55 @@ const LoginForm: React.FC = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-
-  const dispatch = useDispatch();
-  const [login ]= useLoginMutation();
+  
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const onSubmit = async (data: any) => {
+    setIsLoading(true);
     try {
-      const adjustedData = {
-        userName: data.userName,
-        password: data.password,
-      };
-      const user = await login(adjustedData).unwrap();
+      const response = await fetch('https://bank-dashboard-6acc.onrender.com/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
 
-      // Dispatch the access token and user data to Redux store
-      dispatch(setCredentials({ accessToken: user.data.accessToken, user: user.data }));
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
 
-      console.log('Login successful:', user);
-      window.location.href= "/"
+      const responseData = await response.json();
+      Cookies.set('loginResponse', JSON.stringify(responseData));
+      console.log('Success:', responseData);
+      // You can redirect the user or perform other actions here
     } catch (error) {
-      console.error('Failed to login:', error);
+      console.error('Error:', error);
+      setErrorMessage('Login failed. Please try again.');
+    } finally {
+      setIsLoading(false);
     }
   };
 
-
-    const isLoading = false; // Set this to true if the component is loading
-
   return (
-    <div className="flex items-center justify-center   h-[50vh]">
-      <div className="flex-col items-center justify-center  h-24  w-[50vh] ">
-        <form onSubmit={handleSubmit(onSubmit)} className=" p-4 rounded-2xl ">
-          <div className="flex  flex-col justify-center items-center my-4 ">
+    <div className="flex items-center justify-center h-[50vh]">
+      <div className="flex-col items-center justify-center h-24 w-[50vh]">
+        <form onSubmit={handleSubmit(onSubmit)} className="p-4 rounded-2xl">
+          <div className="flex flex-col justify-center items-center my-4">
             <Image
               src={logo.icon}
               alt="next logo"
-              width={150} 
-              height={50} 
+              width={150}
+              height={50}
               className="h-30 w-40 m-auto my-4"
             />
-            <h1 className=" font-bold text-4xl text-gray-700 font-serif ">NEXT BANK</h1>
+            <h1 className="font-bold text-4xl text-gray-700 font-serif">NEXT BANK</h1>
           </div>
 
           <div className="my-10">
             <div>
-              <label
-                htmlFor="userName"
-                className="block font-bold mb-2 text-gray-700"
-              >
+              <label htmlFor="userName" className="block font-bold mb-2 text-gray-700">
                 UserName
               </label>
               <input
@@ -74,24 +74,20 @@ const LoginForm: React.FC = () => {
                 type="text"
                 placeholder="Username"
                 {...register("userName", { required: "Username is required" })}
-                className="w-full m-auto border-gray-200 border-2  rounded-lg shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-14 px-2.5 "
+                className="w-full m-auto border-gray-200 border-2 rounded-lg shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-14 px-2.5"
               />
               {errors.userName && (
                 <div className="flex gap-1">
                   <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                  <p className="text-red-500">
-                    {errors.userName.message as string}
-                  </p>
+                  <p className="text-red-500">{errors.userName.message as string}</p>
                 </div>
               )}
             </div>
           </div>
+
           <div className="my-10">
             <div>
-              <label
-                htmlFor="password"
-                className="block font-bold mb-2 text-gray-700"
-              >
+              <label htmlFor="password" className="block font-bold mb-2 text-gray-700">
                 Password
               </label>
               <input
@@ -99,21 +95,27 @@ const LoginForm: React.FC = () => {
                 type="password"
                 placeholder="Password"
                 {...register("password", { required: "Password is required" })}
-                className="w-full m-auto border-gray-200 border-2  rounded-lg shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-14 px-2.5"
+                className="w-full m-auto border-gray-200 border-2 rounded-lg shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-14 px-2.5"
               />
               {errors.password && (
                 <div className="flex gap-1">
                   <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                  <p className="text-red-500">
-                    {errors.password.message as string}
-                  </p>
+                  <p className="text-red-500">{errors.password.message as string}</p>
                 </div>
               )}
             </div>
           </div>
+
+          {errorMessage && (
+            <div className="text-red-500 text-center mb-4">
+              {errorMessage}
+            </div>
+          )}
+
           <button
             type="submit"
-             className={`${colors.blue}  text-white px-4 py-2 mt-4 w-full rounded-3xl text-xl`}>
+            className={`${colors.blue} text-white px-4 py-2 mt-4 w-full rounded-3xl text-xl`}
+          >
             {isLoading ? (
               <ArrowPathIcon className="h-5 w-5 animate-spin mr-2 text-white" />
             ) : (
@@ -139,6 +141,3 @@ const LoginForm: React.FC = () => {
 };
 
 export default LoginForm;
-
-
-

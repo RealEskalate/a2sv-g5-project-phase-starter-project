bb54c571e9ecd0b1386d9677f47009eb32d27b3b
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-17 05:45:14 -0700

    primitive.object id changed to id (string)

--- backend/ASTU-backend-group-5/internal/usecase/blog/interface.go
+++ backend/ASTU-backend-group-5/internal/usecase/blog/interface.go
@@ -11,30 +11,30 @@ import (
 type BlogUseCase interface {
 	// Blog operations
 	CreateBlog(ctx context.Context, blog *domain.Blog) error
-	GetBlogByID(ctx context.Context, id primitive.ObjectID) (*domain.Blog, error)
-	UpdateBlog(ctx context.Context, id primitive.ObjectID, blog *domain.Blog) error
-	DeleteBlog(ctx context.Context, id primitive.ObjectID) error
+	GetBlogByID(ctx context.Context, id string) (*domain.Blog, error)
+	UpdateBlog(ctx context.Context, id string, blog *domain.Blog) error
+	DeleteBlog(ctx context.Context, id string) error
 	GetAllBlogs(ctx context.Context) ([]*domain.Blog, error)
 	FilterBlogs(ctx context.Context, filter domain.BlogFilter) ([]*domain.Blog, error)
 	PaginateBlogs(ctx context.Context, filter domain.BlogFilter, page, pageSize int) ([]*domain.Blog, error)
 
 	// Tag operations
-	AddTagToBlog(ctx context.Context, blogID primitive.ObjectID, tag domain.BlogTag) error
-	RemoveTagFromBlog(ctx context.Context, blogID primitive.ObjectID, tagID primitive.ObjectID) error
+	AddTagToBlog(ctx context.Context, blogID string, tag domain.BlogTag) error
+	RemoveTagFromBlog(ctx context.Context, blogID string, tagID string) error
 
 	// Comment operations
 	AddComment(ctx context.Context, comment *domain.Comment) error
-	GetCommentsByBlogID(ctx context.Context, blogID primitive.ObjectID) ([]*domain.Comment, error)
+	GetCommentsByBlogID(ctx context.Context, blogID string) ([]*domain.Comment, error)
 
 	// Like operations
 	AddLike(ctx context.Context, like *domain.Like) error
-	GetLikesByBlogID(ctx context.Context, blogID primitive.ObjectID) ([]*domain.Like, error)
+	GetLikesByBlogID(ctx context.Context, blogID string) ([]*domain.Like, error)
 
 	// View operations
 	AddView(ctx context.Context, view *domain.View) error
-	GetViewsByBlogID(ctx context.Context, blogID primitive.ObjectID) ([]*domain.View, error)
+	GetViewsByBlogID(ctx context.Context, blogID string) ([]*domain.View, error)
 
 	// Real-time updates(will be implemented later)
 	WatchBlogs(ctx context.Context, pipeline []primitive.M) (<-chan domain.Blog, error)
-	WatchBlogByID(ctx context.Context, id primitive.ObjectID) (<-chan domain.Blog, error)
+	WatchBlogByID(ctx context.Context, id string) (<-chan domain.Blog, error)
 }

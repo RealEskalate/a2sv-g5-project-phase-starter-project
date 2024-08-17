6ebfcc61afff9a9f0ce9be7ecac7664cfae5ab34
dipherent1 <Bini.Blue.1996@gmail.com>
2024-08-17 16:37:18 +0300

    chore: Add Post, Comment, Tag, and LikeDislike models to Domain package

--- backend/AASTU-backend-group-7/Domain/blog.go
+++ backend/AASTU-backend-group-7/Domain/blog.go
@@ -1,2 +1,41 @@
 package Domain
 
+import "time"
+
+type Post struct {
+	ID          uint `gorm:"primaryKey"`
+	Title       string
+	Content     string `gorm:"type:text"`
+	Slug        string `gorm:"uniqueIndex"`
+	PublishedAt time.Time
+	UpdatedAt   time.Time
+	IsPublished bool
+	Views       uint
+	AuthorID    uint
+	Comments    []Comment
+	Tags        []*Tag `gorm:"many2many:post_tags;"`
+}
+
+type Comment struct {
+	ID        uint   `gorm:"primaryKey"`
+	Content   string `gorm:"type:text"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	AuthorID  uint
+	PostID    uint
+}
+
+type Tag struct {
+	ID    uint    `gorm:"primaryKey"`
+	Name  string  `gorm:"uniqueIndex"`
+	Slug  string  `gorm:"uniqueIndex"`
+	Posts []*Post `gorm:"many2many:post_tags;"`
+}
+
+type LikeDislike struct {
+    ID       uint `gorm:"primaryKey"`
+    PostID   uint
+    UserID   uint
+    IsLike   bool // true for like, false for dislike
+    // other fields
+}
--- backend/AASTU-backend-group-7/Domain/user.go
+++ backend/AASTU-backend-group-7/Domain/user.go
@@ -8,17 +8,27 @@ import (
 
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" bson:"_id,omitempty"`
+	UsernName string             `json:"username"`
 	Email     string             `json:"email" validate:"required"`
 	Password  string             `json:"password,omitempty" validate:"required"`
 	Role      string             `json:"Role"`
-	CreatedAt time.Time          `json:"created_at"`
+	ProfilePicture string
+    Bio      string
+    CreatedAt time.Time
+    UpdatedAt time.Time
+    Posts    []Post
 }
 
 // this could have been handled in a better way but i was too lazy to do it
 type OmitedUser struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" bson:"_id,omitempty"`
+	UsernName string             `json:"username"`
 	Email     string             `json:"email" validate:"required"`
 	Password  string             `json:"-"`
-	Role      string             `json:"Role" `
-	CreatedAt time.Time          `json:"created_at"`
+	Role      string             `json:"Role"`
+	ProfilePicture string
+    Bio      string
+    CreatedAt time.Time
+    UpdatedAt time.Time
+    Posts    []Post
 }

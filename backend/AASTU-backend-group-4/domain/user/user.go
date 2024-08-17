c45427c316a8236dce0e187fe3e8fd62df492466
Samrawit Gebremaryam Bahta <samrawitgebremaryam121@gmail.com>
2024-08-17 16:26:33 +0300

    Login and Authentication created.

--- backend/AASTU-backend-group-4/domain/user/user.go
+++ backend/AASTU-backend-group-4/domain/user/user.go
@@ -1,6 +1,10 @@
 package user
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
 	ID        primitive.ObjectID `json:"id" bson:"_id"`
@@ -12,16 +16,33 @@ type User struct {
 	Role      string             `json:"role" bson:"role"` // e.g., "Admin" or "User"
 }
 
+type LoginRequest struct {
+	Email    string `form:"email" `
+	Username string `form:"username" `
+	Password string `form:"password" binding:"required"`
+}
+
+type LoginResponse struct {
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
+}
+
 type UserUsecase interface {
 	// RegisterUser(user User) error
-	// LoginUser(email, password string) (string, error)
+	// LoginUser(ctx context.Context, request User) (LoginResponse, error)
+	GetByEmail(ctx context.Context, email string) (User, error)
+	GetByUsername(ctx context.Context, username string) (User, error)
 	// LogOutUser(userID primitive.ObjectID) error
 	// ForgetPassword(email string) error
 	// UpdateUser(userID primitive.ObjectID, updatedUser *User) error
 }
 
 type UserRepository interface {
+	GetByEmail(ctx context.Context, email string) (User, error)
+	GetByUsername(ctx context.Context, username string) (User, error)
 }
 
 type UserDatabase interface {
+	// GetByEmail(email string) (User, error)
+	// GetByUsername(username string) (User, error)
 }
--- backend/AASTU-backend-group-4/usecase/user_usecase/user_usecase.go
+++ backend/AASTU-backend-group-4/usecase/user_usecase/user_usecase.go
@@ -1,6 +1,7 @@
 package user_usecase
 
 import (
+	"context"
 	"time"
 
 	"blog-api/domain/user"
@@ -9,11 +10,37 @@ import (
 type userUsecase struct {
 	repo           user.UserRepository
 	contextTimeout time.Duration
+	secret         string
 }
 
-func NewProfileUsecase(userRepository user.UserRepository, timeout time.Duration) user.UserUsecase {
+func NewUserUsecase(userRepository user.UserRepository, timeout time.Duration, secret string) user.UserUsecase {
 	return &userUsecase{
 		repo:           userRepository,
 		contextTimeout: timeout,
+		secret:         secret,
 	}
+}
+
+func (uc *userUsecase) GetByEmail(ctx context.Context, email string) (user.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, uc.contextTimeout)
+	defer cancel()
+
+	u, err := uc.repo.GetByEmail(ctx, email)
+	if err != nil {
+		return user.User{}, err
+	}
+
+	return u, nil
+}
+
+func (uc *userUsecase) GetByUsername(ctx context.Context, username string) (user.User, error) {
+	ctx, cancel := context.WithTimeout(ctx, uc.contextTimeout)
+	defer cancel()
+
+	u, err := uc.repo.GetByUsername(ctx, username)
+	if err != nil {
+		return user.User{}, err
+	}
+
+	return u, nil
 }

ba6411bd484702512eabd039ccaf844fb0da643e
kibrnew <kibrnewgedamu123@gmail.com>
2024-08-17 15:37:25 +0300

    this is services

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/page.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import ServiceList from "./serviceComponenet/ServiceList";
+import BenefitComp from "./serviceComponenet/BenefitComp";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="p-4 flex  flex-col border-2 bg-gray-200 w-full h-full gap-5 lg:p-8">
+      <div className=" flex gap-3 justify-start w-[100vw] md:w-full overflow-x-scroll scrollbar-hidden md:overflow-hidden lg:justify-between">
+        <BenefitComp />
+        <BenefitComp />
+        <BenefitComp />
+      </div>
+
+      <div className="flex flex-col gap-[7px]">
+        <h1 className="font-semibold text-lg text-[#343C6A]">Bank Services List</h1>
+        <ServiceList />
+        <ServiceList />
+        <ServiceList />
+        <ServiceList />
+        <ServiceList />
+        <ServiceList />
+      </div>
+    </div>
+  );
 };
 
 export default Services;

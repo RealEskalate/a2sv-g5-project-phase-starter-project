cc14098e3e2c9f20158b49fb745d752f141b9961
Afomia Tadesse <afomiat79@gmail.com>
2024-08-16 17:26:02 -0700

    Logout

--- backend/AASTU-backend-group-9/repository/token_repository.go
+++ backend/AASTU-backend-group-9/repository/token_repository.go
@@ -1,21 +1,23 @@
 package repository
 
 import (
-    "context"
-    "blog/domain"
-    "go.mongodb.org/mongo-driver/bson"
-    "go.mongodb.org/mongo-driver/bson/primitive"
-    "go.mongodb.org/mongo-driver/mongo"
+	"blog/database"
+	"blog/domain"
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	
 )
 
 type MongoTokenRepository struct {
-    db *mongo.Collection
+    db database.Collection
+    Collection string
 }
 
-func NewMongoTokenRepository(db *mongo.Collection) domain.TokenRepository {
+func NewMongoTokenRepository(db database.Collection) domain.TokenRepository {
     return &MongoTokenRepository{db: db}
 }
-
 func (repo *MongoTokenRepository) SaveToken(ctx context.Context, token *domain.Token) error {
     _, err := repo.db.InsertOne(ctx, token)
     return err
@@ -28,12 +30,12 @@ func (repo *MongoTokenRepository) FindTokenByAccessToken(ctx context.Context, ac
 }
 
 func (repo *MongoTokenRepository) FindTokenByRefreshToken(ctx context.Context, refreshToken string) (*domain.Token, error) {
-    var token domain.Token
-    err := repo.db.FindOne(ctx, bson.M{"refresh_token": refreshToken}).Decode(&token)
-    return &token, err
+	var token domain.Token
+	err := repo.db.FindOne(ctx, bson.M{"refresh_token": refreshToken}).Decode(&token)
+	return &token, err
 }
 
 func (repo *MongoTokenRepository) DeleteToken(ctx context.Context, tokenID primitive.ObjectID) error {
-    _, err := repo.db.DeleteOne(ctx, bson.M{"_id": tokenID})
-    return err
+	_, err := repo.db.DeleteOne(ctx, bson.M{"_id": tokenID})
+	return err
 }

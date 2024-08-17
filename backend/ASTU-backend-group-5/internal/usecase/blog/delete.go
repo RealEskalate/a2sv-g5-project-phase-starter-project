f522ad49650c430165486ae9ffeaa6f1ce47d122
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-17 06:53:10 -0700

    user usecase and repo implemented

--- backend/ASTU-backend-group-5/internal/usecase/blog/delete.go
+++ backend/ASTU-backend-group-5/internal/usecase/blog/delete.go
@@ -1,1 +1,17 @@
 package blog
+
+import (
+	"context"
+	"fmt"
+	"log"
+)
+
+// DeleteBlog deletes a blog by its ID
+func (u *blogUseCase) DeleteBlog(ctx context.Context, id string) error {
+	err := u.repo.DeleteBlog(ctx, id)
+	if err != nil {
+		log.Printf("Error deleting blog with ID %s: %v", id, err)
+		return fmt.Errorf("failed to delete blog: %w", err)
+	}
+	return nil
+}

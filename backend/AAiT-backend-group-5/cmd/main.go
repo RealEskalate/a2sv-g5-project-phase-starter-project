cfcf27f18993c0aa6f00744bb6b43997a220e452
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-08-18 00:42:37 +0300

    finish-setting-db-and-user-repo.aait.backend.g5.bisrat

--- backend/AAiT-backend-group-5/cmd/main.go
+++ backend/AAiT-backend-group-5/cmd/main.go
@@ -1,1 +1,14 @@
 package main
+
+import config "github.com/aait.backend.g5.main/backend/Config"
+
+
+
+func main() {
+
+	// Initialize MongoDB connection
+	client := config.ConnectDB()
+	db := config.GetDatabase(client)
+
+		
+}

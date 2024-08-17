f40b41698162cab3997d1ebda00d3efbd818b02b
ephrem <ephremmamo555@gmail.com>
2024-08-17 16:04:47 +0300

    feat(user): update first name errors

---
+++ backend/AAiT-backend-group-13/doamin/models/user/users.go
@@ -0,0 +1,201 @@
+package usermodel
+
+import (
+	"regexp"
+
+	"github.com/google/uuid"
+	er "github.com/group13/blog/domain/errors"
+	ihash "github.com/group13/blog/domain/hash_interface"
+	"github.com/nbutton23/zxcvbn-go"
+)
+
+const (
+	minPasswordStrengthScore = 3
+	usernamePattern          = `^[a-zA-Z0-9_]+$`
+	emailPattern             = `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	minUsernameLength        = 3
+	maxUsernameLength        = 20
+	minFirstNameLength       = 3
+	maxFirstNameLength       = 250
+)
+
+var (
+	usernameRegex = regexp.MustCompile(usernamePattern)
+	emailRegex    = regexp.MustCompile(emailPattern)
+)
+
+// User represents the aggregate user with private fields.
+type User struct {
+	id           uuid.UUID
+	firstName    string
+	lastName     string
+	username     string
+	email        string
+	passwordHash string
+	isAdmin      bool
+}
+
+// Config holds parameters for creating a new User.
+type Config struct {
+	FirstName      string
+	LastName       string
+	Username       string
+	Email          string
+	PlainPassword  string
+	IsAdmin        bool
+	PasswordHasher ihash.Service
+}
+
+// New creates a new User with the provided configuration.
+func New(config Config) (*User, error) {
+	if err := validateUsername(config.Username); err != nil {
+		return nil, err
+	}
+
+	if err := validatePassword(config.PlainPassword); err != nil {
+		return nil, err
+	}
+
+	if err := validateEmail(config.Email); err != nil {
+		return nil, err
+	}
+
+	if err := validateFirstName(config.FirstName); err != nil {
+		return nil, err
+	}
+
+	passwordHash, err := config.PasswordHasher.Hash(config.PlainPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	//returns user with specified fields
+	return &User{
+		id:           uuid.New(),
+		username:     config.Username,
+		passwordHash: passwordHash,
+		isAdmin:      config.IsAdmin,
+		email:        config.Email,
+		firstName:    config.FirstName,
+		lastName:     config.LastName,
+	}, nil
+}
+
+// validateUsername validates the username.
+func validateUsername(username string) error {
+	if len(username) < minUsernameLength {
+		return er.UsernameTooShort
+	}
+	if len(username) > maxUsernameLength {
+		return er.UsernameTooLong
+	}
+	if !usernameRegex.MatchString(username) {
+		return er.UsernameInvalidFormat
+	}
+	return nil
+}
+
+// validatePassword checks the strength of the password.
+func validatePassword(password string) error {
+	result := zxcvbn.PasswordStrength(password, nil)
+	if result.Score < minPasswordStrengthScore {
+		return er.WeakPassword
+	}
+	return nil
+}
+
+func validateEmail(email string) error {
+	if !emailRegex.MatchString(email) {
+		return er.EmailInvalidFormat
+	}
+	return nil
+}
+
+func validateFirstName(firstName string) error {
+	if len(firstName) < minFirstNameLength {
+		return er.UsernameTooShort
+	}
+	if len(firstName) > maxFirstNameLength {
+		return er.UsernameTooLong
+	}
+	return nil
+}
+
+// ID returns the user's ID.
+func (u *User) ID() uuid.UUID {
+	return u.id
+}
+
+// Username returns the user's username.
+func (u *User) Username() string {
+	return u.username
+}
+
+// FirstName returns the user's firstname.
+func (u *User) FirstName() string {
+	return u.firstName
+}
+
+// LastName returns user's lastname.
+func (u *User) LastName() string {
+	return u.lastName
+}
+
+// Email returns user's email.
+func (u *User) Email() string {
+	return u.email
+}
+
+// PasswordHash returns the user's password hash.
+func (u *User) PasswordHash() string {
+	return u.passwordHash
+}
+
+// IsAdmin returns whether the user is an admin.
+func (u *User) IsAdmin() bool {
+	return u.isAdmin
+}
+
+// UpdateUsername updates the user's username after validation.
+func (u *User) UpdateUsername(newUsername string) error {
+	if err := validateUsername(newUsername); err != nil {
+		return err
+	}
+	u.username = newUsername
+	return nil
+}
+
+// UpdateFirstName updates the user's firstname after validation.
+func (u *User) UpdateFirstName(newFirstName string) error {
+	if err := validateFirstName(newFirstName); err != nil {
+		return err
+	}
+	u.firstName = newFirstName
+	return nil
+}
+
+// UpdateLastNname updates the user's lastname.
+func (u *User) UpdateLastName(newLastName string) error {
+	u.firstName = newLastName
+	return nil
+}
+
+// UpdatePassword updates the user's password after validation.
+func (u *User) UpdatePassword(newPassword string, passwordHasher ihash.Service) error {
+	if err := validatePassword(newPassword); err != nil {
+		return err
+	}
+
+	hashedPassword, err := passwordHasher.Hash(newPassword)
+	if err != nil {
+		return err
+	}
+
+	u.passwordHash = hashedPassword
+	return nil
+}
+
+// UpdateAdminStatus updates the user's admin status.
+func (u *User) UpdateAdminStatus(isAdmin bool) {
+	u.isAdmin = isAdmin
+}

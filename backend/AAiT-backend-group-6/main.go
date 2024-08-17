99fc45cb534c307e87d894277642f33b102e0b0f
emran <emranhi001@gmail.com>
2024-08-17 18:48:30 +0300

    folder structure change

--- backend/AAiT-backend-group-6/main.go
+++ backend/AAiT-backend-group-6/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"AAiT-backend-group-6/bootstrap"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := bootstrap.App()
+
+	env := app.Env
+
+	db := app.Mongo.Database(env.DBName)
+	defer app.CloseDBConnection()
+
+	timeout := time.Duration(env.ContextTimeout) * time.Second
+
+	gin := gin.Default()
+
+	route.Setup(env, timeout, db, gin)
+
+	gin.Run(env.ServerAddress)
+}

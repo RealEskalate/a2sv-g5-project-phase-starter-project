cd63e9bd485d5d097425a71c774933a120cb3bdf
SamrawitDawit <samrikdawit@gmail.com>
2024-08-18 14:17:31 +0300

    Added Comment feature

--- backend/AAiT-backend-group-10/domain/comment.go
+++ backend/AAiT-backend-group-10/domain/comment.go
@@ -1,1 +1,10 @@
 package domain
+
+import "github.com/google/uuid"
+
+type Comment struct {
+	ID      uuid.UUID `json:"id,omitempty" bson:"id,omitempty"`
+	BlogID  uuid.UUID `json:"blog_id" bson:"blog_id"`
+	UserID  uuid.UUID `json:"user_id" bson:"user_id"`
+	Comment string    `json:"comment" bson:"comment" validate:"required"`
+}

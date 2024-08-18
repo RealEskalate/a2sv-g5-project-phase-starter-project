2fce68e5cafa321655bbed37cb26d7042d3a492a
SamrawitDawit <samrikdawit@gmail.com>
2024-08-18 15:01:58 +0300

    Placed constants in env file

--- backend/AAiT-backend-group-10/delivery/main.go
+++ backend/AAiT-backend-group-10/delivery/main.go
@@ -3,20 +3,26 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"aait.backend.g10/delivery/router"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	clientOption := options.Client().ApplyURI("mongodb://localhost:27017")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	clientOption := options.Client().ApplyURI(os.Getenv("DB_URI"))
 	client, err := mongo.Connect(context.TODO(), clientOption)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	db := client.Database("blog_project")
+	db := client.Database("DB_NAME")
 	router.NewRouter(*db)
 
 }
--- backend/AAiT-backend-group-10/go.mod
+++ backend/AAiT-backend-group-10/go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect

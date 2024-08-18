751a34961ef8b039094b69fbdaa00e3fc9f85094
Chala <88979923+chalaa@users.noreply.github.com>
2024-08-18 06:11:46 +0300

    Add activeLoan Slice (#589)

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/lib/redux/slices/transactionSlice.ts
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/lib/redux/slices/transactionSlice.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { session } from '@/session';
 import { get } from 'http';
 

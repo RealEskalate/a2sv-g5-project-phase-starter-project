9b0897f6e42906529a9037fefa173f05d28a11f2
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-08-18 03:53:49 +0300

    Add user,blog and comment struct for domain and blog_dtos

--- backend/AAiT-backend-group-12/domain/domain.go
+++ backend/AAiT-backend-group-12/domain/domain.go
@@ -1,1 +1,40 @@
 package domain
+
+import (
+	"time"
+)
+
+type Blog struct {
+	ID         string
+	Title      string
+	Content    string
+	UserID     string
+	Tags       []string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	ViewCount  uint
+	LikedBy    []User
+	DislikedBy []User
+	Comments   []Comment
+}
+
+// User represents a user entity in the domain.
+type User struct {
+	ID        string
+	UserName  string
+	Email     string
+	Password  string
+	Role      string
+	CreatedAt time.Time
+}
+
+// Comment represents a comment entity in the domain.
+type Comment struct {
+	ID        string
+	Content   string
+	UserID    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	ViewCount uint
+	Comments  []Comment
+}
--- backend/AAiT-backend-group-12/domain/dtos/blog_dtos.go
+++ backend/AAiT-backend-group-12/domain/dtos/blog_dtos.go
@@ -1,1 +1,43 @@
 package dtos
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// BlogDTO represents the data structure stored in MongoDB for a blog post.
+type BlogDTO struct {
+	ID         primitive.ObjectID   `bson:"_id,omitempty"`
+	Title      string               `bson:"title"`
+	Content    string               `bson:"content"`
+	UserID     primitive.ObjectID   `bson:"user_id"`
+	Tags       []string             `bson:"tags"`
+	CreatedAt  time.Time            `bson:"created_at"`
+	UpdatedAt  time.Time            `bson:"updated_at"`
+	ViewCount  uint                 `bson:"view_count"`
+	LikedBy    []primitive.ObjectID `bson:"liked_by"`
+	DislikedBy []primitive.ObjectID `bson:"disliked_by"`
+	Comments   []CommentDTO         `bson:"comments"`
+}
+
+
+type UserDTO struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	UserName  string             `bson:"username"`
+	Email     string             `bson:"email"`
+	Password  string             `bson:"password"`
+	Role      string             `bson:"role"`
+	CreatedAt time.Time          `bson:"created_at"`
+}
+
+
+type CommentDTO struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty"`
+	Content   string             `bson:"content"`
+	UserID    primitive.ObjectID `bson:"user_id"`
+	CreatedAt time.Time          `bson:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at"`
+	ViewCount uint               `bson:"view_count"`
+	Comments  []CommentDTO       `bson:"comments"`
+}

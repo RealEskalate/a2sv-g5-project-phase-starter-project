cf24e903c793155f03f37f0dde0c78b5b0d54e88
SamrawitDawit <samrikdawit@gmail.com>
2024-08-18 04:31:05 +0300

    Added like feature

--- backend/AAiT-backend-group-10/domain/like.go
+++ backend/AAiT-backend-group-10/domain/like.go
@@ -1,1 +1,13 @@
 package domain
+
+import "github.com/google/uuid"
+
+const (
+	CollectionLike = "likes"
+)
+
+type Like struct {
+	IsLike bool      `json:"is_like" bson:"is_like"`
+	UserID uuid.UUID `json:"user_id" bson:"user_id"`
+	BlogID uuid.UUID `json:"blog_id" bson:"blog_id"`
+}

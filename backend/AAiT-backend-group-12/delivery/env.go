b9bc7c436dd3f46e3d809f35d39a5dbd77b8b6fc
Nathan M.S <natms24@gmail.com>
2024-08-18 04:11:56 +0300

    Add root admin credentials as env variables

--- backend/AAiT-backend-group-12/delivery/env.go
+++ backend/AAiT-backend-group-12/delivery/env.go
@@ -17,8 +17,11 @@ var ENV struct {
 	REFRESH_TOKEN_LIFESPAN string
 	PORT                   int
 	ROUTE_PREFIX           string
+	ROOT_USERNAME          string
+	ROOT_PASSWORD          string
 }
 
+/* Loads environment variables from .env file and verifies that all required variables are set */
 func LoadEnvironmentVariables() error {
 	err := godotenv.Load()
 	if err != nil {
@@ -32,6 +35,8 @@ func LoadEnvironmentVariables() error {
 	ENV.ACCESS_TOKEN_LIFESPAN = os.Getenv("ACCESS_TOKEN_LIFESPAN")
 	ENV.ROUTE_PREFIX = os.Getenv("ROUTE_PREFIX")
 	ENV.REFRESH_TOKEN_LIFESPAN = os.Getenv("REFRESH_TOKEN_LIFESPAN")
+	ENV.ROOT_USERNAME = os.Getenv("ROOT_USERNAME")
+	ENV.ROOT_PASSWORD = os.Getenv("ROOT_PASSWORD")
 	port, err := strconv.ParseInt(os.Getenv("PORT"), 10, 64)
 	if err != nil {
 		return fmt.Errorf("error parsing PORT number: %v", err.Error())
@@ -62,9 +67,25 @@ func LoadEnvironmentVariables() error {
 		return fmt.Errorf("error: couldn't load environment variable 'JWT_SECRET_TOKEN'")
 	}
 
+	if ENV.ROOT_USERNAME == "" {
+		return fmt.Errorf("error: couldn't load environment variable 'ROOT_USERNAME'")
+	}
+
+	if ENV.ROOT_PASSWORD == "" {
+		return fmt.Errorf("error: couldn't load environment variable 'ROOT_PASSWORD'")
+	}
+
 	if ENV.PORT == 0 {
 		return fmt.Errorf("error: couldn't load environment variable 'PORT'")
 	}
 
 	return nil
+}
+
+/* Removes the root credentials from the environment */
+func UnsetRootCredentials() {
+	ENV.ROOT_USERNAME = ""
+	ENV.ROOT_PASSWORD = ""
+	os.Unsetenv("ROOT_USERNAME")
+	os.Unsetenv("ROOT_PASSWORD")
 }

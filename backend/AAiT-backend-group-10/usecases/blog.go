2f5123ed0dc60906f4135aacab23fca6bf6371b5
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-08-18 14:18:48 +0300

    Blog controllers and router with basic functionality

--- backend/AAiT-backend-group-10/usecases/blog.go
+++ backend/AAiT-backend-group-10/usecases/blog.go
@@ -8,34 +8,47 @@ import (
 	"github.com/google/uuid"
 )
 
+type IBlogUseCase interface {
+	CreateBlog(blog *domain.Blog) (*domain.Blog, error)
+	GetAllBlogs() ([]domain.Blog, error)
+	GetBlogByID(id uuid.UUID) (*domain.Blog, error)
+	UpdateBlog(blog *domain.Blog) error
+	DeleteBlog(id uuid.UUID) error
+	AddView(id uuid.UUID) error
+}
+
 type BlogUseCase struct {
-	BlogRepo interfaces.BlogRepoInterface
+	blogRepo interfaces.IBlogRepository
+}
+
+func NewBlogUseCase(repo interfaces.IBlogRepository) *BlogUseCase {
+	return &BlogUseCase{blogRepo: repo}
 }
 
 func (b *BlogUseCase) CreateBlog(blog *domain.Blog) (*domain.Blog, error) {
 	blog.ID = uuid.New()
-	blog.CreatedAt = time.Now()
-	blog.UpdatedAt = time.Now()
-	return blog, b.BlogRepo.Create(blog)
+	blog.CreatedAt = time.Now().UTC()
+	blog.UpdatedAt = time.Now().UTC()
+	return blog, b.blogRepo.Create(blog)
 }
 
 func (b *BlogUseCase) GetAllBlogs() ([]domain.Blog, error) {
-	return b.BlogRepo.FindAll()
+	return b.blogRepo.FindAll()
 }
 
 func (b *BlogUseCase) GetBlogByID(id uuid.UUID) (*domain.Blog, error) {
-	return b.BlogRepo.FindByID(id)
+	return b.blogRepo.FindByID(id)
 }
 
 func (b *BlogUseCase) UpdateBlog(blog *domain.Blog) error {
-	blog.UpdatedAt = time.Now()
-	return b.BlogRepo.Update(blog)
+	blog.UpdatedAt = time.Now().UTC()
+	return b.blogRepo.Update(blog)
 }
 
 func (b *BlogUseCase) DeleteBlog(id uuid.UUID) error {
-	return b.BlogRepo.Delete(id)
+	return b.blogRepo.Delete(id)
 }
 
 func (b *BlogUseCase) AddView(id uuid.UUID) error {
-	return b.BlogRepo.AddView(id)
+	return b.blogRepo.AddView(id)
 }

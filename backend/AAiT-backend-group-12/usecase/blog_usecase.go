25122eef418b0fee11d98285906841fd59e42310
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-08-18 07:53:48 +0300

    finish the blogUseCase

--- backend/AAiT-backend-group-12/usecase/blog_usecase.go
+++ backend/AAiT-backend-group-12/usecase/blog_usecase.go
@@ -1,2 +1,61 @@
 package usecase
 
+import (
+	"blog_api/domain"
+	"context"
+	"time"
+)
+
+type BlogUseCase struct {
+	blogRepo       domain.BlogRepositoryInterface
+	contextTimeOut time.Duration
+}
+
+
+func NewBlogUseCase(repo domain.BlogRepositoryInterface, t time.Duration) *BlogUseCase {
+	return &BlogUseCase{
+		blogRepo:       repo,
+		contextTimeOut: t,
+	}
+}
+
+// CreateBlogPost implements domain.BlogUseCaseInterface.
+func (b *BlogUseCase) CreateBlogPost(ctx context.Context, blog *domain.Blog) error {
+	ctx, cancel := context.WithTimeout(ctx, b.contextTimeOut)
+	defer cancel()
+
+	err := b.blogRepo.InsertBlogPost(ctx, blog)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// DeleteBlogPost implements domain.BlogUseCaseInterface.
+func (b *BlogUseCase) DeleteBlogPost(ctx context.Context, blogId string) error {
+	ctx, cancel := context.WithTimeout(ctx, b.contextTimeOut)
+	defer cancel()
+
+	err := b.blogRepo.DeleteBlogPost(ctx, blogId)
+	if err != nil{
+		return err
+	}
+	return nil
+}
+
+// EditBlogPost implements domain.BlogUseCaseInterface.
+func (b *BlogUseCase) EditBlogPost(ctx context.Context, blogId string, blog *domain.Blog) error {
+	ctx, cancel := context.WithTimeout(ctx, b.contextTimeOut)
+	defer cancel()
+
+	err := b.blogRepo.UpdateBlogPost(ctx, blogId, blog)
+	if err != nil{
+		return err
+	}
+	return nil
+}
+
+// GetBlogPost implements domain.BlogUseCaseInterface.
+func (b *BlogUseCase) GetBlogPost(ctx context.Context, blogId string) (*domain.Blog, error) {
+	panic("implemented by robel")
+}

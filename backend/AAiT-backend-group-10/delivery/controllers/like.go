9aeea14a287f43c8bc4d16c4f6e2c2e28692a66c
SamrawitDawit <samrikdawit@gmail.com>
2024-08-18 04:20:07 +0300

    Added like feature

--- backend/AAiT-backend-group-10/delivery/controllers/like.go
+++ backend/AAiT-backend-group-10/delivery/controllers/like.go
@@ -1,1 +1,39 @@
 package controllers
+
+import (
+	"aait.backend.g10/domain"
+	"aait.backend.g10/usecases"
+	"github.com/gin-gonic/gin"
+)
+
+type LikeCOntroller struct {
+	LikeUseCase usecases.LikeUsecaseInterface
+}
+
+func (cont *LikeCOntroller) LikeBlog(c *gin.Context) {
+	var like domain.Like
+	if err := c.BindJSON(&like); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	err := cont.LikeUseCase.LikeBlog(like)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"message": "Like added successfully"})
+}
+
+func (cont *LikeCOntroller) DeleteLike(c *gin.Context) {
+	var like domain.Like
+	if err := c.BindJSON(&like); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	err := cont.LikeUseCase.DeleteLike(like)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"message": "Like deleted successfully"})
+}
--- backend/AAiT-backend-group-10/delivery/main.go
+++ backend/AAiT-backend-group-10/delivery/main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"aait.backend.g10/delivery/router"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	clientOption := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, err := mongo.Connect(context.TODO(), clientOption)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	db := client.Database("blog_project")
+	router.NewRouter(*db)
+
+}

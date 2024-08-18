b9380624dd903fb82d1c6f197855450d4c17a942
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-08-18 18:15:04 +0300

    finish the blog router

--- backend/AAiT-backend-group-12/delivery/router/blog_router.go
+++ backend/AAiT-backend-group-12/delivery/router/blog_router.go
@@ -1,10 +1,23 @@
 package router
 
 import (
+	"blog_api/repository"
+	"blog_api/delivery/controllers"
+	"blog_api/usecase"
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func NewBlogRouter(collection *mongo.Collection, taskGroup *gin.RouterGroup) {
+	br := repository.NewBlogRepository(collection)
+	bu := usecase.NewBlogUseCase(br, time.Second * 100)
+
+	bc := controllers.NewBlogController(bu)
+
+	taskGroup.POST("/blogs", bc.CreateBlogHandler)
+	taskGroup.PUT("/blogs/:id", bc.UpdateBlogHandler)
+	taskGroup.DELETE("/blogs/:id", bc.DeleteBlogHandler)
 
 }

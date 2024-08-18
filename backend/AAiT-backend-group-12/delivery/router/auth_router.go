78e9146564b5cd299b565d08ef784de355a46622
Nathan M.S <natms24@gmail.com>
2024-08-18 04:02:51 +0300

    Add router files

--- backend/AAiT-backend-group-12/delivery/router/auth_router.go
+++ backend/AAiT-backend-group-12/delivery/router/auth_router.go
@@ -1,1 +1,10 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewAuthRouter(collection *mongo.Collection, authGroup *gin.RouterGroup) {
+
+}

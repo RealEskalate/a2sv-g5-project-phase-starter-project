7de779a0471fb7bb3070556521013f37c88de5c5
Nathan M.S <natms24@gmail.com>
2024-08-18 03:52:00 +0300

    Add infrastructure functions

--- backend/AAiT-backend-group-12/domain/domain.go
+++ backend/AAiT-backend-group-12/domain/domain.go
@@ -1,1 +1,13 @@
 package domain
+
+import "github.com/gin-gonic/gin"
+
+/*
+Defines the names of the collections in the DB
+*/
+const (
+	CollectionUsers = "users"
+	CollectionBlogs = "blogs"
+)
+
+type Response gin.H

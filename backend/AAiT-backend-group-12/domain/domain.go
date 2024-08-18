dd3165774826afb831f69453ee96bbde1fc02fae
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-08-18 07:13:31 +0300

    finish the blog repository

--- backend/AAiT-backend-group-12/domain/domain.go
+++ backend/AAiT-backend-group-12/domain/domain.go
@@ -1,40 +1,55 @@
 package domain
 
 import (
+	"context"
 	"time"
 )
 
 type Blog struct {
-	ID         string
-	Title      string
-	Content    string
-	UserID     string
-	Tags       []string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-	ViewCount  uint
-	LikedBy    []User
-	DislikedBy []User
-	Comments   []Comment
+	ID         string    `json:"id"`
+	Title      string    `json:"title"`
+	Content    string    `json:"content"`
+	UserID     string    `json:"user_id"`
+	Tags       []string  `json:"tags"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	ViewCount  uint      `json:"view_count"`
+	LikedBy    []User    `json:"liked_by"`
+	DislikedBy []User    `json:"disliked_by"`
+	Comments   []Comment `json:"comment"`
 }
 
 // User represents a user entity in the domain.
 type User struct {
-	ID        string
-	UserName  string
-	Email     string
-	Password  string
-	Role      string
-	CreatedAt time.Time
+	ID        string    `json:"id"`
+	UserName  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // Comment represents a comment entity in the domain.
 type Comment struct {
-	ID        string
-	Content   string
-	UserID    string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	ViewCount uint
-	Comments  []Comment
+	ID        string    `json:"id"`
+	Content   string    `json:"content"`
+	UserID    string    `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	ViewCount uint      `json:"view_count"`
+	Comments  []Comment `json:"comment"`
+}
+
+type BlogRepositoryInterface interface {
+	FindBlogPostByID(ctx context.Context, id string) (*Blog, error)
+	InsertBlogPost(ctx context.Context, blog *Blog) error
+	UpdateBlogPost(ctx context.Context, id string, blog *Blog) error
+	DeleteBlogPost(ctx context.Context, id string) error
+}
+
+type BlogUseCaseInterface interface {
+	GetBlogPost(ctx context.Context, id string) (*Blog, error)
+	CreateBlogPost(ctx context.Context, blog *Blog) error
+	EditBlogPost(ctx context.Context, id string, blog *Blog) error
+	DeleteBlogPost(ctx context.Context, id string)
 }

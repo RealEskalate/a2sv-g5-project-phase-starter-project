6f7a42b45de1819d09b7ef85745474de48d0f394
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-08-18 14:18:16 +0300

    Blog controllers and router with basic functionality

--- backend/AAiT-backend-group-10/delivery/router/router.go
+++ backend/AAiT-backend-group-10/delivery/router/router.go
@@ -1,1 +1,25 @@
 package router
+
+import (
+	"aait.backend.g10/delivery/controllers"
+	"aait.backend.g10/repositories"
+	"aait.backend.g10/usecases"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewRouter(db *mongo.Database) {
+	router := gin.Default()
+	blogRepo := repositories.NewBlogRepository(db, "blogs")
+	blogUseCase := usecases.NewBlogUseCase(blogRepo)
+	blogController := controllers.NewBlogController(blogUseCase)
+
+	router.POST("/blogs", blogController.CreateBlog)
+	router.GET("/blogs", blogController.GetAllBlogs)
+	router.GET("/blogs/:id", blogController.GetBlogByID)
+	router.PUT("/blogs/:id", blogController.UpdateBlog)
+	router.DELETE("/blogs/:id", blogController.DeleteBlog)
+	router.PATCH("/blogs/:id/view", blogController.AddView)
+
+	router.Run(":8080")
+}

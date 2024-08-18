37168439b35efbfc54054382fe8ae54c37cd186c
SamrawitDawit <samrikdawit@gmail.com>
2024-08-18 04:20:07 +0300

    Added like feature

--- backend/AAiT-backend-group-10/delivery/router/router.go
+++ backend/AAiT-backend-group-10/delivery/router/router.go
@@ -1,1 +1,23 @@
 package router
+
+import (
+	"aait.backend.g10/delivery/controllers"
+	"aait.backend.g10/domain"
+	"aait.backend.g10/repositories"
+	"aait.backend.g10/usecases"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewRouter(db mongo.Database) {
+	router := gin.Default()
+	// router.Use(cors.Default())
+	// router.Use(middleware.AuthMiddleware())
+	likeRepo := repositories.NewLikeRepository(db, domain.CollectionLike)
+	likeController := controllers.LikeCOntroller{
+		LikeUseCase: usecases.NewLikeUseCase(likeRepo),
+	}
+	router.PUT("/like", likeController.LikeBlog)
+	router.DELETE("/like", likeController.DeleteLike)
+	router.Run(":8080")
+}

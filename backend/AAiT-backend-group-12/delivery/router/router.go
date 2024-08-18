205aa8020b1b2313798dbfde48d83c7c5b7fb0e0
Nathan M.S <natms24@gmail.com>
2024-08-18 04:02:51 +0300

    Add router files

--- backend/AAiT-backend-group-12/delivery/router/router.go
+++ backend/AAiT-backend-group-12/delivery/router/router.go
@@ -1,5 +1,23 @@
 package router
 
-func SetupRouter() {
+import (
+	"blog_api/domain"
+	"fmt"
 
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetupRouter(port int, routePrefix string, db *mongo.Database) {
+	router := gin.Default()
+
+	// auth
+	authRouter := router.Group("/auth")
+	NewAuthRouter(db.Collection(domain.CollectionUsers), authRouter)
+
+	// blog
+	blogAuthor := router.Group("/auth")
+	NewBlogRouter(db.Collection(domain.CollectionBlogs), blogAuthor)
+
+	router.Run(fmt.Sprintf(":%v", port))
 }

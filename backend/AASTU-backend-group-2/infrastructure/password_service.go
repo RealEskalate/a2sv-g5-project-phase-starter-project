1c2d5e75eeb7d5505827e55acc0c49b3063800f7
DanielAbabu <Daniel0Ababu@gmail.com>
2024-08-18 22:19:56 +0300

    admin and forget password

--- backend/AASTU-backend-group-2/infrastructure/password_service.go
+++ backend/AASTU-backend-group-2/infrastructure/password_service.go
@@ -1,17 +1,48 @@
 package infrastructure
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
 
-//compares the inputted password from the existing hash
+	"github.com/dchest/passwordreset"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// compares the inputted password from the existing hash
 func PasswordComparator(hash string, password string) bool {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) != nil
 }
 
-//hashes the password with a SHA-256 encryption
+// hashes the password with a SHA-256 encryption
 func PasswordHasher(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
 	return string(hash), nil
+}
+
+// Handels forgot password
+func ForgotPasswordHandler(email string) error {
+	secretKey := DotEnvLoader("Reset_Password")
+
+	token := passwordreset.NewToken(email, time.Hour*1, []byte(secretKey), []byte(secretKey))
+	if err := sendResetEmail(email, token); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func VerifyToken(token string) (string, error) {
+	secretKey := DotEnvLoader("Reset_Password")
+	tokenDataFunc := func(email string) ([]byte, error) {
+		return []byte(email), nil
+	}
+
+	email, err := passwordreset.VerifyToken(token, tokenDataFunc, []byte(secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return email, nil
 }
--- backend/AASTU-backend-group-2/repositories/user_repository.go
+++ backend/AASTU-backend-group-2/repositories/user_repository.go
@@ -2,7 +2,10 @@ package repositories
 
 import (
 	"blog_g2/domain"
+	"blog_g2/infrastructure"
+	"context"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -30,6 +33,33 @@ func (urepo *UserRepository) LoginUser(user domain.User) (string, error) {
 }
 
 func (urepo *UserRepository) ForgotPassword(email string) error {
+	var user domain.User
+
+	query := bson.M{"email": email}
+	if err := urepo.collection.FindOne(context.TODO(), query).Decode(&user); err != nil {
+		return err
+	}
+
+	return infrastructure.ForgotPasswordHandler(email)
+}
+
+func (urepo *UserRepository) ResetPassword(token string, newPassword string) error {
+	email, err := infrastructure.VerifyToken(token)
+	if err != nil {
+		return err
+	}
+	hashedPassword, err := infrastructure.PasswordHasher(newPassword)
+	if err != nil {
+		return err
+	}
+	filter := bson.M{"email": email}
+	update := bson.M{"$set": bson.M{"password": string(hashedPassword)}}
+
+	_, err = urepo.collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -37,6 +67,14 @@ func (urepo *UserRepository) LogoutUser() error {
 	return nil
 }
 
-func (urepo *UserRepository) PromoteDemoteUser(userid string) error {
+func (urepo *UserRepository) PromoteDemoteUser(userid string, isAdmin bool) error {
+
+	filter := bson.M{"_id": userid}
+	update := bson.M{"$set": bson.M{"isadmin": true}}
+
+	_, err := urepo.collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		return err
+	}
 	return nil
 }

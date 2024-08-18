d0400267b9e2111a4c7c709c63b1c047b67d72d1
Yonatan Tizazu <yonatantizazu12@gmail.com>
2024-08-18 21:46:41 +0300

    modified the comment for blog controller

--- backend/AAiT-backend-group-12/delivery/controllers/blog_controller.go
+++ backend/AAiT-backend-group-12/delivery/controllers/blog_controller.go
@@ -19,13 +19,8 @@ func NewBlogController(bu domain.BlogUseCaseInterface) *BlogController{
 	}
 }
 
+
 // CreateBlogHandler handles the HTTP request for creating a new blog post.
-// It binds the JSON data from the request body to a domain.Blog struct,
-// validates the struct using the validate library, and then calls the
-// CreateBlogPost method of the blogUseCase to create the blog post.
-// If any errors occur during the process, it returns a JSON response
-// with the corresponding error message. If the blog post is created
-// successfully, it returns a JSON response with a success message.
 func (bc *BlogController) CreateBlogHandler(c *gin.Context){
 	var blog domain.Blog
 	if err := c.ShouldBindJSON(&blog); err != nil{
@@ -46,12 +41,8 @@ func (bc *BlogController) CreateBlogHandler(c *gin.Context){
 	c.JSON(http.StatusOK, domain.Response{"message": "blog created successfully"})
 }
 
+
 // UpdateBlogHandler handles the HTTP request to update a blog post.
-// It expects the blog ID to be provided as a parameter in the URL.
-// The updated blog data should be sent in the request body as JSON.
-// If the request body is not valid JSON or if there is an error while updating the blog post,
-// it will return a JSON response with an error message.
-// If the update is successful, it will return a JSON response with a success message.
 func (bc *BlogController) UpdateBlogHandler (c *gin.Context){
 	blogId := c.Param("id") 
 	var blog domain.Blog
@@ -69,9 +60,6 @@ func (bc *BlogController) UpdateBlogHandler (c *gin.Context){
 
 
 // DeleteBlogHandler handles the HTTP DELETE request to delete a blog post.
-// It takes the blog ID as a parameter from the URL path and calls the DeleteBlogPost method of the BlogUseCase to delete the blog post.
-// If the deletion is successful, it responds with a JSON message indicating success.
-// If there is an error during the deletion process, it responds with a JSON error message.
 func (bc *BlogController) DeleteBlogHandler (c *gin.Context){
 	blogId := c.Param("id") 
 
@@ -100,6 +88,7 @@ func (bc *BlogController) GetBlogHandler(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"total": total, "blogs": blogs})
 }
+
 
 // GetBlogByIDHandler handles the HTTP GET request to retrieve a single blog post by its ID.
 func (bc *BlogController) GetBlogByIDHandler(c *gin.Context) {

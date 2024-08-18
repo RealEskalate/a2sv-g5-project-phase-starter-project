9357e7cff75f823b669fa39603b1a98364989b99
Amantsehay <amanueltsehay11@gmail.com>
2024-08-18 08:37:41 +0300

    repository initilized

--- backend/AAiT-backend-group-3/internal/repositories/implementation/blog_repository.go
+++ backend/AAiT-backend-group-3/internal/repositories/implementation/blog_repository.go
@@ -1,1 +1,94 @@
-package repositories
+package  repositories
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"AAIT-backend-group-3/internal/domain/models"
+	"AAIT-backend-group-3/internal/repository_interfaces"
+)
+
+type MongoBlogRepository struct {
+	collection *mongo.Collection
+}
+
+func NewMongoBlogRepository(db *mongo.Database, collectionName string) *MongoBlogRepository {
+	return &MongoBlogRepository{
+		collection: db.Collection(collectionName),
+	}
+}
+
+func (r *MongoBlogRepository) CreateBlog(ctx context.Context, blog *models.Blog) error {
+	_, err := r.collection.InsertOne(ctx, blog)
+	return err
+}
+
+func (r *MongoBlogRepository) GetBlogByID(ctx context.Context, blogID primitive.ObjectID) (*models.Blog, error) {
+	var blog models.Blog
+	err := r.collection.FindOne(ctx, bson.M{"_id": blogID}).Decode(&blog)
+	if err != nil {
+		return nil, err
+	}
+	return &blog, nil
+}
+
+func (r *MongoBlogRepository) GetBlogs(ctx context.Context, filter map[string]interface{}, search string, page int, limit int) ([]*models.Blog, error) {
+	var blogs []*models.Blog
+
+	// Construct the filter
+	filterBson := bson.M{}
+	if search != "" {
+		filterBson["$text"] = bson.M{"$search": search}
+	}
+	for key, value := range filter {
+		filterBson[key] = value
+	}
+
+	options := options.Find()
+	options.SetSkip(int64((page - 1) * limit))
+	options.SetLimit(int64(limit))
+
+	cursor, err := r.collection.Find(ctx, filterBson, options)
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	for cursor.Next(ctx) {
+		var blog models.Blog
+		if err := cursor.Decode(&blog); err != nil {
+			return nil, err
+		}
+		blogs = append(blogs, &blog)
+	}
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+
+	return blogs, nil
+}
+
+func (r *MongoBlogRepository) EditBlog(ctx context.Context, blogID primitive.ObjectID, newBlog *models.Blog) error {
+	_, err := r.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": blogID},
+		bson.M{"$set": newBlog},
+	)
+	return err
+}
+
+func (r *MongoBlogRepository) DeleteBlog(ctx context.Context, blogID primitive.ObjectID) error {
+	_, err := r.collection.DeleteOne(ctx, bson.M{"_id": blogID})
+	return err
+}
+
+func (r *MongoBlogRepository) AddCommentToTheList(ctx context.Context, blogID primitive.ObjectID, comment *models.Comment) error {
+	_, err := r.collection.UpdateOne(
+		ctx,
+		bson.M{"_id": blogID},
+		bson.M{"$push": bson.M{"comments": comment}},
+	)
+	return err
+}
--- backend/AAiT-backend-group-3/internal/repositories/interfaces/blog_repository_interface.go
+++ backend/AAiT-backend-group-3/internal/repositories/interfaces/blog_repository_interface.go
@@ -1,1 +1,18 @@
-package repositories
+package repository_interface
+
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"AAIT-backend-group-3/internal/domain/models"
+)
+
+
+type BlogRepository interface {
+	CreateBlog(ctx context.Context, blog *models.Blog) error
+	GetBlogByID(ctx context.Context, blogID primitive.ObjectID) (*models.Blog, error)
+	GetBlogs(ctx context.Context, filter map[string]interface{}, search string, page int, limit int) ([]*models.Blog, error)
+	EditBlog(ctx context.Context,blogID primitive.ObjectID, newBlog *models.Blog ) error
+	DeleteBlog(ctx context.Context, blogID primitive.ObjectID) error
+	AddCommentToTheList(ctx content.Context, blogID primitive.ObjectID, comment *models.Comment) error
+}

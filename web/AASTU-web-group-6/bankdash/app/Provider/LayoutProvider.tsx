0ec856a4d1dc576c7f3f14e8022dc766e7ad011d
Natnael Meseret <natnaelmeseret5@gmail.com>
2024-08-19 00:35:28 +0300

    fix nav and sidebar responsiveness

--- web/AASTU-web-group-6/bankdash/app/Provider/LayoutProvider.tsx
+++ web/AASTU-web-group-6/bankdash/app/Provider/LayoutProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState } from "react";
 import { usePathname } from "next/navigation";
 import Sidebar from "../components/Layout/Sidebar";
 import NavBar from "../components/Layout/NavBar";
@@ -9,18 +9,38 @@ const LayoutProvider = ({ children }: { children: ReactNode }) => {
   const pathname = usePathname();
 
   const noLayoutPaths = ["/login", "/signup"];
-
   const shouldRenderLayout = !noLayoutPaths.includes(pathname);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
 
   return (
     <>
       {shouldRenderLayout ? (
         <section className="w-full flex relative">
-          <div className="sidebar-container fixed z-20 h-screen w-[20%] bg-white pr-1">
-            <Sidebar />
+          {/* Sidebar */}
+          <div
+            className={`fixed z-20 h-screen sm:w-[50%] lg:w-[22%] lg:max-w-[220px] bg-white pr-1 top-0 transition-transform duration-300 ease-in-out transform ${
+              isOpen ? "translate-x-0" : "-translate-x-full"
+            } lg:translate-x-0`}
+          >
+            <Sidebar isOpen={isOpen} closeSidebar={toggle} />
           </div>
-          <div className="relative flex flex-col w-full pl-[20%]">
-            <NavBar />
+
+          {/* Overlay for small screens */}
+          {isOpen && (
+            <div
+              className="fixed inset-0 z-10 bg-black opacity-50 sm:block lg:hidden"
+              onClick={toggle}
+            ></div>
+          )}
+
+          {/* Main Content */}
+          <div className="relative flex flex-col w-full sm:pl-0 lg:pl-[216px]">
+            <NavBar openSidebar={toggle} />
             <section className="content flex w-full z-0 pt-20">
               {children}
             </section>

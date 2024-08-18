dfd9fc04ec2553be43c94b14fb041884e10c899c
SamrawitDawit <samrikdawit@gmail.com>
2024-08-18 13:57:38 +0300

    Updated the like sobject to include uuid id

--- backend/AAiT-backend-group-10/domain/like.go
+++ backend/AAiT-backend-group-10/domain/like.go
@@ -1,12 +1,15 @@
 package domain
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+)
 
 const (
 	CollectionLike = "likes"
 )
 
 type Like struct {
+	ID     uuid.UUID `json:"id,omitempty" bson:"id,omitempty"`
 	IsLike bool      `json:"is_like" bson:"is_like"`
 	UserID uuid.UUID `json:"user_id" bson:"user_id"`
 	BlogID uuid.UUID `json:"blog_id" bson:"blog_id"`
--- backend/AAiT-backend-group-10/domain/user.go
+++ backend/AAiT-backend-group-10/domain/user.go
@@ -1,1 +1,14 @@
 package domain
+
+import "github.com/google/uuid"
+
+type User struct {
+	ID           uuid.UUID `json:"id,omitempty" bson:"id,omitempty"`
+	Email        string    `json:"email" bson:"email" validate:"required,email,unique"`
+	Password     string    `json:"password" bson:"password" validate:"required,strong,min=8"`
+	FullName     string    `json:"full_name" bson:"full_name" validate:"required"`
+	Bio          string    `json:"bio,omitempty" bson:"bio,omitempty"`
+	ImageURL     string    `json:"image_url,omitempty" bson:"image_url,omitempty"`
+	IsAdmin      bool      `json:"is_admin" bson:"is_admin"`
+	RefreshToken string    `json:"refresh_token" bson:"refresh_token"`
+}

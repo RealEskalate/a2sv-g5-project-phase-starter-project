1bb0617194763c867d12965d3799b9985df03986
dagota12 <84585431+dagota12@users.noreply.github.com>
2024-08-18 14:48:08 +0300

    feat(astu-backend-g2) created models and entities  (#513)
    * defining initial entities
    user and blog models are defined
    
    * done creating entities and valdation
    
    * feat: models update

--- backend/ASTU-backend-group-2/domain/login.go
+++ backend/ASTU-backend-group-2/domain/login.go
@@ -1,1 +1,10 @@
 package domain
+
+type LoginRequest struct {
+	Email    string `json:"email" bson:"email" binding:"required,email"`
+	Password string `json:"password" bson:"password" binding:"required,min=4,max=30"`
+}
+type LoginResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}

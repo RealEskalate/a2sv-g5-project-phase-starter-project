21ddbe41519bbb57401c74ed52c01c9568ad2ad7
Nathan M.S <natms24@gmail.com>
2024-08-18 14:57:05 +0300

    Add accessToken and refreshToken functionality

--- backend/AAiT-backend-group-12/delivery/main.go
+++ backend/AAiT-backend-group-12/delivery/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "log"
+import (
+	"blog_api/delivery/env"
+	"blog_api/delivery/router"
+	initdb "blog_api/infrastructure/db"
+	"log"
+)
 
 func main() {
-	err := LoadEnvironmentVariables()
+	err := env.LoadEnvironmentVariables()
 	if err != nil {
 		log.Fatal(err.Error())
 		return
 	}
+
+	client, err := initdb.ConnectDB(env.ENV.DB_ADDRESS, env.ENV.DB_NAME)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	// defer initdb.DisconnectDB(client)
+
+	database := client.Database(env.ENV.DB_NAME)
+	router.SetupRouter(env.ENV.PORT, env.ENV.ROUTE_PREFIX, database)
 }

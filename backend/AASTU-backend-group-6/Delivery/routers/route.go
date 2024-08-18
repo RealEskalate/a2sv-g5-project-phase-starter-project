56f05cecdfce5b37bdc96285f34ee7752c393f66
Adane Moges <adman19940805@gmail.com>
2024-08-18 18:07:51 +0300

    chore: Update login route and controller to handle user login functionality

--- backend/AASTU-backend-group-6/Delivery/routers/route.go
+++ backend/AASTU-backend-group-6/Delivery/routers/route.go
@@ -20,6 +20,8 @@ func Router(server *gin.Engine, config *infrastructure.Config, DB mongo.Database
 	NewBlogrouter(blogRouter, blog_controller)
 	userRouter := server.Group("")
 	// NewUserrouter(config, DB , userRouter)
+	exp := time.Duration(time.Now().Add(time.Duration(config.RefreshTokenExpiryHour)).Unix())
 	NewSignupRoute(config, DB, userRouter)
-	NewRefreshTokenRouter(config,time.Duration(time.Now().Add(time.Duration(config.RefreshTokenExpiryHour)).Unix()) ,DB,userRouter)
+	NewRefreshTokenRouter(config, exp, DB, userRouter)
+	NewLoginRouter(config, exp, DB, userRouter)
 }

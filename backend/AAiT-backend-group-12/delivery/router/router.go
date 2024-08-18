ba3d42cceb322bfc94318853b4cde6cc74633f16
Nathan M.S <natms24@gmail.com>
2024-08-18 10:17:34 +0300

    Add signup route

--- backend/AAiT-backend-group-12/delivery/router/router.go
+++ backend/AAiT-backend-group-12/delivery/router/router.go
@@ -12,11 +12,11 @@ func SetupRouter(port int, routePrefix string, db *mongo.Database) {
 	router := gin.Default()
 
 	// auth
-	authRouter := router.Group("/auth")
+	authRouter := router.Group("/api" + routePrefix + "/auth")
 	NewAuthRouter(db.Collection(domain.CollectionUsers), authRouter)
 
 	// blog
-	blogAuthor := router.Group("/auth")
+	blogAuthor := router.Group("/api" + routePrefix + "/auth")
 	NewBlogRouter(db.Collection(domain.CollectionBlogs), blogAuthor)
 
 	router.Run(fmt.Sprintf(":%v", port))

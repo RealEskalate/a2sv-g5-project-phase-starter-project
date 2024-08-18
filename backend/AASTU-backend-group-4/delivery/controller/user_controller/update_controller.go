2e49ed2fc9d67ef6bb9d441adc9e289173d8fe71
Samrawit Gebremaryam Bahta <samrawitgebremaryam121@gmail.com>
2024-08-18 23:53:09 +0300

    Created user profile update

---
+++ backend/AASTU-backend-group-4/delivery/controller/user_controller/update_controller.go
@@ -0,0 +1,32 @@
+package user_controller
+
+import (
+	"blog-api/domain/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func (uc *UserController) UpdateUserProfile(c *gin.Context) {
+	var updateRequest user.UpdateRequest
+
+	if err := c.ShouldBindJSON(&updateRequest); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
+		return
+	}
+
+	userIDParam := c.Param("id")
+	userID, err := primitive.ObjectIDFromHex(userIDParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user ID"})
+		return
+	}
+
+	if err := uc.usecase.UpdateUser(c, userID, &updateRequest); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user profile"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User profile updated successfully"})
+}

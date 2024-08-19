21fc7f7ff8aca231853e456f2943cfd30709f209
c7-2know <betelhemyimamd@gmail.com>
2024-08-19 18:20:16 +0300

    add changes on profile interface

--- backend/AAiT-backend-group-11/domain/interfaces/profile.go
+++ backend/AAiT-backend-group-11/domain/interfaces/profile.go
@@ -5,9 +5,13 @@ import  "backend-starter-project/domain/entities"
 type ProfileRepository interface {
 	GetUserProfile(userId string) (*entities.Profile, error)
 	UpdateUserProfile(profile *entities.Profile) (*entities.Profile, error)
+	CreateUserProfile(profile *entities.Profile) (*entities.Profile, error)
+	DeleteUserProfile(user_id string) error
 }
 
 type ProfileService interface {
 	GetUserProfile(userId string) (*entities.Profile, error)
 	UpdateUserProfile(profile *entities.Profile) (*entities.Profile, error)
+	CreateUserProfile(profile *entities.Profile) (*entities.Profile, error)
+	DeleteUserProfile(user_id string) error
 }

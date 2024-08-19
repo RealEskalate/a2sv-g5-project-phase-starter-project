16abcedc9bced9a49924eb938f0ab1337751db09
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-19 11:47:47 +0300

    feat(domain): update user model to embed resetcode

--- backend/AAiT-backend-group-13/domain/models/user/user.go
+++ backend/AAiT-backend-group-13/domain/models/user/user.go
@@ -216,6 +216,9 @@ func (u *User) IsAdmin() bool {
 // IsActive returns whether the user is an active.
 func (u *User) IsActive() bool {
 	return u.isActive
+// IsAdmin returns whether the user is an admin.
+func (u *User) ResetCode() *ResetCode {
+	return u.resetCode
 }
 
 // UpdateUsername updates the user's username after validation.

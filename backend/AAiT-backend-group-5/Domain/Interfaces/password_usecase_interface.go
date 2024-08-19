ef6379429dad3535a2310c221a4273fb8a3904a9
Hermon Getachew <78665128+Mercury1565@users.noreply.github.com>
2024-08-19 11:10:58 +0300

    add_set_password_for_signingUp_aait.backend.g5.hermon

--- backend/AAiT-backend-group-5/Domain/Interfaces/password_usecase_interface.go
+++ backend/AAiT-backend-group-5/Domain/Interfaces/password_usecase_interface.go
@@ -6,8 +6,8 @@ import (
 	models "github.com/aait.backend.g5.main/backend/Domain/Models"
 )
 
-type ForgotPasswordUsecase interface {
-	GenerateResetURL(ctx context.Context, email string) (string, models.ErrorResponse)
-	SendResetEmail(ctx context.Context, email string, resetURL string) models.ErrorResponse
-	UpdateUserPassword(ctx context.Context, newPassword string, userID string) models.ErrorResponse
+type PasswordUsecase interface {
+	GenerateResetURL(ctx context.Context, email string) (string, *models.ErrorResponse)
+	SendResetEmail(ctx context.Context, email string, resetURL string) *models.ErrorResponse
+	UpdateUserPassword(ctx context.Context, newPassword string, userID string) *models.ErrorResponse
 }

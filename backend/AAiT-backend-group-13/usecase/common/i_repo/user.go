6638180fe63a4948712790b81349680dcb1be0b9
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-19 11:50:37 +0300

    feat(usecase): define user repository interface

---
+++ backend/AAiT-backend-group-13/usecase/common/i_repo/user.go
@@ -0,0 +1,13 @@
+package irepo
+
+import (
+	"github.com/google/uuid"
+	usermodel "github.com/group13/blog/domain/models/user"
+)
+
+type User interface {
+	Save(user *usermodel.User) error
+	FindById(id uuid.UUID) (*usermodel.User, error)
+	FindByUsername(username string) (*usermodel.User, error)
+	FindByEmail(email string) (*usermodel.User, error)
+}

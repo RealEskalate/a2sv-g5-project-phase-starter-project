74457754e5edd62cb8797137be0d577f89b9ced2
dipherent1 <Bini.Blue.1996@gmail.com>
2024-08-19 22:19:41 +0300

    chore: Add GetAllPosts endpoint with filter functionality

--- backend/AASTU-backend-group-7/Delivery/controllers/blog_controller.go
+++ backend/AASTU-backend-group-7/Delivery/controllers/blog_controller.go
@@ -3,7 +3,10 @@ package controllers
 import (
 	"blogapp/Domain"
 	"blogapp/Utils"
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -215,11 +218,32 @@ func (controller *blogController) GetComments(c *gin.Context) {
 }
 
 func (controller *blogController) GetAllPosts(c *gin.Context) {
-	// queryparams := c.Request.URL.Query()
-	// fmt.Println(queryparams)
-	// fmt.Println("hehe")
+	queryparams := c.Request.URL.Query()
+	filter := Domain.Filter{}
 
-	posts, err, statusCode := controller.BlogUseCase.GetAllPosts(c)
+	// fill in filter values from the request query
+	if len(queryparams) > 0 {
+		filter.Slug = queryparams.Get("slug")
+		filter.AuthorName = queryparams.Get("author_id")
+		filter.Limit ,_= strconv.Atoi(queryparams.Get("limit"))
+		filter.Page, _ = strconv.Atoi(queryparams.Get("page"))
+	}
+
+	if tags, ok := queryparams["tags"]; ok && len(tags) > 0 {
+		filter.Tags = strings.Split(tags[0], ",") // Splitting by comma to get slice of tags
+	}
+
+	if sort, ok := queryparams["sort"]; ok && len(sort) > 0 {
+		var sortMap map[string]int
+		err := json.Unmarshal([]byte(sort[0]), &sortMap) // Assuming sort is passed as a valid JSON string
+		if err != nil {
+			// handle error, maybe set a default sort or return an error
+		} else {
+			filter.Sort = sortMap
+		}
+	}
+
+	posts, err, statusCode := controller.BlogUseCase.GetAllPosts(c, filter)
 	if err != nil {
 		c.JSON(statusCode, gin.H{"error": err.Error()})
 		return
@@ -272,7 +296,6 @@ func (controller *blogController) AddTagToPost(c *gin.Context) {
 	}
 
 	slug := Utils.GenerateSlug(tag.Name)
-
 
 	err, statusCode = controller.BlogUseCase.AddTagToPost(c, postID, slug)
 	if err != nil {

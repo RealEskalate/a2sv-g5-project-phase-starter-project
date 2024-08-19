f8fdbd00161a9d257587b1bf3f27bcaa8e8cd4e3
Abenezer Seifu <abenezerseifu123@gmail.com>
2024-08-19 10:33:00 +0300

    utilized .env file to get db uri, name & collection name

--- backend/AAiT-backend-group-10/delivery/main.go
+++ backend/AAiT-backend-group-10/delivery/main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"aait.backend.g10/delivery/router"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
-	clientOption := options.Client().ApplyURI("mongodb://localhost:27017")
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	clientOption := options.Client().ApplyURI(os.Getenv("DB_URI"))
 	client, err := mongo.Connect(context.TODO(), clientOption)
 
 	if err != nil {

1fd42ba377f22ec26768c82e75d4891dcbd88c59
beki <beki@kali>
2024-08-19 11:14:36 +0300

    Investment page UI and some edits on lonas page

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/investments/investment_components/Trending.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/investments/investment_components/Trending.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
+import React from 'react';
+import TrendingTable from './TrendingTable';
 export default function Trending() {
     return (
-        <div>
-          
+        <div className='w-[95%] md:w-[25%] mx-auto md:mx-0'>
+          <h1  className="my-4 font-[600] text-[22px] text-[#333B69] ml-5 md:ml-0" >Trending Stock</h1>
+          <TrendingTable />
         </div>
       )
 }

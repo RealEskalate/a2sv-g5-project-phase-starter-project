1a24f2beca228791ced4de5d0b5a08de45a16c14
anatoli-derese <anatoli.derese@aait.edu.et>
2024-08-19 10:40:44 +0300

    comments functionality iimplemented

---
+++ backend/AAiT-backend-group-8/Infrastructure/interfaces.go
@@ -0,0 +1,29 @@
+package infrastructure
+
+import (
+	domain "AAiT-backend-group-8/Domain"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type IUserRepsitory interface {
+	RegisterNewUser(domain.User) error
+	GetSingleUser(email string) (*domain.User, error)
+	PromoteUser(userID primitive.ObjectID) error
+	// ForgetPassword()
+	// DeleteRefreshToken(primitive.ObjectID) error
+	DemoteUser(userID primitive.ObjectID) error
+	DeleteUser(userID primitive.ObjectID) error
+	UpdateUser(user domain.User) error
+}
+
+type IUserUseCase interface {
+	RegisterNewUser(user domain.User) error
+	Login(email, password string) (string, error)
+	GetSingleUser(email string) *domain.User
+	PromoteUser(userID string) error
+	DemoteUser(userID string) error
+	UpdateUser(userID string, user *domain.User) error
+}
+
+

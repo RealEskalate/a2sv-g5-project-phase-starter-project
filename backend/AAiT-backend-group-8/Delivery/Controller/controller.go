ec7492443852f4a47f6f3a959fad61510fc6b17a
Abzaek <abzaek@hotmail.com>
2024-08-19 22:06:35 +0300

    add blogUseCase field to controller struct and add initializing function

--- backend/AAiT-backend-group-8/Delivery/Controller/controller.go
+++ backend/AAiT-backend-group-8/Delivery/Controller/controller.go
@@ -1,7 +1,18 @@
 package controllers
 
-import usecase "AAiT-backend-group-8/Usecase"
+import (
+	"AAiT-backend-group-8/Domain"
+	usecase "AAiT-backend-group-8/Usecase"
+)
 
 type Controller struct {
 	commentUseCase usecase.CommentUseCase
+	blogUseCase    Domain.IBlogUseCase
+}
+
+func NewController(blogCase Domain.IBlogUseCase, commentUseCase usecase.CommentUseCase) *Controller {
+	return &Controller{
+		blogUseCase:    blogCase,
+		commentUseCase: commentUseCase,
+	}
 }

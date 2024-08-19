e501d2343cc4fd4dca3f105d6c4312667fff5b2c
ephrem <ephremmamo555@gmail.com>
2024-08-19 11:27:07 +0300

    feat(usecase): update user handler

--- backend/AAiT-backend-group-13/usecase/user/command/update/handler.go
+++ backend/AAiT-backend-group-13/usecase/user/command/update/handler.go
@@ -3,39 +3,26 @@
 package promotcmd
 
 import (
-	"github.com/google/uuid"
-	usermodel "github.com/group13/blog/domain/models/user"
 	icmd "github.com/group13/blog/usecase/common/cqrs/command"
+	irepo "github.com/group13/blog/usecase/common/i_repo"
 )
-
-type UserRepository interface {
-	Save(*usermodel.User) error
-	ByUsername(string) (*usermodel.User, error)
-	ById(uuid.UUID) (bool, error)
-}
 
 // Handler handles the promote command logic.
 type Handler struct {
-	userRepo UserRepository
+	userRepo irepo.UserRepository
 }
 
 // Ensure Handler implement icmd.Handler
 var _ icmd.IHandler[*Command, bool] = &Handler{}
 
 // New creates a new instance of the Handler with the provided user repository.
-func New(userRepo UserRepository) *Handler {
+func New(userRepo irepo.UserRepository) *Handler {
 	return &Handler{userRepo: userRepo}
 }
 
 // Promote promotes a user to admin status or demote user based on the provided command.
 func (h *Handler) Handle(cmd *Command) (bool, error) {
 	user, err := h.userRepo.ByUsername(cmd.Username)
-	if err != nil {
-		return false, err
-	}
-
-	_, err = h.userRepo.ById(cmd.PromoterID)
-
 	if err != nil {
 		return false, err
 	}

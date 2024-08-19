211d8dd8e48d03e4998aa08c43a69d507b3d7e89
dagota12 <chernetdagim@gmail.com>
2024-08-19 12:07:48 +0300

    chore: update structs and models

--- backend/ASTU-backend-group-2/domain/signup.go
+++ backend/ASTU-backend-group-2/domain/signup.go
@@ -2,21 +2,19 @@ package domain
 
 import (
 	"context"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type UserIn struct {
-	ID         primitive.ObjectID `json:"id" bson:"_id"`
-	FirstName  string             `json:"first_name" bson:"first_name" binding:"required,min=3,max=30"`
-	LastName   string             `json:"last_name" bson:"last_name" binding:"max=30"`
-	Email      string             `json:"email" bson:"email" binding:"required,email"`
-	Password   string             `json:"password" bson:"password" binding:"required,min=4,max=30,StrongPassword"`
-	Bio        string             `json:"bio" bson:"bio"`
-	ProfileImg string             `json:"profile_img" bson:"profile_img"`
-	IsOwner    bool               `json:"-" bson:"is_owner"`
-	Role       string             `json:"-" bson:"role"` //may make only tobe admin or user
-	Tokens     []string           `json:"tokens" bson:"tokens"`
+	FirstName  string `json:"first_name" bson:"first_name" binding:"required,min=3,max=30"`
+	LastName   string `json:"last_name" bson:"last_name" binding:"max=30"`
+	Email      string `json:"email" bson:"email" binding:"required,email"`
+	Password   string `json:"password" bson:"password" binding:"required,min=4,max=30,StrongPassword"`
+	Bio        string `json:"bio" bson:"bio"`
+	ProfileImg string `json:"profile_img" bson:"profile_img"`
+}
+type SignupResponse struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
 type SignupUsecase interface {
--- backend/ASTU-backend-group-2/domain/user.go
+++ backend/ASTU-backend-group-2/domain/user.go
@@ -11,6 +11,7 @@ type User struct {
 	FirstName  string             `json:"first_name" bson:"first_name" binding:"required,min=3,max=30"`
 	LastName   string             `json:"last_name" bson:"last_name" binding:"max=30"`
 	Email      string             `json:"email" bson:"email" binding:"required,email"`
+	Active     bool               `json:"is_active" bson:"is_active"`
 	Bio        string             `json:"bio" bson:"bio"`
 	ProfileImg string             `json:"profile_img" bson:"profile_img"`
 	Password   string             `json:"password" bson:"password" binding:"required,min=4,max=30,StrongPassword"`
@@ -30,13 +31,36 @@ type UserOut struct {
 	IsOwner    bool               `json:"is_owner" bson:"is_owner"`
 	Role       string             `json:"role" bson:"role"` //may make only tobe admin or user
 }
+type UserUpdate struct {
+	FirstName  string `json:"first_name" bson:"first_name"`
+	LastName   string `json:"last_name" bson:"last_name"`
+	Bio        string `json:"bio" bson:"bio"`
+	ProfileImg string `json:"profile_img" bson:"profile_img"`
+}
 
+// user knows the password and wants to update
+type UpdatePassword struct {
+	OldPassword string `json:"old_password" binding:"required"`
+	NewPassword string `json:"new_password" binding:"required"`
+}
 
+// user forgot the password and wants to reset
+// reset passowrd token will be exreacted from the url /reset-password/:user_id/:<reset password token>
+type ResetPassword struct {
+	NewPassword string `json:"new_password" binding:"required"`
+}
 
 type UserRepository interface {
-	GetByID(c context.Context, userID string) (*User, error)          // Retrieves a user's profile by ID
-	Update(c context.Context, userID string, updatedProfile *User) error // Updates a user's profile
-	Delete(c context.Context, userID string) error                      // Deletes a user's profile
-	PromoteToAdmin(c context.Context, userID string) error              // Promotes a user to admin
-	DemoteToUser(c context.Context, userID string) error                // Demotes an admin to a regular user
+	GetByID(c context.Context, userID string) (*UserOut, error)                       // Retrieves a user's profile by ID
+	UpdateProfile(c context.Context, userID string, updatedProfile *UserUpdate) error // Updates a user's profile
+	PromoteToAdmin(c context.Context, userID string) error                            // Promotes a user to admin
+	DemoteToUser(c context.Context, userID string) error                              // Demotes an admin to a regular user
+	Delete(c context.Context, userID string) error                                    // Deletes a user's profile
+}
+type UserUsecase interface {
+	GetUserProfile(c context.Context, userID string) (*User, error)                 // Retrieves a user's profile by ID
+	UpdateUserProfile(c context.Context, userID string, updatedProfile *User) error // Updates a user's profile
+	DeleteUserProfile(c context.Context, userID string) error                       // Deletes a user's profile
+	PromoteUserToAdmin(c context.Context, userID string) error                      // Promotes a user to admin
+	DemoteAdminToUser(c context.Context, userID string) error                       // Demotes an admin to a regular user
 }

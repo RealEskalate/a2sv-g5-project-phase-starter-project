611a0a51f9979dedb829248d261089c73d2c24ce
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-19 11:28:12 +0300

    feat(chat): add a repository implemetation for  the chat domain

---
+++ backend/ASTU-backend-group-4/chat/mongodb/chat_storage.go
@@ -0,0 +1,78 @@
+package mongodb
+
+import (
+	"context"
+	"errors"
+
+	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/chat"
+	// "github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/pkg/infrastructure"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+const collectionName = "chats"
+
+type ChatRepository struct {
+	Database *mongo.Database
+}
+
+func NewChatRepository(database *mongo.Database, collectionName string) *ChatRepository {
+	return &ChatRepository{
+		Database: database,
+	}
+}
+
+func (chatRepository *ChatRepository) CreateChat(ctx context.Context, newChat chat.Chat) (chat.Chat, error) {
+	collection := chatRepository.Database.Collection(collectionName)
+	_, err := collection.InsertOne(ctx, newChat)
+	if err != nil {
+		return chat.Chat{}, err
+	}
+
+	return newChat, nil
+}
+
+func (chatRepository *ChatRepository) AddMessage(chatID string, message chat.Message, ctx context.Context) error {
+	collection := chatRepository.Database.Collection(collectionName)
+	update := bson.M{
+		"$push": bson.M{"history": message},
+	}
+
+	_, err := collection.UpdateByID(ctx, chatID, update)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (chatRepository *ChatRepository) GetChat(chatID string, ctx context.Context) (chat.Chat, error) {
+	collection := chatRepository.Database.Collection(collectionName)
+	filter := bson.M{"_id": chatID}
+	var chat chat.Chat
+	if err := collection.FindOne(ctx, filter).Decode(&chat); err != nil {
+		return chat, nil
+	}
+
+	return chat, nil
+}
+
+// func(chatRepository *ChatRepository) GetChats(ctx context.Context, pagination infrastructure.PaginationRequest) (infrastructure.PaginationResponse[chat.Chat], error){
+
+// }
+
+func (chatRepository *ChatRepository) DeleteChat(chatID string, ctx context.Context) error {
+	collection := chatRepository.Database.Collection(collectionName)
+	filter := bson.M{"_id": chatID}
+
+	deleteResult, err := collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	if deleteResult.DeletedCount == 0{
+		return errors.New("chat not found")
+	}
+
+	return nil
+}

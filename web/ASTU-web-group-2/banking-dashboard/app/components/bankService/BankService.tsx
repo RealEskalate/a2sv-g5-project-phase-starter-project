427694b5de46f5efa1d5dd805b721023fb93acde
fasill <fasilhawultie19@gmail.com>
2024-08-19 18:15:35 +0300

    empty icon on lists empty

--- web/ASTU-web-group-2/banking-dashboard/app/components/bankService/BankService.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/bankService/BankService.tsx
@@ -15,9 +15,9 @@ const BankService = ({
 }: BankServiceType) => {
   return (
     <div
-      className={`${inter.className} flex justify-between bg-white rounded-[22px] h-fit p-5 items-center`}
+      className={`${inter.className} flex justify-between bg-white rounded-[22px] h-fit pl-5 pt-5 pb-5 items-center`}
     >
-      <div className="flex gap-3 items-center">
+      <div className="flex gap-4 items-center w-[100%]">
         <img src={icon} alt="business-loans" />
         <div>
           <div className="font-medium ">{name}</div>
@@ -25,22 +25,24 @@ const BankService = ({
         </div>
       </div>
 
-      <div>
+      <div className=" w-[60%]" >
         <div className="font-medium">{numberOfUsers}</div>
         <div className="font-normal text-[#718EBF]">users</div>
       </div>
-      <div>
+      <div  className=" w-[60%]">
         <div className="font-medium">{status}</div>
         <div className="font-normal text-[#718EBF]">status</div>
       </div>
-      <div>
+      <div className=" w-[60%]">
         <div className="font-medium">{type}</div>
         <div className="font-normal text-[#718EBF]">type</div>
       </div>
+      <div className="w-[50%]">
 
       <button className="h-fit px-5 border rounded-3xl border-[#718EBF] text-[#718EBF] hover:border-[#1814F3] hover:text-[#1814F3]">
         View Details
       </button>
+      </div>
     </div>
   );
 };

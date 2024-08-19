61dbd26e3f16e172dc939e6ef0a364211240f50f
amha-mersha <amhamersh@gmail.com>
2024-08-19 15:12:20 +0300

    mid rebase

--- backend/AAiT-backend-group-1/usecase/user_usecase.go
+++ backend/AAiT-backend-group-1/usecase/user_usecase.go
@@ -38,6 +38,26 @@ func (userUC *UserUseCase) Register(cxt *gin.Context, user *domain.User) domain.
 	if errTimeout != nil {
 		return &domain.CustomError{Message: errTimeout.Error(), Code: http.StatusInternalServerError}
 	}
+	userRepo domain.UserRepository
+}
+
+func NewUserUseCase(userRespository domain.UserRepository) UserUseCase {
+	return UserUseCase{
+		userRepo: userRespository,
+	}
+}
+
+var (
+	timeoutStr = os.Getenv("CONTEXT_TIMEOUT")
+	timeout    int64
+	err        error
+)
+
+timeout, err = strconv.ParseInt(timeoutStr, 10, 64)
+	if errTimeout != nil {
+		return &domain.CustomError{Message: errTimeout.Error(), Code: http.StatusInternalServerError}
+	}
+func (userUC *UserUseCase) Register(cxt *gin.Context, user *domain.User) domain.Error {
 	context, cancel := context.WithTimeout(cxt, time.Duration(timeout)*time.Second)
 	defer cancel()
 	errValidity := user.Validate()

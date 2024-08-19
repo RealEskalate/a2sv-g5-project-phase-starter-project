d2ad3af32e044ab7b9ea3c0db8b3e03ea303cb4c
rebumatadele <118250996+rebumatadele@users.noreply.github.com>
2024-08-19 12:56:24 +0300

    signin page and api call files added

--- web/AASTU-web-group-2/a2sv-banking-system/app/page.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/page.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import QuickTransfer from "@/app/dashboard/components/QuickTransfer"
-const page = () => {
-  return (
-    <div>
-        <QuickTransfer />
-    </div>
-  )
-}
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { options } from "./api/auth/[...nextauth]/options";
+const page = async () => {
+  const session = await getServerSession(options);
 
-export default page
+  if (!session) {
+    console.log("NO SESSION")
+    redirect("/api/auth/signin");
+
+  } else {
+    console.log(session)
+    redirect("./dashboard");
+  }
+};
+
+export default page;

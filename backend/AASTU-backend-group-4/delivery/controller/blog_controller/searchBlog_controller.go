f41f0f04780dd5c4e9efa601b7407e152642e320
Samrawit Gebremaryam Bahta <samrawitgebremaryam121@gmail.com>
2024-08-19 22:04:49 +0300

    created search blog by using title , author or both functionality.

--- backend/AASTU-backend-group-4/delivery/controller/blog_controller/searchBlog_controller.go
+++ backend/AASTU-backend-group-4/delivery/controller/blog_controller/searchBlog_controller.go
@@ -1,1 +1,20 @@
 package blog_controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (bc *BlogController) SearchBlog(c *gin.Context) {
+	title := c.Query("title")
+	author := c.Query("author")
+
+	blogs, err := bc.usecase.SearchBlog(c.Request.Context(), title, author)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to search blogs"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"blogs": blogs})
+}
--- backend/AASTU-backend-group-4/usecase/blog_usecase/searchBlog_usecase.go
+++ backend/AASTU-backend-group-4/usecase/blog_usecase/searchBlog_usecase.go
@@ -1,1 +1,15 @@
 package blog_usecase
+
+import (
+	domain "blog-api/domain/blog"
+	"context"
+	"errors"
+)
+
+func (bu *BlogUsecase) SearchBlog(ctx context.Context, title string, author string) ([]*domain.Blog, error) {
+	if title == "" && author == "" {
+		return nil, errors.New("at least one search criterion (title or author) must be provided")
+	}
+
+	return bu.repo.SearchBlog(ctx, title, author)
+}

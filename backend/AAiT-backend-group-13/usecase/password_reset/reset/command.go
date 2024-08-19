bf98aeb74ac47aa0b8be66b87bb4b6ff2a15f333
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-19 14:05:25 +0300

    docs: add godoc for usecase/password_reset/reset/command.go

--- backend/AAiT-backend-group-13/usecase/password_reset/reset/command.go
+++ backend/AAiT-backend-group-13/usecase/password_reset/reset/command.go
@@ -2,7 +2,12 @@ package resetpassword
 
 import "github.com/google/uuid"
 
+// Command represents the command to reset a user's password.
+// It contains the user ID and the new password.
 type Command struct {
-	Id          uuid.UUID
+	// Id is the unique identifier of the user whose password is being reset.
+	Id uuid.UUID
+
+	// NewPassword is the new password for the user.
 	NewPassword string
 }

175b18f01eb572f3679894afea04818a8f76614c
dipherent1 <Bini.Blue.1996@gmail.com>
2024-08-19 10:14:43 +0300

    chore: Update user_dto.go to include Posts field

--- backend/AASTU-backend-group-7/Dtos/user_dto.go
+++ backend/AASTU-backend-group-7/Dtos/user_dto.go
@@ -1,16 +1,19 @@
 package Dtos
 
-import "time"
+import (
+	"time"
+)
 
 type RegisterUserDto struct {
-	Email          string    `json:"email" validate:"required,email"`
-	Password       string    `json:"password" validate:"required"`
-	UserName       string    `json:"username" `
-	Role           string    `json:"-",omitempty default:"user"`
-	ProfilePicture string    `json:"profile_picture"`
-	Bio            string    `json:"bio"`
-	CreatedAt      time.Time `json:"created_at"`
-	UpdatedAt      time.Time `json:"updated_at"`
+	Email          string        `json:"email" validate:"required,email"`
+	Password       string        `json:"password" validate:"required"`
+	UserName       string        `json:"username" `
+	Role           string        `json:"-",omitempty default:"user"`
+	ProfilePicture string        `json:"profile_picture"`
+	Bio            string        `json:"bio"`
+	CreatedAt      time.Time     `json:"created_at"`
+	UpdatedAt      time.Time     `json:"updated_at"`
+	Posts          []*interface{} `json:"posts"`
 }
 
 type LoginUserDto struct {

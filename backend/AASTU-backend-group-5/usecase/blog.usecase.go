3426765b462592f840a33b9264cce0ad254b8d56
ermi9s <ermiayele1122334477@gmail.com>
2024-08-19 23:51:41 +0300

    add blog routes

--- backend/AASTU-backend-group-5/usecase/blog.usecase.go
+++ backend/AASTU-backend-group-5/usecase/blog.usecase.go
@@ -5,26 +5,53 @@ import (
 )
 
 type BlogUseCase struct {
-	BlogRepo domain.Blog_Rerpository_interface
+	BlogRepo domain.Blog_Repository_interface
 }
 
-func (uc *BlogUseCase) CreateBlog(blog domain.Blog) (*domain.Blog, error) {
+func NewBlogUsecase(repo domain.Blog_Repository_interface) *BlogUseCase{
+	return &BlogUseCase{
+		BlogRepo: repo,
+	}
+}
+
+func (uc *BlogUseCase) CreateBlog(blog domain.Blog) (domain.Blog, error) {
 	createdBlog, err := uc.BlogRepo.CreateBlogDocunent(blog)
 	if err != nil {
-		return nil, err
+		return domain.Blog{},err
 	}
-	return &createdBlog, nil
-}
-func (uc *BlogUseCase) GetOneBlog(id string) ([]domain.Blog, error) {
-	return uc.BlogRepo.GetOneBlogDocunent(id)
-}
-func (uc *BlogUseCase) UpdateBlog(id string, updatedBlog domain.Blog) (domain.Blog, error) {
-	return uc.BlogRepo.UpdateBlogDocunent(id, updatedBlog)
+	return createdBlog, nil
 }
 
-func (uc *BlogUseCase) DeleteBlog(id string) error {
-	return uc.BlogRepo.DeleteBlogDocunent(id)
+func (uc *BlogUseCase) GetBlogs(limit int , page_number int) ([]domain.Blog, error){
+	blogs , err := uc.BlogRepo.GetBlogDocunents(page_number , limit)
+	if err != nil {
+		return []domain.Blog{},err
+	}
+	return blogs,nil
+}
+func (uc *BlogUseCase) GetOneBlog(id string) (domain.Blog, error) {
+	blogs , err := uc.BlogRepo.GetOneBlogDocunent(id)
+	if err != nil {
+		return domain.Blog{},err
+	}
+	return blogs,nil
+}
+func (uc *BlogUseCase) UpdateBlog(id string, updatedBlog domain.Blog) (domain.Blog, error) {
+	blog, err := uc.BlogRepo.UpdateBlogDocunent(id, updatedBlog)
+	if err != nil {
+		return domain.Blog{},err
+	}
+
+	return blog,nil
+}
+
+func (uc *BlogUseCase) DeleteBlog(id string , user_id string) error {
+	return uc.BlogRepo.DeleteBlogDocument(id , user_id)
 }
 func (uc *BlogUseCase) FilterBlog(ans map[string]string) ([]domain.Blog, error) {
-	return uc.BlogRepo.FilterBlogDocunent(ans)
+	blogs , err := uc.BlogRepo.FilterBlogDocunent(ans)
+	if err != nil {
+		return []domain.Blog{},err
+	}
+	return blogs,nil
 }

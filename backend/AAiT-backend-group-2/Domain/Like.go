f07786261a0de886e66b73e8520f164537f37b4c
mentesnot-2 <sibhatmentesnot@gmail.com>
2024-08-19 14:21:45 +0300

    implemented blog crud and modified blog domain struct to have context

--- backend/AAiT-backend-group-2/Domain/Like.go
+++ backend/AAiT-backend-group-2/Domain/Like.go
@@ -6,5 +6,6 @@ type Like struct {
 	ID        string    `bson:"_id" json:"id"`
 	BlogID    string    `bson:"blog_id" json:"blog_id"`
 	Author    string    `bson:"author" json:"author"`
+	IsLiked  bool      `bson:"is_liked" json:"is_liked"`
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 }

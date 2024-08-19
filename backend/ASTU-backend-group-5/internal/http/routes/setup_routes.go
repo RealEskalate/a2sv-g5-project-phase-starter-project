81f2ba1257cd942913b8b936ce9af5c43727b7d1
kika1s1 <tamiratkebede120@gmail.com>
2024-08-19 14:50:59 +0300

    All blog end points are added and tested

--- backend/ASTU-backend-group-5/internal/http/routes/setup_routes.go
+++ backend/ASTU-backend-group-5/internal/http/routes/setup_routes.go
@@ -5,9 +5,10 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func SetUpRoute(router *gin.Engine, userCollection *mongo.Collection) {
+func SetUpRoute(router *gin.Engine, blogsCollection, commentsCollection, likesCollection, viewsCollection, tagsCollection, userCollection *mongo.Collection) {
 	RegisterUserRoutes(userCollection, router)
 	RegisterVerificationRoutes(userCollection, router)
 	RegisterAdminUserRoutes(userCollection, router)
-	// RegisterBlogRoutes(collection, router)
+	RegisterBlogRoutes(blogsCollection, commentsCollection, likesCollection, viewsCollection, tagsCollection, router)
+	
 }

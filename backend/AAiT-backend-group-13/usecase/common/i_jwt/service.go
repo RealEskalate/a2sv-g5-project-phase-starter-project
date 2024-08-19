80cfa635d679f14459cce3b9649b6c780d682fe0
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-19 11:50:21 +0300

    feat(usecase): update jwt service interface
    - add token type parametr to generate function. this will be help full
    to make this service reusable.

--- backend/AAiT-backend-group-13/usecase/common/i_jwt/service.go
+++ backend/AAiT-backend-group-13/usecase/common/i_jwt/service.go
@@ -6,10 +6,16 @@ import (
 	usermodel "github.com/group13/blog/domain/models/user"
 )
 
+const (
+	Access  = "acccess"
+	Refresh = "refresh"
+	Reset   = "reset"
+)
+
 // Service defines methods to generate and decode JWTs.
 type Service interface {
 	// Generate creates a JWT for a user.
-	Generate(user *usermodel.User) (string, error)
+	Generate(user *usermodel.User, tokenType string) (string, error)
 
 	// Decode parses a JWT and returns claims.
 	Decode(token string) (jwt.MapClaims, error)

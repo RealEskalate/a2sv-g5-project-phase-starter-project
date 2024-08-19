56e9a22728535f3936d74f17d9c6329c25b216de
SamrawitDawit <samrikdawit@gmail.com>
2024-08-19 11:59:44 +0300

    Added one endpoint

--- backend/AAiT-backend-group-10/delivery/controllers/comment.go
+++ backend/AAiT-backend-group-10/delivery/controllers/comment.go
@@ -2,24 +2,38 @@ package controllers
 
 import (
 	"aait.backend.g10/domain"
-	"aait.backend.g10/usecases/interfaces"
+	"aait.backend.g10/usecases"
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 )
 
 type CommentController struct {
-	CommentUsecase interfaces.CommentUsecaseInterface
+	CommentUsecase usecases.CommentUsecaseInterface
 }
 
 func (cont *CommentController) GetComments(c *gin.Context) {
-	blogID := c.Param("blog_id")
-	blog_id, _ := uuid.Parse(blogID)
-	comments, err := cont.CommentUsecase.GetComments(blog_id)
+	blogID, err := uuid.Parse(c.Param("blog_id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID"})
+	}
+	comments, err := cont.CommentUsecase.GetComments(blogID)
 	if err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(200, gin.H{"comments": comments})
+}
+func (cont *CommentController) GetCommentsCount(c *gin.Context) {
+	blogID, err := uuid.Parse(c.Param("blog_id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID"})
+	}
+	commentCount, err := cont.CommentUsecase.GetCommentsCount(blogID)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(200, gin.H{"comment count": commentCount})
 }
 
 func (cont *CommentController) AddComment(c *gin.Context) {
@@ -37,12 +51,16 @@ func (cont *CommentController) AddComment(c *gin.Context) {
 }
 
 func (cont *CommentController) UpdateComment(c *gin.Context) {
+	commentID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID"})
+	}
 	var comment domain.Comment
 	if err := c.BindJSON(&comment); err != nil {
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-	err := cont.CommentUsecase.UpdateComment(comment)
+	err = cont.CommentUsecase.UpdateComment(commentID, comment)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
@@ -51,11 +69,11 @@ func (cont *CommentController) UpdateComment(c *gin.Context) {
 }
 
 func (cont *CommentController) DelelteComment(c *gin.Context) {
-	blogID := c.Param("blog_id")
-	userID := c.Param("user_id")
-	blog_id, _ := uuid.Parse(blogID)
-	user_id, _ := uuid.Parse(userID)
-	if err := cont.CommentUsecase.DelelteComment(blog_id, user_id); err != nil {
+	commentID, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid ID"})
+	}
+	if err := cont.CommentUsecase.DelelteComment(commentID); err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}

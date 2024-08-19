6e21490e8fa33ee4f8a331c676ec8fa96c8eddc0
Kuma Telila <kumatelila26@gmail.com>
2024-08-19 10:46:29 +0300

    Dashboard responsive done

--- web/ASTU-web-group-2/banking-dashboard/app/components/card/CardForCreditCards.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/card/CardForCreditCards.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import React from "react";
 
-const Card = ({
+const CardForCreditCards = ({
   title,
   button,
   link,
   children,
-  className = "flex flex-col lg:w-[730px] lg:h-[300px] md:w-[487px] md:h-[299px] h-[254]",
+  className = "flex flex-col lg:w-[730px] lg:h-[300px] md:w-[487px] md:h-[299px] h-[254px] max-md:w-[550px]",
 }: {
   title: string;
   button: string;
@@ -15,18 +15,20 @@ const Card = ({
   className?: string;
 }) => {
   return (
-    <div className={`${className}`}>
+    <div className={`${className} max-md:overflow-x-auto`}> 
       <div className="flex justify-between">
         <p className="font-semibold my-3 text-[18px] md:text-[18px] lg:text-[22px] text-[#343C6A]">{title}</p>
         <Link href={link}>
-          <p className="font-semibold my-3 text[12px] text-[#343C6A] text-right pr-3">
+          <p className="font-semibold my-3 text-[12px] text-[#343C6A] text-right pr-3">
             {button}
           </p>
         </Link>
       </div>
-      {children}
+      <div className="flex gap-[30px] max-md:overflow-x-auto">
+        {children}
+      </div>
     </div>
   );
 };
 
-export default Card;
+export default CardForCreditCards;

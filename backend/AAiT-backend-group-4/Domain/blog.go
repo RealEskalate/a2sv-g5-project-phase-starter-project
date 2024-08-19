bddd138349ad3e115ce02684eddb50c845976ff7
Mesay-AK <mesabe77@gmail.com>
2024-08-19 13:40:57 +0300

    Updating Feedbacks

--- backend/AAiT-backend-group-4/Domain/blog.go
+++ backend/AAiT-backend-group-4/Domain/blog.go
@@ -1,12 +1,19 @@
 package domain
 
 import (
+	"context"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Tag string
+type FilterParam string
+
+const (
+	Date       FilterParam = "date"
+	Popularity FilterParam = "popularity"
+)
 
 const (
 	Tech            Tag = "Tech"
@@ -24,6 +31,7 @@ type Author struct {
 type Comment struct {
 	User_ID   string    `json:"user_id"`
 	User_Name string    `json:"user_name"`
+	Content   string    `json:"content"`
 	Date      time.Time `json:"date"`
 }
 
@@ -39,9 +47,52 @@ type Blog struct {
 	Title       string             `json:"title" bson:"title" validate:"required,min=5,max=100"`
 	Content     string             `json:"content" bson:"content" validate:"required,min=5"`
 	Author_Info Author             `json:"author_info" bson:"author_info"`
-	Date        time.Time          `json:"date" bson:"date"`
 	Tags        []Tag              `json:"tags" bson:"tags"`
+	Popularity  float64            `json:"popularity" bson:"popularity"`
 	Feedbacks   Feedback           `json:"feedbacks" bson:"feedbacks"`
 	Created_At  time.Time          `json:"created_at" bson:"created_at"`
 	Updated_At  time.Time          `json:"updated_at" bson:"updated_at"`
+}
+
+type Filter struct {
+	AuthorName *string
+	Tags       *[]Tag
+	BlogTitle  *string
+	Popularity *float64
+	Sort_By    *FilterParam
+}
+
+type BlogUpdate struct {
+	Title       *string    `json:"title" bson:"title" validate:"required,min=5,max=100"`
+	Content     *string    `json:"content" bson:"content" validate:"required,min=5"`
+	Author_Info *Author    `json:"author_info" bson:"author_info"`
+	Tags        *[]Tag     `json:"tags" bson:"tags"`
+	Popularity  *float64   `json:"popularity" bson:"popularity"`
+	Feedbacks   *Feedback  `json:"feedbacks" bson:"feedbacks"`
+	Updated_At  *time.Time `json:"updated_at" bson:"updated_at"`
+}
+
+
+type BlogRepository interface {
+	// CreateBlog inserts a new blog into the collection
+	CreateBlog(c context.Context, blog *Blog) error
+	// FetchByBlogID retrieves a blog by its ID
+	FetchByBlogID(c context.Context, blogID string) (Blog, error)
+	// FetchAll retrieves all blogs from the collection
+	FetchAll(c context.Context) ([]Blog, error)
+	// FetchByBlogAuthor retrieves blogs by the author's ID
+	FetchByBlogAuthor(c context.Context, authorID string) ([]Blog, error)
+	// FetchByBlogTitle retrieves blogs by their title
+	FetchByBlogTitle(c context.Context, title string) ([]Blog, error)
+	// UpdateBlog updates a blog in the collection by its ID
+	UpdateBlog(ctx context.Context, id primitive.ObjectID, blog BlogUpdate) error
+	// DeleteBlog deletes a blog from the collection by its ID
+	DeleteBlog(ctx context.Context, id primitive.ObjectID) error
+	// BlogExists checks if a blog exists by its ID
+	BlogExists(ctx context.Context, id primitive.ObjectID) (bool, error)
+	// UserIsAuthor checks if a user is the author of a blog by their user ID and the blog ID
+	UserIsAuthor(ctx context.Context, blogID primitive.ObjectID, userID string) (bool, error)
+	// UpdateFeedback retrives blogs by it's id and updates it's feedback
+	UpdateFeedback(ctx context.Context, id string, updateFunc func(*Feedback) error) error
+
 }
--- backend/AAiT-backend-group-4/Repositories/repository.go
+++ backend/AAiT-backend-group-4/Repositories/repository.go
@@ -1,1 +1,279 @@
 package repositories
+
+import (
+	domain "aait-backend-group4/Domain"
+	"fmt"
+	"time"
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	// "golang.org/x/net/context"
+	"context"
+)
+
+// blogRepository implements the domain.BlogRepository interface
+type blogRepository struct {
+  database   mongo.Database
+  collection string
+}
+
+// NewBlogRepository creates a new instance of blogRepository
+func NewBlogRepository(database mongo.Database, collection string) domain.BlogRepository {
+  return &blogRepository{
+    database,
+    collection,
+  }
+}
+
+// Create inserts a new blog into the collection
+func (br *blogRepository) CreateBlog(c context.Context, blog *domain.Blog) error {
+  collection := br.database.Collection(br.collection)
+
+  _, err := collection.InsertOne(c, blog)
+
+  return err
+}
+
+// FetchByBlogID retrieves a blog by its ID
+func (br *blogRepository) FetchByBlogID(c context.Context, blogID string) (domain.Blog, error) {
+  collection := br.database.Collection(br.collection)
+
+  var blog domain.Blog
+
+  idHex, err := primitive.ObjectIDFromHex(blogID)
+  if err != nil {
+    return blog, err
+  }
+
+  err = collection.FindOne(c, bson.M{"_id": idHex}).Decode(&blog)
+  return blog, err
+}
+
+// FetchByBlogAuthor retrieves blogs by the author's ID
+func (br *blogRepository) FetchByBlogAuthor(c context.Context, authorID string) ([]domain.Blog, error) {
+  collection := br.database.Collection(br.collection)
+
+  var blogs []domain.Blog
+
+  cursor, err := collection.Find(c, bson.M{"author_info.author_id": authorID})
+  if err != nil {
+    return nil, err
+  }
+
+  err = cursor.All(c, &blogs)
+  if blogs == nil {
+    return []domain.Blog{}, err
+  }
+
+  return blogs, err
+}
+
+// FetchByBlogTitle retrieves blogs by their title
+func (br *blogRepository) FetchByBlogTitle(c context.Context, title string) ([]domain.Blog, error) {
+  collection := br.database.Collection(br.collection)
+
+  var blogs []domain.Blog
+
+  cursor, err := collection.Find(c, bson.M{"title": bson.M{"$regex": title, "$options": "i"}})
+  if err != nil {
+    return nil, err
+  }
+
+  err = cursor.All(c, &blogs)
+  if blogs == nil {
+    return []domain.Blog{}, err
+  }
+
+  return blogs, err
+}
+
+// FetchAll retrieves all blogs from the collection
+func (br *blogRepository) FetchAll(c context.Context) ([]domain.Blog, error) {
+  collection := br.database.Collection(br.collection)
+
+  var blogs []domain.Blog
+
+  cursor, err := collection.Find(c, bson.M{})
+  if err != nil {
+    return nil, err
+  }
+
+  err = cursor.All(c, &blogs)
+  if blogs == nil {
+    return []domain.Blog{}, err
+  }
+
+  return blogs, err
+}
+
+// UpdateBlog updates a blog in the collection by its ID
+// changes the updated time
+// UpdateBlog updates a blog in the collection by its ID
+// changes the updated time
+func (br *blogRepository) UpdateBlog(c context.Context, id primitive.ObjectID, blog domain.BlogUpdate) error {
+  collection := br.database.Collection(br.collection)
+
+  updateFields := bson.M{}
+
+  if blog.Title != nil {
+    updateFields["title"] = *blog.Title
+  }
+
+  if blog.Content != nil {
+    updateFields["content"] = *blog.Content
+  }
+
+  if blog.Author_Info != nil {
+    updateFields["author_info"] = *blog.Author_Info
+  }
+
+  if blog.Tags != nil {
+    updateFields["tags"] = *blog.Tags
+  }
+
+  if blog.Feedbacks != nil {
+    updateFields["feedbacks"] = *blog.Feedbacks
+  }
+
+  // Add Updated_At field
+  updateFields["updated_at"] = time.Now()
+
+  update := bson.D{{Key: "$set", Value: updateFields}}
+  result, err := collection.UpdateOne(
+    c,
+    bson.D{{Key: "_id", Value: id}},
+    update,
+  )
+
+  if err != nil {
+    return err
+  }
+
+  if result.ModifiedCount == 0 {
+    return fmt.Errorf("blog not found")
+  }
+
+  return nil
+}
+
+// DeleteBlog deletes a blog from the collection by its ID
+func (br *blogRepository) DeleteBlog(c context.Context, id primitive.ObjectID) error {
+  collection := br.database.Collection(br.collection)
+
+  _, err := collection.DeleteOne(c, bson.M{"_id": id})
+  return err
+}
+
+// BlogExists checks if a blog exists by its ID
+func (br *blogRepository) BlogExists(c context.Context, id primitive.ObjectID) (bool, error) {
+  collection := br.database.Collection(br.collection)
+
+  var blog domain.Blog
+  err := collection.FindOne(c, bson.M{"_id": id}).Decode(&blog)
+  if err != nil {
+    if err == mongo.ErrNoDocuments {
+      return false, nil
+    }
+    return false, err
+  }
+
+  return true, nil
+}
+
+// UserIsAuthor checks if a user is the author of a blog by their user ID and the blog ID
+func (br *blogRepository) UserIsAuthor(c context.Context, blogID primitive.ObjectID, userID string) (bool, error) {
+  collection := br.database.Collection(br.collection)
+
+  var blog domain.Blog
+  err := collection.FindOne(c, bson.M{"_id": blogID, "author_info.author_id": userID}).Decode(&blog)
+  if err != nil {
+    if err == mongo.ErrNoDocuments {
+      return false, nil
+    }
+    return false, err
+  }
+
+  return true, nil
+}
+
+// IncreamentViewCount of a blog when a blog is fetched
+
+func (br *blogRepository) UpdateFeedback(c context.Context, id string, updateFunc func(*domain.Feedback) error) error {
+
+    filter := bson.M{"_id": id}
+    var blogPost domain.Blog
+
+    err := br.database.Collection(br.collection).FindOne(context.TODO(), filter).Decode(&blogPost)
+    if err != nil {
+        return err
+    }
+
+    err = updateFunc(&blogPost.Feedbacks)
+    if err != nil {
+        return err
+    }
+
+    update := bson.M{"$set": bson.M{"feedback": blogPost.Feedbacks}}
+    _, err = br.database.Collection(br.collection).UpdateOne(context.TODO(), filter, update)
+
+    return err
+}
+
+
+// adds a comment in to the feedback list of the blog
+func AddComment(feedback *domain.Feedback, comment domain.Comment) error {
+    feedback.Comments = append(feedback.Comments, comment)
+    return nil
+}
+
+// Increments the number of views in the blogs feedback
+func IncrementViewCount(feedback *domain.Feedback) error {
+    feedback.View_count++
+    return nil
+}
+
+// Increments the number of likes in the blogs feedback
+func AddLike(feedback *domain.Feedback) error {
+    feedback.Likes++
+    return nil
+}
+
+// Increments the number of likes in the blogs feedback
+func DecrementLike(feedback *domain.Feedback) error {
+    feedback.Dislikes--
+    return nil
+}
+
+
+func RemoveComments(feedback *domain.Feedback, requesterUserID string, isAdmin bool) error {
+    var newComments []domain.Comment
+    commentFound := false
+
+    for _, comment := range feedback.Comments {
+        
+        if comment.User_ID == requesterUserID || isAdmin {
+
+            if !isAdmin && comment.User_ID != requesterUserID {
+                return errors.New("unauthorized: you can only remove your own comments or you must be an admin")
+            }
+
+            commentFound = true
+            continue
+        }
+        newComments = append(newComments, comment)
+    }
+
+    if !commentFound {
+        return errors.New("no comments found for the requester")
+    }
+
+    feedback.Comments = newComments
+    return nil
+}
+
+
+
+
+

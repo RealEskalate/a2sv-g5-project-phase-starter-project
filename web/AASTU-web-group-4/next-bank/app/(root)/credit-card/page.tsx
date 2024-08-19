52b32d9373919d0bb2e1c1b6288ed546139c2ee1
matiyasjosh <torbaw457@gmail.com>
2024-08-19 14:35:05 +0300

    modified:   web/AASTU-web-group-4/next-bank/app/(root)/credit-card/page.tsx 	modified:   web/AASTU-web-group-4/next-bank/app/(root)/layout.tsx 	modified:   web/AASTU-web-group-4/next-bank/app/layout.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/CardList.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/CardSetting.tsx 	modified:   web/AASTU-web-group-4/next-bank/package-lock.json 	modified:   web/AASTU-web-group-4/next-bank/package.json 	modified:   web/AASTU-web-group-4/next-bank/tailwind.config.ts

--- web/AASTU-web-group-4/next-bank/app/(root)/credit-card/page.tsx
+++ web/AASTU-web-group-4/next-bank/app/(root)/credit-card/page.tsx
@@ -4,14 +4,15 @@ import { colors } from '@/constants'
 import Component from '@/components/DoughnutChart'
 import AddNewCard from '@/components/AddNewCard'
 import CardSetting from '@/components/CardSetting'
+import CardList from '@/components/CardList'
 
 const CreditCard = () => {
   const cards:string[] = [colors.blue, colors.white, colors.blue, colors.white]
   return (
-    <div className='ml-40'>
-      <div className="myCards w-[80%] mx-auto mt-4">
+    <div className='lg:ml-72 ml-5 overflow-x-hidden mx-auto'>
+      <div className="myCards max-w-[97%] mt-4">
         <h1 className='text-[19px] mb-3 font-bold text-[#333B69]'>My Cards</h1>
-        <div className="flex overflow-x-auto space-x-4 pr-3">
+        <div className="flex overflow-x-auto space-x-4 md:pr-3 pr-1 scrollbar-none">
           {cards.map((bg, index) => (
             <span key={index} className='p-3'>
               <ResponsiveCreditCard backgroundColor={bg} />
@@ -20,19 +21,24 @@ const CreditCard = () => {
         </div>
       </div>
       
-      <div className="flex w-[80%] mx-auto">
-        <div className="doughnutChart w-[30%] my-6">
+      <div className="flex flex-col md:flex-row gap-16">
+        <div className="doughnutChart lg:w-[350px] md:w-[231] w-[325px] my-6">
           <h1 className='text-[19px] mb-3 font-bold text-[#333B69]'>Card Expense Statistics</h1>
           <Component />
         </div>
+        <div className="cardlist lg:w-[730px] md:w-[487px] sm-w-[325] my-6">
+          <h1 className='text-[19px] mb-3 font-bold text-[#333B69]'>Card List</h1>
+          <CardList />
+        </div>
       </div>
 
-      <div className="flex flex-col md:flex-row w-[80%] mx-auto mb-16">
-        <div className='mb-2 sm:mb-0 md:mr-10'>
+      <div className="flex flex-col md:flex-row w-[80%] mb-16">
+        <div className='md:mb-2 mb-0 md:mr-10'>
           <AddNewCard/>
         </div>
 
         <div>
+        <h1 className='text-[19px] mb-3 font-bold text-[#333B69]'>Card Setting</h1>
           <CardSetting />
         </div>
       </div>
--- web/AASTU-web-group-4/next-bank/app/layout.tsx
+++ web/AASTU-web-group-4/next-bank/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { colors } from "@/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} ${colors.graybg}`}>
         {children}
         </body>
     </html>

e534f72a85cfefc38316ed1edf77a3465bfce916
Kiya Kebe <112753911+kiyakebe@users.noreply.github.com>
2024-08-19 17:05:35 +0300

    astu.web.g1.kiya.600 astu web g1 kiya signup page (#841)
    * created: signUpFrom
    
    * setup: signup page
    
    * finished: signin form
    
    * styled: signup form
    
    * updated: Added error message to InputGroup
    
    * finished: signup page

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/InputGroup.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/InputGroup.tsx
@@ -6,6 +6,7 @@ const InputGroup = ({
   registerName,
   register,
   placeholder,
+  errorMessage,
 }: InputGroupType) => {
   return (
     <div className="w-full space-y-1 my-3">
@@ -16,9 +17,10 @@ const InputGroup = ({
         type={inputType}
         id={id}
         placeholder={placeholder}
-        // {...register(registerName)}
+        {...register(registerName)}
         className="w-full border-2 border-[#DFEAF2] p-5 py-3 rounded-xl placeholder:text-blue-steel focus:border-blue-steel outline-none"
       />
+      {errorMessage && <p className="text-red-400"> {errorMessage} </p>}
     </div>
   );
 };

8f53aa257ec8d0feb7f2f8bb8658c889dd58a6da
Yonatan Alebachew <yonatanalebachew7@gmail.com>
2024-08-19 18:00:19 +0300

    like and comment crud operations finished

--- backend/AAiT-backend-group-1/delivery/controllers/blogController.go
+++ backend/AAiT-backend-group-1/delivery/controllers/blogController.go
@@ -14,13 +14,11 @@ type blogController struct {
 	blogUsecase domain.BlogUseCase
 }
 
-
 func NewBlogController(bu domain.BlogUseCase) domain.BlogController {
 	return &blogController{
 		blogUsecase: bu,
 	}
 }
-
 
 func (bc *blogController) CreateBlog(c *gin.Context) {
 	var blog domain.Blog
@@ -28,10 +26,15 @@ func (bc *blogController) CreateBlog(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	err := bc.blogUsecase.CreateBlog(&blog)
+	authorID := c.GetString("user_id")
+	authorUsername := c.GetString("username")
+	blog.AuthorUsername = authorUsername
+	err := bc.blogUsecase.CreateBlog(&blog, authorID)
+
 	if err != nil {
 		c.JSON(err.StatusCode(), gin.H{"error": err.Error()})
 	}
+
 	c.JSON(http.StatusOK, gin.H{"data": blog})
 }
 
@@ -76,16 +79,28 @@ func (bc *blogController) DeleteBlog(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Blog deleted successfully"})
 }
 
-func (bc *blogController) SearchBlogs(c *gin.Context) {
+func (bc *blogController) SearchBlogsByTitle(c *gin.Context) {
 	// Implement the logic for searching blogs
 	title := c.Query("title")
-	author := c.Query("author")
-	blogs, err := bc.blogUsecase.SearchBlogs(title , author)
+	blogs, err := bc.blogUsecase.SearchBlogsByTitle(title)
 	if err != nil {
-		c.JSON(err.StatusCode() , err.Error())
+		c.JSON(err.StatusCode(), err.Error())
+		return
 	}
 
-	c.JSON(http.StatusOK , blogs)
+	c.JSON(http.StatusOK, blogs)
+}
+
+func (bc *blogController) SearchBlogsByAuthor(c *gin.Context) {
+	// Implement the logic for searching blogs by author
+	author := c.Query("author")
+	blogs, err := bc.blogUsecase.SearchBlogsByAuthor(author)
+	if err != nil {
+		c.JSON(err.StatusCode(), err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, blogs)
 }
 
 func (bc *blogController) FilterBlogs(c *gin.Context) {
@@ -95,53 +110,51 @@ func (bc *blogController) FilterBlogs(c *gin.Context) {
 		tags = []string{" "}
 	}
 	timeStr := c.Query("time")
-layout := "2006-01-02"
-var timeValue time.Time
-var err error
+	layout := "2006-01-02"
+	var timeValue time.Time
+	var err error
 
-if timeStr == "" {
-    // Set the default time value if no time query parameter is provided
-    timeValue, err = time.Parse(layout, "2021-01-01")
-} else {
-    // Parse the provided time string
-    timeValue, err = time.Parse(layout, timeStr)
-}
-
-if err != nil {
-    log.Println("Invalid time format, using default value:", err)
-    timeValue, _ = time.Parse(layout, "2021-01-01")
-}
-
-
-	popular := c.DefaultQuery("popular", "false")
-	blogs, err := bc.blogUsecase.FilterBlogs(tags , timeValue , popular == "true")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError , err.Error())
+	if timeStr == "" {
+		// Set the default time value if no time query parameter is provided
+		timeValue, err = time.Parse(layout, "2021-01-01")
+	} else {
+		// Parse the provided time string
+		timeValue, err = time.Parse(layout, timeStr)
 	}
 
-	c.JSON(http.StatusOK , blogs)
+	if err != nil {
+		log.Println("Invalid time format, using default value:", err)
+		timeValue, _ = time.Parse(layout, "2021-01-01")
+	}
+
+	popular := c.DefaultQuery("popular", "false")
+	blogs, err := bc.blogUsecase.FilterBlogs(tags, timeValue, popular == "true")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+	}
+	c.JSON(http.StatusOK, blogs)
 }
 
 func (bc *blogController) LikeBlog(c *gin.Context) {
 	var blogID = c.Param("id")
 	userID := c.GetString("user_id")
-	err := bc.blogUsecase.LikeBlog(userID , blogID)
+	err := bc.blogUsecase.LikeBlog(userID, blogID)
 	if err != nil {
-		c.JSON(err.StatusCode() , err.Error())
+		c.JSON(err.StatusCode(), err.Error())
 		return
 	}
-	c.JSON(http.StatusOK , gin.H{"message" : "Blog liked successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Blog liked successfully"})
 }
 
 func (bc *blogController) DislikeBlog(c *gin.Context) {
 	var blogID = c.Param("id")
 	userID := c.GetString("user_id")
-	err := bc.blogUsecase.DisLike(userID , blogID)
+	err := bc.blogUsecase.DisLike(userID, blogID)
 	if err != nil {
-		c.JSON(err.StatusCode() , err.Error())
+		c.JSON(err.StatusCode(), err.Error())
 		return
 	}
-	c.JSON(http.StatusOK , gin.H{"message" : "Blog Disliked successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Blog Disliked successfully"})
 }
 
 func (bc *blogController) AddComment(c *gin.Context) {
@@ -151,9 +164,9 @@ func (bc *blogController) AddComment(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	userId , _ := primitive.ObjectIDFromHex(c.GetString("user_id"))
+	userId, _ := primitive.ObjectIDFromHex(c.GetString("user_id"))
 	comment.AuthorID = userId
-	err := bc.blogUsecase.AddComment(blogID , &comment)
+	err := bc.blogUsecase.AddComment(blogID, &comment)
 	if err != nil {
 		c.JSON(err.StatusCode(), gin.H{"error": err.Error()})
 		return
@@ -166,26 +179,27 @@ func (bc *blogController) DeleteComment(c *gin.Context) {
 	// Implement the logic for deleting a comment
 	var commentID = c.Param("comment_id")
 	var blogID = c.Param("id")
-	err := bc.blogUsecase.DeleteComment(blogID , commentID)
+	err := bc.blogUsecase.DeleteComment(blogID, commentID)
 	if err != nil {
-		c.JSON(err.StatusCode() , err.Error())
+		c.JSON(err.StatusCode(), err.Error())
 		return
 	}
-	c.JSON(http.StatusOK , gin.H{"message" : "Comment deleted successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Comment deleted successfully"})
 }
 
 func (bc *blogController) EditComment(c *gin.Context) {
 	// Implement the logic for editing a comment
 	var comment domain.Comment
 	var commentID = c.Param("comment_id")
+	blogID := c.Param("blog_id")
 	if err := c.ShouldBindJSON(&comment); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
-	} 
-	err := bc.blogUsecase.EditComment(commentID , &comment)
+	}
+	err := bc.blogUsecase.EditComment(blogID, commentID, &comment)
 	if err != nil {
-		c.JSON(err.StatusCode() , err.Error())
+		c.JSON(err.StatusCode(), err.Error())
 		return
 	}
-	c.JSON(http.StatusOK , gin.H{"message" : "Comment edited successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Comment edited successfully"})
 }
--- backend/AAiT-backend-group-1/domain/usecase_interfaces.go
+++ backend/AAiT-backend-group-1/domain/usecase_interfaces.go
@@ -17,18 +17,19 @@ type UserUseCase interface {
 }
 
 type BlogUseCase interface {
-	CreateBlog(blog *Blog) Error
+	CreateBlog(blog *Blog , authorID string) Error
 	GetBlog(blogID string) (*Blog, Error)
-	GetBlogs() ([]*Blog, Error)
+	GetBlogs() ([]Blog, Error)
 	UpdateBlog(blogID string, blog *Blog) Error
 	DeleteBlog(blogID string) Error
-	SearchBlogs(title, author string) ([]*Blog, Error)
-	FilterBlogs(tags []string, dateAfter time.Time, popular bool) ([]*Blog, Error)
+	SearchBlogsByTitle(title string) ([]Blog, Error)
+	SearchBlogsByAuthor(author string) ([]Blog, Error)
+	FilterBlogs(tags []string, dateAfter time.Time, popular bool) ([]Blog, Error)
 	LikeBlog(userID, blogID string) Error
 	
 	AddComment(blogID string, comment *Comment) Error
 	DeleteComment(blogID, commentID string) Error
-	EditComment(commentID string, comment *Comment) Error
+	EditComment(blogID string , commentID string, comment *Comment) Error
 	Like(blogId string , userID string) Error
 	DisLike(blogId string , userID string) Error
 }

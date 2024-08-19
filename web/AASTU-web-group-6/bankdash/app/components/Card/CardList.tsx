21159a72eaf5a6c9b8c44e6ac58f14ddde702425
betselotT <betselottesfa9@gmail.com>
2024-08-19 03:49:49 -0300

    integrated services page from the api

--- web/AASTU-web-group-6/bankdash/app/components/Card/CardList.tsx
+++ web/AASTU-web-group-6/bankdash/app/components/Card/CardList.tsx
@@ -29,23 +29,23 @@ const CardList: React.FC<ListCardProps> = ({
 }) => {
   return (
     <div>
-      <div className="ml-10 mb-5 flex items-center h-24 bg-white gap-24 border rounded-3xl">
+      <div className="mb-5 flex items-center h-24 bg-white lg:gap-24 gap-5 border rounded-3xl">
         <div className={`{icon rounded-full p-3 ${color} ml-4`}>
           <img src={img} alt="" />
         </div>
         <div>
-          <p className="font-medium text-base text-[#232323] -ml-20">{title}</p>
-          <p className="text-[#718EBF] -ml-20 text-[15px]">{desc}</p>
+          <p className="font-medium text-base text-[#232323] lg:-ml-20">{title}</p>
+          <p className="text-[#718EBF] lg:-ml-20 text-[15px]">{desc}</p>
         </div>
         <div>
           <p className="font-medium text-base text-[#232323]">{colOne}</p>
           <p className="text-[#718EBF] text-[15px]">{descOne}</p>
         </div>
-        <div>
+        <div className="hidden lg:block">
           <p className="font-meduim text-base text-[#232323]">{colTwo}</p>
           <p className="text-[#718EBF] text-[15px]">{descTwo}</p>
         </div>
-        <div>
+        <div className="hidden lg:block">
           <p className="font-medium text-base text-[#232323]">{colThree}</p>
           <p className="text-[#718EBF] text-[15px]">{descThree}</p>
         </div>

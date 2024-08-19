d4f73de50368c3003bb4969d02d83d62e540624a
dagmaros27 <dagxx27@gmail.com>
2024-08-19 10:45:17 +0300

    blog repository refactored, removed the context dependency

--- backend/AAiT-backend-group-11/domain/interfaces/blog.go
+++ backend/AAiT-backend-group-11/domain/interfaces/blog.go
@@ -7,28 +7,28 @@ import (
 )
 
 type BlogRepository interface {
-	CreateBlogPost(c context.Context, blogPost *entities.BlogPost) (*entities.BlogPost, error)
-	GetBlogPostById(c context.Context, blogPostId string) (*entities.BlogPost, error)
-	UpdateBlogPost(c context.Context, blogPost *entities.BlogPost) (*entities.BlogPost, error)
-	DeleteBlogPost(c context.Context, blogPostId string) error
-	GetBlogPosts(c context.Context, page, pageSize int, sortBy string) ([]entities.BlogPost,error)
-	SearchBlogPosts(c context.Context, criteria string, tags []string, startDate, endDate time.Time)([]entities.BlogPost, error)
-	FilterBlogPosts(c context.Context, tags []string, dateRange []time.Time, sortBy string) ([]entities.BlogPost, error)
-    LikeBlogPost(c context.Context,blogPostId, userId string) error
-	DislikeBlogPost(c context.Context,blogPostId, userId string) error
-	IncrementViewPost(c context.Context, postID, userID string) error
-	CountBlogPosts(c context.Context) (int, error)
+	CreateBlogPost( blogPost *entities.BlogPost, userId string) (*entities.BlogPost, error)
+	GetBlogPostById( blogPostId string) (*entities.BlogPost, error)
+	UpdateBlogPost( blogPost *entities.BlogPost) (*entities.BlogPost, error)
+	DeleteBlogPost( blogPostId string) error
+	GetBlogPosts( page, pageSize int, sortBy string) ([]entities.BlogPost,error)
+	SearchBlogPosts( criteria string, tags []string, startDate, endDate time.Time)([]entities.BlogPost, error)
+	FilterBlogPosts( tags []string, dateRange []time.Time, sortBy string) ([]entities.BlogPost, error)
+    LikeBlogPost(blogPostId, userId string) error
+	DislikeBlogPost(blogPostId, userId string) error
+	IncrementViewPost( postID, userID string) error
+	CountBlogPosts() (int, error)
 
 }
 
 type BlogService interface {
-	CreateBlogPost(c context.Context, blogPost *entities.BlogPost) (*entities.BlogPost, error)
-	GetBlogPostById(c context.Context, blogPostId string) (*entities.BlogPost, error)
-	UpdateBlogPost(c context.Context, blogPost *entities.BlogPost) (*entities.BlogPost, error)
-	DeleteBlogPost(c context.Context, blogPostId string) error
-	GetBlogPosts(c context.Context, page, pageSize int, sortBy string) ([]entities.BlogPost,int, error)
-	SearchBlogPosts(c context.Context, criteria string, tags []string, startDate, endDate time.Time) ([]entities.BlogPost, error)
-	FilterBlogPosts(c context.Context, tags []string, dateRange []time.Time, sortBy string) ([]entities.BlogPost, error)
+	CreateBlogPost(blogPost *entities.BlogPost, userId string) (*entities.BlogPost, error)
+	GetBlogPostById(blogPostId string) (*entities.BlogPost, error)
+	UpdateBlogPost(blogPost *entities.BlogPost) (*entities.BlogPost, error)
+	DeleteBlogPost(blogPostId string) error
+	GetBlogPosts(page, pageSize int, sortBy string) ([]entities.BlogPost,int, error)
+	SearchBlogPosts(criteria string, tags []string, startDate, endDate time.Time) ([]entities.BlogPost, error)
+	FilterBlogPosts(tags []string, dateRange []time.Time, sortBy string) ([]entities.BlogPost, error)
 }
 
 

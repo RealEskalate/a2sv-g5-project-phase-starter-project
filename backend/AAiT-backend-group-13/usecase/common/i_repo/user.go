3e07a627c472ba93402f69d2de2e344236e93fd5
ephrem <ephremmamo555@gmail.com>
2024-08-19 15:08:22 +0300

    resolve merge conflicts

--- backend/AAiT-backend-group-13/usecase/common/i_repo/user.go
+++ backend/AAiT-backend-group-13/usecase/common/i_repo/user.go
@@ -3,11 +3,16 @@ package irepo
 import (
 	"github.com/google/uuid"
 	usermodel "github.com/group13/blog/domain/models/user"
+
+	ihash "github.com/group13/blog/domain/i_hash"
 )
 
-type User interface {
+type UserRepository interface {
 	Save(user *usermodel.User) error
 	FindById(id uuid.UUID) (*usermodel.User, error)
 	FindByUsername(username string) (*usermodel.User, error)
-	FindByEmail(email string) (*usermodel.User, error)
+	CheckUsernameAvailability(username string) error
+	CheckEmailAvailability(email string) error
+	MatchPassword(password string, hashedPassword string, hash ihash.Service) bool
+	GenerateValidationLink(user usermodel.User) (string, error)
 }

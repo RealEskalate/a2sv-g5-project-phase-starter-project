7d419e561e600e11be390661bdaee8dd7ae9f89e
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-19 11:27:07 +0300

    fix(chat): refactor chat.go

--- backend/ASTU-backend-group-4/chat/chat.go
+++ backend/ASTU-backend-group-4/chat/chat.go
@@ -4,40 +4,37 @@ import (
 	"context"
 	"time"
 
-	"github.com/RealEskalate/astu-backend-g4/backend/ASTU-backend-group-4/pkg/infrastructure"
+	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/pkg/infrastructure"
 )
 
 type Chat struct {
-	ID 		   string	   `json:"id,omitempty" bson:"_id,omitempty" `
-	Title	   string	   `json:"title,omitempty"`
-	History	   []Message   `json:"history,omitempty"`
-	UserID	   string	   `json:"user_id,omitempty"`
-	CreatedAt  time.Time   `json:"created_at,omitempty"`
-	UpdatedAt  time.Time   `json:"updated_at,omitempty"`
+	ID        string    `json:"id,omitempty" bson:"_id,omitempty" `
+	Title     string    `json:"title,omitempty"`
+	History   []Message `json:"history"`
+	UserID    string    `json:"user_id,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
 
-
-type Message struct{
-	Text    string	   `json:"text,omitempty"`
-	Role    string	   `json:"role,omitempty"`	
-	SentAt  time.Time  `json:"sent_at,omitempty"`
+type Message struct {
+	Text   string    `json:"text,omitempty"`
+	Role   string    `json:"role,omitempty"`
+	SentAt time.Time `json:"sent_at,omitempty"`
 }
 
-
-type Usecase interface{
+type Usecase interface {
 	CreateChat(userID, title string) (Chat, error)
 	DeleteChat(chatID string) error
 	GenerateChatTitle(message Message) (string, error)
 	GetChat(chatID string) (Chat, error)
 	GetChats() ([]Chat, error)
-	SendMessage(chatID string, message Message) (error)
+	SendMessage(chatID string, message Message) error
 }
 
-
-type Repository interface{
-	CreateChat(userID, title string, ctx context.Context) (Chat, error)
-	AddMessage(chatID string, message Message, ctx context.Context) (error)
+type Repository interface {
+	CreateChat(chat Chat, ctx context.Context) (Chat, error)
+	AddMessage(chatID string, message Message, ctx context.Context) error
 	GetChat(chatID string, ctx context.Context) (Chat, error)
 	GetChats(ctx context.Context, pagination infrastructure.PaginationRequest) (infrastructure.PaginationResponse[Chat], error)
-	DeleteChat(chatID string, ctx context.Context) (error)
+	DeleteChat(chatID string, ctx context.Context) error
 }

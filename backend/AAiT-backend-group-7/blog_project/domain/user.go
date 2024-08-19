77af4b9f983992b4e4479f9c59cdcb8f150e59ae
Hailemari <hailemariamkefale19@gmail.com>
2024-08-19 15:12:41 +0300

    add repositories

--- backend/AAiT-backend-group-7/blog_project/domain/user.go
+++ backend/AAiT-backend-group-7/blog_project/domain/user.go
@@ -7,15 +7,15 @@ import (
 )
 
 type User struct {
-	ID           int    `json:"id"`
-	Username     string `json:"username"`
-	Password     string `json:"password"`
-	Email        string `json:"email"`
-	Blogs        []int  `json:"blogs"`
-	Role         string `json:"role"`
-	Bio          string `json:"bio"`
-	Phone        string `json:"phone"`
-	ProfilePic   string `json:"profile_pic"`
+	ID         int      `json:"id"`
+	Username   string   `json:"username"`
+	Password   string   `json:"password"`
+	Email      string   `json:"email"`
+	Blogs      []int    `json:"blogs"`
+	Role       string   `json:"role"`
+	Bio        string   `json:"bio"`
+	Phone      string   `json:"phone"`
+	ProfilePic string   `json:"profile_pic"`
 }
 
 type IUserRepository interface {
@@ -26,9 +26,9 @@ type IUserRepository interface {
 	DeleteUser(ctx context.Context, id int) error
 	SearchByUsername(ctx context.Context, username string) (User, error)
 	SearchByEmail(ctx context.Context, email string) (User, error)
-	AddBlog(ctx context.Context, userID int, blogID int) (User, error)
-	RefreshToken(ctx context.Context, username string, token string) (User, error)
-	CreateToken(ctx context.Context, username string, token string) (User, error)
+	AddBlog(ctx context.Context, userID int, blog Blog) (User, error)
+	StoreRefreshToken(ctx context.Context, userID int, refreshToken string) error
+	ValidateRefreshToken(ctx context.Context, userID int, refreshToken string) (bool, error)
 }
 
 type IUserUsecase interface {

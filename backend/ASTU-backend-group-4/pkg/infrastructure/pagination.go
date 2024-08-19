fecf7b3f568a9d094ce10d6b9afa431f1be3531f
Mohammed Ali <mohaali482@gmail.com>
2024-08-19 14:55:42 +0300

    feat: add a constructor for the pagination struct

--- backend/ASTU-backend-group-4/pkg/infrastructure/pagination.go
+++ backend/ASTU-backend-group-4/pkg/infrastructure/pagination.go
@@ -12,3 +12,18 @@ type PaginationResponse[T interface{}] struct {
 	TotalPages int   `json:"total_pages,omitempty"`
 	Items      []T   `json:"items,omitempty"`
 }
+
+func NewPaginationResponse[T interface{}](limit, page int, count int64, items []T) PaginationResponse[T] {
+	totalPages := int(count) / limit
+	if int(count)%limit != 0 {
+		totalPages++
+	}
+
+	return PaginationResponse[T]{
+		Limit:      limit,
+		Page:       page,
+		Count:      count,
+		TotalPages: totalPages,
+		Items:      items,
+	}
+}

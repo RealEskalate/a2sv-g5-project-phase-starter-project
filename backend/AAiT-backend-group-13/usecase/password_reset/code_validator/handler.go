57df20424a7f83b62ae9d0b4b645ad3a0900117b
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-19 14:05:25 +0300

    docs: add godoc for usecase/password_reset/code_validator/handler.go

--- backend/AAiT-backend-group-13/usecase/password_reset/code_validator/handler.go
+++ backend/AAiT-backend-group-13/usecase/password_reset/code_validator/handler.go
@@ -9,19 +9,25 @@ import (
 	irepo "github.com/group13/blog/usecase/common/i_repo"
 )
 
+// Handler handles the validation of password reset codes.
 type Handler struct {
 	userrepo   irepo.User
 	jwtService ijwt.Service
 }
 
+// Ensure Handler implements the icmd.IHandler interface.
 var _ icmd.IHandler[Command, string] = &Handler{}
 
+// New creates a new instance of Handler.
 func New(userrepo irepo.User, jwtService ijwt.Service) *Handler {
-	return &Handler{userrepo: userrepo,
+	return &Handler{
+		userrepo:   userrepo,
 		jwtService: jwtService,
 	}
 }
 
+// Handle validates the reset code from the Command and returns a JWT token if successful.
+// It checks if the code is valid, unexpired, and associated with the correct user.
 func (h *Handler) Handle(cmd Command) (string, error) {
 	user, err := h.userrepo.FindById(cmd.Id)
 	if err != nil {
@@ -35,7 +41,7 @@ func (h *Handler) Handle(cmd Command) (string, error) {
 
 	exprTime := user.ResetCode().Expr
 	if exprTime.After(time.Now()) {
-		return "", er.NewUnauthorized("time expierd")
+		return "", er.NewUnauthorized("time expired")
 	}
 
 	if err = user.UpdateResetCode(nil); err != nil {

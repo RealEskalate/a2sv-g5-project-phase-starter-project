59e641fd6afa60039647a83720a378a76218d77e
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-19 14:19:11 +0300

    feat(chat): add the GetChats method for chatRepository implementation

--- backend/ASTU-backend-group-4/chat/mongodb/chat_storage.go
+++ backend/ASTU-backend-group-4/chat/mongodb/chat_storage.go
@@ -3,11 +3,13 @@ package mongodb
 import (
 	"context"
 	"errors"
+	"math"
 
 	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/chat"
-	// "github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/pkg/infrastructure"
+	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/pkg/infrastructure"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const collectionName = "chats"
@@ -57,9 +59,44 @@ func (chatRepository *ChatRepository) GetChat(chatID string, ctx context.Context
 	return chat, nil
 }
 
-// func(chatRepository *ChatRepository) GetChats(ctx context.Context, pagination infrastructure.PaginationRequest) (infrastructure.PaginationResponse[chat.Chat], error){
+func (chatRepository *ChatRepository) GetChats(ctx context.Context, pagination infrastructure.PaginationRequest) (infrastructure.PaginationResponse[chat.Chat], error) {
+	collection := chatRepository.Database.Collection(collectionName)
+	findOptions := options.Find()
+	findOptions.SetSkip(int64(pagination.Page - 1))
+	findOptions.SetLimit(int64(pagination.Limit))
 
-// }
+	count, err := collection.CountDocuments(ctx, bson.M{})
+	if err != nil {
+		return infrastructure.PaginationResponse[chat.Chat]{}, err
+	}
+
+	totalPages := int(math.Ceil(float64(count) / float64(int64(pagination.Limit))))
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return infrastructure.PaginationResponse[chat.Chat]{}, err
+	}
+
+	var items []chat.Chat
+	for cursor.Next(ctx) {
+		var item chat.Chat
+		err := cursor.Decode(&item)
+		if err != nil {
+			return infrastructure.PaginationResponse[chat.Chat]{}, err
+		}
+
+		items = append(items, item)
+	}
+
+	paginationReponse := infrastructure.PaginationResponse[chat.Chat]{
+		Limit:      pagination.Limit,
+		Page:       pagination.Page,
+		Count:      count,
+		TotalPages: totalPages,
+		Items:      items,
+	}
+
+	return paginationReponse, nil
+}
 
 func (chatRepository *ChatRepository) DeleteChat(chatID string, ctx context.Context) error {
 	collection := chatRepository.Database.Collection(collectionName)
@@ -70,7 +107,7 @@ func (chatRepository *ChatRepository) DeleteChat(chatID string, ctx context.Cont
 		return err
 	}
 
-	if deleteResult.DeletedCount == 0{
+	if deleteResult.DeletedCount == 0 {
 		return errors.New("chat not found")
 	}
 

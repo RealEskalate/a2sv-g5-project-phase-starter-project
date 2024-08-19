17b06f4d8989edcaee739d7405af46a60dea92b1
SamrawitDawit <samrikdawit@gmail.com>
2024-08-19 18:04:05 +0300

    Added middleware

--- backend/AAiT-backend-group-10/infrastructures/jwt.go
+++ backend/AAiT-backend-group-10/infrastructures/jwt.go
@@ -1,1 +1,46 @@
 package infrastructures
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"aait.backend.g10/domain"
+	"github.com/dgrijalva/jwt-go"
+)
+
+
+func GenerateToken(user domain.User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, &jwt.StandardClaims{
+		ExpiresAt: time.Now().Add(24 * time.Hour).Unix(),
+	})
+	token.Claims = jwt.MapClaims{
+		"id":       user.ID,
+		"email":    user.Email,
+		"is_admin": user.IsAdmin,
+	}
+
+	jwt_token, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return "", err
+	}
+	return jwt_token, nil
+}
+
+func CheckToken(authPart string) (*jwt.Token, error) {
+	token, err := jwt.Parse(authPart, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func FindClaim(token *jwt.Token) (jwt.MapClaims, bool) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	return claims, ok
+}
--- backend/AAiT-backend-group-10/infrastructures/middlware.go
+++ backend/AAiT-backend-group-10/infrastructures/middlware.go
@@ -1,1 +1,67 @@
 package infrastructures
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		defer context.Next()
+
+		authHeader := context.GetHeader("Authorization")
+
+		if authHeader == "" {
+			context.JSON(401, gin.H{"Error": "Authoriztion header is required"})
+			context.Abort()
+			return
+		}
+
+		authPart := strings.Split(authHeader, "")
+
+		if len(authPart) != 2 || strings.ToLower(authPart[0]) != "bearer" {
+			context.JSON(401, gin.H{"message": "Invalid Authoriztion header"})
+			context.Abort()
+			return
+		}
+
+		token, err := CheckToken(authPart[1])
+
+		if token == nil || !token.Valid {
+			errMsg := "Invalid or expired token"
+
+			if err != nil {
+				errMsg = err.Error()
+			}
+			context.JSON(401, gin.H{"error": errMsg})
+			context.Abort()
+			return
+		}
+
+		claims, ok := FindClaim(token)
+		if !ok {
+			context.JSON(401, gin.H{"error": "Invalid token claims"})
+			context.Abort()
+			return
+		}
+		role := claims["is_admin"]
+		id := claims["id"]
+
+		context.Set("is_admin", role)
+		context.Set("id", id)
+
+	}
+}
+
+func AdminMiddleWare() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		defer context.Next()
+		is_admin, exists := context.Get("is_admin")
+		if !exists || is_admin != true {
+			context.JSON(403, gin.H{"message": "Sorry, you are not eligible for this"})
+			context.Abort()
+			return
+		}
+	}
+}

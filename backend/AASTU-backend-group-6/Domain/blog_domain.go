ab9a3e1f58d5207d1c9d45efb9d607569b450338
Bz14 <bezueyerusalem@gmail.com>
2024-08-20 13:39:50 +0300

    Popularity tracking endpoint implemented.

--- backend/AASTU-backend-group-6/Domain/blog_domain.go
+++ backend/AASTU-backend-group-6/Domain/blog_domain.go
@@ -15,7 +15,11 @@ type Blog struct {
 	Creater_id primitive.ObjectID `bson:"creater_id"`
 	CreatedAt  time.Time          `bson:"createdAt"`
 	UpdatedAt  time.Time          `bson:"updatedAt"`
+	ViewCount  int64			  `bson:"view_count"`
 	Comments   []Comment          `bson:"comments"`
+	Likes      []primitive.ObjectID `bson:"likes"`
+	DisLikes   []primitive.ObjectID `bson:"dislikes"`
+	Popularity int64			  `bson:"popularity"`
 	Blog_image string             `bson:"blog_image"`
 }
 
@@ -29,11 +33,12 @@ type Comment struct {
 
 type BlogUsecase interface {
 	CreateBlog(user_id string, blog Blog, role string) (Blog, error)
-	GetBlogByID(blog_id string) (Blog, error)
+	GetBlogByID(blog_id string, isCalled bool) (Blog, error)
 	GetBlogs(pageNo string, pageSize string, popularity string) ([]Blog, Pagination, error)
 	UpdateBlogByID(user_id string, blog_id string, blog Blog, role string) (Blog, error)
 	DeleteBlogByID(user_id string, blog_id string, role string) ErrorResponse
 	CommentOnBlog(user_id string, user_name string, comment Comment) error
+	ReactOnBlog(user_id string, reactionType string, blog_id string) ErrorResponse
 
 	SearchBlogByTitleAndAuthor(title string, author string, pageNo string, pageSize string, popularity string) ([]Blog, Pagination, ErrorResponse)
 	FilterBlogsByTag(tags []string, pageNo string, pageSize string, startDate string, endDate string, popularity string) ([]Blog, Pagination, error)
@@ -43,11 +48,12 @@ type BlogUsecase interface {
 }
 type BlogRepository interface {
 	CreateBlog(user_id string, blog Blog, role string) (Blog, error)
-	GetBlogByID(blog_id string) (Blog, error)
+	GetBlogByID(blog_id string, isCalled bool) (Blog, error)
 	GetBlogs(pageNo int64, pageSize int64, popularity string) ([]Blog, Pagination, error)
 	UpdateBlogByID(user_id string, blog_id string, blog Blog) (Blog, error)
 	DeleteBlogByID(user_id string, blog_id string) ErrorResponse
 	CommentOnBlog(user_id string, user_name string, comment Comment) error
+	ReactOnBlog(user_id string, reactionType bool, blog_id string) ErrorResponse
 
 	SearchBlogByTitleAndAuthor(title string, author string, pageNo int64, pageSize int64, popularity string) ([]Blog, Pagination, error)
 	FilterBlogsByTag(tags []string, pageNo int64, pageSize int64, startDate time.Time, endDate time.Time, popularity string) ([]Blog, Pagination, error)

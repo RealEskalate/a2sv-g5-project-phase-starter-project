bbd5db8eb94e25b3be98979572f6fc0926919886
amha-mersha <amhamersh@gmail.com>
2024-08-20 12:51:11 +0300

    added reset password to usercase

--- backend/AAiT-backend-group-1/domain/usecase_interfaces.go
+++ backend/AAiT-backend-group-1/domain/usecase_interfaces.go
@@ -6,9 +6,11 @@ import (
 )
 
 type UserUseCase interface {
-	Register(context context.Context, user *User) Error
+	RegisterStart(context context.Context, user *User) Error
+	RegisterEnd(context context.Context, token string) Error
 	Login(context context.Context, username, password string) (map[string]string, Error)
 	ForgotPassword(context context.Context, email string) Error
+	ResetPassword(context context.Context, token string)
 	Logout(context context.Context, token map[string]string) Error
 	PromoteUser(context context.Context, userID string) Error
 	DemoteUser(context context.Context, userID string) Error
@@ -19,7 +21,7 @@ type BlogUseCase interface {
 <<<<<<< HEAD
 	CreateBlog(blog *Blog, authorID string) Error
 =======
-	CreateBlog(blog *Blog , authorID string) Error
+	CreateBlog(blog *Blog, authorID string) Error
 >>>>>>> d260d430 (like and comment crud operations finished)
 	GetBlog(blogID string) (*Blog, Error)
 	GetBlogs() ([]Blog, Error)
@@ -29,21 +31,12 @@ type BlogUseCase interface {
 	SearchBlogsByAuthor(author string) ([]Blog, Error)
 	FilterBlogs(tags []string, dateAfter time.Time, popular bool) ([]Blog, Error)
 	LikeBlog(userID, blogID string) Error
-<<<<<<< HEAD
-
-	AddComment(blogID string, comment *Comment) Error
-	DeleteComment(blogID, commentID string) Error
-	EditComment(blogID string, commentID string, comment *Comment) Error
-	Like(blogId string, userID string) Error
-	DisLike(blogId string, userID string) Error
-=======
 	
 	AddComment(blogID string, comment *Comment) Error
 	DeleteComment(blogID, commentID string) Error
 	EditComment(blogID string , commentID string, comment *Comment) Error
 	Like(blogId string , userID string) Error
 	DisLike(blogId string , userID string) Error
->>>>>>> a142a8e6 (blog controller added)
 }
 
 type BlogAssistantUseCase interface {

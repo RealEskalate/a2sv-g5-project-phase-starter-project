2d4fa6c0ec89e3e955fd7128278f3c0577e5852c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-20 14:41:18 +0300

    build: tie everything together in main.go

--- backend/AAiT-backend-group-13/usecase/user/command/signup/signup_handler.go
+++ backend/AAiT-backend-group-13/usecase/user/command/signup/signup_handler.go
@@ -6,16 +6,16 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"io"
-	"time"
-	usermodel "github.com/group13/blog/domain/models/user"
 	er "github.com/group13/blog/domain/errors"
 	ihash "github.com/group13/blog/domain/i_hash"
-	result "github.com/group13/blog/usecase/user/result"
+	usermodel "github.com/group13/blog/domain/models/user"
 	icmd "github.com/group13/blog/usecase/common/cqrs/command"
 	iemail "github.com/group13/blog/usecase/common/i_email"
 	ijwt "github.com/group13/blog/usecase/common/i_jwt"
 	irepo "github.com/group13/blog/usecase/common/i_repo"
+	result "github.com/group13/blog/usecase/user/result"
+	"io"
+	"time"
 )
 
 type SignUpHandler struct {
@@ -27,23 +27,20 @@ type SignUpHandler struct {
 
 type SignUpConfig struct {
 	UserRepo     irepo.UserRepository
-	jwtService   ijwt.Service
+	JwtService   ijwt.Service
 	HashService  ihash.Service
-	emailService iemail.Service
+	EmailService iemail.Service
 }
-
 
 // Ensure Handler implements icmd.IHandler
 var _ icmd.IHandler[*SignUpCommand, *result.SignUpResult] = &SignUpHandler{}
 
-
-
 func NewSignUpHandler(config SignUpConfig) *SignUpHandler {
 	return &SignUpHandler{
 		repo:         config.UserRepo,
-		jwtService:   config.jwtService,
+		jwtService:   config.JwtService,
 		hashService:  config.HashService,
-		emailService: config.emailService,
+		emailService: config.EmailService,
 	}
 }
 
@@ -55,11 +52,8 @@ func (h *SignUpHandler) Handle(command *SignUpCommand) (*result.SignUpResult, er
 		PlainPassword:  command.password,
 		FirstName:      command.firstName,
 		LastName:       command.lastName,
-		IsAdmin:        false ,
+		IsAdmin:        false,
 		PasswordHasher: h.hashService,
-
-		
-
 	}
 
 	user, err := usermodel.New(*cfg)
@@ -113,42 +107,40 @@ func (h *SignUpHandler) Handle(command *SignUpCommand) (*result.SignUpResult, er
 	}, nil
 }
 
-
 func (h *SignUpHandler) GenerateValidationLink(user usermodel.User) (string, error) {
 	userID := user.ID().String()
 	expiryDay := time.Now().Add(time.Hour * 24).Format(time.RFC3339)
 	username := user.Username()
-    // Concatenate the values
-    value := userID + "|" + expiryDay + "|" + username
+	// Concatenate the values
+	value := userID + "|" + expiryDay + "|" + username
 
-    // Encrypt the concatenated value
-    encryptedValue, err := h.encrypt(value)
-    if err != nil {
-        return "", er.NewUnexpected("failed to encrypt value")
-    }
+	// Encrypt the concatenated value
+	encryptedValue, err := h.encrypt(value)
+	if err != nil {
+		return "", er.NewUnexpected("failed to encrypt value")
+	}
 
-    validationLink := fmt.Sprintf("https://localhost:8080/validate?=%s", encryptedValue)
+	validationLink := fmt.Sprintf("https://localhost:8080/validate?=%s", encryptedValue)
 	return validationLink, nil
 }
 
 func (h *SignUpHandler) encrypt(plainText string) (string, error) {
-    key := []byte("thisis32bitlongpassphraseimusing!") // 32 bytes key for AES-256
+	key := []byte("thisis32bitlongpassphraseimusing!") // 32 bytes key for AES-256
 
-    block, err := aes.NewCipher(key)
-    if err != nil {
-        return "", err
-    }
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
 
-    ciphertext := make([]byte, aes.BlockSize+len(plainText))
-    iv := ciphertext[:aes.BlockSize]
+	ciphertext := make([]byte, aes.BlockSize+len(plainText))
+	iv := ciphertext[:aes.BlockSize]
 
-    if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-        return "", err
-    }
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
 
-    stream := cipher.NewCFBEncrypter(block, iv)
-    stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plainText))
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], []byte(plainText))
 
-    return hex.EncodeToString(ciphertext), nil
+	return hex.EncodeToString(ciphertext), nil
 }
-

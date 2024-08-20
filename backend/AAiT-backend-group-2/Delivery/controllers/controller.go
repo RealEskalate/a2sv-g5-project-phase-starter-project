b14a66d62d55596e4a43ad74e9cf8f91bb36e1bd
mentesnot-2 <sibhatmentesnot@gmail.com>
2024-08-20 10:05:17 +0300

    finished crud

--- backend/AAiT-backend-group-2/Delivery/controllers/controller.go
+++ backend/AAiT-backend-group-2/Delivery/controllers/controller.go
@@ -1,1 +1,126 @@
 package controllers
+
+import (
+	"AAiT-backend-group-2/Domain"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+type BlogController struct {
+	blogUseCase domain.BlogUseCase
+}
+
+func NewBlogController(bu domain.BlogUseCase) *BlogController {
+	return &BlogController{
+		blogUseCase: bu,
+	}
+}
+
+func (bc *BlogController) CreateBlog(c *gin.Context) {
+	var req domain.RequestBlog
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+	author := c.GetString("user_id")
+	if author == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+	err := bc.blogUseCase.CreateBlog(c.Request.Context(), &req, "author")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"message": "Blog created successfully"})
+}
+
+func (bc *BlogController) GetAllBlogs(c *gin.Context) {
+
+	pageStr := c.DefaultQuery("page", "1")
+	pageSizeStr := c.DefaultQuery("pageSize", "10")
+	sortBy := c.DefaultQuery("sortBy", "created_at")
+	sortOrder := c.DefaultQuery("sortOrder", "desc")
+
+	page, err := strconv.Atoi(pageStr)
+	if err != nil || page < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid page number"})
+		return
+	}
+
+	pageSize, err := strconv.Atoi(pageSizeStr)
+	if err != nil || pageSize < 1 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid page size"})
+		return
+	}
+
+	blogs, total, err := bc.blogUseCase.GetAllBlogs(c.Request.Context(), page, pageSize, sortBy, sortOrder)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data":      blogs,
+		"total":     total,
+		"page":      page,
+		"pageSize":  pageSize,
+		"sortBy":    sortBy,
+		"sortOrder": sortOrder,
+	})
+}
+
+func (bc *BlogController) GetBlogByID(c *gin.Context) {
+	id := c.Param("id")
+	blog, err := bc.blogUseCase.GetBlogByID(c.Request.Context(), id)
+	if err != nil {
+		if err.Error() == "blog not found" {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Blog not found"})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		}
+		return
+	}
+	c.JSON(http.StatusOK, blog)
+}
+
+func (bc *BlogController) UpdateBlog(c *gin.Context) {
+	id := c.Param("id")
+	author := c.GetString("user_id")
+
+	if author == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized",
+		})
+	}
+	var req domain.RequestBlog
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+	err := bc.blogUseCase.UpdateBlog(c.Request.Context(), &req, "author", id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Blog updated successfully"})
+}
+
+func (bc *BlogController) DeleteBlog(c *gin.Context) {
+	id := c.Param("id")
+	author := c.GetString("user_id")
+	role := c.GetString("role")
+
+	if author == "" && (role == "" || role != "admin") {
+		c.JSON(http.StatusUnauthorized, gin.H{
+			"error": "Unauthorized",
+		})
+	}
+	err := bc.blogUseCase.DeleteBlog(c.Request.Context(), author, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Blog deleted successfully"})
+}
--- backend/AAiT-backend-group-2/Delivery/main.go
+++ backend/AAiT-backend-group-2/Delivery/main.go
@@ -1,1 +1,41 @@
 package main
+
+import (
+	"AAiT-backend-group-2/Delivery/controllers"
+	"AAiT-backend-group-2/Delivery/routers"
+	"AAiT-backend-group-2/Infrastructure"
+	"AAiT-backend-group-2/Repositories"
+	"AAiT-backend-group-2/Usecases"
+	"context"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	
+	mongoConfig := infrastructure.NewMongoDBConfig(os.Getenv("MONGO_URI"), os.Getenv("MONGO_DB"))
+	mongoClient, err := mongoConfig.Connect()
+	
+	if err != nil {
+		log.Fatal("Failed to connect to MongoDB")
+	}
+	defer mongoClient.Disconnect(context.TODO())
+
+	db := mongoClient.Database(os.Getenv("MONGO_DB"))
+	blogRepo := repositories.NewBlogRepository(db)
+	blogUseCase := usecases.NewBlogUsecase(blogRepo)
+	blogController := controllers.NewBlogController(blogUseCase)
+
+	router := routers.SetupRouter(blogController)
+
+	router.Run(":8080")
+
+}

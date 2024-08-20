9075d1bd543dbc3f9c75fa890c82bb27a20d9f2e
SamrawitDawit <samrikdawit@gmail.com>
2024-08-20 15:27:18 +0300

    Modified the Update comment end point

--- backend/AAiT-backend-group-10/usecases/interfaces/comment_repo_interface.go
+++ backend/AAiT-backend-group-10/usecases/interfaces/comment_repo_interface.go
@@ -10,6 +10,6 @@ type CommentRepositoryInterface interface {
 	GetComments(blogID uuid.UUID) ([]domain.Comment, error)
 	GetCommentsCount(blogID uuid.UUID) (int, error)
 	AddComment(comment domain.Comment) error
-	UpdateComment(commentID uuid.UUID, updatedComment domain.Comment) error
+	UpdateComment(updatedComment domain.Comment) error
 	DelelteComment(commentID uuid.UUID) error
 }

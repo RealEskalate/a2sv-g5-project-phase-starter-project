f2aab728619dfa7792ef4afc623da1adf57c2d51
bisratberhanu <bisratbnegus@gmail.com>
2024-08-20 07:30:21 +0300

    token manager implement

--- backend/AAiT-backend-group-9/Blog_Starter/usecase/login_usecase.go
+++ backend/AAiT-backend-group-9/Blog_Starter/usecase/login_usecase.go
@@ -2,28 +2,63 @@ package usecase
 
 import (
 	"Blog_Starter/domain"
+	"Blog_Starter/utils"
 	"context"
+	"fmt"
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginUseCase struct {
 	LoginRepository domain.LoginRepository
+	UserRepository domain.UserRepository
+	TokenManager utils.TokenManager
 	ContextTimeout  time.Duration
 }
 
 
-func NewLoginUseCase(loginRepository domain.LoginRepository, timeout time.Duration) domain.LoginUsecase {
+func NewLoginUseCase(loginRepository domain.LoginRepository, userRepository domain.UserRepository,tokenManager utils.TokenManager ,timeout time.Duration) domain.LoginUsecase {
 	return &LoginUseCase{
 		LoginRepository: loginRepository,
+		UserRepository: userRepository,
+		TokenManager: tokenManager,
 		ContextTimeout:  timeout,
 	}
 }
 
 // Login implements domain.LoginUsecase.
-func (l *LoginUseCase) Login(c context.Context, user *domain.UserLogin) (*domain.LoginResponse, error) {
+func (l *LoginUseCase) Login(c context.Context, req *domain.UserLogin) (*domain.LoginResponse, error) {
 	ctx, cancel:= context.WithTimeout(c, l.ContextTimeout)
 	defer cancel()
-	return l.LoginRepository.Login(ctx, user)
+	user,err:= l.UserRepository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return nil, err
+	}
+	hashedPassword,err :=  bcrypt.GenerateFromPassword([]byte (req.Password),bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	if user.Password!= string(hashedPassword){
+		return nil, fmt.Errorf("password incorrect")
+	}
+	accessToken,err:= l.TokenManager.CreateAccessToken(user,"secret", 1)
+	if err!=nil{
+		return nil, err
+	}
+	refreshToken,err := l.TokenManager.CreateRefreshToken(user, "secret", int(24))
+	if err!=nil{
+		return nil, err
+	}
+	
+	_, err:= l.UserRepository.UpdateToken(ctx,accessToken, refreshToken, user.UserID.String())
+	if err!=nil{
+		return nil, err
+	}
+
+	return l.LoginRepository.Login(ctx, req)	
+
+
 }
 
 // UpdatePassword implements domain.LoginUsecase.

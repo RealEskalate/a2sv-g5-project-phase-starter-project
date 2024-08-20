cb09cb6c963de5314126f713749108b3bb8b8e58
ermi9s <ermiayele1122334477@gmail.com>
2024-08-20 11:24:25 +0300

    add database connection config

--- backend/AASTU-backend-group-5/delivery/routes/main.route.go
+++ backend/AASTU-backend-group-5/delivery/routes/main.route.go
@@ -1,8 +1,21 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/RealEskalate/blogpost/config"
+	"github.com/RealEskalate/blogpost/database"
+	"github.com/gin-gonic/gin"
+)
 
-func SetUp(router *gin.Engine){
+func SetUp(router *gin.Engine) {
+	var clinect config.ServerConnection
+	clinect.Connect_could()
+
+	userCollection := &database.MongoCollection{
+		Collection: clinect.Client.Database("blogPost").Collection("Users"),
+	}
 	aiRoute := router.Group("")
+	userRoute := router.Group("")
+
 	NewAiRequestRoute(aiRoute)
+	NewUserRoute(userRoute, userCollection)
 }

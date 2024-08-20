5e092b450ab510a8852d315f544e59c7d1cac639
Natnael Meseret <natnaelmeseret5@gmail.com>
2024-08-20 23:39:30 +0300

    Fix: nav responsiveness for small device and dynamic title on nav

--- web/AASTU-web-group-6/bankdash/app/components/Layout/NavBar.tsx
+++ web/AASTU-web-group-6/bankdash/app/components/Layout/NavBar.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -8,18 +7,38 @@ import {
   faSun,
 } from "@fortawesome/free-solid-svg-icons";
 import { useAppDispatch, useAppSelector } from "@/app/Redux/store/store";
-import { setDarkMode, toggleDarkMode } from "@/app/Redux/slices/darkModeSlice";
+import { toggleDarkMode } from "@/app/Redux/slices/darkModeSlice";
+import { usePathname } from "next/navigation"; // Import usePathname from Next.js
+import Image from "next/image";
 
 const NavBar = ({ openSidebar }: { openSidebar: () => void }) => {
   const isDarkMode = useAppSelector((state) => state.darkMode.darkMode);
   const dispatch = useAppDispatch();
+  const pathname = usePathname(); // Get the current route path
 
   const onDarkMode = () => {
     dispatch(toggleDarkMode());
   };
 
+  // Map routes to titles
+  const pageTitles: { [key: string]: string } = {
+    "/": "Overview",
+    "/transaction": "Transactions",
+    "/account": "Accounts",
+    "/investment": "Investments",
+    "/credit-cards": "Credit Cards",
+    "/loan": "Loans",
+    "/service": "Services",
+    "/privilages": "My Privileges",
+    "/settings/editprofile": "Settings",
+    "/login": "LogOut",
+  };
+
+  // Fallback to "Overview" if the route doesn't match
+  const currentPageTitle = pageTitles[pathname] || "Overview";
+
   return (
-    <div className="w-full fixed left-0 z-10 flex flex-row justify-center items-center bg-white dark:bg-[#232328] xs:px-[3%] sm:px-[4%] sm:gap-[6%] lg:pl-[240px] pr-[3%] py-4">
+    <div className="w-full fixed left-0 z-10 flex flex-row justify-center items-center bg-white dark:bg-[#232328] xxs:px-[3%] sm:px-[4%] sm:gap-[6%] lg:pl-[240px] pr-[3%] py-4">
       <button
         onClick={openSidebar}
         className="bg-[#F5F7FA] dark:bg-gray-600 rounded-[12px] p-3 py-2 flex items-center hover:bg-[#d0e6f6] dark:hover:bg-gray-600 lg:hidden"
@@ -29,14 +48,14 @@ const NavBar = ({ openSidebar }: { openSidebar: () => void }) => {
           className="text-2xl text-gray-700 dark:text-gray-300"
         />
       </button>
-      <h1 className="text-3xl font-semibold text-[#343C6A] dark:text-white xs:hidden sm:hidden lg:block">
-        Overview
+      <h1 className="xxs:text-2xl md:text-3xl font-semibold text-[#343C6A] dark:text-white xxs:flex xxs:grow xxs:justify-center md:justify-start">
+        {currentPageTitle}
       </h1>
-      <div className="flex justify-end gap-5 grow ">
-        <div className="relative flex gap-2 items-center text-base text-[#8BA3CB] dark:text-gray-400 xs:grow xs:fixed xs:top-24 xs:left-2 xs:w-[90%] sm:grow-0 sm:relative sm:top-0 sm:w-fit">
+      <div className="flex justify-end gap-5 md:grow ">
+        <div className="search-box relative flex gap-2 items-center text-base text-[#8BA3CB] dark:text-gray-400 xxs:bg-white xxs:dark:bg-[#232328] xxs:fixed xxs:top-16 xxs:w-full xxs:left-0 xxs:px-4 xxs:py-2 xxs:pb-4 xxs:shadow-sm xxs:grow sm:px-[4%] md:static md:top-0 md:left-0 md:shadow-none md:bg-transparent md:p-0">
           <FontAwesomeIcon
             icon={faSearch}
-            className="absolute left-5 text-xl"
+            className="relative xxs:left-10 md:left-12 text-xl"
           />
           <input
             className="w-full bg-[#F5F7FA] dark:bg-gray-700 py-2 pl-12 pr-6 rounded-3xl border-solid border-2 border-blue-50 dark:border-gray-600 placeholder:text-base placeholder:text-[#8BA3CB] dark:placeholder:text-gray-400 focus:outline-none focus:border-blue-200 dark:focus:border-gray-500"
@@ -49,7 +68,7 @@ const NavBar = ({ openSidebar }: { openSidebar: () => void }) => {
 
         <button
           onClick={onDarkMode}
-          className="flex items-center bg-[#F5F7FA] dark:bg-gray-700 rounded-full p-3 hover:bg-gray-200 dark:hover:bg-gray-600 xs:w-[50px]"
+          className="flex items-center bg-[#F5F7FA] dark:bg-gray-700 rounded-full p-3 hover:bg-gray-200 dark:hover:bg-gray-600 xxs:hidden md:flex"
         >
           <FontAwesomeIcon
             icon={isDarkMode ? faSun : faMoon}
@@ -57,7 +76,7 @@ const NavBar = ({ openSidebar }: { openSidebar: () => void }) => {
           />
         </button>
 
-        <button className="bg-[#F5F7FA] dark:bg-gray-700 rounded-full p-3 xs:hidden md:flex">
+        <button className="bg-[#F5F7FA] dark:bg-gray-700 rounded-full p-3 xxs:hidden md:flex">
           <Image
             src="/assets/bell-icon.svg"
             alt="notification"

fcc826be99d606f6ac515bf6888f0661ae6de4c4
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-20 14:41:18 +0300

    build: tie everything together in main.go

--- backend/AAiT-backend-group-13/infrastructure/repo/reaction/reaction.go
+++ backend/AAiT-backend-group-13/infrastructure/repo/reaction/reaction.go
@@ -92,21 +92,6 @@ func (r Repo) FindReactionByBlogId(blogId uuid.UUID) (*[]reaction.Reaction, erro
 	return &reactions, nil
 }
 
-func (r *Repository) FindReactionByUserIdAndBlogId(userId, blogId string) (*reaction.Reaction, error) {
-	var reaction reaction.Reaction
-
-	filter := bson.M{
-		"userId": userId,
-		"blogId": blogId,
-	}
-
-	err := r.collection.FindOne(context.Background(), filter).Decode(&reaction)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return nil, nil // No reaction found
-		}
-		return nil, err // Other error occurred
-	}
-
-	return &reaction, nil
+func (r Repo) FindReactionByUserIdAndBlogId(userId uuid.UUID, blogId uuid.UUID) (*reaction.Reaction, error) {
+	return nil, nil
 }

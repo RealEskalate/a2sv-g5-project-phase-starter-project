de61b50d53f1a0be6eea8d51b377825f4c77784b
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-20 05:52:51 -0700

    acces control implemented only users who created a post can delte or update it

--- backend/ASTU-backend-group-5/internal/ai/moderate_blog.go
+++ backend/ASTU-backend-group-5/internal/ai/moderate_blog.go
@@ -2,7 +2,6 @@ package ai
 
 import (
 	"blogApp/internal/config"
-	"blogApp/internal/domain"
 	"bytes"
 	"encoding/json"
 	"io"
@@ -54,41 +53,4 @@ func ModerateBlog(blog_content []interface{}, blog_title string) (bool, string, 
 		return false, "", err
 	}
 	return result["is_valid"].(bool), result["message"].(string), nil
-}
-
-func GetAiBlog(query, chat_id string) (domain.Blog, error) {
-
-	conf, err := config.Load()
-	if err != nil {
-		return domain.Blog{}, err
-	}
-
-	endpoint := conf.AI_API_DOMAIN + "/blog_assistant/"
-	data := map[string]string{
-		"query":   query,
-		"chat_id": chat_id,
-	}
-	marshal, err := json.Marshal(data)
-	if err != nil {
-		return domain.Blog{}, err
-	}
-	resp, err := http.Post(endpoint, "application/json", bytes.NewBuffer(marshal))
-	if err != nil {
-		return domain.Blog{}, err
-	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return domain.Blog{}, err
-	}
-	var result map[string]interface{}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return domain.Blog{}, err
-	}
-	return domain.Blog{
-		Title:   result["title"].(string),
-		Content: result["content"].([]interface{}),
-	}, nil
-
 }
--- backend/ASTU-backend-group-5/internal/usecase/blog/interface.go
+++ backend/ASTU-backend-group-5/internal/usecase/blog/interface.go
@@ -10,8 +10,8 @@ type BlogUseCase interface {
 	// Blog operations
 	CreateBlog(ctx context.Context, blog *domain.Blog, authorId string) error
 	GetBlogByID(ctx context.Context, id string) (*domain.Blog, error)
-	UpdateBlog(ctx context.Context, id string, blog *domain.Blog) error
-	DeleteBlog(ctx context.Context, id string) error
+	UpdateBlog(ctx context.Context, id string, blog *domain.Blog, authorId string) error
+	DeleteBlog(ctx context.Context, id, userId string) error
 	GetAllBlogs(ctx context.Context) ([]*domain.Blog, error)
 	FilterBlogs(ctx context.Context, filter domain.BlogFilter) ([]*domain.Blog, error)
 	PaginateBlogs(ctx context.Context, filter domain.BlogFilter, page, pageSize int) ([]*domain.Blog, error)

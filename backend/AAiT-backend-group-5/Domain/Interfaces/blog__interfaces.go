7aa6c5b06e18dd256aea124876dcb2f0993193f1
solo2112 <dawitabrham0021@gmail.com>
2024-08-20 14:45:36 +0300

    controller-updated

---
+++ backend/AAiT-backend-group-5/Domain/Interfaces/blog__interfaces.go
@@ -0,0 +1,59 @@
+package interfaces
+
+import (
+	"context"
+
+	dtos "github.com/aait.backend.g5.main/backend/Domain/DTOs"
+	models "github.com/aait.backend.g5.main/backend/Domain/Models"
+	"github.com/gin-gonic/gin"
+)
+
+type BlogRepository interface {
+	CreateBlog(ctx context.Context, blog *models.Blog) (*models.Blog, *models.ErrorResponse)
+	GetBlog(ctx context.Context, id string) (*models.Blog, *models.ErrorResponse)
+	GetBlogs(ctx context.Context) ([]*models.Blog, *models.ErrorResponse)
+	SearchBlogs(ctx context.Context, filter dtos.FilterBlogRequest) ([]*models.Blog, *models.ErrorResponse)
+	UpdateBlog(ctx context.Context, blogID string, blog *models.Blog) *models.ErrorResponse
+	DeleteBlog(ctx context.Context, id string) *models.ErrorResponse
+	AddComment(ctx context.Context, comment models.Comment) *models.ErrorResponse
+	GetComments(ctx context.Context, blogID string) ([]models.Comment, *models.ErrorResponse)
+	GetPopularity(ctx context.Context, blogID string) (*models.Popularity, *models.ErrorResponse)
+	TrackPopularity(ctx context.Context, popularity dtos.TrackPopularityRequest) *models.ErrorResponse
+	IncreaseView(ctx context.Context, blogID string) *models.ErrorResponse
+}
+
+type BlogUsecase interface {
+	CreateBlog(ctx context.Context, blog *models.Blog) (*dtos.BlogResponse, *models.ErrorResponse)
+	GetBlog(ctx context.Context, id string) (*dtos.BlogResponse, *models.ErrorResponse)
+	GetBlogs(ctx context.Context) ([]*dtos.BlogResponse, *models.ErrorResponse)
+	SearchBlogs(ctx context.Context, filter dtos.FilterBlogRequest) ([]*dtos.BlogResponse, *models.ErrorResponse)
+	UpdateBlog(ctx context.Context, blogID string, blog *models.Blog) *models.ErrorResponse
+	DeleteBlog(ctx context.Context, deleteBlogReq dtos.DeleteBlogRequest) *models.ErrorResponse
+	GetBlogPopularity(ctx context.Context, blogID string) (models.Popularity,*models.ErrorResponse)
+	AddComment(ctx context.Context, comment models.Comment) *models.ErrorResponse
+}
+
+type BlogController interface {
+	CreateBlogController(ctx *gin.Context)
+	GetBlogController(ctx *gin.Context)
+	GetBlogsController(ctx *gin.Context)
+	SearchBlogsController(ctx *gin.Context)
+	UpdateBlogController(ctx *gin.Context)
+	DeleteBlogController(ctx *gin.Context)
+	TrackPopularityController(ctx *gin.Context)
+	AddCommentController(ctx *gin.Context)
+}
+
+type BlogPopularityActionRepository interface {
+	Like(ctx context.Context, popularity dtos.TrackPopularityRequest) *models.ErrorResponse
+	Dislike(ctx context.Context, popularity dtos.TrackPopularityRequest) *models.ErrorResponse
+	GetBlogPopularityAction(ctx context.Context, blogID string, userID string) (models.PopularityAction, *models.ErrorResponse)
+	UndoLike(ctx context.Context, popularity dtos.TrackPopularityRequest) *models.ErrorResponse
+	UndoDislike(ctx context.Context, popularity dtos.TrackPopularityRequest) *models.ErrorResponse
+}
+
+type BlogHelper interface {
+	CreateSlug(blogTitle string) string
+	Marshal(data interface{}) (string, *models.ErrorResponse)
+	Unmarshal(dataJSON string, result interface{}) *models.ErrorResponse
+}
--- backend/AAiT-backend-group-5/go.mod
+++ backend/AAiT-backend-group-5/go.mod
@@ -16,9 +16,11 @@ require (
 require (
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -34,10 +36,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/montanaflynn/stats v0.7.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
-	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/golang/mock v1.6.0 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
@@ -54,6 +52,7 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/uuid v1.6.0
 	github.com/hashicorp/hcl v1.0.0 // indirect

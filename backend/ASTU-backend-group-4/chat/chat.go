c5df3dbdf4e75caa7da61607f36a3b827c9d32da
Yonas Sintayehu <119401357+sanoy-si@users.noreply.github.com>
2024-08-20 15:47:23 +0300

    implement the AI-services of the chat domain (#1019)
    * feat(chat): add the AIservice interface
    
    * feat(chat): add the ai-service implementation
    
    * fix(chat): update the parameter and return type of some methods
    
    * feat(chat): add the gemini ai dependencies
    
    ---------
    
    Co-authored-by: Yonas Sintayehu <amyonassintayehy@gmail.com>
    Co-authored-by: Mohammed Ali <68723236+mohaali482@users.noreply.github.com>

--- backend/ASTU-backend-group-4/chat/chat.go
+++ backend/ASTU-backend-group-4/chat/chat.go
@@ -23,12 +23,12 @@ type Message struct {
 }
 
 type Usecase interface {
-	CreateChat(ctx context.Context, userID, title string) (Chat, error)
+	CreateChat(ctx context.Context, userID string) (Chat, error)
 	DeleteChat(ctx context.Context, chatID string) error
 	GenerateChatTitle(ctx context.Context, message Message) (string, error)
 	GetChat(ctx context.Context, chatID string) (Chat, error)
 	GetChats(ctx context.Context, pagination infrastructure.PaginationRequest) (infrastructure.PaginationResponse[Chat], error)
-	SendMessage(ctx context.Context, chatID string, message Message) error
+	SendMessage(ctx context.Context, chatID string, message Message) (Message, error)
 }
 
 type Repository interface {
@@ -37,4 +37,9 @@ type Repository interface {
 	DeleteChat(ctx context.Context, chatID string) error
 	GetChat(ctx context.Context, chatID string) (Chat, error)
 	GetChats(ctx context.Context, pagination infrastructure.PaginationRequest) (infrastructure.PaginationResponse[Chat], error)
+}
+
+type AIService interface {
+	SendMessage(ctx context.Context, history []Message, message Message) (Message, error)
+	GenerateChatTitle(ctx context.Context, text string) (string, error)
 }

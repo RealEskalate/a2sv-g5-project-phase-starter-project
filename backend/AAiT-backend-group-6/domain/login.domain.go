28a52e306d0f63c3a1c93215465feb97e62dbfbf
emran <emranhi001@gmail.com>
2024-08-20 09:38:11 +0300

    added forget password router

--- backend/AAiT-backend-group-6/domain/login.domain.go
+++ backend/AAiT-backend-group-6/domain/login.domain.go
@@ -14,10 +14,6 @@ type LoginResponse struct {
 	RefreshToken string `json:"refreshToken"`
 }
 
-type LoginRepository interface{
-	GetUserByEmail(c context.Context, email string) (User, error)
-}
-
 type LoginUsecase interface {
 	GetUserByEmail(c context.Context, email string) (User, error)
 	CreateAccessToken(user *User, secret string, expiry int) (accessToken string, err error)

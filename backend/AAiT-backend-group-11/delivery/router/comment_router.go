eb973a7a7242df06d141305001a056144282a03e
dagmaros27 <dagxx27@gmail.com>
2024-08-20 09:12:45 +0300

    comment router setup

--- backend/AAiT-backend-group-11/delivery/router/comment_router.go
+++ backend/AAiT-backend-group-11/delivery/router/comment_router.go
@@ -1,1 +1,27 @@
 package route
+
+import (
+	"backend-starter-project/delivery/controller"
+	"backend-starter-project/repository"
+	"backend-starter-project/service"
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewCommmentRouter(db *mongo.Database,  group *gin.RouterGroup) {
+	ur := repository.NewUserRepository(db.Collection("users"))
+	br := repository.NewBlogRepository(db.Collection("blogs"),context.TODO())
+	cr := repository.NewCommentRepository(db.Collection("comments"), context.TODO())
+	cs := service.NewCommentService(cr,br,ur)
+	cc := controller.NewCommentController(cs)
+	
+
+	
+		group.POST("/:blogId",cc.AddComment)
+		group.GET("/:blogId", cc.GetCommentsByBlogPostId)
+		group.PUT("/:id", cc.UpdateComment)
+		group.DELETE("/:id",  cc.DeleteComment)
+
+}
--- backend/AAiT-backend-group-11/domain/interfaces/profile.go
+++ backend/AAiT-backend-group-11/domain/interfaces/profile.go
@@ -5,9 +5,11 @@ import  "backend-starter-project/domain/entities"
 type ProfileRepository interface {
 	GetUserProfile(userId string) (*entities.Profile, error)
 	UpdateUserProfile(profile *entities.Profile) (*entities.Profile, error)
+	DeleteUserProfile(userId string) error
 }
 
 type ProfileService interface {
 	GetUserProfile(userId string) (*entities.Profile, error)
 	UpdateUserProfile(profile *entities.Profile) (*entities.Profile, error)
+	DeleteUserProfile(userId string) error
 }

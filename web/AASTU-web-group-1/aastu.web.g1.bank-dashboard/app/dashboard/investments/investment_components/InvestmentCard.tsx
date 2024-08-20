8df61c51d9f2fc9541bda4907bbdfc7cb4c210b2
beki <beki@kali>
2024-08-20 11:43:20 +0300

    edit minor issues

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/investments/investment_components/InvestmentCard.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/investments/investment_components/InvestmentCard.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import Image from 'next/image'
 export default function InvestmentCard(props:any){
   return (
-    <div className="flex bg-white rounded-3xl mb-2 w-[90%] md:w-full h-[100px] mx-auto p-2 justify-between md:justify-evenly">
+    <div className="flex bg-white rounded-3xl mb-2 w-[90%] md:w-full h-[100px] mx-auto p-2 justify-between">
       <div className='my-auto w-[15%] md:w-[10%] h-auto'>
         <Image src={props.icon} alt='' width={1} height={1} className='my-auto size-[25px] md:size-[50px] rounded-full'  />
       </div>
       <div className='my-auto w-[50%] md:w-[30%]'>
-        <h1>{props.name}</h1>
+        <h1 className='text-[14px] md:text-[16px] font-[600] pb-1' >{props.name}</h1>
         <div className='flex flex-row gap-1 flex-wrap'>
             {props.type.map((item:any) => (
               <p className='text-bg-gray font-[400] text-blue-900 text-[14px] md:text-[16px] text-wrap w-[100px] ml-0' key={item}>{item}</p>
@@ -15,11 +15,11 @@ export default function InvestmentCard(props:any){
         </div>
       </div>
       <div className='my-auto w-[30%] hidden lg:table-cell'>
-        <h1 className='text-[16px] md:text-[20px]' >{props.investmentValue}</h1>
+        <h1 className='text-[14px] md:text-[16px] font-[600] pb-1' >{props.investmentValue}</h1>
         <p className='text-blue-800 text-[10px] md:text-[16px]'>Envestment Value</p>
       </div>
       <div className='my-auto w-[40%] md:w-[30%] pl-16 md:pl-0'>
-        <h1 className='text-emerald-700'>{props.returnValue}</h1>
+        <h1 className='text-emerald-700 text-[14px] md:text-[16px] font-[600] pb-1'>{props.returnValue}</h1>
         <p className='text-blue-800 hidden md:table-cell my-auto'>Return Value</p>
       </div>
     </div>

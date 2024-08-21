59fda356cf66db738cf725b9035b4dd85debb8b6
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-21 23:53:09 +0300

    fix: fix bugs on password resetting functionality - usecase/password_reset/reset_handler.go

--- backend/AAiT-backend-group-13/usecase/password_reset/reset_handler.go
+++ backend/AAiT-backend-group-13/usecase/password_reset/reset_handler.go
@@ -1,7 +1,8 @@
 package passwordreset
 
 import (
-	"github.com/google/uuid"
+	"log"
+
 	er "github.com/group13/blog/domain/errors"
 	ihash "github.com/group13/blog/domain/i_hash"
 	ijwt "github.com/group13/blog/usecase/common/i_jwt"
@@ -29,35 +30,32 @@ func NewResetHandler(userRepo irepo.UserRepository, hashService ihash.Service, j
 // verifying the user's identity, updating their password, and saving the changes.
 // It returns a boolean indicating success or an error if any step fails.
 func (h *ResetHandler) Handle(cmd *ResetCommand) (bool, error) {
-	// Decode the provided JWT token
 	claims, err := h.jwtService.Decode(cmd.token)
 	if err != nil {
 		return false, er.NewUnauthorized(err.Error())
 	}
 
-	// Validate that the token is for a password reset
+	log.Println(claims)
 	if isForReset, ok := claims["is_for_reset"].(bool); !ok || !isForReset {
 		return false, er.NewUnauthorized("invalid token")
 	}
 
-	// Extract user ID from claims
-	id, ok := claims["id"].(uuid.UUID)
+	email, ok := claims["email"].(string)
+	log.Println(email, ok)
 	if !ok {
 		return false, er.NewUnauthorized("invalid token")
 	}
 
-	// Find user by ID
-	user, err := h.userRepo.FindById(id)
+	user, err := h.userRepo.FindByEmail(email)
+	log.Println(user)
 	if err != nil {
 		return false, er.NewUnauthorized(err.Error())
 	}
 
-	// Update the user's password
 	if err := user.UpdatePassword(cmd.NewPassword, h.hashService); err != nil {
 		return false, er.NewUnauthorized(err.Error())
 	}
 
-	// Save the updated user
 	if err := h.userRepo.Save(user); err != nil {
 		return false, er.NewUnauthorized(err.Error())
 	}

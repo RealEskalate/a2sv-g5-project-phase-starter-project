600607e13242caced882cf4fc43a8ffa847ce4b3
yesetoda <107122034+yesetoda@users.noreply.github.com>
2024-08-21 10:12:48 +0300

    forced the authorization on the blog routes

--- backend/ASTU-backend-group-1/delivery/routers/BlogRouter.go
+++ backend/ASTU-backend-group-1/delivery/routers/BlogRouter.go
@@ -19,30 +19,30 @@ func (gr *BlogRoute) GinBlogRouter() {
 	router := gin.Default()
 	blogRouter := router.Group("/blogs")
 	{
-		blogRouter.POST("/", gr.usecase.HandleCreateBlog)
-		blogRouter.GET("/", gr.usecase.HandleGetAllBlogs)
-		blogRouter.GET("/popular", gr.usecase.HandleGetPopularBlog)
-		blogRouter.GET("/filter", gr.usecase.HandleFilterBlogs)
-		blogRouter.GET("/:blogId", gr.usecase.HandleGetBlogById)
-		blogRouter.PATCH("/:blogId", gr.usecase.HandleBlogUpdate)
-		blogRouter.DELETE("/:blogId", gr.authController.AdminMiddlewareGin(), gr.usecase.HandleBlogDelete)
-		blogRouter.POST("/:blogId/interact/:type", gr.usecase.HandleBlogLikeOrDislike)
+		blogRouter.POST("/", gr.authController.UserMiddlewareGin(), gr.usecase.HandleCreateBlog)
+		blogRouter.GET("/", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetAllBlogs)
+		blogRouter.GET("/popular", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetPopularBlog)
+		blogRouter.GET("/filter", gr.authController.UserMiddlewareGin(), gr.usecase.HandleFilterBlogs)
+		blogRouter.GET("/:blogId", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetBlogById)
+		blogRouter.PATCH("/:blogId", gr.authController.UserMiddlewareGin(), gr.usecase.HandleBlogUpdate)
+		blogRouter.DELETE("/:blogId", gr.authController.UserMiddlewareGin(), gr.authController.AdminMiddlewareGin(), gr.usecase.HandleBlogDelete)
+		blogRouter.POST("/:blogId/interact/:type", gr.authController.UserMiddlewareGin(), gr.usecase.HandleBlogLikeOrDislike)
 
 		// TODO: check if there is a blog with such id
 		commentRouter := blogRouter.Group("/:blogId/comments")
 		{
-			commentRouter.GET("/", gr.usecase.HandleGetAllComments)
-			commentRouter.POST("/", gr.usecase.HandleCommentOnBlog)
-			commentRouter.GET("/:commentId", gr.usecase.HandleGetCommentById)
-			commentRouter.POST("/:commentId/interact/:type", gr.usecase.HandleCommentLikeOrDislike)
+			commentRouter.GET("/", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetAllComments)
+			commentRouter.POST("/", gr.authController.UserMiddlewareGin(), gr.usecase.HandleCommentOnBlog)
+			commentRouter.GET("/:commentId", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetCommentById)
+			commentRouter.POST("/:commentId/interact/:type", gr.authController.UserMiddlewareGin(), gr.usecase.HandleCommentLikeOrDislike)
 
 			repliesRouter := commentRouter.Group("/:commentId/replies")
 			{
-				repliesRouter.GET("/", gr.usecase.HandleGetAllRepliesForComment)
-				repliesRouter.POST("/", gr.usecase.HandleReplyOnComment)
-				repliesRouter.GET("/:replyId", gr.usecase.HandleGetReplyById)
+				repliesRouter.GET("/", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetAllRepliesForComment)
+				repliesRouter.POST("/", gr.authController.UserMiddlewareGin(), gr.usecase.HandleReplyOnComment)
+				repliesRouter.GET("/:replyId", gr.authController.UserMiddlewareGin(), gr.usecase.HandleGetReplyById)
 				// todo: test the below functions
-				repliesRouter.POST("/:replyId/interact/:type", gr.usecase.HandleReplyLikeOrDislike)
+				repliesRouter.POST("/:replyId/interact/:type", gr.authController.UserMiddlewareGin(), gr.usecase.HandleReplyLikeOrDislike)
 			}
 		}
 

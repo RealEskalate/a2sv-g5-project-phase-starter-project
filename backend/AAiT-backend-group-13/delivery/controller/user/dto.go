8f553da33d64fb9aadd8080339349f4d708fa810
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-21 23:53:09 +0300

    fix: fix bugs on password resetting functionality - delivery/controller/user/dto.go

--- backend/AAiT-backend-group-13/delivery/controller/user/dto.go
+++ backend/AAiT-backend-group-13/delivery/controller/user/dto.go
@@ -1,7 +1,5 @@
 package usercontroller
 
-import "github.com/google/uuid"
-
 type LoginDto struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -16,14 +14,13 @@ type SignUpDto struct {
 }
 
 type ValidateCodeDto struct {
-	Code  int64  `json:"code" binding:"required"`
+	Code  int    `json:"code" binding:"required"`
 	Email string `json:"email" binding:"required"`
 }
 
 type ResetPasswordDto struct {
-	Id          uuid.UUID `json:"id" binding:"required"`
-	Token       string    `json:"token" binding:"required"`
-	NewPassword string    `json:"newPassword" binding:"required"`
+	Token       string `json:"token" binding:"required"`
+	NewPassword string `json:"newPassword" binding:"required"`
 }
 
 type ForgotPasswordDto struct {

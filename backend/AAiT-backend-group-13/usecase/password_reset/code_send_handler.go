68f52179d5ea021dd46afc36ec4a944001bf3512
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-21 23:53:09 +0300

    fix: fix bugs on password resetting functionality - usecase/password_reset/code_send_handler.go

--- backend/AAiT-backend-group-13/usecase/password_reset/code_send_handler.go
+++ backend/AAiT-backend-group-13/usecase/password_reset/code_send_handler.go
@@ -1,46 +1,82 @@
 package passwordreset
 
 import (
+	"crypto/rand"
 	"log"
-	"math/rand"
 	"time"
 
-	er "github.com/group13/blog/domain/errors"
-	"github.com/group13/blog/domain/models"
+	ihash "github.com/group13/blog/domain/i_hash"
 	icmd "github.com/group13/blog/usecase/common/cqrs/command"
+	iemail "github.com/group13/blog/usecase/common/i_email"
 	irepo "github.com/group13/blog/usecase/common/i_repo"
 )
 
+const otpChars = "1234567890"
+
 // SendcodeHandler handles the logic for sending password reset codes.
 type SendcodeHandler struct {
-	userrepo irepo.UserRepository
+	userRepo     irepo.UserRepository
+	emailService iemail.Service
+	hashService  ihash.Service
 }
 
 // Ensure Handler implements the icmd.IHandler interface.
 var _ icmd.IHandler[string, time.Time] = &SendcodeHandler{}
 
 // New creates a new instance of Handler.
-func NewSendcodeHandler(userrepo irepo.UserRepository) *SendcodeHandler {
-	return &SendcodeHandler{userrepo: userrepo}
+func NewSendcodeHandler(userrepo irepo.UserRepository, emailService iemail.Service, hashService ihash.Service) *SendcodeHandler {
+	return &SendcodeHandler{
+		userRepo:     userrepo,
+		emailService: emailService,
+		hashService:  hashService,
+	}
 }
 
 // Handle generates and sets a password reset code for the user identified by the given email.
 // It returns the expiration time of the reset code or an error if the process fails.
 func (h *SendcodeHandler) Handle(email string) (time.Time, error) {
 	log.Println("Finding user by username")
-	user, err := h.userrepo.FindByEmail(email)
+	user, err := h.userRepo.FindByEmail(email)
 	if err != nil {
-		return time.Now(), er.NewUnauthorized(err.Error())
+		return time.Now(), err
 	}
 
 	exprTime := time.Now().Add(time.Minute * 17).UTC()
-	if err = user.UpdateResetCode(&models.ResetCode{
-		Code: rand.Int63(),
-		Expr: exprTime,
-	}); err != nil {
+	code, err := generateOTP(6)
+	if err != nil {
 		return exprTime, err
 	}
 
-	err = h.userrepo.Save(user)
+	if err = user.UpdateResetCode(code, exprTime, h.hashService); err != nil {
+		return exprTime, err
+	}
+
+	err = h.userRepo.Save(user)
+	if err != nil {
+		return exprTime, err
+	}
+
+	mailReciver := []string{email}
+	mail := iemail.NewResetPasswordEmail(mailReciver, code)
+	err = h.emailService.Send(mail)
+	if err != nil {
+		return exprTime, err
+	}
+
 	return exprTime, err
+}
+
+func generateOTP(length int) (string, error) {
+	buffer := make([]byte, length)
+	_, err := rand.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	otpCharsLength := len(otpChars)
+	for i := 0; i < length; i++ {
+		buffer[i] = otpChars[int(buffer[i])%otpCharsLength]
+	}
+
+	return string(buffer), nil
 }

b506ae5ea9f832f0dff3af1e3412f9de829a0e03
manuel-heav <manuelheav32@gmail.com>
2024-08-21 17:07:37 +0300

    Chganged links

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/page.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/page.tsx
@@ -36,7 +36,7 @@ const Services = () => {
       console.log(token);
       try {
         const response = await fetch(
-          "https://bank-dashboard-6acc.onrender.com/bank-services?page=0&size=10",
+          "https://bank-dashboard-1tst.onrender.com/bank-services?page=0&size=10",
           {
             headers: {
               Authorization: `Bearer ${token}`,

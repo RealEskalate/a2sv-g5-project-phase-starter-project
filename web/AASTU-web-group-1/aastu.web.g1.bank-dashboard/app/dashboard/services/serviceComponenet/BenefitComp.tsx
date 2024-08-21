f703e874a6965016a0f1fe5470529b441f0c0e30
kibrnew <kibrnewgedamu123@gmail.com>
2024-08-21 11:04:46 +0300

    finilazed

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/serviceComponenet/BenefitComp.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/serviceComponenet/BenefitComp.tsx
@@ -1,5 +1,4 @@
 import { useUser } from "@/contexts/UserContext";
-import { SafteyIcon } from "../serviceIcons/icons";
 import Image from "next/image";
 
 interface itemsProp {
@@ -10,19 +9,29 @@ interface itemsProp {
 interface Benefitprop {
   items: itemsProp;
 }
+
 const BenefitComp = ({ items }: Benefitprop) => {
-  console.log(items.icon);
-  const {isDarkMode} = useUser();
+  const { isDarkMode } = useUser();
   return (
-    <div className="flex p-5 border-2 rounded-2xl bg-white ">
+    <div
+      className={`flex p-5 border-2 rounded-2xl ${
+        isDarkMode
+          ? "bg-gray-800 border-gray-600 text-gray-300"
+          : "bg-white border-gray-200 text-gray-900"
+      }`}
+    >
       <div className="ml-3 mr-1 w-10 h-10">
-        <Image src={items.icon} alt="" width={30} height={30} />
+        <Image src={items.icon} alt="Benefit Icon" width={30} height={30} />
       </div>
-      <div className="flex flex-col px-2 flex-grow justify-center ">
+      <div className="flex flex-col px-2 flex-grow justify-center">
         <div className="text-lg">{items.name}</div>
-        <div className="text-xs text-[#718EBF] w-[130px]">
+        <div
+          className={`text-xs w-[130px] ${
+            isDarkMode ? "text-gray-500" : "text-gray-600"
+          }`}
+        >
           Unlimited protection
-        </div>{" "}
+        </div>
       </div>
     </div>
   );

b53e3d78bbbe609d3a4ed11250e89ed9566c1cd9
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-21 23:53:09 +0300

    fix: fix bugs on password resetting functionality - delivery/controller/base/controller.go

--- backend/AAiT-backend-group-13/delivery/controller/base/controller.go
+++ backend/AAiT-backend-group-13/delivery/controller/base/controller.go
@@ -4,24 +4,38 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	errapi "github.com/group13/blog/delivery/errors"
 )
 
-// Controller Responses
+// BaseHandler is a base struct for all HTTP request handlers that provides basic HTTP functionalities.
 type BaseHandler struct{}
 
+// Problem handles errors by writing an appropriate response to the Gin context.
+// The primary usage is to hid some messages.
+func (h *BaseHandler) Problem(c *gin.Context, err errapi.Error) {
+	var shadowedErr errapi.Error
+	switch err.StatusCode() {
+	case errapi.BadRequest, errapi.Conflict, errapi.NotFound, errapi.Forbidden:
+		shadowedErr = err
+	case errapi.Authentication:
+		shadowedErr = errapi.NewAuthentication("invalid credentials")
+	default:
+		shadowedErr = errapi.NewServerError("something went wrong")
+	}
+	h.respondError(c, shadowedErr)
+}
+
+// RespondError writes an error response to the Gin context.
+func (h *BaseHandler) respondError(c *gin.Context, err errapi.Error) {
+	c.JSON(err.StatusCode(), gin.H{"error": err.Error()})
+}
+
+// RespondWithCookies writes a response with cookies to the Gin context.
 func (h *BaseHandler) RespondWithCookies(c *gin.Context, status int, v interface{}, cookies []*http.Cookie) {
 	for _, cookie := range cookies {
 		http.SetCookie(c.Writer, cookie)
 	}
 	h.Respond(c, status, v)
-}
-
-func (h *BaseHandler) RemoveCookie(c *gin.Context, name string) {
-	http.SetCookie(c.Writer, &http.Cookie{
-		Name:  name,
-		Value: "",
-		Path:  "/",
-	})
 }
 
 // RespondWithLocation writes a response with a Location header to the Gin context.
@@ -38,4 +52,3 @@ func (h *BaseHandler) Respond(c *gin.Context, status int, v interface{}) {
 		c.JSON(status, v)
 	}
 }
-

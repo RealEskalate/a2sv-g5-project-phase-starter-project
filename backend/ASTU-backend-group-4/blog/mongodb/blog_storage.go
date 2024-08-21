0f36c5937a01e131c52c988a2ab7371506e45647
Mohammed Ali <mohaali482@gmail.com>
2024-08-21 10:47:45 +0300

    feat: change filter of unlike blog

--- backend/ASTU-backend-group-4/blog/mongodb/blog_storage.go
+++ backend/ASTU-backend-group-4/blog/mongodb/blog_storage.go
@@ -267,7 +267,8 @@ func (b *BlogStorage) UpdateBlog(ctx context.Context, id string, blog blogDomain
 
 // UnlikeBlog implements BlogRepository.
 func (b *BlogStorage) UnlikeBlog(ctx context.Context, like blogDomain.Like) error {
-	_, err := b.db.Collection(likeCollection).DeleteOne(ctx, like)
+	filter := bson.D{{Key: "blog_id", Value: like.BlogID}, {Key: "user_id", Value: like.UserID}}
+	_, err := b.db.Collection(likeCollection).DeleteOne(ctx, filter)
 	if err != nil {
 		log.Default().Printf("Failed to unlike blog: %v", err)
 		return blogDomain.ErrUnableToUnLikeBlog

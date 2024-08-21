91084e81038aedb9d56bc3bf43d7773470689418
Abenezer Workineh <abenezer037005@gmail.com>
2024-08-22 01:38:31 +0300

    implemented sorting, pagination, and views

--- backend/AASTU-backend-group-8/domain/blog.go
+++ backend/AASTU-backend-group-8/domain/blog.go
@@ -7,12 +7,12 @@ import (
 )
 
 type BlogPost struct {
-	// ID          string      `json:"id" gorm:"primaryKey"`
-	ID        primitive.ObjectID `json:"id"  bson:"_id,omitempty"`
+	ID        primitive.ObjectID `json:"_id" bson:"_id,omitempty"`
 	Title     string             `json:"title" validate:"required,min=5,max=255"`
 	Content   string             `json:"content" validate:"required"`
 	AuthorID  primitive.ObjectID `json:"author_id"` // Foreign key to User model
-	Tags      []string           `json:"tags" gorm:"type:text[]"`
+	Tags      []string           `json:"tags"`
+	Veiws     int                `json:"views"`
 	CreatedAt time.Time          `json:"created_at"`
 	UpdatedAt time.Time          `json:"updated_at"`
 	//PublishedAt *time.Time `json:"published_at"`  Optional
@@ -24,8 +24,8 @@ type SearchBlogPost struct {
 }
 
 type BlogUsecaseInterface interface {
-	CreateBlogPost(blog *BlogPost) (*BlogPost, error)
-	GetAllBlogPosts() ([]BlogPost, error)
+	CreateBlogPost(blog *BlogPost) (string, error)
+	GetAllBlogPosts(Pagination, string, int) ([]BlogPost, error)
 	GetBlogByID(id primitive.ObjectID) (*BlogPost, error)
 	UpdateBlogPost(id primitive.ObjectID, blog *BlogPost) (*BlogPost, error)
 	// SearchBlogPosts(query *SearchBlogPost) ([]BlogPost, error) // Add this method
@@ -35,10 +35,10 @@ type BlogUsecaseInterface interface {
 // domain/blog_repository_interface.go
 
 type BlogRepositoryInterface interface {
-	Save(blog *BlogPost) error
-	GetAllBlog() ([]BlogPost, error)
+	Save(blog *BlogPost) (string, error)
+	GetAllBlog(Pagination, string, int) ([]BlogPost, error)
 	GetBlogByID(id primitive.ObjectID) (*BlogPost, error)
-	Update(blog *BlogPost) (*BlogPost, error)
+	Update(id primitive.ObjectID, blog *BlogPost) (*BlogPost, error)
 	// Search(title string) ([]BlogPost, error) // Add this method
 	Delete(id primitive.ObjectID) error
 }

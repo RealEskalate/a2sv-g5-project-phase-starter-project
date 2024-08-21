ba9b197b3a97aa75aaa81d1abf0db7905ac80d4b
Mohammed Shemim <Msh-19>
2024-08-21 11:13:12 +0300

    initial setup of creditCards post fetching

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/credit-cards/components/InputForm.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/credit-cards/components/InputForm.tsx
@@ -1,68 +1,107 @@
+"use client"
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cardSchema } from "@/schema";
+import { postCards } from "@/lib/api";
+
+
+
+
+
+
+type FormData = z.infer<typeof cardSchema>;
+
 const InputForm = () => {
-    return (
-      <div>
-        <form>
-          <div className="md:grid md:grid-cols-2 gap-4">
-            <div className="flex flex-col my-4">
-              <label className="text-[#515B6F] font-semibold">Card Type</label>
-              <input
-                type="text"
-                placeholder="Classic"
-                className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
-              />
-              {/* <p className="text-red-500 text-center mt-2">
-                  {errors.name?.message}
-                </p> */}
-            </div>
-            <div className="flex flex-col my-4">
-              <label className="text-[#515B6F] font-semibold">Name On Card</label>
-              <input
-                type="text"
-                placeholder="My Cards"
-                className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
-                // {...register("email")}
-              />
-              {/* <p className="text-red-500 text-center mt-2">
-                  {errors.email?.message}
-                </p> */}
-            </div>
-          </div>
-          <div className="md:grid md:grid-cols-2 gap-4">
-            <div className="flex flex-col my-4">
-              <label className="text-[#515B6F] font-semibold">Card Number</label>
-              <input
-                type="number"
-                placeholder="**** **** **** ****"
-                className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
-                // {...register("password")}
-              />
-              {/* <p className="text-red-500 text-center mt-2">
-                  {errors.password?.message}
-                </p> */}
-            </div>
-            <div className="flex flex-col my-4">
-              <label className="text-[#515B6F] font-semibold">
-                Expiration Date
-              </label>
-              <input
-                type="date"
-                placeholder="00:00:00 UTC on 1st January 1970"
-                className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
-                // {...register("confirmPassword")}
-              />
-              {/* <p className="text-red-500 text-center mt-2">
-                  {errors.confirmPassword?.message}
-                </p> */}
-            </div>
-          </div>
-          {/* {error && (
-              <p className="text-red-500 text-center mt-2 mb-5">{error}</p>
-            )} */}
-          <button className="bg-[#1814F3] sm:w-[100%] text-white p-2 sm:rounded-full md:max-w-[160px] md:rounded-md  ">
-            Add to Cart
-          </button>
-        </form>
-      </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(cardSchema),
+  });
+
+  const onSubmit = async (data: FormData) => {
+    
+    try {
+      const updatedForm = {...data,balance:300}
+      await postCards(updatedForm);
+    } catch (error) {
+      console.error("Failed to submit form:", error);
+    }
   };
-export default InputForm  
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="md:grid md:grid-cols-2 gap-4">
+          <div className="flex flex-col my-4">
+            <label className="text-[#515B6F] font-semibold">Card Type</label>
+            <input
+              type="text"
+              placeholder="Classic"
+              className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
+              {...register("cardType")}
+            />
+            {errors.cardType && (
+              <p className="text-red-500 text-center mt-2">
+                {errors.cardType?.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col my-4">
+            <label className="text-[#515B6F] font-semibold">Name On Card</label>
+            <input
+              type="text"
+              placeholder="John Doe"
+              className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
+              {...register("cardHolder")}
+            />
+            {errors.cardHolder && (
+              <p className="text-red-500 text-center mt-2">
+                {errors.cardHolder?.message}
+              </p>
+            )}
+          </div>
+        </div>
+        <div className="md:grid md:grid-cols-2 gap-4">
+          <div className="flex flex-col my-4">
+            <label className="text-[#515B6F] font-semibold">Card Number</label>
+            <input
+              type="text"
+              placeholder="**** **** **** ****"
+              className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
+              {...register("passcode")}
+            />
+            {errors.passcode && (
+              <p className="text-red-500 text-center mt-2">
+                {errors.passcode?.message}
+              </p>
+            )}
+          </div>
+          <div className="flex flex-col my-4">
+            <label className="text-[#515B6F] font-semibold">
+              Expiration Date
+            </label>
+            <input
+              type="date"
+              className="inputField mb-2 rounded-xl py-2 px-2 border border-gray-300"
+              {...register("expiryDate")}
+            />
+            {errors.expiryDate && (
+              <p className="text-red-500 text-center mt-2">
+                {errors.expiryDate?.message}
+              </p>
+            )}
+          </div>
+        </div>
+        <button className="bg-[#1814F3] sm:w-[100%] text-white p-2 sm:rounded-full md:max-w-[160px] md:rounded-md">
+          Add to Cart
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default InputForm;

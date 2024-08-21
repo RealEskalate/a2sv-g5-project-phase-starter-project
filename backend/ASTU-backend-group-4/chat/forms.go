488c605a9e8f637cc11197c696b7fceea7ddcf19
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-21 11:54:48 +0300

    feat(chat): add forms to be used for validations in the usecase

--- backend/ASTU-backend-group-4/chat/forms.go
+++ backend/ASTU-backend-group-4/chat/forms.go
@@ -2,6 +2,18 @@ package chat
 
 type CreateChatForm struct {
 	UserID string `json:"user_id,omitempty" validate:"required,hexadecimal"`
-	Title string `json:"title,omitempty" validate:"required,min=2,max=30"`
+	Title  string `json:"title,omitempty" validate:"required,min=2,max=30"`
 }
 
+type TextForm struct {
+	Text string `json:"text,omitempty" validate:"required,min=2,max=30"`
+}
+
+type DefalutChatForm struct {
+	ChatID string `json:"chat_id,omitempty" validate:"required,hexadecimal"`
+	UserID string `json:"user_id,omitempty" validate:"required,hexadecimal"`
+}
+
+type UserIDForm struct {
+	UserID string `json:"user_id,omitempty" validate:"required,hexadecimal"`
+}

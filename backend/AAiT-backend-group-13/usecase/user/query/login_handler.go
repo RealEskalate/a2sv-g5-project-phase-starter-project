f025ea87ffa5eeb79da58479079e18f5d5eb463c
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-21 23:53:09 +0300

    fix: fix bugs on password resetting functionality - usecase/user/query/login_handler.go

---
+++ backend/AAiT-backend-group-13/usecase/user/query/login_handler.go
@@ -0,0 +1,79 @@
+package userqry
+
+import (
+	"log"
+
+	er "github.com/group13/blog/domain/errors"
+	ihash "github.com/group13/blog/domain/i_hash"
+	icmd "github.com/group13/blog/usecase/common/cqrs/command"
+	iemail "github.com/group13/blog/usecase/common/i_email"
+	ijwt "github.com/group13/blog/usecase/common/i_jwt"
+	irepo "github.com/group13/blog/usecase/common/i_repo"
+	result "github.com/group13/blog/usecase/user/result"
+)
+
+// LoginHandler handles user login requests.
+type LoginHandler struct {
+	repo         irepo.UserRepository
+	jwtService   ijwt.Service
+	hashService  ihash.Service
+	emailService iemail.Service
+}
+
+// LoginConfig holds the configuration for creating a LoginHandler.
+type LoginConfig struct {
+	UserRepo     irepo.UserRepository
+	JwtService   ijwt.Service
+	HashService  ihash.Service
+	EmailService iemail.Service
+}
+
+// NewLoginHandler creates a new instance of LoginHandler with the provided configuration.
+func NewLoginHandler(config LoginConfig) *LoginHandler {
+	return &LoginHandler{
+		repo:         config.UserRepo,
+		jwtService:   config.JwtService,
+		hashService:  config.HashService,
+		emailService: config.EmailService,
+	}
+}
+
+// Ensure LoginHandler implements icmd.IHandler
+var _ icmd.IHandler[*LoginQuery, *result.LoginInResult] = &LoginHandler{}
+
+// Handle processes the login command and returns the login result with tokens.
+func (h *LoginHandler) Handle(command *LoginQuery) (*result.LoginInResult, error) {
+	log.Printf("Finding user by username: %s", command)
+	user, err := h.repo.FindByUsername(command.username)
+	if user.Username() == "" || err != nil {
+		return nil, er.NewUnauthorized("user not found")
+	} else if !user.IsActive() {
+		return nil, er.NewUnauthorized("user is not activated")
+	}
+
+	log.Println(user, "found this user", user.PasswordHash(), "now checking password")
+	ok, err := h.hashService.Match(user.PasswordHash(), command.password)
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		return nil, er.NewUnauthorized("password is incorrect")
+	}
+
+	user.MakeActive()
+
+	token, err := h.jwtService.Generate(user, ijwt.Access)
+	if err != nil {
+		return nil, err
+	}
+
+	refreshToken, err := h.jwtService.Generate(user, ijwt.Refresh)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result.LoginInResult{
+		Token:        token,
+		RefreshToken: refreshToken,
+	}, nil
+}

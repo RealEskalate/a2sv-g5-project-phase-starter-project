df009219ea74bd145a9f67a9d5adbb1765bc514f
Sofonyas Tizazu <sofstzz@gmail.com>
2024-08-21 21:24:10 +0300

    comment and reaction cache

--- backend/AAiT-backend-group-13/usecase/comment/query/get_all_handler.go
+++ backend/AAiT-backend-group-13/usecase/comment/query/get_all_handler.go
@@ -1,26 +1,56 @@
 package commentqry
 
 import (
+	"log"
+
 	"github.com/google/uuid"
 	"github.com/group13/blog/domain/models"
 	icmd "github.com/group13/blog/usecase/common/cqrs/command"
+	cache "github.com/group13/blog/usecase/common/i_cache"
 	irepo "github.com/group13/blog/usecase/common/i_repo"
 )
 
 // Handler is responsible for handling the Get comments query by blog ID.
 type GetAllHandler struct {
 	repo irepo.Comment // Repository for comment.
+	commentCache cache.ICache
 }
 
 // Ensure Handler implements the IHandler interface
 var _ icmd.IHandler[uuid.UUID, *[]models.Comment] = &GetAllHandler{}
 
 // NewGetAllHandler creates a new instance of Handler with the provided comment repository.
-func NewGetAllHandler(commmentRepo irepo.Comment) *GetAllHandler {
-	return &GetAllHandler{repo: commmentRepo}
+func NewGetAllHandler(commmentRepo irepo.Comment, cacheService *cache.ICache) *GetAllHandler {
+	return &GetAllHandler{
+		repo: commmentRepo,
+		commentCache: *cacheService,
+	}
 }
 
 // Handle processes the Get query by blog ID and returns the corresponding comments.
 func (h *GetAllHandler) Handle(id uuid.UUID) (*[]models.Comment, error) {
-	return h.repo.GetCommentsByBlogId(id)
+	cacheKey := id.String()
+	
+	if cachedComments, err := h.commentCache.Get(cacheKey); err == nil && cachedComments != nil {
+		if comments, ok := cachedComments.(*[]models.Comment); ok {
+			return comments, nil
+		}
+	}
+
+
+	var err error
+	var comments *[]models.Comment
+
+	comments, err = h.repo.GetCommentsByBlogId(id) 
+	
+	if err != nil {
+		return nil, err
+	}
+
+	// Store the retrieved blogs in the cache
+	if err := h.commentCache.Set(cacheKey, comments); err != nil {
+		// Log the caching error but don't fail the request
+		log.Println("Failed to cache comments:", err)
+	}
+	return comments, nil
 }
--- backend/AAiT-backend-group-13/usecase/comment/query/get_handler.go
+++ backend/AAiT-backend-group-13/usecase/comment/query/get_handler.go
@@ -1,26 +1,58 @@
 package commentqry
 
 import (
+	"log"
+
 	"github.com/google/uuid"
 	"github.com/group13/blog/domain/models"
 	icmd "github.com/group13/blog/usecase/common/cqrs/command"
+	cache "github.com/group13/blog/usecase/common/i_cache"
 	irepo "github.com/group13/blog/usecase/common/i_repo"
 )
 
 // GetHandler handles queries to retrieve a comment by its ID.
 type GetHandler struct {
 	repo irepo.Comment // Repository for comments.
+	commentCache cache.ICache
 }
 
 // Ensure GetHandler implements icmd.IHandler
 var _ icmd.IHandler[uuid.UUID, *models.Comment] = &GetHandler{}
 
 // NewGetHandler creates a new instance of GetHandler with the provided comment repository.
-func NewGetHandler(commentRepo irepo.Comment) *GetHandler {
-	return &GetHandler{repo: commentRepo}
+func NewGetHandler(commentRepo irepo.Comment, commentCache *cache.ICache) *GetHandler {
+	return &GetHandler{
+		repo: commentRepo,
+		commentCache: *commentCache,
+	}
 }
 
 // Handle processes the query to retrieve a comment by its ID.
 func (h *GetHandler) Handle(id uuid.UUID) (*models.Comment, error) {
-	return h.repo.GetCommentById(id)
+	cacheKey := id.String()
+	
+	if cachedComment, err := h.commentCache.Get(cacheKey); err == nil && cachedComment != nil {
+		if comment, ok := cachedComment.(*models.Comment); ok {
+			return comment, nil
+		}
+	} 
+
+
+	var err error
+	var comment *models.Comment 
+
+	comment, err = h.repo.GetCommentById(id)
+	
+	if err != nil {
+		return nil, err
+	}
+
+	// Store the retrieved blogs in the cache
+	if err := h.commentCache.Set(cacheKey, comment); err != nil {
+		// Log the caching error but don't fail the request
+		log.Println("Failed to cache comment:", err)
+	}
+	
+	return comment, nil
+	
 }

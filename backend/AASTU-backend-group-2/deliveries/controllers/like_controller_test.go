05bf137976633d688579d96823abdee463b71a20
Abemchegen <88394629+Abemchegen@users.noreply.github.com>
2024-08-21 18:28:11 +0300

    testing

---
+++ backend/AASTU-backend-group-2/deliveries/controllers/like_controller_test.go
@@ -0,0 +1,128 @@
+package controllers_test
+
+import (
+	"blog_g2/deliveries/controllers"
+	"blog_g2/domain"
+	"blog_g2/mocks"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type LikeControllerTestSuite struct {
+	suite.Suite
+	controller   *controllers.LikeController
+	mockUsecase  *mocks.LikeUsecase
+	mockContext  *gin.Context
+	mockResponse *httptest.ResponseRecorder
+}
+
+func (suite *LikeControllerTestSuite) SetupTest() {
+	suite.mockUsecase = new(mocks.LikeUsecase)
+	suite.controller = controllers.NewLikeController(suite.mockUsecase)
+	suite.mockResponse = httptest.NewRecorder()
+	suite.mockContext, _ = gin.CreateTestContext(suite.mockResponse)
+}
+
+func (suite *LikeControllerTestSuite) TestCreateLike_Unauthorized() {
+	suite.mockContext.Request = httptest.NewRequest(http.MethodPost, "/likes", nil)
+	suite.controller.CreateLike(suite.mockContext)
+	assert.Equal(suite.T(), http.StatusUnauthorized, suite.mockResponse.Code)
+}
+
+func (suite *LikeControllerTestSuite) TestCreateLike_Success() {
+	// Setting up the context and parameters
+	suite.mockContext.Params = gin.Params{gin.Param{Key: "postID", Value: "post123"}}
+	suite.mockContext.Request = httptest.NewRequest(http.MethodPost, "/likes/post123", nil)
+	suite.mockContext.Set("role", "user")
+	suite.mockContext.Set("userID", "user456")
+
+	// Mocking the use case
+	suite.mockUsecase.On("CreateLike", mock.Anything, "user456", "post123").Return(nil)
+
+	// Calling the method
+	suite.controller.CreateLike(suite.mockContext)
+
+	// Assertions
+	assert.Equal(suite.T(), http.StatusOK, suite.mockResponse.Code)
+	assert.JSONEq(suite.T(), `{"message":"Post liked successfully"}`, suite.mockResponse.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *LikeControllerTestSuite) TestDeleteLike_Unauthorized() {
+	suite.mockContext.Request = httptest.NewRequest(http.MethodDelete, "/likes", nil)
+	suite.controller.DeleteLike(suite.mockContext)
+	assert.Equal(suite.T(), http.StatusUnauthorized, suite.mockResponse.Code)
+}
+
+func (suite *LikeControllerTestSuite) TestDeleteLike_Success() {
+	// Setting up the context and parameters
+	suite.mockContext.Params = gin.Params{gin.Param{Key: "id", Value: "like123"}}
+	suite.mockContext.Request = httptest.NewRequest(http.MethodDelete, "/likes/like123", nil)
+	suite.mockContext.Set("role", "user")
+	suite.mockContext.Set("userID", "like123")
+
+	// Mocking the use case
+	suite.mockUsecase.On("DeleteLike", mock.Anything, "like123").Return(nil)
+
+	// Calling the method
+	suite.controller.DeleteLike(suite.mockContext)
+
+	// Assertions
+	assert.Equal(suite.T(), http.StatusOK, suite.mockResponse.Code)
+	assert.JSONEq(suite.T(), `{"message":"Post unliked successfully"}`, suite.mockResponse.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *LikeControllerTestSuite) TestGetLikes_Unauthorized() {
+	suite.mockContext.Request = httptest.NewRequest(http.MethodGet, "/likes", nil)
+	suite.controller.GetLikes(suite.mockContext)
+	assert.Equal(suite.T(), http.StatusUnauthorized, suite.mockResponse.Code)
+}
+
+func (suite *LikeControllerTestSuite) TestGetLikes_Success() {
+	// Setting up the context and parameters
+	suite.mockContext.Params = gin.Params{gin.Param{Key: "postID", Value: "post123"}}
+	suite.mockContext.Request = httptest.NewRequest(http.MethodGet, "/likes/post123", nil)
+	suite.mockContext.Set("role", "user")
+
+	// Mocking the use case with a sample Like object
+	mockLikes := []domain.Like{
+		{
+			ID:     primitive.NewObjectID(),
+			UserID: primitive.NewObjectID(),
+			BlogID: primitive.NewObjectID(),
+		},
+	}
+	suite.mockUsecase.On("GetLikes", mock.Anything, "post123").Return(mockLikes, nil)
+
+	// Calling the method
+	suite.controller.GetLikes(suite.mockContext)
+
+	// Constructing the expected JSON response
+	expectedResponse := `{
+		"likes": [
+			{
+				"_id": "` + mockLikes[0].ID.Hex() + `",
+				"user_id": "` + mockLikes[0].UserID.Hex() + `",
+				"post_id": "` + mockLikes[0].BlogID.Hex() + `"
+			}
+		]
+	}`
+
+	// Assertions
+	assert.Equal(suite.T(), http.StatusOK, suite.mockResponse.Code)
+	assert.JSONEq(suite.T(), expectedResponse, suite.mockResponse.Body.String())
+
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func TestLikeControllerTestSuite(t *testing.T) {
+	suite.Run(t, new(LikeControllerTestSuite))
+}
---
+++ backend/AASTU-backend-group-2/deliveries/controllers/user_controller_test.go
@@ -0,0 +1,221 @@
+package controllers
+
+import (
+	"blog_g2/domain"
+	"bytes"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"blog_g2/mocks"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type UserControllerTestSuite struct {
+	suite.Suite
+	controller  *UserController
+	mockUsecase *mocks.UserUsecase
+}
+
+func (suite *UserControllerTestSuite) SetupTest() {
+	gin.SetMode(gin.TestMode)
+	suite.mockUsecase = new(mocks.UserUsecase)
+	suite.controller = NewUserController(suite.mockUsecase)
+}
+
+func (suite *UserControllerTestSuite) TestUpdateUserDetails_Success() {
+	userID := primitive.NewObjectID()
+	user := domain.User{ID: userID, UserName: "testuser", Email: "test@example.com"}
+	suite.mockUsecase.On("UpdateUserDetails", mock.Anything, &user).Return(nil).Once()
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Set("userid", userID.Hex())
+	context.Request = httptest.NewRequest(http.MethodPut, "/user/update", bytes.NewReader([]byte(`{"username":"testuser","email":"test@example.com"}`)))
+
+	suite.controller.UpdateUserDetails(context)
+
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+	expectedResponse := `{"message":"User details updated successfully"}`
+	assert.JSONEq(suite.T(), expectedResponse, recorder.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *UserControllerTestSuite) TestUpdateUserDetails_BadRequest() {
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Set("userid", "invalid")
+	context.Request = httptest.NewRequest(http.MethodPut, "/user/update", bytes.NewReader([]byte(`{"username":"testuser"}`)))
+
+	suite.controller.UpdateUserDetails(context)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, recorder.Code)
+}
+
+func (suite *UserControllerTestSuite) TestRegisterUser_Success() {
+	// Define the expected user data
+	expectedUser := domain.User{
+		UserName: "testuser",
+		Email:    "test@example.com",
+		Password: "passwoRd123!",
+	}
+
+	// Set up the mock expectation
+	suite.mockUsecase.On("RegisterUser", mock.Anything, mock.MatchedBy(func(user *domain.User) bool {
+		return user.UserName == expectedUser.UserName &&
+			user.Email == expectedUser.Email &&
+			user.Password == expectedUser.Password
+	})).Return(nil).Once()
+
+	// Prepare the recorder and context
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/register", bytes.NewReader([]byte(`{"username":"testuser","email":"test@example.com","password":"passwoRd123!"}`)))
+
+	// Call the RegisterUser method
+	suite.controller.RegisterUser(context)
+
+	// Assert the response status
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+
+	assert.Contains(suite.T(), recorder.Body.String(), `"message":"User registered successfully"`)
+
+	// Verify that mock expectations were met
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *UserControllerTestSuite) TestRegisterUser_BadRequest() {
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/register", bytes.NewReader([]byte(`{"username":"testuser"}`)))
+
+	suite.controller.RegisterUser(context)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, recorder.Code)
+}
+
+func (suite *UserControllerTestSuite) TestLoginUser_Success() {
+	user := domain.User{Email: "test@example.com", Password: "password123"}
+	suite.mockUsecase.On("LoginUser", mock.Anything, user).Return("mocked-token", nil).Once()
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/login", bytes.NewReader([]byte(`{"email":"test@example.com","password":"password123"}`)))
+
+	suite.controller.LoginUser(context)
+
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+	expectedResponse := `{"message":"user logged in","token":"mocked-token"}`
+	assert.JSONEq(suite.T(), expectedResponse, recorder.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *UserControllerTestSuite) TestLoginUser_BadRequest() {
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/login", bytes.NewReader([]byte(`{"email":"test@example.com"}`)))
+
+	suite.controller.LoginUser(context)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, recorder.Code)
+}
+
+func (suite *UserControllerTestSuite) TestForgotPassword_Success() {
+	suite.mockUsecase.On("ForgotPassword", mock.Anything, "test@example.com").Return(nil).Once()
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/forgot-password", bytes.NewReader([]byte(`{"email":"test@example.com"}`)))
+
+	suite.controller.ForgotPassword(context)
+
+	assert.Equal(suite.T(), http.StatusAccepted, recorder.Code)
+	expectedResponse := `{"message":"email succefully sent to the email provided"}`
+	assert.JSONEq(suite.T(), expectedResponse, recorder.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+// func (suite *UserControllerTestSuite) TestForgotPassword_BadRequest() {
+// 	recorder := httptest.NewRecorder()
+// 	context, _ := gin.CreateTestContext(recorder)
+// 	context.Request = httptest.NewRequest(http.MethodPost, "/user/forgot-password", bytes.NewReader([]byte(`{"email":"invalid"}`)))
+
+// 	suite.controller.ForgotPassword(context)
+
+// 	assert.Equal(suite.T(), http.StatusBadRequest, recorder.Code)
+// }
+
+func (suite *UserControllerTestSuite) TestResetPassword_Success() {
+	suite.mockUsecase.On("ResetPassword", mock.Anything, "mock-token", "newpassword").Return(nil).Once()
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/reset-password?token=mock-token", bytes.NewReader([]byte(`{"newPassword":"newpassword"}`)))
+
+	suite.controller.ResetPassword(context)
+
+	assert.Equal(suite.T(), http.StatusAccepted, recorder.Code)
+	expectedResponse := `{"message":"Password has been reset successfully"}`
+	assert.JSONEq(suite.T(), expectedResponse, recorder.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *UserControllerTestSuite) TestResetPassword_BadRequest() {
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/reset-password?token=mock-token", bytes.NewReader([]byte(`{"newPassword":""}`)))
+
+	suite.controller.ResetPassword(context)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, recorder.Code)
+}
+
+func (suite *UserControllerTestSuite) TestLogoutUser_Success() {
+	suite.mockUsecase.On("LogoutUser", mock.Anything, "mock-userid").Return(nil).Once()
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Set("userid", "mock-userid")
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/logout", nil)
+
+	suite.controller.LogoutUser(context)
+
+	assert.Equal(suite.T(), http.StatusOK, recorder.Code)
+	expectedResponse := `{"message":"User logged out successfully"}`
+	assert.JSONEq(suite.T(), expectedResponse, recorder.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *UserControllerTestSuite) TestPromoteDemoteUser_Success() {
+	suite.mockUsecase.On("PromoteDemoteUser", mock.Anything, "mock-id", true).Return(nil).Once()
+
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/promote-demote?id=mock-id&isadmin=true", nil)
+
+	suite.controller.PromoteDemoteUser(context)
+
+	assert.Equal(suite.T(), http.StatusAccepted, recorder.Code)
+	expectedResponse := `{"message":"user admin privilege succesfully updated"}`
+	assert.JSONEq(suite.T(), expectedResponse, recorder.Body.String())
+	suite.mockUsecase.AssertExpectations(suite.T())
+}
+
+func (suite *UserControllerTestSuite) TestPromoteDemoteUser_BadRequest() {
+	recorder := httptest.NewRecorder()
+	context, _ := gin.CreateTestContext(recorder)
+	context.Request = httptest.NewRequest(http.MethodPost, "/user/promote-demote", nil)
+
+	suite.controller.PromoteDemoteUser(context)
+
+	assert.Equal(suite.T(), http.StatusBadRequest, recorder.Code)
+}
+
+func TestUserControllerTestSuite(t *testing.T) {
+	suite.Run(t, new(UserControllerTestSuite))
+}

34e912374c56734ce10fdbdbe6c2bd20b1be1202
Haileamlak <haileamlakbeat@gmail.com>
2024-08-21 10:22:05 +0300

    aait.backend.g1: Add cache service using redis

---
+++ backend/AAiT-backend-group-1/infrastructure/cache_service.go
@@ -0,0 +1,62 @@
+package cache
+
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type CacheService interface {
+	Set(key, value string, expiration time.Duration) error
+	Get(key string) (string, error)
+	Delete(key string) error
+}
+
+type cacheService struct {
+	client *redis.Client
+	ctx    context.Context
+}
+
+// NewcacheService creates a new cacheService instance.
+func NewcacheService(redisAddr, redisPassword string, redisDB int) CacheService {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: redisPassword, 
+		DB:       redisDB,       
+	})
+
+	return &cacheService{
+		client: rdb,
+		ctx:    context.Background(),
+	}
+}
+
+// Set sets a value in the cache with an optional expiration time.
+func (cs *cacheService) Set(key, value string, expiration time.Duration) error {
+	err := cs.client.Set(cs.ctx, key, value, expiration).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get retrieves a value from the cache.
+func (cs *cacheService) Get(key string) (string, error) {
+	val, err := cs.client.Get(cs.ctx, key).Result()
+	if err == redis.Nil {
+		return "", nil // Key does not exist
+	} else if err != nil {
+		return "", err
+	}
+	return val, nil
+}
+
+// Delete removes a value from the cache.
+func (cs *cacheService) Delete(key string) error {
+	err := cs.client.Del(cs.ctx, key).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}

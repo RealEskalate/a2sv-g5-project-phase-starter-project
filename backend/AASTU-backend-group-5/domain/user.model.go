d3e5e988370d2f1d8c443a0444b811c8db832108
ermi9s <ermiayele1122334477@gmail.com>
2024-08-21 17:40:59 +0300

    add forget password and reset password

--- backend/AASTU-backend-group-5/domain/user.model.go
+++ backend/AASTU-backend-group-5/domain/user.model.go
@@ -6,24 +6,23 @@ import (
 
 // actual user model
 type User struct {
-	ID                primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	UserName          string             `json:"username" bson:"username"`
-	Bio               string             `json:"bio" bson:"bio"`
-	ProfilePicture    Media              `json:"profile_picture,omitempty" bson:"profile_picture,omitempty"`
-	Email             string             `json:"email" bson:"email"`
-	Is_Admin          bool               `json:"is_admin" bson:"is_admin"`
-	Password          string             `json:"password,omitempty" bson:"password,omitempty"`
-	VerificationToken string             `json:"verification_token,omitempty" bson:"verification_token,omitempty"`
-	IsVerified        bool               `json:"is_verified" bson:"is_verified"`
-	OAuthProvider     string             `json:"oauth_provider,omitempty" bson:"oauth_provider,omitempty"`
-	OAuthID           string             `json:"oauth_id,omitempty" bson:"oauth_id,omitempty"`
+	ID             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UserName       string             `json:"username" bson:"username"`
+	Bio            string             `json:"bio,omitempty" bson:"bio,omitempty"`
+	ProfilePicture Media              `json:"profile_picture,omitempty" bson:"profile_picture,omitempty"`
+	Email          string             `json:"email" bson:"email"`
+	Is_Admin       bool               `json:"is_admin" bson:"is_admin"`
+	Password       string             `json:"password,omitempty" bson:"password,omitempty"`
+	IsVerified     bool               `json:"is_verified" bson:"is_verified"`
+	OAuthProvider  string             `json:"oauth_provider,omitempty" bson:"oauth_provider,omitempty"`
+	OAuthID        string             `json:"oauth_id,omitempty" bson:"oauth_id,omitempty"`
 }
 
 // user model that will be returned from the server
 type ResponseUser struct {
 	ID             string `json:"_id" bson:"_id"`
 	UserName       string `json:"username" bson:"username"`
-	Bio            string `json:"bio" bson:"bio"`
+	Bio            string `json:"bio,omitempty" bson:"bio,omitempty"`
 	ProfilePicture Media  `json:"profile_picture,omitempty" bson:"profile_picture,omitempty"`
 	Email          string `json:"email" bson:"email"`
 	Is_Admin       bool   `json:"is_admin" bson:"is_admin"`
@@ -31,8 +30,8 @@ type ResponseUser struct {
 
 type UpdateUser struct {
 	UserName       string `json:"username" bson:"username"`
-	Bio            string `json:"bio" bson:"bio"`
-	VerificationToken string             `json:"verification_token,omitempty" bson:"verification_token,omitempty"`
+	Bio            string `json:"bio,omitempty" bson:"bio,omitempty"`
+	IsVerified     bool   `json:"is_verified" bson:"is_verified"`
 	ProfilePicture string `json:"profile_picture" bson:"profile_picture"`
 }
 
@@ -57,12 +56,12 @@ type UpdatePassword struct {
 	Password        string `json:"password" bson:"password"`
 	ConfirmPassword string `json:"confirm_password" bson:"confirm_password"`
 }
-
+ 
 type VerifyEmail struct {
-	Email    string `json:"email" bson:"email"`
+	Email string `json:"email" bson:"email"`
 }
 
-// from actual user model to response model to be don in usecase
+// from actual user model to response model to be done in usecase
 func CreateResponseUser(user User) ResponseUser {
 	return ResponseUser{
 		ID:             user.ID.Hex(),

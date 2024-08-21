26fb4e638f8f1650ef20f24c4a626a37f791c84f
Nahom-Derese <nahomderese@gmail.com>
2024-08-21 15:48:34 +0300

    refactor: Add verification token and timestamps to User struct and implement ActivateUser function

--- backend/ASTU-backend-group-2/domain/user.go
+++ backend/ASTU-backend-group-2/domain/user.go
@@ -23,6 +23,10 @@ type User struct {
 	IsOwner    bool               `json:"is_owner" bson:"is_owner"`
 	Role       string             `json:"role" bson:"role"` //may make only tobe admin or user
 	Tokens     []string           `json:"tokens" bson:"tokens"`
+	VerToken   string             `json:"verify_token" bson:"verfiy_token"`
+	CreatedAt  primitive.DateTime `json:"created_at" bson:"created_at"`
+	UpdatedAt  primitive.DateTime `json:"updated_at" bson:"updated_at"`
+	LastLogin  primitive.DateTime `json:"last_login" bson:"last_login"`
 }
 
 // this structure defined for data sent as a response
@@ -62,6 +66,7 @@ type UserUsecase interface {
 
 	GetUsers(c context.Context, limit int64, page int64) (*[]User, mongopagination.PaginationData, error)
 	UpdateUser(c context.Context, userID string, updatedUser *User) error
+	ActivateUser(c context.Context, userID string) error
 	DeleteUser(c context.Context, userID string) error
 	IsUserActive(c context.Context, userID string) (bool, error)
 
@@ -78,6 +83,7 @@ type UserRepository interface {
 	GetUserById(c context.Context, userId string) (*User, error)
 	GetUsers(c context.Context, limit int64, page int64) (*[]User, mongopagination.PaginationData, error)
 	UpdateUser(c context.Context, userID string, updatedUser *User) (*User, error)
+	ActivateUser(c context.Context, userID string) (*User, error)
 	DeleteUser(c context.Context, userID string) error
 	IsUserActive(c context.Context, userID string) (bool, error)
 	RevokeRefreshToken(c context.Context, userID, refreshToken string) error

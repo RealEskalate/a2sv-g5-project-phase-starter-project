fc802244c7249a646ee932e701241fb2e3144291
Amantsehay <amanueltsehay11@gmail.com>
2024-08-21 17:34:47 +0300

    df

--- backend/AAiT-backend-group-3/internal/delivery/routers/blog_routers.go
+++ backend/AAiT-backend-group-3/internal/delivery/routers/blog_routers.go
@@ -7,10 +7,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func CreateBlogRouter(router *gin.Engine, blogController *controllers.BlogController, authMiddleware middlewares.IAuthMiddleware){
-	router.POST("/blogs", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN") ,blogController.CreateBlog)
-	router.PATCH("/blogs/id", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER") , blogController.EditBlog)
-	router.DELETE("/blogs/:id", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER") ,blogController.DeleteBlog)
-	router.GET("/blogs/:id", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER") ,blogController.GetBlogByID)
-	router.GET("/blogs", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER") ,blogController.GetBlogs)
+func CreateBlogRouter(router *gin.Engine, blogController controllers.BlogControllerInterface, authMiddleware middlewares.IAuthMiddleware) {
+	router.POST("/blogs", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN"), blogController.CreateBlog)
+	router.PATCH("/blogs/id", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER"), blogController.UpdateBlog)
+	router.DELETE("/blogs/:id", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER"), blogController.DeleteBlog)
+	router.GET("/blogs/:id", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER"), blogController.GetBlogByID)
+	router.GET("/blogs", authMiddleware.Authentication(), authMiddleware.RoleAuth("ADMIN", "USER"), blogController.GetBlogs)
 }

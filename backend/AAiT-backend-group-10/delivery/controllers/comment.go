442623819ccd123bb564623c01dbe3d4b6a61b6e
SamrawitDawit <samrikdawit@gmail.com>
2024-08-21 10:12:17 +0300

    Updated the controller to use custom errors

--- backend/AAiT-backend-group-10/delivery/controllers/comment.go
+++ backend/AAiT-backend-group-10/delivery/controllers/comment.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"aait.backend.g10/domain"
 	"aait.backend.g10/usecases"
 	"github.com/gin-gonic/gin"
@@ -14,73 +16,73 @@ type CommentController struct {
 func (cont *CommentController) GetComments(c *gin.Context) {
 	blogID, err := uuid.Parse(c.Param("blog_id"))
 	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
 	}
-	comments, err := cont.CommentUsecase.GetComments(blogID)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	comments, cerr := cont.CommentUsecase.GetComments(blogID)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"comments": comments})
+	c.JSON(http.StatusOK, gin.H{"comments": comments})
 }
 func (cont *CommentController) GetCommentsCount(c *gin.Context) {
 	blogID, err := uuid.Parse(c.Param("blog_id"))
 	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
 	}
-	commentCount, err := cont.CommentUsecase.GetCommentsCount(blogID)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	commentCount, cerr := cont.CommentUsecase.GetCommentsCount(blogID)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"comment count": commentCount})
+	c.JSON(http.StatusOK, gin.H{"comment count": commentCount})
 }
 
 func (cont *CommentController) AddComment(c *gin.Context) {
 	var comment domain.Comment
 	if err := c.BindJSON(&comment); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	err := cont.CommentUsecase.AddComment(comment)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+	cerr := cont.CommentUsecase.AddComment(comment)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": cerr.Error()})
 		return
 	}
-	c.JSON(201, gin.H{"message": "Comment added successfully"})
+	c.JSON(http.StatusCreated, gin.H{"message": "Comment added successfully"})
 }
 
 func (cont *CommentController) UpdateComment(c *gin.Context) {
 	commentID, err := uuid.Parse(c.Param("id"))
 	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
 	}
 	var comment domain.Comment
 	if err := c.BindJSON(&comment); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	requesterID := c.MustGet("id").(uuid.UUID)
 	comment.UserID = requesterID
 	comment.ID = commentID
-	err = cont.CommentUsecase.UpdateComment(requesterID, comment)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+	cerr := cont.CommentUsecase.UpdateComment(requesterID, comment)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"message": "Comment updated successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Comment updated successfully"})
 }
 
 func (cont *CommentController) DelelteComment(c *gin.Context) {
 	commentID, err := uuid.Parse(c.Param("id"))
 	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
 	}
 	requesterID := c.MustGet("id").(uuid.UUID)
 	requesterRole := c.MustGet("is_admin").(bool)
-	if err := cont.CommentUsecase.DelelteComment(commentID, requesterID, requesterRole); err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+	if cerr := cont.CommentUsecase.DelelteComment(commentID, requesterID, requesterRole); err != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"message": "Comment deleted successfuly"})
+	c.JSON(http.StatusOK, gin.H{"message": "Comment deleted successfuly"})
 }
--- backend/AAiT-backend-group-10/delivery/controllers/like.go
+++ backend/AAiT-backend-group-10/delivery/controllers/like.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"aait.backend.g10/domain"
 	"aait.backend.g10/usecases"
 	"github.com/gin-gonic/gin"
@@ -14,43 +16,43 @@ type LikeController struct {
 func (cont *LikeController) LikeBlog(c *gin.Context) {
 	var like domain.Like
 	if err := c.BindJSON(&like); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	err := cont.LikeUseCase.LikeBlog(like)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	cerr := cont.LikeUseCase.LikeBlog(like)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": cerr.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"message": "Like added successfully"})
+	c.JSON(http.StatusCreated, gin.H{"message": "Like added successfully"})
 }
 
 func (cont *LikeController) DeleteLike(c *gin.Context) {
 	var like domain.Like
 	if err := c.BindJSON(&like); err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 	requester_id := c.MustGet("id").(uuid.UUID)
 	like.UserID = requester_id
-	err := cont.LikeUseCase.DeleteLike(like)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	cerr := cont.LikeUseCase.DeleteLike(like)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": cerr.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"message": "Like deleted successfully"})
+	c.JSON(http.StatusOK, gin.H{"message": "Like deleted successfully"})
 }
 
 func (cont *LikeController) BlogLikeCount(c *gin.Context) {
 	blogID, err := uuid.Parse(c.Param("blog_id"))
 	if err != nil {
-		c.JSON(400, gin.H{"error": "Invalid blog ID"})
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid blog ID"})
 		return
 	}
-	count, err := cont.LikeUseCase.BlogLikeCount(blogID)
-	if err != nil {
-		c.JSON(500, gin.H{"error": err.Error()})
+	count, cerr := cont.LikeUseCase.BlogLikeCount(blogID)
+	if cerr != nil {
+		c.JSON(cerr.StatusCode, gin.H{"error": cerr.Error()})
 		return
 	}
-	c.JSON(200, gin.H{"like count": count})
+	c.JSON(http.StatusOK, gin.H{"like count": count})
 }

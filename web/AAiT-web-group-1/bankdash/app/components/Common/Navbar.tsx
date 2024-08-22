f98824b5516333aca42f381a6a9005129be88fa5
Sophonias Demiss <sophidemiss@gmail.com>
2024-08-22 20:47:35 +0300

    Added dashboard components

---
+++ web/AAiT-web-group-1/bankdash/app/components/Common/Navbar.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import logo from "../../../public/images/Logo.svg";
+import bell from "../../../public/images/bell.svg";
+import settings from "../../../public/images/settings.svg";
+import hamburger from "../../../public/images/ham.jpg";
+import Image from "next/image";
+import search from "../../../public/images/search.svg";
+// import Sidebar from "./Sidebar";
+interface Props {
+  title: string;
+  profilepic: string;
+}
+
+const Header = ({ title, profilepic }: Props) => {
+  return (
+    <div className="flex md:pl-8 bg-white md:pr-10 pb-1 items-center justify-between">
+      <div className="w-1/6 md:block hidden">
+        <Image src={logo} className="ml-1" alt="LOGO" />
+      </div>
+
+      <div className="md:w-5/6 w-full md:pl-4 flex">
+        <div className="flex flex-row flex-wrap justify-center md:items-center align-middle w-full space-y-4 md:space-y-0 ">
+          <div className="md:hidden  flex justify-start align-middle pt-3 w-1/3">
+            <Image
+              className="w-8 h-8 order-1 bg-white"
+              src={hamburger}
+              alt="hamburger"
+            />
+          </div>
+
+          <div className="md:text-2xl md:order-none text-xl order-2 md:mr-96 md:w-fit w-1/3 md:mb-0 text-[#343C6A] font-bold">
+            <h1 className="ml-2 font-bold">{title}</h1>
+          </div>
+
+          <div className="flex md:ml-32 order-4 md:order-none bg-[#F5F7FA] h-10 md:w-48 w-full items-center rounded-full  ">
+            <Image src={search} className="w-3 h-3 ml-2" alt="search" />
+            <input
+              className="bg-[#F5F7FA] md:w-38 w-10/12 px-4 outline-none placeholder:bg-[#F5F7FA] placeholder:text-xs"
+              type="text"
+              placeholder={`search for something`}
+            />
+          </div>
+
+          <Image
+            className="w-10 ml-7 h-20 md:block hidden"
+            src={settings}
+            alt="settings"
+          />
+          <Image
+            className="w-10 h-20 ml-7 self-end md:block hidden"
+            src={bell}
+            alt="bell"
+          />
+          <div className="flex justify-end md:w-fit w-1/3 md:mb-5 order-3 md:order-3 md:ml-7">
+            <Image
+              className="w-10 h-10"
+              src={profilepic}
+              alt="profile_picture"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ web/AAiT-web-group-1/bankdash/app/components/Common/Sidebar.tsx
@@ -0,0 +1,192 @@
+import React from "react";
+import dashboard from "../../../public/images/Dashboard.svg";
+import dashboardNone from "../../../public/images/Dashboard-none.svg";
+import transaction from "../../../public/images/transaction.svg";
+import transactionBlue from "../../../public/images/transaction-blue.svg";
+import account from "../../../public/images/user 3 1.svg";
+import accountBlue from "../../../public/images/user-blue.svg";
+import investments from "../../../public/images/economic-investment 1.svg";
+import investmentsBlue from "../../../public/images/investment-blue.svg";
+import credit from "../../../public/images/credit-card 1.svg";
+import creditBlue from "../../../public/images/credit-blue.svg";
+import loans from "../../../public/images/loan 1.svg";
+import loansBlue from "../../../public/images/loan-blue.svg";
+import service from "../../../public/images/service.svg";
+import serviceBlue from "../../../public/images/service-blue.svg";
+import setting from "../../../public/images/setting.svg";
+import settingBlue from "../../../public/images/setting-blue.svg";
+import Image from "next/image";
+import Link from "next/link";
+
+interface Props {
+  selected: string | string[];
+}
+
+const Sidebar = ({ selected }: Props) => {
+  return (
+    <div className="w-1/6 bg-white left-0 h-full pt-1 hidden md:block bg-fixed ">
+      <ul className="flex mt-7 justify-center space-y-7 flex-col">
+
+          <li
+            className={
+              selected === "Dashboard"
+                ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+                : `text-[#B1B1B1] relative flex space-x-5  ` + `  cursor-pointer`
+            }
+          >
+            {selected === "Dashboard" ? (
+              <div>
+                <div className="w-1 h-10 bg-blue-800 absolute left-0 -top-1rounded-r-lg"></div>
+                <Image className="ml-10" src={dashboard} alt="dash" />
+              </div>
+            ) : (
+              <Image className="ml-10" src={dashboardNone} alt="dash" />
+            )}
+            <h2>Dashboard</h2>
+          </li>
+
+
+
+        <li
+          className={
+            selected === "Transactions"
+              ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5  ` + `  cursor-pointer`
+          }
+        >
+          {selected === "Transactions" ? (
+            <div>
+              <div className="w-1 h-10 bg-blue-800 absolute left-0 -top-1 rounded-r-lg"></div>
+              <Image className="ml-10" src={transactionBlue} alt="dash" />
+            </div>
+          ) : (
+            <Image className="ml-10" src={transaction} alt="dash" />
+          )}
+          <h2>Transactions</h2>
+        </li>
+
+
+
+        <li
+          className={
+            selected === "Accounts"
+              ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5 ` + `  cursor-pointer`
+          }
+        >
+          {selected === "Accounts" ? (
+            <div>
+              <div className="w-1 h-10 bg-blue-800 absolute left-0 -top-1 rounded-r-lg"></div>
+              <Image className="ml-10" src={accountBlue} alt="dash" />
+            </div>
+          ) : (
+            <Image className="ml-10" src={account} alt="dash" />
+          )}
+          <h2>Accounts</h2>
+        </li>
+
+
+
+        <li
+          className={
+            selected === "Investments"
+              ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5 ` + `  cursor-pointer`
+          }
+        >
+          {selected === "Investments" ? (
+            <div>
+              <div className="w-1 h-10 bg-blue-800 absolute left-0 -top-1 rounded-r-lg"></div>
+              <Image className="ml-10" src={investmentsBlue} alt="dash" />
+            </div>
+          ) : (
+            <Image className="ml-10" src={investments} alt="dash" />
+          )}
+          <h2>Investments</h2>
+        </li>
+
+
+
+        <li
+          className={
+            selected === "Credit Cards"
+              ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5  ` + `  cursor-pointer`
+          }
+        >
+          {selected === "Credit Cards" ? (
+            <div>
+              <div className="w-1 h-10 bg-blue-800 absolute left-0 -top-1 rounded-r-lg"></div>
+              <Image className="ml-10" src={creditBlue} alt="dash" />
+            </div>
+          ) : (
+            <Image className="ml-10" src={credit} alt="dash" />
+          )}
+          <h2>Credit Cards</h2>
+        </li>
+
+
+
+        <li
+          className={
+            selected === "Loans"
+              ? `text-[#2D60FF] relative font-semibold ]  flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5 ` + `  cursor-pointer `
+          }
+        >
+          {selected === "Loans" ? (
+            <div>
+              <div className="w-1 h-10 bg-blue-800  absolute left-0 --top-1 rounded-r-lg"></div>
+              <Image className="ml-10" src={loansBlue} alt="dash" />
+            </div>
+          ) : (
+            <Image className="ml-10" src={loans} alt="dash" />
+          )}
+          <h2>Loans</h2>
+        </li>
+
+
+
+        <li
+          className={
+            selected === "Services"
+              ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5  ` + `  cursor-pointer`
+          }
+        >
+          {selected === "Services" ? (
+            <div>
+              <div className="w-1 h-10 bg-blue-800 absolute left-0 --top-1rounded-r-lg"></div>
+              <Image className="ml-10" src={serviceBlue} alt="dash" />
+            </div>
+          ) : (
+            <Image className="ml-10" src={service} alt="dash" />
+          )}
+          <h2>Services</h2>
+        </li>
+
+
+
+        <li
+          className={
+            selected === "Setting"
+              ? `text-[#2D60FF] relative font-semibold ] flex space-x-5`
+              : `text-[#B1B1B1] relative flex space-x-5  ` + `  cursor-pointer`
+          }
+        >
+          {selected === "Setting" ? (
+            <Image className="ml-10" src={settingBlue} alt="dash" />
+          ) : (
+            <Image className="ml-10" src={setting} alt="dash" />
+          )}
+          <h2>Setting</h2>
+        </li>
+
+
+
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;

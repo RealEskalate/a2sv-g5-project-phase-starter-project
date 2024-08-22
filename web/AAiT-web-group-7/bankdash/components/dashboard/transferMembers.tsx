c521f69f8889d5a842c2279e9b41c279af7e0562
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-08-22 14:16:16 +0300

    dashboard page

---
+++ web/AAiT-web-group-7/bankdash/components/dashboard/transferMembers.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import React from 'react'
+import img from '@/public/person.jpg'
+
+interface Person {
+    name: string;
+    role: string;
+}
+
+const TransferMembers = ({person, selectedPerson}: {person: Person, selectedPerson:string}) => {
+  return (
+    <div className="flex flex-col items-center">
+              <Image
+                src={img}
+                alt={person.name}
+                className="w-16 h-16 rounded-full object-cover mb-2"
+                width={64}
+                height={64}
+              />
+              <p className={`font-semibold ${selectedPerson == person.name? "text-[#5f7df4]": ""} text-sm `}>{person.name}</p>
+              <p className={`text-sm text-gray-500 ${selectedPerson == person.name? "text-[#5f7df4]": ""}`}>{person.role}</p>
+            </div>
+  )
+}
+
+export default TransferMembers

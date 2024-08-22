cca2f01fbfab5d2f26f558938a6181c94a3575c9
Johna210 <solomonjohna21@gmail.com>
2024-08-22 10:18:02 +0300

    setup redis and cached routes

--- backend/AAiT-backend-group-4/Bootstrap/app.go
+++ backend/AAiT-backend-group-4/Bootstrap/app.go
@@ -1,16 +1,21 @@
 package bootstrap
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"github.com/redis/go-redis/v9"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type Application struct {
 	Env   *Env
 	Mongo mongo.Client
+	Redis *redis.Client
 }
 
 func App() Application {
 	app := &Application{}
 	app.Env = NewEnv()
 	app.Mongo = NewMongoDatabase(app.Env)
+	app.Redis = ConnectRedis(app.Env)
 	return *app
 }
 

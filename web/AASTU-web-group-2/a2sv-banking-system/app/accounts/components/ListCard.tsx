9dc70ae5a877486ab24f0829245fcbb556ef47f1
rebumatadele <118250996+rebumatadele@users.noreply.github.com>
2024-08-22 09:29:46 +0300

    fixed the loader page

--- web/AASTU-web-group-2/a2sv-banking-system/app/accounts/components/ListCard.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/accounts/components/ListCard.tsx
@@ -15,26 +15,42 @@ type Column = {
 
 interface Props {
   column: Column;
-  width: string
+  width: string;
 }
 
 const ListCard = ({ column, width }: Props) => {
   return (
-      <div className={`flex gap-3 items-center rounded-2xl px-5 py-4 bg-white ${width}`}>
-        <div className={`text-3xl px-2 py-2 rounded-full ${column.iconStyle}`}>
-          <column.icon />
-        </div>
-        <div className="flex justify-between w-full">
-          {column.data.map((item, index) => (
-            <div key={index}>
-              <div className={`${item.headingStyle}`}>
-                {item.heading}
-              </div>
-              <div className={item.dataStyle}>{item.text}</div>
-            </div>
-          ))}
+    <div
+      className={`flex gap-3 items-center rounded-2xl px-5 py-4 bg-white ${width}`}
+    >
+      <div className={`text-3xl px-2 py-2 rounded-full ${column.iconStyle}`}>
+        <column.icon />
+      </div>
+      <div className="flex justify-between w-full">
+        {column.data.map((item, index) => (
+          <div key={index}>
+            <div className={`${item.headingStyle}`}>{item.heading}</div>
+            <div className={item.dataStyle}>{item.text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const ListCardLoading = () => {
+  return (
+    <div className="flex gap-3 items-center rounded-2xl px-5 py-4 bg-white w-[48%] md:w-[23%] animate-pulse">
+      <div className="text-3xl px-2 py-2 rounded-xl bg-gray-200">
+        <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+      </div>
+      <div className="flex justify-between w-full flex-col">
+        <div>
+          <div className="h-4 bg-gray-300 rounded w-full mb-2"></div>
+          <div className="h-4 bg-gray-300 rounded w-full"></div>
         </div>
       </div>
+    </div>
   );
 };
 

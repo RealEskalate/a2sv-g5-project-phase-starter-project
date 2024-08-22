b0bf06e18a9aaa9e35f4e82f77c47c81b20c9fbc
Fraol7 <fraolmulugeta17@gmail.com>
2024-08-22 16:53:22 +0300

    setting up nextjs

---
+++ web/AAiT-web-group-8/bank-dash/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'via.placeholder.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;

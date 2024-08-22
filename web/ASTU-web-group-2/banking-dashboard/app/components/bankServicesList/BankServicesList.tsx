b2c01c4f6b55e8a8d67fe6dbed4e8b3c2ccedfcf
wondmD <wondmenehdereje@gmail.com>
2024-08-22 16:54:36 +0300

    adede responsiveness to component

--- web/ASTU-web-group-2/banking-dashboard/app/components/bankServicesList/BankServicesList.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/bankServicesList/BankServicesList.tsx
@@ -22,9 +22,13 @@ const BankServicesList = () => {
 
   if (isLoading) {
     return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="w-16 h-16 border-t-4 border-b-4 border-blue-500 rounded-full animate-spin"></div>
-      </div>
+      <div className="flex justify-center items-center flex-col flex-initial flex-wrap h-[225px] w-full bg-white animate-pulse rounded-[25px]">
+            <div className="flex flex-row gap-2">
+              <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+              <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.3s]"></div>
+              <div className="w-4 h-4 rounded-full bg-blue-700 animate-bounce [animation-delay:.7s]"></div>
+            </div>
+          </div>
     );
   }
   const data = res.data!.content!;

4b98be9e3c943cce9f5fb60e17e99c2d16a2e4b1
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-22 14:37:22 +0300

    chore: Update the function to be testable and refactor code structure

--- backend/ASTU-backend-group-1/repository/blogrespository.go
+++ backend/ASTU-backend-group-1/repository/blogrespository.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"time"
 
+	"github.com/sv-tools/mongoifc"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,10 +15,10 @@ import (
 )
 
 type MongoBlogRepository struct {
-	collection *mongo.Collection
+	collection mongoifc.Collection
 }
 
-func NewBlogRepository(collection *mongo.Collection) domain.BlogRepository {
+func NewBlogRepository(collection mongoifc.Collection) domain.BlogRepository {
 	return &MongoBlogRepository{
 		collection: collection,
 	}

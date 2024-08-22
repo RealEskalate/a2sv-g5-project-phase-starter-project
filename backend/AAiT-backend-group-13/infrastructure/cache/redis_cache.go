c43eb6863c542849f1a7cd702af39963d0d9da08
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-22 11:15:08 +0300

    style: run gopls as formmter

--- backend/AAiT-backend-group-13/infrastructure/cache/redis_cache.go
+++ backend/AAiT-backend-group-13/infrastructure/cache/redis_cache.go
@@ -18,40 +18,38 @@ var _ icache.ICache = &RedisCache{}
 
 func NewRedisCache(client *redis.Client, expiryDay time.Duration) *RedisCache {
 	return &RedisCache{
-		client: client,
+		client:    client,
 		expiryDay: time.Second * expiryDay,
 	}
 }
-
 
 func (r *RedisCache) Get(key string) (interface{}, error) {
 	val, err := r.client.Get(context.Background(), key).Result()
 	if err != nil {
 		if err == redis.Nil {
-			return nil, nil 
+			return nil, nil
 		}
-		return nil, err 
+		return nil, err
 	}
 
 	var result interface{}
 	err = json.Unmarshal([]byte(val), &result)
 	if err != nil {
-		return nil, err 
+		return nil, err
 	}
 
 	return result, nil
 }
 
-
 func (r *RedisCache) Set(key string, value interface{}) error {
 	jsonData, err := json.Marshal(value)
 	if err != nil {
-		return err 
+		return err
 	}
 
 	err = r.client.Set(context.Background(), key, jsonData, r.expiryDay).Err()
 	if err != nil {
-		return err 
+		return err
 	}
 
 	return nil

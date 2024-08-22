3612cb9e0e1fc79d7647945db25f12df262c227d
SalasDelil <salsdelil8@gmail.com>
2024-08-22 14:40:21 +0300

    Add mycard, myexpense, recent transaction, and weekly acrivity bar chart components

--- web/AAiT-web-group-2/bank-dashboard/app/page.tsx
+++ web/AAiT-web-group-2/bank-dashboard/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import MyCards from "./components/MyCards";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
       <h1>Bank dashboard</h1>
-        
+      <MyCards />
     </main>
   );
 }

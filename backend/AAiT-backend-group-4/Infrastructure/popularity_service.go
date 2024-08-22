7db166d812b60994ca53f7192852a1017098d87e
Johna210 <solomonjohna21@gmail.com>
2024-08-22 15:11:50 +0300

    Added popularity service

--- backend/AAiT-backend-group-4/Infrastructure/popularity_service.go
+++ backend/AAiT-backend-group-4/Infrastructure/popularity_service.go
@@ -15,5 +15,4 @@ func CalculatePopularity(fb *domain.Feedback) float64 {
 	val := (float64(fb.Likes) * 2) + (float64(fb.View_count) * 1.2) +
 		(float64(len(fb.Comments)) * 1.5) - float64(fb.Dislikes)
 	return val
-
 }

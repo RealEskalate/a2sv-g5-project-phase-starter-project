5700714e227f97f16f844384c1c7e5aaffbc4ff9
Sofonyas Tizazu <sofstzz@gmail.com>
2024-08-22 15:18:35 +0300

    review and recommend endpoints for ai integration

--- backend/AAiT-backend-group-13/usecase/ai_recommendation/query/gemini_generate_handler.go
+++ backend/AAiT-backend-group-13/usecase/ai_recommendation/query/gemini_generate_handler.go
@@ -2,6 +2,8 @@ package geminiService
 
 import (
 	"context"
+	"time"
+
 	"github.com/google/generative-ai-go/genai"
 	icmd "github.com/group13/blog/usecase/common/cqrs/command"
 )
@@ -20,10 +22,15 @@ func NewReccomendationHandler(model *genai.GenerativeModel ) *RecomendationHandl
 
 func (h *RecomendationHandler) Handle(cmd *RecommendationCommand) (*genai.GenerateContentResponse, error) {	
 	ctx := context.Background()
+	q := "Generate a blog on:"
+	ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
+    defer cancel()
+	
 	resp, err := h.model.GenerateContent(
 		ctx,
-		genai.Text(*cmd.request),        
+		genai.Text( q + *cmd.request),        
 	)
+	cancel()
 
 	return resp, err
 }

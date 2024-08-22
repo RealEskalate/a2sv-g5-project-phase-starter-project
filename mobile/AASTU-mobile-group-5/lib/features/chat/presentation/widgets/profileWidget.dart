3c5d3813e07a50cae9a8cf7f2f8b2f05e43b0d19
makda <makdayosephzewge@gmail.com>
2024-08-22 15:29:01 -0400

    profile widget updated

--- mobile/AASTU-mobile-group-5/lib/features/chat/presentation/widgets/profileWidget.dart
+++ mobile/AASTU-mobile-group-5/lib/features/chat/presentation/widgets/profileWidget.dart
@@ -1,39 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ProfileWidget extends StatelessWidget {
+  const ProfileWidget({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        alignment: Alignment.center,
-        
-        
-        decoration:BoxDecoration(
-          color: Colors.blue.shade300,
-          shape: BoxShape.circle
-        ),
-        child: Card(
-          shape: CircleBorder(),                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-          
-          child: ClipOval(
-            child: Container(
-              child: Container(
-                width: 100,
-                height: 100,
-                 child: Container(
-                  width: 600,
-                  height: 600,
-                  child: Image.asset('assets/images/Alex.png',
-                  fit: BoxFit.fill, 
-                      width: double.infinity, 
-                      height: double.infinity,
-                  ),
-                          
-              ),
-              ),
-            ),
-          ),
-        ),
+    return CircleAvatar(
+      radius: 32,
+      backgroundColor:Colors.blue.shade300,
+      child: CircleAvatar(
+        radius:150,
+        backgroundImage:  Image.asset('assets/images/Alex.png',).image,
       ),
     );
     

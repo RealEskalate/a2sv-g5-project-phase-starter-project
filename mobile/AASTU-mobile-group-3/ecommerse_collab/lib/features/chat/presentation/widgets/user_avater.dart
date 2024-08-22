f6197cc4a2e712d9a02e5dac8f9a2b3601b4f75b
Mihretthe <mihretekalgn@gmail.com>
2024-08-22 23:36:57 +0300

    Pushing the avaters

--- mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/presentation/widgets/user_avater.dart
+++ mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/presentation/widgets/user_avater.dart
@@ -1,3 +1,19 @@
-class UserAvater{
-  
+import 'package:flutter/material.dart';
+
+class UserAvater extends StatelessWidget {
+  final String image;
+  const UserAvater({super.key, required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.amber[100],
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Image.asset(image),
+    );
+  }
 }

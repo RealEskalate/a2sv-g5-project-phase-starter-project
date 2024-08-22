b69ad1ab1685b161a7c6a532ae4330b0d6c1470e
Mohammed Ali <mohaali482@gmail.com>
2024-08-22 15:11:49 +0300

    feat(main): finished working on the main file

--- backend/ASTU-backend-group-4/cmd/api/main.go
+++ backend/ASTU-backend-group-4/cmd/api/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -12,4 +14,23 @@ func init() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+}
+
+func getHost() string {
+	hostUrl := os.Getenv("HOST_URL")
+	if hostUrl != "" {
+		return hostUrl
+	}
+
+	return "localhost:8000"
+}
+
+func main() {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	// routers.Setup(c.Database(os.Getenv("MONGO_DB")), r)
+
+	r.Run(getHost())
 }

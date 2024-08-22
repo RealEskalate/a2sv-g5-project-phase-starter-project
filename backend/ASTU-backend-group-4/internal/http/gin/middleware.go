d28f158215e41636f7ac9885a7bf4c08e5d22b3e
Kidistbezabih <kidistbezabh@gmail.com>
2024-08-22 15:18:41 +0300

    finish the implementation of auth middleware

---
+++ backend/ASTU-backend-group-4/internal/http/gin/middleware.go
@@ -0,0 +1,62 @@
+package gin
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "auth header"})
+			c.Abort()
+			return
+		}
+		authParts := strings.Split(authHeader, " ")
+		if len(authParts) != 2 || strings.ToLower(authParts[0]) != "bearer" {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "unautorized access"})
+			c.Abort()
+			return
+		}
+		tokenClaim := jwt.MapClaims{}
+		_, err := jwt.ParseWithClaims(authParts[1], tokenClaim, validateSigningMethod)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
+		}
+		c.Set("userID", tokenClaim["id"])
+		c.Set("isAdmin", tokenClaim["isadmin"])
+
+		c.Next()
+	}
+}
+
+func AdminMidleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// uses_name, user_exist := c.Get("userame")
+		role, role_exist := c.Get("isAdmin")
+		roleParsed := role.(bool)
+
+		if !role_exist || !roleParsed {
+			c.JSON(http.StatusUnauthorized, gin.H{"message": "unautorized access"})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
+
+func validateSigningMethod(token *jwt.Token) (interface{}, error) {
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+	}
+	return os.Getenv("SECTER_KEY"), nil
+}

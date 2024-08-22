f9a8fd14467b573dc06517b5fa432468de6dfb4a
kidistbezabih <97032250+kidistbezabih@users.noreply.github.com>
2024-08-22 06:56:52 +0300

    implement user activation functionality and relateds (#1341)
    * implement user activation functionality and relateds
    
    * add git ignore
    
    * feat: add .env file

--- backend/ASTU-backend-group-4/auth/usecase.go
+++ backend/ASTU-backend-group-4/auth/usecase.go
@@ -2,63 +2,68 @@ package auth
 
 import (
 	"context"
+	"encoding/base64"
+	"fmt"
 	"os"
+	"time"
 
+	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/pkg/infrastructure"
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthUserUsecase struct {
-	repository AuthRepository
+	repository   AuthRepository
+	emailService infrastructure.EmailService
 }
 
-func NewAuthUserUsecase(repository AuthRepository) AuthUserUsecase {
-	return AuthUserUsecase{
-		repository: repository,
+func NewAuthUserUsecase(repository AuthRepository, emailService infrastructure.EmailService) AuthServices {
+	return &AuthUserUsecase{
+		repository:   repository,
+		emailService: emailService,
 	}
 
 }
 
-func (au *AuthUserUsecase) Login(ctx context.Context, info LoginForm) (string, error) {
+func (au *AuthUserUsecase) Login(ctx context.Context, info LoginForm) (string, string, error) {
 	// panic("not implemented") // TODO: Implement
 	// var userInfo LoginForm
 	// find the user name and match the hashed password with ith info.password
 	user, err := au.repository.GetUserByUsername(ctx, info.Username)
 	if err != nil {
-		return "", ErrNoUserWithUsername
+		return "", "", ErrNoUserWithUsername
 	}
 	if !user.IsActive {
-		return "", ErrAccountNotActivated
+		return "", "", ErrAccountNotActivated
 	}
 	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(info.Password))
 	if err != nil {
-		return "", ErrIncorrectPassword
+		return "", "", ErrIncorrectPassword
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":        user.ID,
-		"name":      user.Name,
-		"username":  user.Username,
-		"email":     user.Email,
-		"isadmin":   user.IsAdmin,
-		"isactive":  user.IsActive,
-		"create":    user.CreatedAt,
-		"updatedat": user.UpdatedAt,
-	})
-	jwtToken, err := token.SignedString(os.Getenv("SECRET_KEY"))
 
+	refToken, err := au.GenerateToken(user, "refresh")
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
-	return jwtToken, nil
+	accessToken, err := au.GenerateToken(user, "access")
+	if err != nil {
+		return "", "", err
+	}
+	err = au.repository.RegisterRefreshToken(ctx, user.ID, refToken)
+	if err != nil {
+		return "", "", err
+	}
+
+	return refToken, accessToken, nil
 }
 
 func (au *AuthUserUsecase) RegisterUser(ctx context.Context, user User) error {
 	// var newUser User
-	user, err := au.repository.GetUserByEmail(ctx, user.Email)
+	_, err := au.repository.GetUserByEmail(ctx, user.Email)
 	if err == nil {
 		return ErrUserExistWithThisEmail
 	}
-	user, err = au.repository.GetUserByUsername(ctx, user.Username)
+	_, err = au.repository.GetUserByUsername(ctx, user.Username)
 	if err == nil {
 		return ErrUserExistWithThisUsername
 	}
@@ -69,13 +74,23 @@ func (au *AuthUserUsecase) RegisterUser(ctx context.Context, user User) error {
 	}
 	user.Password = string(hashedpasswors)
 	user.IsActive = false
-	err = au.repository.CreateUser(ctx, user)
+	// user.IsAdmin = false	activationLink := fmt.Sprintf("http://localhost/activate/%s/%s", user.ID, tokenString)
+
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = time.Now()
+	_, err = au.repository.CreateUser(ctx, user)
 	if err != nil {
 		return ErrCantCreateUser
 	}
-	return nil
-	// id, err := au.repository.CreateUser(ctx, user)
+	tokenString, err := au.GenerateActivateToken(user.Password, user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	from := os.Getenv("FROM")
+	activationLink := fmt.Sprintf("http://localhost/activate/%s/%s", user.ID, tokenString)
+	au.emailService.SendEmail(from, user.Email, "click the link to activate you account"+activationLink)
 
+	return nil
 }
 
 func (au *AuthUserUsecase) UpdateProfile(ctx context.Context, user User) error {
@@ -86,14 +101,69 @@ func (au *AuthUserUsecase) UpdateProfile(ctx context.Context, user User) error {
 	return nil
 }
 
-func (au *AuthUserUsecase) Activate(userID string, token string) {
+func (au *AuthUserUsecase) Activate(ctx context.Context, userID string, token string) error {
+	// refreshToekn, err := au.repository.GetRefreshToken(ctx, token)
+	user, err := au.repository.GetUserByID(ctx, userID)
+	if err != nil {
+		return err
+	}
+	expectedToken, err := au.GenerateActivateToken(user.Password, user.UpdatedAt)
+	if err != nil {
+		return err
+	}
+	if expectedToken != token {
+		return err
+	}
 
+	user.IsActive = true
+	user.UpdatedAt = time.Now()
+
+	_, err = au.repository.UpdateUser(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func (au *AuthUserUsecase) Logout(userID string) {
-	panic("not implemented") // TODO: Implement
+func (au *AuthUserUsecase) Logout(ctx context.Context, userID string) {
+	token, err := au.repository.GetRefreshToken(ctx, userID)
+	if err != nil {
+		return
+	}
+	au.repository.DeleteRefreshToken(ctx, token)
 }
 
-func (au *AuthUserUsecase) GenerateToken(user User) (string, error) {
-	panic("not implemented") // TODO: Implement
+func (au *AuthUserUsecase) GenerateActivateToken(hashedpassword string, updatedat time.Time) (string, error) {
+	token := hashedpassword + updatedat.String()
+	tokenbyte, err := bcrypt.GenerateFromPassword([]byte(token), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	token = base64.StdEncoding.EncodeToString(tokenbyte)
+	return token, nil
+}
+
+func (au *AuthUserUsecase) GenerateToken(user User, tokenType string) (string, error) {
+	secretKey := os.Getenv("SECRET_KEY")
+
+	claims := jwt.MapClaims{
+		"id":       user.ID,
+		"name":     user.Name,
+		"username": user.Username,
+		"email":    user.Email,
+		"isadmin":  user.IsAdmin,
+		"isactive": user.IsActive,
+		"exp":      time.Now().Add(time.Hour).Unix(),
+		"type":     tokenType,
+	}
+	if tokenType == "refresh" {
+		claims["exp"] = time.Now().Add(7 * 24 * time.Hour).Unix()
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+
 }
---
+++ backend/ASTU-backend-group-4/pkg/infrastructure/email.go
@@ -0,0 +1,40 @@
+package infrastructure
+
+import (
+	"errors"
+	"fmt"
+	"net/smtp"
+)
+
+type EmailService interface {
+	SendEmail(from, to, body string) error
+}
+
+type Email struct {
+	Username string
+	Password string
+	Host     string
+	Port     string
+}
+
+func NewEmail(username, password, host, port string) Email {
+	return Email{
+		Username: username,
+		Password: password,
+		Host:     host,
+		Port:     port,
+	}
+}
+
+func (e *Email) SendEmail(from, to, body string) error {
+	auth := smtp.PlainAuth("", e.Username, e.Password, e.Host)
+	message := []byte(fmt.Sprintf("This Message of From %s To %s", from, to) +
+		"Subject: Account Activation \r\n" +
+		fmt.Sprintf("%s\r\n", body))
+	addr := fmt.Sprintf("%s:%s", e.Host, e.Port)
+	err := smtp.SendMail(addr, auth, from, []string{to}, message)
+	if err != nil {
+		return errors.New("failed to send an email")
+	}
+	return nil
+}

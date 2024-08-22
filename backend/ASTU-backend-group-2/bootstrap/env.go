274f2487e9bcbe6997b5a6571d4b74574761db3f
dagota12 <chernetdagim@gmail.com>
2024-08-22 17:33:56 +0300

    fix: the blog filteration and pagination also added image uploat functionality static files that are stored on the server it should be updated to use third party image server

--- backend/ASTU-backend-group-2/bootstrap/env.go
+++ backend/ASTU-backend-group-2/bootstrap/env.go
@@ -7,28 +7,28 @@ import (
 )
 
 type Env struct {
-	AppEnv                 string `mapstructure:"APP_ENV"`
-	ServerAddress          string `mapstructure:"SERVER_ADDRESS"`
-	ContextTimeout         int    `mapstructure:"CONTEXT_TIMEOUT"`
-	DBHost                 string `mapstructure:"DB_HOST"`
-	DBPort                 string `mapstructure:"DB_PORT"`
-	DBUser                 string `mapstructure:"DB_USER"`
-	DBPass                 string `mapstructure:"DB_PASS"`
-	DBName                 string `mapstructure:"DB_NAME"`
-	AccessTokenExpiryHour  int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
-	RefreshTokenExpiryHour int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
-	AccessTokenSecret      string `mapstructure:"ACCESS_TOKEN_SECRET"`
+	AppEnv                     string `mapstructure:"APP_ENV"`
+	ServerAddress              string `mapstructure:"SERVER_ADDRESS"`
+	ContextTimeout             int    `mapstructure:"CONTEXT_TIMEOUT"`
+	DBHost                     string `mapstructure:"DB_HOST"`
+	DBPort                     string `mapstructure:"DB_PORT"`
+	DBUser                     string `mapstructure:"DB_USER"`
+	DBPass                     string `mapstructure:"DB_PASS"`
+	DBName                     string `mapstructure:"DB_NAME"`
+	AccessTokenExpiryHour      int    `mapstructure:"ACCESS_TOKEN_EXPIRY_HOUR"`
+	RefreshTokenExpiryHour     int    `mapstructure:"REFRESH_TOKEN_EXPIRY_HOUR"`
+	AccessTokenSecret          string `mapstructure:"ACCESS_TOKEN_SECRET"`
 	VerificationTokenExpiryMin int    `mapstructure:"VERIFICATION_TOKEN_EXPIRY_MIN"`
-	VerificationTokenSecret  string `mapstructure:"VERIFICATION_TOKEN_SECRET"`  
-	RefreshTokenSecret     string `mapstructure:"REFRESH_TOKEN_SECRET"`
-	SenderEmail            string `mapstructure:"SENDER_EMAIL"`
-	SmtpPort               string `mapstructure:"SMTP_PORT"`
-	SmtpHost               string `mapstructure:"SMTP_HOST"`
-	SenderPassword         string `mapstructure:"SENDER_PASSWORD"` 
-	GoogleClientID         string `mapstructure:"GOOGLE_CLIENT_ID"`
-	GoogleClientSecret     string `mapstructure:"GOOGLE_CLIENT_SECRET"`
-	GoogleUrlCallback      string `mapstructure:"GOOGLE_URL_CALLBACK"`
-	SessionSecret          string `mapstructure:"SESSION_SECRET"`
+	VerificationTokenSecret    string `mapstructure:"VERIFICATION_TOKEN_SECRET"`
+	RefreshTokenSecret         string `mapstructure:"REFRESH_TOKEN_SECRET"`
+	SenderEmail                string `mapstructure:"SENDER_EMAIL"`
+	SmtpPort                   string `mapstructure:"SMTP_PORT"`
+	SmtpHost                   string `mapstructure:"SMTP_HOST"`
+	SenderPassword             string `mapstructure:"SENDER_PASSWORD"`
+	GoogleClientID             string `mapstructure:"GOOGLE_CLIENT_ID"`
+	GoogleClientSecret         string `mapstructure:"GOOGLE_CLIENT_SECRET"`
+	GoogleUrlCallback          string `mapstructure:"GOOGLE_URL_CALLBACK"`
+	SessionSecret              string `mapstructure:"SESSION_SECRET"`
 }
 
 func NewEnv() *Env {
@@ -44,7 +44,7 @@ func NewEnv() *Env {
 	if err != nil {
 		log.Fatal("Environment can't be loaded: ", err)
 	}
-
+	log.Println(env)
 	if env.AppEnv == "development" {
 		log.Println("The App is running in development env")
 	}

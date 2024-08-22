0e848357b4b7957eb93efc58af95f6d0f8219497
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-22 15:27:04 +0300

    chore: Update MongoDB URI in config.yaml and main.go

--- backend/ASTU-backend-group-1/delivery/main.go
+++ backend/ASTU-backend-group-1/delivery/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	infrastructure "astu-backend-g1/Infrastructure"
+	"astu-backend-g1/config"
 	"astu-backend-g1/delivery/controllers"
 	router "astu-backend-g1/delivery/routers"
 	"astu-backend-g1/repository"
@@ -14,15 +15,15 @@ import (
 )
 
 func main() {
-	clientOptions := options.Client().ApplyURI("mongodb+srv://Legend:AmIHDrlwCqmxgjy2@pycluster.ajv4lb8.mongodb.net/?retryWrites=true&w=majority&appName=pycluster")
+	config,err := config.LoadConfig()
+	clientOptions := options.Client().ApplyURI(config.Database.Uri)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
 		panic(err)
 	}
 	blogCollections := client.Database("BlogAPI").Collection("Blogs")
 	userCollections := client.Database("BlogAPI").Collection("Users")
-	// auth := infrastructure.NewAuthMiddleware(userCollections)
-	// auther := infrastructure.GeneralAuthorizer(auth)
+
 	authController := infrastructure.NewAuthController()
 	_ = client.Database("BlogAPI").Collection("Tokens")
 	blogRepo := repository.NewBlogRepository(mongoifc.WrapCollection(blogCollections))

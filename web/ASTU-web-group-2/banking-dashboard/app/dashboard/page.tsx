1be1a9ec2d27f2f2b3cc421745005f0ec5d5e95b
dura12 <duranu104@gmail.com>
2024-08-22 11:35:47 +0300

    landing page

---
+++ web/ASTU-web-group-2/banking-dashboard/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "../components/dashboard/footer";
+import About from "../components/dashboard/about";
+import Services from "../components/dashboard/services";
+import React from 'react'
+
+const LandingPAge = () => {
+  return (
+    <div>
+        <Services/>
+        <About/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default LandingPAge

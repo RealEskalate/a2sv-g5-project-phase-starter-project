3ddf28b1ca93ed9e3c7d33ea2c6edf9cc2296fca
Dagim Mesfin <101246148+DagmMesfin@users.noreply.github.com>
2024-08-22 11:18:01 +0300

    [fix]: user usecase test complete

--- backend/AASTU-backend-group-2/usecase/user_usecase_test.go
+++ backend/AASTU-backend-group-2/usecase/user_usecase_test.go
@@ -1,105 +1,105 @@
 package usecase_test
 
-// import (
-// 	"blog_g2/domain"
-// 	"blog_g2/mocks"
-// 	"blog_g2/usecase"
-// 	"context"
-// 	"time"
+import (
+	"blog_g2/domain"
+	"blog_g2/mocks"
+	"blog_g2/usecase"
+	"context"
+	"time"
 
-// 	"github.com/stretchr/testify/suite"
-// )
+	"github.com/stretchr/testify/suite"
+)
 
-// // UserUseCasetestSuite struct to hold any shared resources or setup for the tests
-// type UserUseCasetestSuite struct {
-// 	suite.Suite
-// 	mockUserRepository *mocks.UserRepository
-// 	UserUsecase        domain.UserUsecase
-// }
+// UserUseCasetestSuite struct to hold any shared resources or setup for the tests
+type UserUseCasetestSuite struct {
+	suite.Suite
+	mockUserRepository *mocks.UserRepository
+	UserUsecase        domain.UserUsecase
+}
 
-// // SetupTest runs before each test case
-// func (s *UserUseCasetestSuite) SetupTest() {
-// 	s.mockUserRepository = new(mocks.UserRepository)
-// 	s.UserUsecase = usecase.NewUserUsecase(s.mockUserRepository, time.Second*2)
-// }
+// SetupTest runs before each test case
+func (s *UserUseCasetestSuite) SetupTest() {
+	s.mockUserRepository = new(mocks.UserRepository)
+	s.UserUsecase = usecase.NewUserUsecase(s.mockUserRepository, time.Second*2)
+}
 
-// // TearDownTest runs after each test case
-// func (s *UserUseCasetestSuite) TearDownTest() {
-// 	// Clean up resources if needed
-// }
+// TearDownTest runs after each test case
+func (s *UserUseCasetestSuite) TearDownTest() {
+	// Clean up resources if needed
+}
 
-// // TestRegisterUser tests the RegisterUser method
-// func (s *UserUseCasetestSuite) TestRegisterUser() {
-// 	user := domain.User{
-// 		UserName: "testuser",
-// 		Email:    "",
-// 		Password: "password",
-// 	}
+// TestRegisterUser tests the RegisterUser method
+func (s *UserUseCasetestSuite) TestRegisterUser() {
+	user := domain.User{
+		UserName: "testuser",
+		Email:    "",
+		Password: "password",
+	}
 
-// 	s.mockUserRepository.On("RegisterUser", &user).Return(nil)
+	s.mockUserRepository.On("RegisterUser", &user).Return(nil)
 
-// 	err := s.UserUsecase.RegisterUser(context.Background(), &user)
+	err := s.UserUsecase.RegisterUser(context.Background(), &user)
 
-// 	s.NoError(err)
-// }
+	s.NoError(err)
+}
 
-// // TestLoginUser tests the LoginUser method
-// func (s *UserUseCasetestSuite) TestLoginUser() {
-// 	user := domain.User{
-// 		UserName: "testuser",
-// 		Email:    "",
-// 		Password: "password",
-// 	}
+// TestLoginUser tests the LoginUser method
+func (s *UserUseCasetestSuite) TestLoginUser() {
+	user := domain.User{
+		UserName: "testuser",
+		Email:    "",
+		Password: "password",
+	}
 
-// 	s.mockUserRepository.On("LoginUser", user).Return("my-token", nil)
+	s.mockUserRepository.On("LoginUser", user).Return("my-token", nil)
 
-// 	_, err := s.UserUsecase.LoginUser(context.Background(), user)
+	_, err := s.UserUsecase.LoginUser(context.Background(), user)
 
-// 	s.NoError(err)
-// }
+	s.NoError(err)
+}
 
-// // TestForgotPassword tests the ForgotPassword method
-// func (s *UserUseCasetestSuite) TestForgotPassword() {
-// 	email := "testuser@gmail.com"
+// TestForgotPassword tests the ForgotPassword method
+func (s *UserUseCasetestSuite) TestForgotPassword() {
+	email := "testuser@gmail.com"
 
-// 	s.mockUserRepository.On("ForgotPassword", email).Return(nil)
+	s.mockUserRepository.On("ForgotPassword", email).Return(nil)
 
-// 	err := s.UserUsecase.ForgotPassword(context.Background(), email)
+	err := s.UserUsecase.ForgotPassword(context.Background(), email)
 
-// 	s.NoError(err)
-// }
+	s.NoError(err)
+}
 
-// // TestLogoutUser tests the LogoutUser method
-// func (s *UserUseCasetestSuite) TestLogoutUser() {
-// 	uid := "123"
+// TestLogoutUser tests the LogoutUser method
+func (s *UserUseCasetestSuite) TestLogoutUser() {
+	uid := "123"
 
-// 	s.mockUserRepository.On("LogoutUser", uid).Return(nil)
+	s.mockUserRepository.On("LogoutUser", uid).Return(nil)
 
-// 	err := s.UserUsecase.LogoutUser(context.Background(), uid)
+	err := s.UserUsecase.LogoutUser(context.Background(), uid)
 
-// 	s.NoError(err)
-// }
+	s.NoError(err)
+}
 
-// // TestPromoteDemoteUser tests the PromoteDemoteUser method
-// func (s *UserUseCasetestSuite) TestPromoteDemoteUser() {
-// 	userid := "123"
-// 	isAdmin := true
+// TestPromoteDemoteUser tests the PromoteDemoteUser method
+func (s *UserUseCasetestSuite) TestPromoteDemoteUser() {
+	userid := "123"
+	isAdmin := true
 
-// 	s.mockUserRepository.On("PromoteDemoteUser", userid, isAdmin).Return(nil)
+	s.mockUserRepository.On("PromoteDemoteUser", userid, isAdmin).Return(nil)
 
-// 	err := s.UserUsecase.PromoteDemoteUser(context.Background(), userid, isAdmin)
+	err := s.UserUsecase.PromoteDemoteUser(context.Background(), userid, isAdmin)
 
-// 	s.NoError(err)
-// }
+	s.NoError(err)
+}
 
-// // TestResetPassword tests the ResetPassword method
-// func (s *UserUseCasetestSuite) TestResetPassword() {
-// 	token := "123"
-// 	newPassword := "newpassword"
+// TestResetPassword tests the ResetPassword method
+func (s *UserUseCasetestSuite) TestResetPassword() {
+	token := "123"
+	newPassword := "newpassword"
 
-// 	s.mockUserRepository.On("ResetPassword", token, newPassword).Return(nil)
+	s.mockUserRepository.On("ResetPassword", token, newPassword).Return(nil)
 
-// 	err := s.UserUsecase.ResetPassword(context.Background(), token, newPassword)
+	err := s.UserUsecase.ResetPassword(context.Background(), token, newPassword)
 
-// 	s.NoError(err)
-// }
+	s.NoError(err)
+}

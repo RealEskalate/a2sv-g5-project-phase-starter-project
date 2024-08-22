aacce1f285259d4b2700d31e917c86f1cf16bf90
Natnael <natibir400@gmail.com>
2024-08-22 14:35:01 +0300

    setup redux store and folder structures

---
+++ web/AAiT-web-group-4/bankdashboard/app/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../lib/redux/slices/Counterslice'
+
+const store =  configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+})
+
+export default store;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

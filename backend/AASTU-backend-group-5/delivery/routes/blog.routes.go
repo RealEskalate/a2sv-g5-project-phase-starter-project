277e15be65067e741a12ec954181809c9e878016
ermi9s <ermiayele1122334477@gmail.com>
2024-08-22 10:03:21 +0300

    fix

--- backend/AASTU-backend-group-5/delivery/routes/blog.routes.go
+++ backend/AASTU-backend-group-5/delivery/routes/blog.routes.go
@@ -1,11 +1,17 @@
 package routes
 
 import (
+	"log"
+	"os"
+
 	"github.com/RealEskalate/blogpost/database"
 	"github.com/RealEskalate/blogpost/delivery/controller"
+	"github.com/RealEskalate/blogpost/infrastructure/middleware"
+	tokenservice "github.com/RealEskalate/blogpost/infrastructure/token_service"
 	"github.com/RealEskalate/blogpost/repository"
 	"github.com/RealEskalate/blogpost/usecase"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func NewBlogRoutes(group *gin.RouterGroup, blog_collection database.CollectionInterface, user_collection database.CollectionInterface) {
@@ -14,13 +20,34 @@ func NewBlogRoutes(group *gin.RouterGroup, blog_collection database.CollectionIn
 
 	user_repo := repository.NewUserRepository(user_collection)
 	user_usecase := usecase.NewUserUseCase(user_repo)
-	ctrl := controller.NewBlogController(blog_usecase, user_usecase)
+	ctrl := controller.NewBlogController(blog_usecase , user_usecase)
 
-	group.GET("/blog", ctrl.GetAllBlogs())
-	group.GET("/my-blog", ctrl.GetMyBlogs())
-	group.GET("/blog/:id", ctrl.GetOneBlog())
-	group.POST("/blog/", ctrl.CreateBlog())
-	group.PUT("/blog/", ctrl.UpdateBlog())
-	group.DELETE("/blog/:id", ctrl.DeleteBlog())
-	group.GET("/search-blog/", ctrl.FilterBlogs())
+	//load middlewares
+	err := godotenv.Load()
+	if err != nil {
+        log.Panic(err.Error())
+    }
+	access_secret := os.Getenv("ACCESSTOKENSECRET")
+	if access_secret == ""{
+		log.Panic("No accesstoken")
+	}
+	
+	refresh_secret := os.Getenv("REFRESHTOKENSECRET")
+	if refresh_secret == ""{
+		log.Panic("No refreshtoken")
+	}
+	TokenSvc := *tokenservice.NewTokenService(access_secret, refresh_secret)
+
+	LoggedInmiddleWare := middleware.LoggedIn(TokenSvc)
+	mustOwn := middleware.RoleBasedAuth(false)
+
+	group.GET("api/blog" , ctrl.GetAllBlogs())
+	group.GET("api/search-blog/" , ctrl.FilterBlogs())
+
+	group.GET("api/blog/:id" , LoggedInmiddleWare , ctrl.GetOneBlog())
+	group.POST("api/blog/",LoggedInmiddleWare, ctrl.CreateBlog())
+
+	group.GET("api/my-blog",LoggedInmiddleWare , mustOwn , ctrl.GetMyBlogs())
+	group.PUT("api/blog/",LoggedInmiddleWare , mustOwn ,ctrl.UpdateBlog())
+	group.DELETE("api/blog/",LoggedInmiddleWare, mustOwn ,ctrl.DeleteBlog())
 }

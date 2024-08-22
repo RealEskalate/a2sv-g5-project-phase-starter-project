50a785e1730999d7556061c015c1c1227d248a59
eyobderese <eyobderese123@gmail.com>
2024-08-22 11:21:58 +0300

    adding caching feature

--- backend/AAiT-backend-group-9/Blog_Starter/usecase/blog_usecase.go
+++ backend/AAiT-backend-group-9/Blog_Starter/usecase/blog_usecase.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"Blog_Starter/domain"
+	"Blog_Starter/utils"
 	"context"
 	"errors"
 
@@ -9,14 +10,16 @@ import (
 )
 
 type BlogUseCase struct {
-	blogRepo domain.BlogRepository
-	userRepo domain.UserRepository
+	blogRepo  domain.BlogRepository
+	userRepo  domain.UserRepository
+	cacheServ utils.Cache
 }
 
-func NewBlogUseCase(blogRepo domain.BlogRepository, userRepo domain.UserRepository) domain.BlogUseCase {
+func NewBlogUseCase(blogRepo domain.BlogRepository, userRepo domain.UserRepository, cacheServ utils.Cache) domain.BlogUseCase {
 	return &BlogUseCase{
-		blogRepo: blogRepo,
-		userRepo: userRepo,
+		blogRepo:  blogRepo,
+		userRepo:  userRepo,
+		cacheServ: cacheServ,
 	}
 }
 
@@ -48,20 +51,47 @@ func (uc *BlogUseCase) CreateBlog(c context.Context, blog *domain.BlogCreate) (*
 	return uc.blogRepo.CreateBlog(c, blogModel)
 }
 
-func (uc *BlogUseCase) GetBlogByID(c context.Context, blogID string) (*domain.Blog,error) {
+func (uc *BlogUseCase) GetBlogByID(c context.Context, blogID string) (*domain.Blog, error) {
+	// check if the blog exist in the cache else go for repository and save it in the cache
+	blog, found := uc.cacheServ.Get(blogID)
+
+	if found {
+		return blog.(*domain.Blog), nil
+	}
+
 	updatedBlog, err := uc.blogRepo.IncrementViewCount(c, blogID)
+	if err != nil {
+		return nil, err
+	}
+	uc.cacheServ.Set(blogID, updatedBlog, 0)
+
 	return updatedBlog, err
 }
 
 func (uc *BlogUseCase) GetAllBlog(c context.Context, skip int64, limit int64, sortBy string) ([]*domain.Blog, *domain.PaginationMetadata, error) {
 
-	
 	// if not make it default to createtimestamp
 	if sortBy != "createtimestamp" && sortBy != "average_rating" && sortBy != "total_rating" && sortBy != "view_count" && sortBy != "like_count" && sortBy != "comment_count" {
 		sortBy = "createtimestamp"
 	}
 
-	return uc.blogRepo.GetAllBlog(c, skip, limit, sortBy)
+	cacheKey := "allblogs" + sortBy + string(skip) + string(limit)
+	paginationKey := "pagination" + sortBy + string(skip) + string(limit)
+	blogs, found := uc.cacheServ.Get(cacheKey)
+	paginations, foundPagination := uc.cacheServ.Get(paginationKey)
+
+	// if both found get the data from the cache
+	if found && foundPagination {
+		return blogs.([]*domain.Blog), paginations.(*domain.PaginationMetadata), nil
+	}
+
+	allBlog, pagination, err := uc.blogRepo.GetAllBlog(c, skip, limit, sortBy)
+	if err != nil {
+		return nil, nil, err
+	}
+	uc.cacheServ.Set(cacheKey, allBlog, 0)
+	uc.cacheServ.Set(paginationKey, pagination, 0)
+	return allBlog, pagination, nil
 }
 
 func (uc *BlogUseCase) UpdateBlog(c context.Context, blog *domain.BlogUpdate, blogID string) (*domain.Blog, error) {
@@ -86,13 +116,22 @@ func (uc *BlogUseCase) UpdateBlog(c context.Context, blog *domain.BlogUpdate, bl
 		Content: blog.Content,
 		Tags:    blog.Tags,
 	}
+	newUpdatedBlog, err := uc.blogRepo.UpdateBlog(c, updatedBlog, blogID)
+	if err != nil {
+		return newUpdatedBlog, err
+	}
 
-	return uc.blogRepo.UpdateBlog(c, updatedBlog, blogID)
+	// delete the blog from the cache
+	// set the blog
+	uc.cacheServ.Delete(blogID)
+	uc.cacheServ.Set(blogID, newUpdatedBlog, 0)
+	return newUpdatedBlog, err
 }
 
 func (uc *BlogUseCase) DeleteBlog(c context.Context, blogID string, userId string) error {
 	// implementation
 	// check if the user is the owner of the blog
+	// delete the blog from the cache
 	blogUserId, err := primitive.ObjectIDFromHex(userId)
 	if err != nil {
 		return err
@@ -106,6 +145,8 @@ func (uc *BlogUseCase) DeleteBlog(c context.Context, blogID string, userId strin
 	if existedBlog.UserID != blogUserId {
 		return errors.New("user is not the owner of the blog")
 	}
+
+	uc.cacheServ.Delete(blogID)
 
 	return uc.blogRepo.DeleteBlog(c, blogID)
 }

9331f98ec13569f126542d611b2f9d9d1bbcdbac
Mohammed Ali <mohaali482@gmail.com>
2024-08-22 10:15:35 +0300

    feat(blog): add implementation for update blog controller

--- backend/ASTU-backend-group-4/internal/http/gin/blog.go
+++ backend/ASTU-backend-group-4/internal/http/gin/blog.go
@@ -2,27 +2,28 @@ package gin
 
 import (
 	"errors"
+	"log"
 	"net/http"
 
 	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/auth"
-	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/blog"
+	blogDomain "github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/blog"
 	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/pkg/infrastructure"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 )
 
 type BlogController struct {
-	blogUseCase blog.BlogUseCase
+	blogUseCase blogDomain.BlogUseCase
 }
 
-func NewBlogController(blogUseCase blog.BlogUseCase) *BlogController {
+func NewBlogController(blogUseCase blogDomain.BlogUseCase) *BlogController {
 	return &BlogController{
 		blogUseCase: blogUseCase,
 	}
 }
 
 func (bc *BlogController) CreateBlog(c *gin.Context) {
-	var blog blog.CreateBlogRequest
+	var blog blogDomain.CreateBlogRequest
 	if err := c.ShouldBindJSON(&blog); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
@@ -43,4 +44,34 @@ func (bc *BlogController) CreateBlog(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, createdBlog)
+}
+
+func (bc *BlogController) UpdateBlog(c *gin.Context) {
+	var blog blogDomain.UpdateBlogRequest
+	if err := c.ShouldBindJSON(&blog); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	userID := c.Value("userID").(string)
+	blogID := c.Param("id")
+	updatedBlog, err := bc.blogUseCase.UpdateBlog(c.Request.Context(), blogID, userID, blog)
+
+	if err != nil {
+		if (errors.As(err, validator.ValidationErrors{})) {
+			c.AbortWithStatusJSON(http.StatusBadRequest, infrastructure.ReturnErrorResponse(err))
+		} else if errors.Is(err, auth.ErrNoUserWithId) {
+			c.AbortWithStatusJSON(http.StatusNotFound, err)
+		} else if errors.Is(err, blogDomain.ErrBlogNotFound) {
+			c.AbortWithStatusJSON(http.StatusNotFound, err)
+		} else if errors.Is(err, blogDomain.ErrInvalidID) {
+			c.AbortWithStatusJSON(http.StatusNotFound, err)
+		} else {
+			log.Default().Println("Error trying to update blog", err, "blog", blog)
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, updatedBlog)
 }

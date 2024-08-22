0f7954ccb71a9f3305bc73f4df381784a2d250e3
kibrnew <kibrnewgedamu123@gmail.com>
2024-08-22 09:48:55 +0300

    geting updeated

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/serviceComponenet/ServiceList.tsx
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/app/dashboard/services/serviceComponenet/ServiceList.tsx
@@ -1,31 +1,25 @@
 "use client";
 import { useUser } from "@/contexts/UserContext";
 import Image from "next/image";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 
 interface itemProp {
   icon: string;
   name: string;
-  details: string;
+  detail: string;
 }
 
-const ServiceList = ({ icon, name, details }: itemProp) => {
+const ServiceList = ({ icon, name }: itemProp) => {
   const { isDarkMode } = useUser();
   return (
     <div
       className={`flex justify-between items-center p-3 md:p5 rounded-xl ${
-        isDarkMode ? "bg-gray-600 text-gray-300" : "bg-white text-gray-900"
+        isDarkMode
+          ? "bg-gray-800 text-gray-300"
+          : "bg-white text-gray-900 border-2 border-gray-200"
       }`}
     >
       <div className="flex ml-1 gap-1">
-        <div className=" flex w-12 h-12 items-center justify-center bg-green-50 rounded-2xl ">
+        <div className="w-10 h-10">
           <Image
             src="/servicesIcons/saftey.svg"
             alt="Service Icon"
@@ -37,7 +31,7 @@ const ServiceList = ({ icon, name, details }: itemProp) => {
           <div className="mt-1 font-medium">{name}</div>
           <div
             className={`text-xs ${
-              isDarkMode ? "text-gray-400" : "text-gray-600"
+              isDarkMode ? "text-gray-500" : "text-gray-600"
             }`}
           >
             It&apos;s a long established fact.
@@ -49,7 +43,7 @@ const ServiceList = ({ icon, name, details }: itemProp) => {
           <div className="mt-1 font-medium">Business loans</div>
           <div
             className={`text-xs ${
-              isDarkMode ? "text-gray-400" : "text-gray-600"
+              isDarkMode ? "text-gray-500" : "text-gray-600"
             }`}
           >
             It&apos;s a long established fact.
@@ -59,7 +53,7 @@ const ServiceList = ({ icon, name, details }: itemProp) => {
           <div className="mt-1 font-medium">Business loans</div>
           <div
             className={`text-xs ${
-              isDarkMode ? "text-gray-400" : "text-gray-600"
+              isDarkMode ? "text-gray-500" : "text-gray-600"
             }`}
           >
             It&apos;s a long established fact.
@@ -69,31 +63,22 @@ const ServiceList = ({ icon, name, details }: itemProp) => {
           <div className="mt-1 font-medium">Business loans</div>
           <div
             className={`text-xs ${
-              isDarkMode ? "text-gray-400" : "text-gray-600"
+              isDarkMode ? "text-gray-500" : "text-gray-600"
             }`}
           >
             It&apos;s a long established fact.
           </div>
         </div>
       </div>
-
-      <Dialog>
-        <DialogTrigger
-          className={`text-sm font-bold lg:border-2 lg:px-6 lg:rounded-full lg:my-auto lg:py-1 ${
-            isDarkMode
-              ? "text-gray-300 border-gray-600 bg-gray-700"
-              : "text-[#1814F3] border-[#1814F3] bg-white"
-          }`}
-        >
-          View Detail
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Details Of The Service</DialogTitle>
-            <DialogDescription>{details}</DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <div
+        className={`text-sm font-bold lg:border-2 lg:px-6 lg:rounded-full lg:my-auto lg:py-1 ${
+          isDarkMode
+            ? "text-gray-300 border-gray-600 bg-gray-700"
+            : "text-gray-900 border-gray-300 bg-white"
+        }`}
+      >
+        View Detail
+      </div>
     </div>
   );
 };

df074a0c5e32186392d44806d55999d9bfc228d0
Abel Zereabruk <abelazereabruk@gmail.com>
2024-08-22 14:04:58 +0300

    adding component

--- web/AAiT-web-group-2/bank-dashboard/app/page.tsx
+++ web/AAiT-web-group-2/bank-dashboard/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
+    <main className=" ">
       <h1>Bank dashboard</h1>
+      <Navbar />
         
     </main>
   );

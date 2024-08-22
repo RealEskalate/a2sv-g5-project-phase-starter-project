4093384beacfda33859ada7e2c0c29466404aec0
MihretDe <mihretdesalegn98@gmail.com>
2024-08-22 17:56:50 +0300

    fix merge conflict

--- web/AASTU-web-group-6/bankdash/app/Services/api/fetchTransaction.ts
+++ web/AASTU-web-group-6/bankdash/app/Services/api/fetchTransaction.ts
@@ -19,26 +19,8 @@ const getTransaction = async (page: number, accessToken: string) => {
     }
     throw error;
   }
-const getTransaction = async (page: number) => {
-    try {
-        const response = await axios({
-            method: "GET",
-            url: `https://bank-dashboard-o9tl.onrender.com/transactions?page=${page}&size=2`,
-            headers: {
-                Authorization: 'Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0bmFob20iLCJpYXQiOjE3MjQyMjc5MDIsImV4cCI6MTcyNDMxNDMwMn0.DydxCzT8qS-LiHc38KHirs0rZDUIuStwQEJANdRe2ceWPefpAil3wYWnfSdod3uG',
-                "Content-Type": "application/json",
-            },
-        });
-        return response.data.data.content;
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            console.error("Axios error:", error.message);
-        } else {
-            console.error("Unexpected error:", error);
-        }
-        throw error;
-    }
-};
+}
+
 
 const getExpense = async (
   page: number,
@@ -72,8 +54,8 @@ const getIncome = async (page: number, accessToken: string) => {
       url: `https://bank-dashboard-1tst.onrender.com/transactions/incomes?page=${page}&size=1`,
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJ0bmFob20iLCJpYXQiOjE3MjQyMjc5MDIsImV4cCI6MTcyNDMxNDMwMn0.DydxCzT8qS-LiHc38KHirs0rZDUIuStwQEJANdRe2ceWPefpAil3wYWnfSdod3uG",
-        "Content-Type": "application/json",
+          `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
       },
     });
     return response.data.data.content;

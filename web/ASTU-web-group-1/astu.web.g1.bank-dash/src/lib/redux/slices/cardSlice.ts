be1e79fe115f08e230011b203a6ebdbc2e7ab9fd
naol86 <naol.kasinet@a2sv.org>
2024-08-22 10:16:27 +0300

    start working on new backend url

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/lib/redux/slices/cardSlice.ts
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/lib/redux/slices/cardSlice.ts
@@ -1,5 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { getSession } from 'next-auth/react';
+import { baseQuery } from '../api/baseQuery';
+import { pageSize } from '@/types/page-size.type';
 
 async function get_session() {
   const session = await getSession();
@@ -8,20 +10,10 @@ async function get_session() {
 
 export const cardApi = createApi({
   reducerPath: 'cardApi',
-  baseQuery: fetchBaseQuery({
-    baseUrl: 'https://bank-dashboard-6acc.onrender.com',
-    prepareHeaders: async (headers, { getState }) => {
-      const session = await get_session();
-      if (session && session.accessToken) {
-        headers.set('Authorization', `Bearer ${session.accessToken}`);
-      }
-      headers.set('Content-Type', 'application/json');
-      return headers;
-    },
-  }),
+  baseQuery: baseQuery(),
   endpoints: (builder) => ({
-    getAllCards: builder.query<void, void>({
-      query: () => '/cards',
+    getAllCards: builder.query<void, pageSize>({
+      query: (page) => `/cards?page=${page.page}&size=${page.size}`,
     }),
   }),
 });

785d6445f4a9559192ba727bbcde336c64300f4f
fasill <fasilhawultie19@gmail.com>
2024-08-22 16:34:02 +0300

    mofification on landing page

--- web/ASTU-web-group-2/banking-dashboard/app/page.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/page.tsx
@@ -1,20 +1,44 @@
-import React from "react";
+'use client';
+import React, { useState, useEffect } from "react";
 import LandingNav from "./components/landing/LandingNav";
 import LandingHome from "./components/landing/LandingHome";
 import Services from "./components/landing/services";
 import About from "./components/landing/about";
 import Footer from "./components/landing/footer";
 
-const page = () => {
+const Page = () => {
+  const [bgWhite, setBgWhite] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const homeSection = document.getElementById('home-section');
+      if (homeSection) {
+        const { bottom } = homeSection.getBoundingClientRect();
+        setBgWhite(bottom <= 0); // Change background if the user has scrolled past LandingHome
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div>
-      <LandingNav />
-      <LandingHome />
-      <Services />
-      <About />
+      <div className={`${bgWhite&&"fixed"}  w-[100%]`}>
+      <LandingNav bgWhite={bgWhite} />
+      </div>
+      <div id="home-section">
+        <LandingHome />
+      </div>
+      <div id="services-section">
+        <Services />
+      </div>
+      <div id="about-section">
+        <About />
+      </div>
       <Footer />
     </div>
   );
 };
 
-export default page;
+export default Page;

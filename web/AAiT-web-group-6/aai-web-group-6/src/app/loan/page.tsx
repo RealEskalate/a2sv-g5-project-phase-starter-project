5019e607215ee6f63441a4a6cb1a996e318a94f9
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-08-22 17:18:46 +0300

    update box items

--- web/AAiT-web-group-6/aai-web-group-6/src/app/loan/page.tsx
+++ web/AAiT-web-group-6/aai-web-group-6/src/app/loan/page.tsx
@@ -1,8 +1,13 @@
+import Loan_box from "@/components/Loan_components/loan_box/Loan_box";
+import Loan_table from "@/components/Loan_components/loan_table/Loan_table";
 import React from "react";
 
 const Loan = () => {
   return(
-    <div></div>
+    <div className="">
+      <Loan_box />
+      <Loan_table />
+    </div>
   );
 };
 

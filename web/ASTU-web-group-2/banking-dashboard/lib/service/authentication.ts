e8b61a528e54048ca0fb157a9fe29d2a913c4853
fasill <fasilhawultie19@gmail.com>
2024-08-22 12:01:17 +0300

    referash access token added

---
+++ web/ASTU-web-group-2/banking-dashboard/lib/service/authentication.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const authenticationApi = createApi({
+  reducerPath: "authentication",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://bank-dashboard-1tst.onrender.com",
+  }),
+
+  endpoints: (builder) => ({
+    refreshAccessToken: builder.mutation({
+      query: (refreshToken: string) => ({
+        url: "/auth/refresh_token",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${refreshToken}`,
+        },
+      }),
+    }),
+  
+  }),
+});
+
+export const {  useRefreshAccessTokenMutation} = authenticationApi;

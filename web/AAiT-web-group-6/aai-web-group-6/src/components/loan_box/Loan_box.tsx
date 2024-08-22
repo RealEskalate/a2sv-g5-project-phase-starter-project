9d6e1d89a20478991f1859f6f5415667379c35c0
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-08-22 15:43:05 +0300

    add loan

---
+++ web/AAiT-web-group-6/aai-web-group-6/src/components/loan_box/Loan_box.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Loan_box = () => {
+  return (
+    <div className='bg-slate-100 h-[100vh]'>
+         <div className= "w-[200px] h-[100px] bg-white flex  p-[15px] gap-4 items-center rounded-[20px]">
+        <div className='w-[50px] h-[50px] bg-blue-300 rounded-[50%]' >
+        </div>
+        <div>
+            <p className='text-[10pt] text-gray-300'>Personal Loans</p>
+            <p className='text-[12pt] font-bold'> $50,000</p>
+        </div>
+    </div>
+
+    </div>
+   
+  )
+}
+
+export default Loan_box

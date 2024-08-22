368f54200802317c00d34c8bfa5f425d5beb8b3d
Abel Wendmu <abelwendmu124@gmail.com>
2024-08-22 14:37:22 +0300

    chore: Update the function to be testable and refactor code structure

--- backend/ASTU-backend-group-1/delivery/main.go
+++ backend/ASTU-backend-group-1/delivery/main.go
@@ -8,6 +8,7 @@ import (
 	usecase "astu-backend-g1/usecases"
 	"context"
 
+	"github.com/sv-tools/mongoifc"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -24,15 +25,16 @@ func main() {
 	// auther := infrastructure.GeneralAuthorizer(auth)
 	authController := infrastructure.NewAuthController()
 	_ = client.Database("BlogAPI").Collection("Tokens")
-	blogRepo := repository.NewBlogRepository(blogCollections)
+	blogRepo := repository.NewBlogRepository(mongoifc.WrapCollection(blogCollections))
 	blogUsecase := usecase.NewBlogUsecase(blogRepo)
 	blogController := controllers.NewBlogController(*blogUsecase)
-	userRepo := repository.NewUserRepository(userCollections)
+
+	userRepo := repository.NewUserRepository(mongoifc.WrapCollection	(userCollections))
 	userUsecase, err := usecase.NewUserUsecase(userRepo)
 	if err != nil {
 		panic(err)
 	}
-	UserController := controllers.NewUserController(userUsecase, userCollections)
+	UserController := controllers.NewUserController(userUsecase)
 	Router := router.NewMainRouter(*UserController, *blogController, authController)
 	Router.GinBlogRouter()
 

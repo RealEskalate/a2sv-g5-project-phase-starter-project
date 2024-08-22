dff9e6641b5ed58894c1ce65c9c4f9ec86a42a25
dododoyo <dolphinmulugeta88@gmail.com>
2024-08-22 11:53:30 +0300

    🧭 Add Navigation Feature

--- web/AAiT-web-group-3/next.config.mjs
+++ web/AAiT-web-group-3/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dododoyo.github.io",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;

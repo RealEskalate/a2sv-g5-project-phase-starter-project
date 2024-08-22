5faf95b53be25f547b9e3fbb9ca9891973b424e7
Eyerusalem Abate <eyerusabate2@gmail.com>
2024-08-22 17:17:31 +0300

    add table elements

---
+++ web/AAiT-web-group-6/aai-web-group-6/src/constants/loan_constants/table_data.ts
@@ -0,0 +1,8 @@
+const headers = ["SL No","Loan Money","Left to repay","Duration","Interest rate","Installment","Repay", ];
+const data = [
+  { id: "01.", loanMoney: "$10,000", leftToRepay: "$5,000", duration: "12 months", interestRate: "5%", installment: "$500", repay: "Repay" },
+  { id: "02.", loanMoney: "$8,000", leftToRepay: "$2,000", duration: "8 months", interestRate: "4%", installment: "$300", repay: "Repay" },
+  { id: "03.", loanMoney: "$15,000", leftToRepay: "$10,000", duration: "24 months", interestRate: "6%", installment: "$600", repay: "Repay" },
+];
+
+export {headers ,data}

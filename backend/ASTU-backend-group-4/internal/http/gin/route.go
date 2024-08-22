c28642249c9220379a6db112e853bfa76c9c5b37
Mohammed Ali <mohaali482@gmail.com>
2024-08-22 15:45:20 +0300

    feat: refactored the routes

--- backend/ASTU-backend-group-4/internal/http/gin/route.go
+++ backend/ASTU-backend-group-4/internal/http/gin/route.go
@@ -6,8 +6,9 @@ import (
 	"github.com/RealEskalate/-g5-project-phase-starter-project/astu/backend/g4/chat"
 	"github.com/gin-gonic/gin"
 )
+
 func SetUpChatRouter(r *gin.Engine, chatHandler *ChatHandler, chatUsecase *chat.ChatUsecase) {
-	r.GET("/", AuthMiddleware(),chatHandler.GetChatsHandler)
+	r.GET("/", AuthMiddleware(), chatHandler.GetChatsHandler)
 	r.POST("/", AuthMiddleware(), chatHandler.CreateChatHandler)
 	r.POST("/", AuthMiddleware(), chatHandler.GenerateChatTitleHandler)
 	r.GET("/:id", AuthMiddleware(), chatHandler.GetChatHandler)
@@ -19,16 +20,16 @@ func SetUpBlogRouter(r *gin.Engine, blogController *BlogController, blogUseCase 
 	r.POST("/", AuthMiddleware(), blogController.CreateBlog)
 	r.GET("/", AuthMiddleware(), blogController.GetBlogs)
 	r.GET("/:id", AuthMiddleware(), blogController.GetBlogByID)
-	r.PUT("/:id", AuthMiddleware(), blogController.UpdateBlog)	
+	r.PUT("/:id", AuthMiddleware(), blogController.UpdateBlog)
 	r.DELETE("/:id", AuthMiddleware(), blogController.DeleteBlog)
 	r.GET("/search", AuthMiddleware(), blogController.SearchBlogs)
 	r.GET("/:id/comments", AuthMiddleware(), blogController.GetCommentsByBlogID)
 	r.POST("/:id/comments", AuthMiddleware(), blogController.CreateComment)
 	r.DELETE("/:id/comments/:comment_id", AuthMiddleware(), blogController.DeleteComment)
-	r.POST("/:id/like", AuthMiddleware(), blogController.LikeBlog)
-	r.POST("/:id/dislike", AuthMiddleware(), blogController.DislikeBlog)
-	r.DELETE("/:id/unlike", AuthMiddleware(), blogController.UnLikeBlog)
-	r.DELETE("/:id/undislike", AuthMiddleware(), blogController.UnDislikeBlog)
+	r.POST("/:id/likes", AuthMiddleware(), blogController.LikeBlog)
+	r.POST("/:id/dislikes", AuthMiddleware(), blogController.DislikeBlog)
+	r.DELETE("/:id/likes", AuthMiddleware(), blogController.UnLikeBlog)
+	r.DELETE("/:id/dislike", AuthMiddleware(), blogController.UnDislikeBlog)
 }
 
 func SetUpAuthRouter(r *gin.Engine, userController *UserController, authUsecase *auth.AuthServices) {

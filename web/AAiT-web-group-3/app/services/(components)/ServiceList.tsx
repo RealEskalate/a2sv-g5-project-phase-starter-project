5739c53f80d92abf3c50c1c1e6ac52ad709f9969
Gadisa21 <gadisadiriba25@gmail.com>
2024-08-23 16:09:08 +0300

    update icons size and color

--- web/AAiT-web-group-3/app/services/(components)/ServiceList.tsx
+++ web/AAiT-web-group-3/app/services/(components)/ServiceList.tsx
@@ -4,15 +4,20 @@ import { IconType } from "react-icons";
 
 interface ServiceListProps {
   icon: IconType;
+  icon_bg?: string;
+  icon_color?: string;
 }
-const ServiceList = ({ icon: Icon }: ServiceListProps) => {
+const ServiceList = ({ icon: Icon,icon_bg,icon_color }: ServiceListProps) => {
   return (
     <div className="flex justify-between bg-white items-center  p-3 md:p-7  rounded-[20px] mb-6 ">
       <div className="flex items-center gap-3">
-        <Icon className=" w-[45px] h-[45px] md:w-[60px] md:h-[60px]" />
+        <div className="w-[45px] h-[45px] rounded-xl flex items-center justify-center " style={{backgroundColor:icon_bg}}>
+        <Icon className=" w-[20px] h-[20px] "  style={{color:icon_color}}/>
+
+        </div>
         <div>
-          <p className="service_list_title whitespace-nowrap">Business Loan</p>
-          <p className="service_list_subtitle text-[#718EBF] whitespace-nowrap">
+          <p className="service_list_title ">Business Loan</p>
+          <p className="service_list_subtitle text-[#718EBF] ">
             It is a long established
           </p>
         </div>
--- web/AAiT-web-group-3/app/services/page.tsx
+++ web/AAiT-web-group-3/app/services/page.tsx
@@ -4,6 +4,7 @@ import { GiShoppingBag } from "react-icons/gi";
 import { AiFillSafetyCertificate } from "react-icons/ai";
 import { FaHandHoldingDollar } from "react-icons/fa6";
 import ServiceList from "./(components)/ServiceList";
+import { FaBriefcase } from "react-icons/fa";
 
 const page = () => {
   return (
@@ -11,14 +12,20 @@ const page = () => {
       <div className="bg-gray-100 px-[25px] md:px-[50px]">
         <div className="mt-10 overflow-x-auto no-scrollbar ">
           <div className="flex space-x-4 md:space-x-16 ">
-            <Card icon={FaShieldHeart} title="Life Insurance" sub_title="Unlimited Protection" />
-            <Card icon={GiShoppingBag} title="Shopping" sub_title="Buy.Think.Grow." />
-            <Card icon={AiFillSafetyCertificate} title="Safety" sub_title="We are your allies" />
+            <Card icon={FaShieldHeart} title="Life Insurance" sub_title="Unlimited Protection" icon_bg="#E7EDFF" icon_color="#396AFF" />
+            <Card icon={GiShoppingBag} title="Shopping" sub_title="Buy.Think.Grow." icon_bg="#FFF5D9" icon_color="#FFBB38"/>
+            <Card icon={AiFillSafetyCertificate} title="Safety" sub_title="We are your allies" icon_bg="#DCFAF8" icon_color="#16DBCC"/>
           </div>
         </div>
         <p className="font-inter font-semibold text-[#343C6A] text-[22px] leading-[26.63px] my-5">Bank Services List</p>
-        <ServiceList icon={FaHandHoldingDollar} />
-        <ServiceList icon={FaHandHoldingDollar} />
+        <ServiceList icon={FaHandHoldingDollar} icon_bg="#FFE0EB" icon_color="#FF82AC" />
+        <ServiceList icon={FaBriefcase} icon_bg="#FFF5D9" icon_color="#FFBB38"  />
+        <ServiceList icon={FaHandHoldingDollar} icon_bg="#FFE0EB" icon_color="#FF82AC" />
+        <ServiceList icon={FaBriefcase} icon_bg="#FFF5D9" icon_color="#FFBB38"  />
+        <ServiceList icon={FaHandHoldingDollar} icon_bg="#FFE0EB" icon_color="#FF82AC" />
+        <ServiceList icon={FaBriefcase} icon_bg="#FFF5D9" icon_color="#FFBB38"  />
+        <ServiceList icon={FaHandHoldingDollar} icon_bg="#FFE0EB" icon_color="#FF82AC" />
+        <ServiceList icon={FaBriefcase} icon_bg="#FFF5D9" icon_color="#FFBB38"  />
       </div>
     </>
   );

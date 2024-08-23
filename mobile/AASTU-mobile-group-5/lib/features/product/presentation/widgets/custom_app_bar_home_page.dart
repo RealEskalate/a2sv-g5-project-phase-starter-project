e413b0ea0d58182ddbb48462c96cc5241ebfe973
Aryam Ezra <aryamiyezra@gmail.com>
2024-08-23 17:24:19 +0300

    :home: added the image upload for the profile pic and the logout fucntion

--- mobile/AASTU-mobile-group-5/lib/features/product/presentation/widgets/custom_app_bar_home_page.dart
+++ mobile/AASTU-mobile-group-5/lib/features/product/presentation/widgets/custom_app_bar_home_page.dart
@@ -7,6 +7,7 @@ import '../../../user/data/data_sources/user_local_data_source.dart';
 import '../../../user/data/data_sources/user_name.dart';
 import '../../../user/domain/use_case/logout_user.dart';
 import 'dark_mode_toggle.dart';
+import 'image_picker_home.dart';
 
 
 class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
@@ -30,24 +31,56 @@ class _CustomAppBarState extends State<CustomAppBar> {
   }
 
   Future<void> _loadUserName() async {
-  try {
-    
-    final name = await fetchUserName();
-    setState(() {
-      userName = name;
-    });
-  } catch (e) {
+    try {
+      final name = await fetchUserName();
+      setState(() {
+        userName = name;
+      });
+    } catch (e) {
       setState(() {
         userName = 'Guest'; // Fallback in case of an error
       });
+    }
   }
-}
+
+  Future<void> _showLogoutConfirmationDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // User must tap a button to close the dialog.
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Confirm Logout'),
+          content: const Text('Are you sure you want to log out?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+            ),
+            TextButton(
+              child: const Text('Logout'),
+              onPressed: () async {
+                Navigator.of(context).pop(); // Close the dialog
+                // Perform logout
+                final localDataSource = getIt<UserLocalDataSource>();
+                final logOut = LogOut(localDataSource);
+                await logOut();
+                // Navigate to the sign-in screen
+                // ignore: use_build_context_synchronously
+                Navigator.pushReplacementNamed(context, '/signin');
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     String formattedDate = DateFormat('yMMMMd').format(DateTime.now());
 
-    final localDataSource = getIt<UserLocalDataSource>();
-    final logOut = LogOut(localDataSource);
     return AppBar(
       automaticallyImplyLeading: false,
       title: Padding(
@@ -58,14 +91,23 @@ class _CustomAppBarState extends State<CustomAppBar> {
             Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10),
-                // (204, 204, 204, 1)
-                // Color(0xFF121212)
-                color: Theme.of(context).brightness == Brightness.dark ? Colors.white : const Color.fromRGBO(204, 204, 204, 1),
+                color: Theme.of(context).brightness == Brightness.dark
+                    ? Colors.white
+                    : const Color.fromRGBO(204, 204, 204, 1),
               ),
               height: 50,
               width: 50,
+              child: ImagePickerHome(
+                containerHeight: double.infinity,
+                containerWidth: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).brightness == Brightness.dark
+                      ? Colors.white
+                      : const Color.fromRGBO(204, 204, 204, 1),
+                ),
+              ),
             ),
-            
             Expanded(
               child: Container(
                 padding: const EdgeInsets.all(8),
@@ -95,7 +137,9 @@ class _CustomAppBarState extends State<CustomAppBar> {
                           userName,
                           style: TextStyle(
                             fontSize: 15,
-                            color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+                            color: Theme.of(context).brightness == Brightness.dark
+                                ? Colors.white
+                                : Colors.black,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
@@ -117,11 +161,8 @@ class _CustomAppBarState extends State<CustomAppBar> {
               child: IconButton(
                 icon: const Icon(Icons.logout_outlined,
                     color: Color.fromARGB(255, 164, 157, 157), size: 30),
-                onPressed: () async {
-                  await logOut();
-                  // Optionally, navigate to the login screen or show a message
-                  // ignore: use_build_context_synchronously
-                  Navigator.pushReplacementNamed(context, '/signin');
+                onPressed: () {
+                  _showLogoutConfirmationDialog(); // Show confirmation dialog
                 },
               ),
             ),
@@ -131,3 +172,137 @@ class _CustomAppBarState extends State<CustomAppBar> {
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:google_fonts/google_fonts.dart';
+// import 'package:intl/intl.dart';
+// import '../../../../service_locator.dart';
+
+// import '../../../user/data/data_sources/user_local_data_source.dart';
+// import '../../../user/data/data_sources/user_name.dart';
+// import '../../../user/domain/use_case/logout_user.dart';
+// import 'dark_mode_toggle.dart';
+
+
+// class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+//   const CustomAppBar({super.key});
+
+//   @override
+//   // ignore: library_private_types_in_public_api
+//   _CustomAppBarState createState() => _CustomAppBarState();
+
+//   @override
+//   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+// }
+
+// class _CustomAppBarState extends State<CustomAppBar> {
+//   String userName = 'Name';
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _loadUserName();
+//   }
+
+//   Future<void> _loadUserName() async {
+//   try {
+    
+//     final name = await fetchUserName();
+//     setState(() {
+//       userName = name;
+//     });
+//   } catch (e) {
+//       setState(() {
+//         userName = 'Guest'; // Fallback in case of an error
+//       });
+//   }
+// }
+//   @override
+//   Widget build(BuildContext context) {
+//     String formattedDate = DateFormat('yMMMMd').format(DateTime.now());
+
+//     final localDataSource = getIt<UserLocalDataSource>();
+//     final logOut = LogOut(localDataSource);
+//     return AppBar(
+//       automaticallyImplyLeading: false,
+//       title: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.start,
+//           children: [
+//             Container(
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(10),
+//                 // (204, 204, 204, 1)
+//                 // Color(0xFF121212)
+//                 color: Theme.of(context).brightness == Brightness.dark ? Colors.white : const Color.fromRGBO(204, 204, 204, 1),
+//               ),
+//               height: 50,
+//               width: 50,
+//             ),
+            
+//             Expanded(
+//               child: Container(
+//                 padding: const EdgeInsets.all(8),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   mainAxisAlignment: MainAxisAlignment.center,
+//                   children: [
+//                     Padding(
+//                       padding: const EdgeInsets.only(top: 0.0),
+//                       child: Text(
+//                         formattedDate,
+//                         style: GoogleFonts.syne(
+//                           fontSize: 13,
+//                           color: const Color.fromRGBO(170, 170, 170, 1),
+//                         ),
+//                       ),
+//                     ),
+//                     Row(
+//                       children: [
+//                         Text('Hello, ',
+//                             style: GoogleFonts.sora(
+//                               fontSize: 15,
+//                               color: const Color.fromRGBO(102, 102, 102, 1),
+//                               fontWeight: FontWeight.w500,
+//                             )),
+//                         Text(
+//                           userName,
+//                           style: TextStyle(
+//                             fontSize: 15,
+//                             color: Theme.of(context).brightness == Brightness.dark ? Colors.white : Colors.black,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//             const DarkThemeToggle(),
+//             Container(
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(10),
+//                 border: Border.all(
+//                   color: const Color.fromRGBO(217, 217, 217, 1),
+//                   width: 1.0,
+//                 ),
+//               ),
+//               child: IconButton(
+//                 icon: const Icon(Icons.logout_outlined,
+//                     color: Color.fromARGB(255, 164, 157, 157), size: 30),
+//                 onPressed: () async {
+//                   await logOut();
+//                   // Optionally, navigate to the login screen or show a message
+//                   // ignore: use_build_context_synchronously
+//                   Navigator.pushReplacementNamed(context, '/signin');
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }

d2197e1a2c41dabbcea8a5c9ff47176fd30f1a64
Fasikagebrehana <fasikagebrehana1@gmail.com>
2024-08-23 10:17:05 +0300

    bottom nav bar and navigation

---
+++ mobile/AASTU-mobile-group-6/lib/features/product/presentation/widgets/bottomnavbar.dart
@@ -0,0 +1,61 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/material.dart';
+
+class Bottomnavbar extends StatefulWidget {
+  const Bottomnavbar({super.key});
+
+  @override
+  State<Bottomnavbar> createState() => _BottomnavbarState();
+}
+
+class _BottomnavbarState extends State<Bottomnavbar> {
+  int _currentidx = 0;
+  @override
+  Widget build(BuildContext context) {
+    return CurvedNavigationBar(
+        height: MediaQuery.of(context).size.width * 0.17,
+        buttonBackgroundColor: Color.fromRGBO(63, 81, 243,1),
+        color: const Color.fromARGB(255, 223, 223, 223),
+        backgroundColor:  Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: const Duration(milliseconds: 300),
+        // RGBO(63, 81, 243,1)
+        onTap: (int newidx) {
+          setState(() {
+            _currentidx = newidx;
+            Icon(Icons.home, color: Colors.white,);
+          });
+          switch (_currentidx) {
+            case 0:
+              Navigator.pushNamed(context, '/home');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/add');
+              break;
+            case 2:
+              Navigator.pushNamed(context, '/search');
+          }
+        },
+        
+      items: [
+        Icon(Icons.home,
+        color: _currentidx == 0 ? Colors.white : Colors.black,
+        ),
+        // label: 'Home'
+        // ),
+        Icon(Icons.add,
+        color: _currentidx == 1 ? Colors.white : Colors.black,
+),
+        // label: 'Add'
+        // // ),
+        // BottomNavigationBarItem(icon: 
+        Icon(Icons.search,
+        color: _currentidx == 2 ? Colors.white : Colors.black,
+        ),
+        // label: 'Search'
+        // ),
+      ],
+    );
+    
+  }
+}

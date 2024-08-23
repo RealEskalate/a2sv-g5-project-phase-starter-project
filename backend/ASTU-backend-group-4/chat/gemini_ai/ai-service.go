36c53ef74923aa3f768a35577a3135b87945e709
Yonas Sintayehu <amyonassintayehy@gmail.com>
2024-08-23 10:22:15 +0300

    fix(chat): update the parameter of the AIService constructor

--- backend/ASTU-backend-group-4/chat/gemini_ai/ai-service.go
+++ backend/ASTU-backend-group-4/chat/gemini_ai/ai-service.go
@@ -9,27 +9,19 @@ import (
 	"github.com/google/generative-ai-go/genai"
 )
 
-
-type AIService struct{
+type AIService struct {
 	model *genai.GenerativeModel
-	BlogPrompt  string
-	TitlePrompt string
 }
 
-
-func NewAIService(model *genai.GenerativeModel, blogPrompt, titlePrompt string) *AIService{
+func NewAIService(model *genai.GenerativeModel) *AIService {
 	return &AIService{
 		model: model,
-		BlogPrompt: blogPrompt,
-		TitlePrompt: titlePrompt,
 	}
 }
 
-
-
-func (aiService *AIService) SendMessage(ctx context.Context, history []chat.Message, message chat.Message) (chat.Message, error){
+func (aiService *AIService) SendMessage(ctx context.Context, history []chat.Message, message chat.Message) (chat.Message, error) {
 	var chatSessionHistory []*genai.Content
-	for _, message := range history{
+	for _, message := range history {
 		content := genai.Content{
 			Parts: []genai.Part{
 				genai.Text(message.Text),
@@ -41,46 +33,27 @@ func (aiService *AIService) SendMessage(ctx context.Context, history []chat.Mess
 		chatSessionHistory = append(chatSessionHistory, &content)
 	}
 
-
 	chatSession := aiService.model.StartChat()
 	chatSession.History = chatSessionHistory
 
-	if chatSession.History == nil{
-		content := genai.Content{
-			Parts: []genai.Part{
-				genai.Text(aiService.BlogPrompt),
-			},
-
-			Role: "user",
-		}
-
-		chatSessionHistory = append(chatSessionHistory, &content)
-	}
-
-
 	response, err := chatSession.SendMessage(ctx, genai.Text(message.Text))
-	if err != nil{
+	if err != nil {
 		return chat.Message{}, err
 	}
 
 	candidate := response.Candidates[0]
 	return chat.Message{
-		Text: fmt.Sprintf("%s", candidate.Content.Parts[0]),
-		Role: candidate.Content.Role,
+		Text:   fmt.Sprintf("%s", candidate.Content.Parts[0]),
+		Role:   candidate.Content.Role,
 		SentAt: time.Now(),
 	}, nil
 }
 
-
-func (aiService *AIService) GenerateChatTitle(ctx context.Context, text string) (string, error){
-	response, err := aiService.model.GenerateContent(ctx, genai.Text(aiService.TitlePrompt + text))
-	if err != nil{
+func (aiService *AIService) GenerateChatTitle(ctx context.Context, text string) (string, error) {
+	response, err := aiService.model.GenerateContent(ctx, genai.Text(text))
+	if err != nil {
 		return "", err
 	}
 
 	return fmt.Sprintf("%s", response.Candidates[0].Content.Parts[0]), nil
 }
-
-
-
-

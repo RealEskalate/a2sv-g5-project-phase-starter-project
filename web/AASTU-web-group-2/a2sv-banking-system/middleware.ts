e3afb2aecd67e439e0191f44cbad28f309552172
rebumatadele <118250996+rebumatadele@users.noreply.github.com>
2024-08-23 10:15:22 +0300

    fixed the middleware build issue

--- web/AASTU-web-group-2/a2sv-banking-system/middleware.ts
+++ web/AASTU-web-group-2/a2sv-banking-system/middleware.ts
@@ -1,33 +1,18 @@
 import { NextRequest, NextResponse } from "next/server";
-import Refresh from "./app/api/auth/[...nextauth]/token/RefreshToken";
 
 export async function middleware(request: NextRequest) {
-  // Check if the session token exists in the cookies
   const sessionToken = request.cookies.get("next-auth.session-token")?.value;
 
-  // If no session token, try to refresh it
-  if (!sessionToken) {
-    const newAccessToken = await Refresh();
-    
-    // Debugging: Log the new access token
-    console.log("New Access Token:", newAccessToken);
-    
-    // If no new access token, redirect to the sign-in page
-    if (!newAccessToken) {
-      return NextResponse.redirect(new URL('/api/auth/signin', request.url));
-    }
+  console.log("Session Token:", sessionToken);
 
-    // If a new access token is obtained, proceed and set it in the cookies
-    const response = NextResponse.next();
-    response.cookies.set("accessToken", newAccessToken);
-    return response;
+  if (!sessionToken) {
+    // Redirect to sign-in if no session token
+    return NextResponse.redirect(new URL('/api/auth/signin', request.url));
   }
 
-  // If a session token exists, allow the request to proceed
   return NextResponse.next();
 }
 
-// Define the paths where this middleware should be applied
 export const config = {
   matcher: [
     '/', 

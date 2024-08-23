aa2c89439f7098745e662d4a52fd80a07e44dbf6
makda <makdayosephzewge@gmail.com>
2024-08-23 04:21:31 -0400

    :tada: usecase done

--- mobile/AASTU-mobile-group-5/lib/features/chat/presentation/widgets/profile_widget.dart
+++ mobile/AASTU-mobile-group-5/lib/features/chat/presentation/widgets/profile_widget.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
 
-// class ProfileWidget extends StatelessWidget {
-//   final String iconUrl;
-//   const ProfileWidget({super.key, required this.iconUrl});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: const EdgeInsets.all(0),
-//       child: CircleAvatar(
-//         radius: 25,
-//         backgroundColor: Colors.blue.shade300,
-//         child: CircleAvatar(
-//           radius: 21.5,
-//           backgroundImage: Image.asset(
-//             iconUrl,
-//           ).image,
-//         ),
-//       ),
-//     );
-//   }
-// }
 class ProfileWidget extends StatelessWidget {
   final bool isOnline;
   final String iconUrl;

678d08b262609645a65644ad1fa888b059acdeee
techkelster <techkelster@gmail.com>
2024-08-23 10:17:32 +0300

    intial setup

---
+++ web/AAiT-web-group-6/aai-web-group-6/src/components/settings/SettingWhole.tsx
@@ -0,0 +1,21 @@
+import EditProfile from "./EditProfile";
+
+const SettingWhole: React.FC = () => {
+  return (
+    <>
+      <div className="bg-white w-[1120px] rounded-md">
+        <div className="ml-14 flex justify-start gap-10 w-[1010px] border-b bg-white pt-5 pb-0 text-slate-400">
+          <span className="border-b-2 active-border brand-color">
+            Edit Profile
+          </span>
+          <span className="">Preferences</span>
+          <span className="">Security</span>
+        </div>
+
+        <EditProfile isActive={false} />
+      </div>
+    </>
+  );
+};
+
+export default SettingWhole;

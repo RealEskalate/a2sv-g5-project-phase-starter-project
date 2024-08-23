a443418c22ff6dd5a4c4112415a2c54c977d8418
KalebAsratemedhin <risekab@gmail.com>
2024-08-23 09:46:10 +0300

    add settings page

--- web/AAiT-web-group-2/bank-dashboard/app/transactions/page.tsx
+++ web/AAiT-web-group-2/bank-dashboard/app/transactions/page.tsx
@@ -6,16 +6,18 @@ import RecentTransactions from '../components/RecentTransactions'
 
 const page = () => {
   return (
-    <div className='bg-background px-8  w-[1160px] max-md:w-[800px] max-sm:px-6 '> 
-        <div className='flex gap-[30px] max-md:gap-[25px] items-center mb-6 text-[#343C6A] max-sm:block'>
-            <div className=''>
+    <div className='bg-background px-8  '> 
+        <div className=' flex gap-[30px] justify-between max-md:gap-[25px] items-center mb-6 text-[#343C6A] max-sm:block'>
+           <div className='sm:w-[550px] overflow-x-scroll flex gap-5 items-center '>
+           <div className=''>
                 <p className='font-[600] text-[22px] py-4'>My Cards</p>
                 <MyCards />
             </div>
-            <div className='max-sm:hidden'>
+            <div className=''>
                 <p className='font-[600] text-[17px] py-4 text-end '>+ Add Cards</p>
                 <MyCards />
             </div>
+           </div>
             <div className=''>
                 <p className='font-[600] text-[22px] py-4'>My Expense</p>
                 <MyExpense />

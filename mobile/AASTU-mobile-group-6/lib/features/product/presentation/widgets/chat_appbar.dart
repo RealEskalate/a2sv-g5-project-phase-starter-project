40e4cea893e89c6a1509744ea57b335e943f28d4
yumi-meron-2 <meron.weldu.t@gmail.com>
2024-08-23 17:27:37 +0200

    chat page ui

---
+++ mobile/AASTU-mobile-group-6/lib/features/product/presentation/widgets/chat_appbar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String name;
+  final String status;
+  final String imageUrl;
+
+  ChatAppBar(this.name, this.status, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0.5,
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Transform.translate(
+            offset: Offset(-10, 0), 
+            child: CircleAvatar(
+              radius: 25,
+              backgroundColor: Colors.pink,
+              backgroundImage: NetworkImage(imageUrl),
+            ),
+          ),
+          // SizedBox(width: 10),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.black,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Text(
+                status,
+                style: TextStyle(
+                  fontSize: 12,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      actions: [
+        IconButton(icon: Icon(Icons.call), onPressed: () {}),
+        IconButton(icon: Icon(Icons.videocam), onPressed: () {}),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}

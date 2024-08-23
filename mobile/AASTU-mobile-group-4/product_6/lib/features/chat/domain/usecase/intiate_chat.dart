9103f6c6804cd2b4b5177bc79402ef12ba7c77b4
Sefukamil20R <Tenzilk20@gmail.com>
2024-08-23 17:39:41 +0300

    bloc added

--- mobile/AASTU-mobile-group-4/product_6/lib/features/chat/domain/usecase/intiate_chat.dart
+++ mobile/AASTU-mobile-group-4/product_6/lib/features/chat/domain/usecase/intiate_chat.dart
@@ -4,10 +4,10 @@ import '../../../../core/errors/failure.dart';
 import '../entity/chat_entity.dart';
 import '../repository/chat_repository.dart';
 
-class InitiateChat {
+class InitiateChatUsecase {
   final ChatRepository repository;
 
-  InitiateChat(this.repository);
+  InitiateChatUsecase(this.repository);
 
     Future<Either<Failure, ChatEntity>> initiateChat(String userId)
  async {

3bc489850dc436412ec4f7735590ecae74bbee47
Nathan M.S <natms24@gmail.com>
2024-08-23 17:49:06 +0300

    Add test:infrastructure ai service utils

--- backend/AAiT-backend-group-12/tests/ai_service_test.go
+++ backend/AAiT-backend-group-12/tests/ai_service_test.go
@@ -65,6 +65,43 @@ func (suite *AIServicesTestSuite) TestGenerateContent_Negative_NoContentParts() 
 	suite.Equal("No content parts found", res)
 }
 
+func (suite *AIServicesTestSuite) TestCleanText() {
+	input := "T***h*i**s *is* s*ome te********xt with unw******ante*d *ch*aracters**"
+	expectedOutput := "This is some text with unwanted characters"
+
+	output := suite.aiService.CleanText(input)
+	suite.Equal(expectedOutput, output)
+}
+
+func (suite *AIServicesTestSuite) TestExtractText_Struct() {
+	type TestData struct {
+		Text string
+	}
+
+	input := TestData{
+		Text: "This is some text",
+	}
+	expectedOutput := "This is some text"
+
+	output := suite.aiService.ExtractText(input)
+	suite.Equal(expectedOutput, output)
+}
+
+func (suite *AIServicesTestSuite) TestExtractText_Direct() {
+	input2 := "This is another text"
+	expectedOutput2 := "This is another text"
+
+	output2 := suite.aiService.ExtractText(input2)
+	suite.Equal(expectedOutput2, output2)
+}
+
+func (suite *AIServicesTestSuite) TestExtractText_Negative() {
+	input2 := 0
+
+	output2 := suite.aiService.ExtractText(input2)
+	suite.Equal("", output2)
+}
+
 func TestAIServices(t *testing.T) {
 	suite.Run(t, new(AIServicesTestSuite))
 }

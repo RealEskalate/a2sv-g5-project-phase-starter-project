fb99ed4647bf8e51290009f70ee1cc24cec76eaf
Bisratasaye12 <bisrat.asaye@aait.edu.et>
2024-08-23 10:16:26 +0300

    merge-repository-and-controller-tests

--- backend/AAiT-backend-group-5/Delivery/Routers/user_profile_route.go
+++ backend/AAiT-backend-group-5/Delivery/Routers/user_profile_route.go
@@ -2,16 +2,16 @@ package routers
 
 import (
 	controllers "github.com/aait.backend.g5.main/backend/Delivery/Controllers"
+	interfaces "github.com/aait.backend.g5.main/backend/Domain/Interfaces"
 	infrastructure "github.com/aait.backend.g5.main/backend/Infrastructure"
 	repository "github.com/aait.backend.g5.main/backend/Repository"
 	usecases "github.com/aait.backend.g5.main/backend/UseCases"
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func NewUserProfileRouter(database mongo.Database, group *gin.RouterGroup) {
+func NewUserProfileRouter(database interfaces.Database, group *gin.RouterGroup) {
 
-	user_repo := repository.NewUserRepository(&database)
+	user_repo := repository.NewUserRepository(database)
 	password_service := infrastructure.NewPasswordService()
 
 	// instantiate userProfile_controller

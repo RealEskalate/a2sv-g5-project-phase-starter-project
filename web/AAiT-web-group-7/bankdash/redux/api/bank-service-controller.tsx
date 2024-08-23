2809e9f2bac6c7816cc1199360b3cf3ccd0ac08b
Melakeselam Yitbarek <Melakeselamyitbarek2012@gmail.com>
2024-08-23 17:01:08 +0300

    add the endpoints in the toolkit

--- web/AAiT-web-group-7/bankdash/redux/api/bank-service-controller.tsx
+++ web/AAiT-web-group-7/bankdash/redux/api/bank-service-controller.tsx
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { get } from 'http';
 
 export const bankserviceApi = createApi({
   reducerPath: 'bankserviceApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
-    // the header depends on where we put our access token. either cookies or local storage
+    baseUrl: "https://bank-dashboard-latest.onrender.com",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
       if (token) {
@@ -14,11 +14,70 @@ export const bankserviceApi = createApi({
     },
   }),
 
-  tagTypes: [],
-  endpoints: (builder) => ({}),
+  tagTypes: ['bank-services'],
+  endpoints: (builder) => ({
+    
+    getbankService: builder.query({
+      query: ({id}) => ({
+        url: `/bank-services/${id}`,
+        method: 'GET',
+      }),
+      providesTags: ['bank-services'],
+    }),
+
+    updatebankService: builder.mutation({
+      query: ({id, name, type, icon, details, numberOfUsers, status}) => ({
+        url: `/bank-services/${id}`,
+        method: 'PUT',
+        body: { name, type, icon, details, numberOfUsers, status },
+      }),
+      invalidatesTags: ['bank-services'],
+    }),
+
+    deletebankService: builder.mutation({
+      query: ({id}) => ({
+        url: `/bank-services/${id}`,
+        method: 'DELETE',
+      }),
+      invalidatesTags: ['bank-services'],
+    }),
+
+    getbankServices: builder.query({
+      query: ({page, size}) => ({
+        url: `/bank-services?page=${page}&size=${size}`,
+        method: 'GET',
+      }),
+      providesTags: ['bank-services'],
+    }),
+    
+    postbankServices: builder.mutation({
+      query: ({name, type, icon, details, numberOfUsers, status}) => ({
+        url: '/bank-services',
+        method: 'POST',
+        body: { name, type, icon, details, numberOfUsers, status },
+      }),
+      invalidatesTags: ['bank-services'],
+    }),
+
+    searchbankService: builder.query({
+      query: ({query}) => ({
+        url: `/bank-services/search?query=${query}`,
+        method: 'GET',
+      }),
+      providesTags: ['bank-services'],
+    }),
+
+  }),
 });
 
-export const {} = bankserviceApi;
+export const {
+  useGetbankServiceQuery,
+  useUpdatebankServiceMutation,
+  useDeletebankServiceMutation,
+  useGetbankServicesQuery,
+  usePostbankServicesMutation,
+  useSearchbankServiceQuery
+} = bankserviceApi;
 
 
 
--- web/AAiT-web-group-7/bankdash/redux/api/user-controller.tsx
+++ web/AAiT-web-group-7/bankdash/redux/api/user-controller.tsx
@@ -1,10 +1,10 @@
+import { current } from '@reduxjs/toolkit';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const userApi = createApi({
   reducerPath: 'userApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: "",
-    // the header depends on where we put our access token. either cookies or local storage
+    baseUrl: "https://bank-dashboard-latest.onrender.com",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem('token');
       if (token) {
@@ -14,11 +14,62 @@ export const userApi = createApi({
     },
   }),
 
-  tagTypes: [],
-  endpoints: (builder) => ({}),
+  tagTypes: ['users'],
+  endpoints: (builder) => ({
+
+    updateUser: builder.mutation({
+      query: ({ name, email, dateOfBirth, username, permanentAddress, postalCode, presentAddress, city, country, profilePicture }) => ({
+        url: '/user/update',
+        method: 'PUT',
+        body: { name, email, dateOfBirth, username, permanentAddress, postalCode, presentAddress, city, country, profilePicture },
+      }),
+      invalidatesTags: ['users'],
+    }),
+
+    userUpdatePreference: builder.mutation({
+      query: ({ currency, sentOrReceiveDigitalCurrency, receiveMerchantOrder, accountRecommendations, timeZone, twoFactorAuthentication }) => ({
+        url: '/user/update-preference',
+        method: 'PUT',
+        body: { currency, sentOrReceiveDigitalCurrency, receiveMerchantOrder, accountRecommendations, timeZone, twoFactorAuthentication },
+      }),
+
+      invalidatesTags: ['users'],
+    }),
+
+    getUser: builder.query({
+      query: ({username}) => ({
+        url: `/user/${username}`,
+        method: 'GET',
+      }),
+      providesTags: ['users'],
+    }),
+
+    userRandomInvestmentData: builder.query({
+      query: ({years, months}) => ({
+        url: `/user/random-investment-data?years=${years}&months=${months}`,
+        method: 'GET',
+      }),
+      providesTags: ['users'],
+    }),
+
+    currentUser: builder.query({
+      query: () => ({
+        url: '/user/current',
+        method: 'GET',
+      }),
+      providesTags: ['users'],
+    }),
+
+  }),
 });
 
-export const {} = userApi;
+export const {
+  useUpdateUserMutation,
+  useUserUpdatePreferenceMutation,
+  useGetUserQuery,
+  useUserRandomInvestmentDataQuery,
+  useCurrentUserQuery,
+} = userApi;
 
 
 

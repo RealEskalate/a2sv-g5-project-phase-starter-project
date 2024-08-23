94555ab6d26a096988362666346a9cf14f199cc9
Samuels5 <samimule20@gmail.com>
2024-08-23 17:05:19 +0300

    shimer of investment

--- web/AASTU-web-group-2/a2sv-banking-system/app/investments/components/Monthly.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/investments/components/Monthly.tsx
@@ -112,17 +112,17 @@ export default function Monthly() {
 
   if (loading || Loading)
     return (
-      <div className="bg-white rounded-3xl border-none p-8 animate-shimmer">
-        <div className="h-6 w-full bg-gray-300 rounded-md mb-4"></div>
-        <div className="h-4 w-3/4 bg-gray-300 rounded-md mb-4"></div>
-        <div className="h-4 w-1/2 bg-gray-300 rounded-md mb-4"></div>
-        <div className="h-4 w-2/3 bg-gray-300 rounded-md"></div>
+      <div className="bg-white dark:bg-gray-800 rounded-3xl border-none p-8 animate-shimmer">
+        <div className="h-6 w-full bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
+        <div className="h-4 w-3/4 bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
+        <div className="h-4 w-1/2 bg-gray-300 dark:bg-gray-600 rounded-md mb-4"></div>
+        <div className="h-4 w-2/3 bg-gray-300 dark:bg-gray-600 rounded-md"></div>
       </div>
     );
   // console.log(data);
   const { monthlyRevenue } = data;
   return (
-    <Card className="bg-white rounded-3xl border-none ">
+    <Card className="bg-white rounded-3xl border-none dark:bg-[#050914] dark:border dark:border-[#333B69]">
       <CardContent className="pt-8 pb-6 ">
         <ChartContainer config={chartConfig}>
           <LineChart

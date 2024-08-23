d6de657e6c013a7dee7a5402800b98d438c63815
Leykun Birhanu <leykunbi12@gmail.com>
2024-08-23 17:18:08 +0300

    implemented mystory UI

--- mobile/AAiT-mobile-group-4/e_commerce_app_clean/lib/features/chat/presentation/widgets/profile_pic_widget.dart
+++ mobile/AAiT-mobile-group-4/e_commerce_app_clean/lib/features/chat/presentation/widgets/profile_pic_widget.dart
@@ -1,22 +1,94 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePicWidget extends StatelessWidget {
   final String imagePath;
-  
+  final bool isMystory;
+  final bool isStory;
   final Color bgColor;
 
   final double radius;
 
-  const ProfilePicWidget({super.key, this.imagePath = 'assets/story_3.png',  required this.bgColor,required this.radius});
+  const ProfilePicWidget(
+      {this.isMystory = false,
+      this.isStory = false,
+      super.key,
+      this.imagePath = 'assets/story_3.png',
+      required this.bgColor,
+      required this.radius});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CircleAvatar(
+    if (isMystory && isStory) {
+      return Center(
+        child: Stack(
+          children: [
+            DottedBorder(
+              borderType: BorderType.Circle,
+              color: Colors.white,
+              strokeWidth: 2,
+              dashPattern: const [52, 3],
+              child: Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.blue,
+                    width: 2,
+                  ),
+                ),
+                child: CircleAvatar(
+                  radius: radius,
+                  backgroundColor: bgColor,
+                  backgroundImage: AssetImage(imagePath),
+                ),
+              ),
+            ),
+            const Positioned(
+              bottom: 2,
+              right: 2,
+              child: CircleAvatar(
+                radius: 7,
+                backgroundColor: Colors.grey,
+                child: CircleAvatar(
+                  radius: 6,
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.add, color: Colors.grey, size: 10),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+    } else if (isStory) {
+      return Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: bgColor,
+            width: 2,
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border: Border.all(
+              color: Colors.blue,
+              width: 2,
+            ),
+          ),
+          child: CircleAvatar(
+            radius: radius,
+            backgroundColor: bgColor,
+            backgroundImage: AssetImage(imagePath),
+          ),
+        ),
+      );
+    } else {
+      return CircleAvatar(
         radius: radius,
         backgroundColor: bgColor,
         backgroundImage: AssetImage(imagePath),
-      ),
-    );
+      );
+    }
   }
 }

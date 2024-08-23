daf40834ca4fa79af518f3bff5a6d144efa47f3b
Mahlet-assefa <mahletassefa07@gmail.com>
2024-08-23 16:21:47 +0300

    updated some repos

--- mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/data/repositories/chat_repository_impl.dart
+++ mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/data/repositories/chat_repository_impl.dart
@@ -1,5 +1,5 @@
 import '../../domain/repository/chat_repository.dart';
 
-class ChatRepositoryImpl extends ChatRepository{
+// class ChatRepositoryImpl extends ChatRepository{
   
-}
+// }
--- mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/domain/repository/chat_repository.dart
+++ mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/domain/repository/chat_repository.dart
@@ -1,3 +1,8 @@
-class ChatRepository{
-  
-}
+// import '../../../authentication/domain/entity/user.dart';
+
+// abstract class ChatRepository {
+//   Future<User> SendMessage({required String userId});
+//   Future<void> ReciveMessage(String chatId);
+//   Future<void> DeletMessage(String chatId);
+//   Future<List<Chat>> GetMyChats();
+// }

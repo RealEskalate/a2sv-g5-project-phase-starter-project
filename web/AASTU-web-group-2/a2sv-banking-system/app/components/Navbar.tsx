d2583b48edbcd062c10df5597a887860cfb882d2
rebumatadele <118250996+rebumatadele@users.noreply.github.com>
2024-08-23 16:14:39 +0300

    logout and progress bar implimented

--- web/AASTU-web-group-2/a2sv-banking-system/app/components/Navbar.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/components/Navbar.tsx
@@ -2,9 +2,14 @@
 import React from "react";
 import { MdOutlineSearch } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { IoMoonOutline, IoSettingsOutline } from "react-icons/io5";
+import {
+  IoLogOutOutline,
+  IoMoonOutline,
+  IoSettingsOutline,
+} from "react-icons/io5";
 import { IoMdNotificationsOutline } from "react-icons/io";
 import { useRouter } from "next/navigation";
+import { signOut } from "next-auth/react";
 interface Props {
   handleClick: () => void;
   toggleDarkMode: () => void;
@@ -13,64 +18,72 @@ import Image from "next/image";
 const Navbar = ({ handleClick, toggleDarkMode }: Props) => {
   const route = useRouter();
   return (
-    <div className="flex flex-col gap-5 py-5 border-b px-10">
-      <div className="flex gap-5 justify-between items-center">
-        <div className="text-2xl text-[#343C6A] md:hidden dark:text-white">
-          <button onClick={handleClick}>
-            <GiHamburgerMenu />
-          </button>
-        </div>
-        <div className="font-bold text-2xl text-[#343C6A] dark:text-[#9faaeb]">
-          Overview
-        </div>
-
-        <div className="flex gap-20">
-          <div className="rounded-full hidden md:flex md:gap-2 bg-[#F5F7FA] text-[#8BA3CB] text-sm font-normal py-3 px-8 ml-2 items-center dark:bg-[#050914] dark:border dark:border-[#333B69]">
-            <MdOutlineSearch className="text-xl" />
-            <input
-              type="text"
-              placeholder="Search for Something"
-              className="bg-transparent border-none outline-none text-[#8BA3CB] placeholder-[#8BA3CB] text-sm flex-grow"
-            />
+      <div className="flex flex-col gap-5 py-5 border-b px-10">
+        <div className="flex gap-5 justify-between items-center">
+          <div className="text-2xl text-[#343C6A] md:hidden dark:text-white">
+            <button onClick={handleClick}>
+              <GiHamburgerMenu />
+            </button>
+          </div>
+          <div className="font-bold text-2xl text-[#343C6A] dark:text-[#9faaeb]">
+            Overview
           </div>
 
-          <div className="hidden md:flex gap-5 text-xl md:items-center">
-            <div
-              className="cursor-pointer text-xl bg-[#F5F7FA] rounded-full px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]"
-              onClick={() => route.push("./bankingSettings")}
-            >
-              <IoSettingsOutline />
+          <div className="flex gap-20">
+            <div className="rounded-full hidden md:flex md:gap-2 bg-[#F5F7FA] text-[#8BA3CB] text-sm font-normal py-3 px-8 ml-2 items-center dark:bg-[#050914] dark:border dark:border-[#333B69]">
+              <MdOutlineSearch className="text-xl" />
+              <input
+                type="text"
+                placeholder="Search for Something"
+                className="bg-transparent border-none outline-none text-[#8BA3CB] placeholder-[#8BA3CB] text-sm flex-grow"
+              />
             </div>
-            <div className="cursor-pointer text-xl bg-[#F5F7FA] rounded-full px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]">
-              <IoMdNotificationsOutline />
+
+            <div className="hidden md:flex gap-5 text-xl md:items-center">
+              <div
+                className="cursor-pointer text-xl bg-[#F5F7FA] rounded-full px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]"
+                onClick={() => route.push("./bankingSettings")}
+              >
+                <IoSettingsOutline />
+              </div>
+              <div className="cursor-pointer text-xl bg-[#F5F7FA] rounded-full px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]">
+                <IoMdNotificationsOutline />
+              </div>
+              <div
+                className="cursor-pointer text-xl bg-[#F5F7FA] rounded-full px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]"
+                onClick={toggleDarkMode}
+              >
+                <IoMoonOutline />
+              </div>
+              <div
+                className="cursor-pointer text-xl bg-[#F5F7FA] rounded-lg px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]"
+                onClick={() => {
+                  signOut();
+                }}
+              >
+                <IoLogOutOutline />
+              </div>
             </div>
-            <div
-              className="cursor-pointer text-xl bg-[#F5F7FA] rounded-full px-2 py-2 dark:bg-[#050914] dark:border dark:border-[#333B69]"
-              onClick={toggleDarkMode}
-            >
-              <IoMoonOutline />
+            <div className="items-center">
+              <Image
+                src="/profile.png"
+                alt="Profile"
+                width={35}
+                height={35}
+              ></Image>
             </div>
           </div>
-          <div className="items-center">
-            <Image
-              src="/profile.png"
-              alt="Profile"
-              width={35}
-              height={35}
-            ></Image>
-          </div>
+        </div>
+
+        <div className="md:hidden rounded-full flex md:gap-2 bg-[#F5F7FA] text-[#8BA3CB] text-sm font-normal py-3 px-8 ml-2 items-center dark:bg-[#050914] dark:border dark:border-[#333B69]">
+          <MdOutlineSearch className="text-xl" />
+          <input
+            type="text"
+            placeholder="Search for Something"
+            className="bg-transparent border-none outline-none text-[#8BA3CB] placeholder-[#8BA3CB] text-sm flex-grow"
+          />
         </div>
       </div>
-
-      <div className="md:hidden rounded-full flex md:gap-2 bg-[#F5F7FA] text-[#8BA3CB] text-sm font-normal py-3 px-8 ml-2 items-center dark:bg-[#050914] dark:border dark:border-[#333B69]">
-        <MdOutlineSearch className="text-xl" />
-        <input
-          type="text"
-          placeholder="Search for Something"
-          className="bg-transparent border-none outline-none text-[#8BA3CB] placeholder-[#8BA3CB] text-sm flex-grow"
-        />
-      </div>
-    </div>
   );
 };
 

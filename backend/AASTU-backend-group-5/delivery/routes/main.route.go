c21a9caac823fff7ace82c04e6e3bf62f897a745
simret <semretb74@gmail.com>
2024-08-23 16:30:06 +0300

    feat: add bookmark

--- backend/AASTU-backend-group-5/delivery/routes/main.route.go
+++ backend/AASTU-backend-group-5/delivery/routes/main.route.go
@@ -27,31 +27,37 @@ func SetUp(router *gin.Engine) {
 		Collection: clinect.Client.Database("BlogPost").Collection("Comments"),
 	}
 
- 	stateCollection := &database.MongoCollection{
+	stateCollection := &database.MongoCollection{
 		Collection: clinect.Client.Database("BlogPost").Collection("States"),
 	}
+	bookmarkCollection := &database.MongoCollection{
+		Collection: clinect.Client.Database("BlogPost").Collection("Bookmarks"),
+	}
+
 	userrepo := repository.NewUserRepository(userCollection)
-  
+
 	aiRoute := router.Group("")
 	userRoute := router.Group("")
 	verifiRoute := router.Group("")
 	uplaodRoute := router.Group("")
 	authRoute := router.Group("")
-  	blogRot := router.Group("")
-  	popularityRoute := router.Group("")
+	blogRot := router.Group("")
+	popularityRoute := router.Group("")
 	likeRoute := router.Group("")
 	dislikeRoute := router.Group("")
 	commentRoute := router.Group("")
+	bookmarkRoute := router.Group("")
 
-	NewUploadRoute(uplaodRoute , *userrepo)
-	NewVerifyEmialRoute(verifiRoute , userCollection)	
+	NewUploadRoute(uplaodRoute, *userrepo)
+	NewVerifyEmialRoute(verifiRoute, userCollection)
 	NewBlogRoutes(blogRot, blogCollection, userCollection)
 	NewAiRequestRoute(aiRoute)
 	NewUserRoute(userRoute, userCollection)
-  	NewAuthRoute(authRoute, userCollection, stateCollection)
-  	NewPopularityRoutes(popularityRoute, blogCollection)
+	NewAuthRoute(authRoute, userCollection, stateCollection)
+	NewPopularityRoutes(popularityRoute, blogCollection)
 	NewLikeRoutes(likeRoute, likeCollection, blogCollection)
 	NewDislikeRoutes(dislikeRoute, dislikeCollection, blogCollection)
 	NewCommentRoutes(commentRoute, commentCollection, blogCollection)
+	NewBookmarkRoutes(bookmarkRoute, bookmarkCollection, userCollection)
 
 }

afaa68195555610aa366bc702c2b30695c90285d
Sura Itana <141846629+surraaI@users.noreply.github.com>
2024-08-23 09:54:33 +0300

    add appBar and input bar to chat screen

--- mobile/AAiT-mobile-group-2/ecom_app/lib/features/chat/presentation/pages/individual_chat_page.dart
+++ mobile/AAiT-mobile-group-2/ecom_app/lib/features/chat/presentation/pages/individual_chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecom_app/features/chat/presentation/widgets/current_user.dart';
 import 'package:ecom_app/features/chat/presentation/widgets/single_text.dart';
 import 'package:flutter/material.dart';
 
@@ -10,9 +11,30 @@ class IndividualChatPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Individual Chat'),
+         appBar: AppBar(
+        leading: const Icon(Icons.arrow_back),
+        title: const CurrentUser(
+          name: 'John Doe',
+          image: 'assets/images/user.png',
+          online: true,
         ),
+        actions: const [
+          IconButton(
+            icon: Icon(
+              Icons.call_outlined,
+              color: Colors.black,
+            ),
+            onPressed: null,
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.videocam_outlined,
+              color: Colors.black,
+            ),
+            onPressed: null,
+          ),
+        ],
+      ),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 50),
           child: SingleChildScrollView(
@@ -44,8 +66,69 @@ class IndividualChatPage extends StatelessWidget {
                 image_content: 'assets/blue_portrait.jpg',
                 time: '10:44 PM',
               ),
+              _buildInputBar(),
             ]),
           ),
+          
         ));
   }
 }
+
+Widget _buildInputBar() {
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
+    child: Row(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.attach_file_outlined,
+            color: Colors.black,
+          
+          ),
+          onPressed: () {
+            // Handle attachment
+          },
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Row(
+              children: [
+                const SizedBox(width: 10),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Write your message",
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.copy,),
+                  onPressed: () {
+                    // Handle copy action
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+        IconButton(
+          icon: const Icon(Icons.camera_alt_outlined),
+          onPressed: () {
+            // Handle camera action
+          },
+        ),
+        IconButton(
+          icon: const Icon(Icons.mic_none_outlined),
+          onPressed: () {
+            // Handle microphone action
+          },
+        ),
+      ],
+    ),
+  );
+}
+

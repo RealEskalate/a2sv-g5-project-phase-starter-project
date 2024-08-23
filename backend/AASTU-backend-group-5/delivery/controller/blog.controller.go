e56de42ddf6d67573a9412216229dbb00510c571
simret <semretb74@gmail.com>
2024-08-23 15:59:13 +0300

    fix: update blog controller, usecase,interface

--- backend/AASTU-backend-group-5/delivery/controller/blog.controller.go
+++ backend/AASTU-backend-group-5/delivery/controller/blog.controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 	"strconv"
 
@@ -45,9 +46,9 @@ func (bc *BlogController) CreateBlog() gin.HandlerFunc {
 
 func (bc *BlogController) GetMyBlogs() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		iuser,_ := c.Get("user")
+		iuser, _ := c.Get("user")
 		user := iuser.(domain.ResponseUser)
-		
+
 		id := user.ID
 		filter := make(map[string]interface{})
 		filter["owner._id"] = id
@@ -61,22 +62,33 @@ func (bc *BlogController) GetMyBlogs() gin.HandlerFunc {
 		c.JSON(http.StatusOK, gin.H{"message": "Your blog posts retrieved successfully!", "blogs": blogs})
 	}
 }
-
 func (bc *BlogController) GetAllBlogs() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		limit, _ := strconv.Atoi(c.DefaultQuery("limit", "10"))
-		pageNumber, _ := strconv.Atoi(c.DefaultQuery("page_number", "1"))
+		page, err := strconv.Atoi(c.Query("page"))
+		if err != nil || page < 1 {
+			page = 1
+		}
 
-		blogs, err := bc.BlogUsecase.GetBlogs(limit, pageNumber)
+		limit, err := strconv.Atoi(c.Query("limit"))
+		if err != nil || limit < 1 {
+			limit = 5
+		}
+
+		posts, err := bc.BlogUsecase.GetBlogs(page, limit)
 		if err != nil {
-			c.JSON(http.StatusNotFound, gin.H{"error": "Failed to retrieve blog posts. Please try again later: " + err.Error()})
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "All blog posts retrieved successfully!", "blogs": blogs})
+		c.JSON(http.StatusOK, gin.H{
+			"data": posts,
+			"meta": gin.H{
+				"limit": limit,
+				"page":  page,
+			},
+		})
 	}
 }
-
 func (bc *BlogController) GetOneBlog() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		idStr := c.Param("id")
@@ -154,6 +166,16 @@ func (bc *BlogController) FilterBlogs() gin.HandlerFunc {
 		startDate := c.Query("start_date")
 		endDate := c.Query("end_date")
 		popularity := c.Query("popularity")
+		if title != "" {
+			c.SetCookie("title", title, 172800, "", "", false, false)
+		}
+		if author != "" {
+			c.SetCookie("author", author, 172800, "", "", false, false)
+		}
+		if len(tags) > 0 {
+			tagsJSON, _ := json.Marshal(tags)
+			c.SetCookie("tags", string(tagsJSON), 172800, "", "", false, false)
+		}
 
 		filters := make(map[string]interface{})
 
@@ -193,53 +215,53 @@ func (bc *BlogController) FilterBlogs() gin.HandlerFunc {
 	}
 }
 func (bc *BlogController) GetUniqueBlog() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        // Retrieve cookie values
-        title, err := c.Cookie("title")
-        if err != nil && err != http.ErrNoCookie {
-            c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to retrieve title cookie"})
-            return
-        }
+	return func(c *gin.Context) {
+		// Retrieve cookie values
+		title, err := c.Cookie("title")
+		if err != nil && err != http.ErrNoCookie {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to retrieve title cookie"})
+			return
+		}
 
-        author, err := c.Cookie("author")
-        if err != nil && err != http.ErrNoCookie {
-            c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to retrieve author cookie"})
-            return
-        }
+		author, err := c.Cookie("author")
+		if err != nil && err != http.ErrNoCookie {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to retrieve author cookie"})
+			return
+		}
 
-        tagsCookie, err := c.Cookie("tags")
-        if err != nil && err != http.ErrNoCookie {
-            c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to retrieve tags cookie"})
-            return
-        }
+		tagsCookie, err := c.Cookie("tags")
+		if err != nil && err != http.ErrNoCookie {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to retrieve tags cookie"})
+			return
+		}
 
-        var tags []string
-        if tagsCookie != "" {
-            if err := json.Unmarshal([]byte(tagsCookie), &tags); err != nil {
-                c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to parse tags from cookie"})
-                return
-            }
-        }
+		var tags []string
+		if tagsCookie != "" {
+			if err := json.Unmarshal([]byte(tagsCookie), &tags); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{"message": "Failed to parse tags from cookie"})
+				return
+			}
+		}
 
-        // Prepare filters based on cookies
-        filters := make(map[string]interface{})
-        if title != "" {
-            filters["title"] = title
-        }
-        if author != "" {
-            filters["owner.username"] = author
-        }
-        if len(tags) > 0 {
-            filters["tag"] = tags
-        }
+		// Prepare filters based on cookies
+		filters := make(map[string]interface{})
+		if title != "" {
+			filters["title"] = title
+		}
+		if author != "" {
+			filters["owner.username"] = author
+		}
+		if len(tags) > 0 {
+			filters["tag"] = tags
+		}
 
-        // Retrieve unique blog posts
-        var posts []*domain.Blog
-        if err := bc.BlogUsecase.GetUniqueBlog(filters, &posts); err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to retrieve unique posts"})
-            return
-        }
+		// Retrieve unique blog posts
+		var posts []domain.Blog
+		if err := bc.BlogUsecase.GetUniqueBlog(filters, &posts); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "Failed to retrieve unique posts"})
+			return
+		}
 
-        c.JSON(http.StatusOK, posts)
-    }
+		c.JSON(http.StatusOK, posts)
+	}
 }

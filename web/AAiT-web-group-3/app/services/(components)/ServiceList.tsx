5dffef45e5dee3c75539d7b6dd8ca8fc19e8d857
Gadisa21 <gadisadiriba25@gmail.com>
2024-08-23 14:41:45 +0300

    Finished the service page design

--- web/AAiT-web-group-3/app/services/(components)/ServiceList.tsx
+++ web/AAiT-web-group-3/app/services/(components)/ServiceList.tsx
@@ -7,27 +7,37 @@ interface ServiceListProps {
 }
 const ServiceList = ({ icon: Icon }: ServiceListProps) => {
   return (
-    <div className="flex justify-between bg-white items-center p-7  rounded-[20px]">
+    <div className="flex justify-between bg-white items-center  p-3 md:p-7  rounded-[20px] mb-6 ">
       <div className="flex items-center gap-3">
-        <Icon className="w-[60px] h-[60px]" />
+        <Icon className=" w-[45px] h-[45px] md:w-[60px] md:h-[60px]" />
         <div>
-          <p className="service_list_title">Business Loan</p>
-          <p className="service_list_subtitle">It is a long established</p>
+          <p className="service_list_title whitespace-nowrap">Business Loan</p>
+          <p className="service_list_subtitle text-[#718EBF] whitespace-nowrap">
+            It is a long established
+          </p>
         </div>
       </div>
-      <div>
+      <div className="hidden md:block">
         <p className="service_list_title">lorem</p>
-        <p className="service_list_subtitle">publishing something</p>
+        <p className="service_list_subtitle text-[#718EBF]">
+          publishing something
+        </p>
       </div>
-      <div>
+      <div className="hidden md:block">
         <p className="service_list_title">lorem</p>
-        <p className="service_list_subtitle">publishing something</p>
+        <p className="service_list_subtitle text-[#718EBF]">
+          publishing something
+        </p>
       </div>
-      <div>
+      <div className="hidden md:block">
         <p className="service_list_title">lorem</p>
-        <p className="service_list_subtitle">publishing something</p>
+        <p className="service_list_subtitle text-[#718EBF]">
+          publishing something
+        </p>
       </div>
-      <button className="service_list_subtitle border border-[#718EBF] rounded-[50px] px-5 py-2">View Details</button>
+      <button className="service_list_Button">
+        View Details
+      </button>
     </div>
   );
 };
--- web/AAiT-web-group-3/app/services/page.tsx
+++ web/AAiT-web-group-3/app/services/page.tsx
@@ -8,17 +8,18 @@ import ServiceList from "./(components)/ServiceList";
 const page = () => {
   return (
     <>
-    <div className="bg-gray-100 px-[50px]">
-    <div className="flex justify-between mt-10">
-     <Card icon={FaShieldHeart} title="Life Insurance" sub_title="Unlimited Protection"/>
-      <Card icon={GiShoppingBag} title="Shopping" sub_title="Buy.Think.Grow."/>
-      <Card icon={AiFillSafetyCertificate} title="Safety" sub_title="We are your allies"/>
-    </div>
-    <p className="font-inter font-semibold text-[#343C6A] text-[22px] leading-[26.63px] my-5">Bank Services List</p>
-     <ServiceList icon={FaHandHoldingDollar}/>
-    </div>
-   
-      
+      <div className="bg-gray-100 px-[25px] md:px-[50px]">
+        <div className="mt-10 overflow-x-auto no-scrollbar ">
+          <div className="flex space-x-4 ">
+            <Card icon={FaShieldHeart} title="Life Insurance" sub_title="Unlimited Protection" />
+            <Card icon={GiShoppingBag} title="Shopping" sub_title="Buy.Think.Grow." />
+            <Card icon={AiFillSafetyCertificate} title="Safety" sub_title="We are your allies" />
+          </div>
+        </div>
+        <p className="font-inter font-semibold text-[#343C6A] text-[22px] leading-[26.63px] my-5">Bank Services List</p>
+        <ServiceList icon={FaHandHoldingDollar} />
+        <ServiceList icon={FaHandHoldingDollar} />
+      </div>
     </>
   );
 };

df5a17d7524d3ff69edea5d7234355a04e877475
Nahom Zewdu <nahiyo86@gmail.com>
2024-08-23 22:07:59 +0300

    Updated all blog files, refactored the code and added new functionalitites

--- backend/AASTU-backend-group-4/repository/like_repository/remove_like.go
+++ backend/AASTU-backend-group-4/repository/like_repository/remove_like.go
@@ -2,14 +2,24 @@ package like_repository
 
 import (
 	"context"
+	"errors"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func (br LikeRepository) RemoveLike(ctx context.Context, likeID primitive.ObjectID) error {
+func (lr *LikeRepository) RemoveLike(ctx context.Context, userID, likeID primitive.ObjectID, isAdmin bool) error {
 	filter := bson.M{"_id": likeID}
 
-	_, err := br.collection.DeleteOne(ctx, filter)
+	existingLike, err := lr.GetLikeByID(ctx, likeID)
+	if err != nil {
+		return errors.New("you haven't liked this blog yet")
+	}
+
+	if !isAdmin && existingLike.UserID != userID {
+		return errors.New("only the user or an admin can remove this like")
+	}
+
+	_, err = lr.collection.DeleteOne(ctx, filter)
 	return err
 }

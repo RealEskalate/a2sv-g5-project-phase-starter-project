259977a5ea49aa04b30a383ebb017311ad889aa8
ephyg <84809084+ephyg@users.noreply.github.com1>
2024-08-23 09:56:51 +0300

    chore: Update signup form endpoint URL

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/AuthForm/SignUpForm.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/Form/AuthForm/SignUpForm.tsx
@@ -100,7 +100,7 @@ const SignUpForm = () => {
 
       console.log('Returned and combined values', finalData);
 
-      const res = await fetch('https://bank-dashboard-1tst.onrender.com/auth/register', {
+      const res = await fetch('https://astu-bank-dashboard.onrender.com/auth/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

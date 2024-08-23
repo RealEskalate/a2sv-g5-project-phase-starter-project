d31ea124e3329c913a10efdc11c33f75a99c4ae1
naol86 <naol.kasinet@a2sv.org>
2024-08-24 00:45:47 +0300

    fix the issuse with side bar icons

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/page.tsx
@@ -1,14 +1,13 @@
-import Contacts from "@/components/Welcome-landingPage/Contacts";
-import Navbar from "@/components/Welcome-landingPage/navbar";
-import WelcomeDashboard from "@/components/Welcome-landingPage/WelcomeDashboard";
+import Contacts from '@/components/Welcome-landingPage/Contacts';
+import WelcomeDashboard from '@/components/Welcome-landingPage/WelcomeDashboard';
 
 export default function Home() {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="w-full min-h-screen bg-slate-50 scroll-smooth">
+    <div className='w-full min-h-screen bg-slate-50 scroll-smooth'>
       <WelcomeDashboard />
       <Contacts />
-      <footer className="text-slate-800 mt-10 text-center py-1 w-full">
+      <footer className='text-slate-800 mt-10 text-center py-1 w-full'>
         <p>&copy; {currentYear} Bank . All rights reserved.</p>
       </footer>
     </div>

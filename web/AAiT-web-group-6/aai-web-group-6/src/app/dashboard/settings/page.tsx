db2e3a3e218baa9052c00fb061517362420c102f
techkelster <techkelster@gmail.com>
2024-08-23 09:02:18 +0300

    settings page

---
+++ web/AAiT-web-group-6/aai-web-group-6/src/app/dashboard/settings/page.tsx
@@ -0,0 +1,10 @@
+import SettingWhole from "@/components/setting/SettingWhole";
+const page: React.FC = () => {
+  return (
+    <div>
+      <SettingWhole />
+    </div>
+  );
+};
+
+export default page;

77be17cdfadbffd9cb79707ddeae913f38af1c63
DanielKinnito <mokonnendaniel@gmail.com>
2024-08-23 11:26:59 +0300

    :zap: Finish writing usecases for the chat feature

--- mobile/AASTU-mobile-group-5/lib/features/chat/domain/repositories/chat_repository.dart
+++ mobile/AASTU-mobile-group-5/lib/features/chat/domain/repositories/chat_repository.dart
@@ -4,8 +4,8 @@ import '../../../../core/failure/failure.dart';
 import '../entities/chat_entity.dart';
 
 abstract class ChatRepository {
-  Future<Either<Failure, void>> myChats();
-  Future<Either<Failure, void>> myChatById();
+  Future<Either<Failure, List<ChatEntity>>> myChats();
+  Future<Either<Failure, ChatEntity>> myChatById();
   Future<Either<Failure, void>> deleteChat(String chatId);
   Future<Either<Failure, ChatEntity>> initiateChat(String sellerId);
 }
--- mobile/AASTU-mobile-group-5/lib/features/chat/domain/use_case/my_chat_by_id.dart
+++ mobile/AASTU-mobile-group-5/lib/features/chat/domain/use_case/my_chat_by_id.dart
@@ -5,13 +5,13 @@ import '../../../../core/use_cases/no_para_use_case.dart';
 import '../entities/chat_entity.dart';
 import '../repositories/chat_repository.dart';
 
-class MyChats extends NoParamsUseCase<Future<Either<Failure, List<ChatEntity>>>> {
+class MyChatById extends NoParamsUseCase<Future<Either<Failure, ChatEntity>>> {
   final ChatRepository repository;
 
-  MyChats(this.repository);
+  MyChatById(this.repository);
 
   @override
-  Future<Either<Failure, List<ChatEntity>>> call() async {
-    return await repository.myChats();
+  Future<Either<Failure, ChatEntity>> call() async {
+    return await repository.myChatById();
   }
 }

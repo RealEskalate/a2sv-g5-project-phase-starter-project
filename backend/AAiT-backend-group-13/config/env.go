d095d331ddfec6ac09b91d9e46307171bbd17490
Beka Birhanu <beka.b.atomsa@gmail.com>
2024-08-23 15:45:30 +0300

    fix: cast int vars to int types

--- backend/AAiT-backend-group-13/config/env.go
+++ backend/AAiT-backend-group-13/config/env.go
@@ -19,15 +19,15 @@ type Config struct {
 	DBConnectionString     string        // Connection string for the database.
 	JWTSecret              string        // Secret key for JWT signing.
 	JWTExpirationInSeconds time.Duration // JWT expiration time.
-	Blog_cache_db          string
-	Blog_cache_expiry      time.Duration
-	Cache_host             string
-	Cache_port             string
-	Google_Api_Key         string
-	MailTrapHost           string // MailTrap SMTP host.
-	MailTrapPort           string // MailTrap SMTP port.
-	MailTrapUsername       string // MailTrap SMTP username.
-	MailTrapPassword       string // MailTrap SMTP password.
+	CacheDB                int           // Cache DB number.
+	CacheExpiry            time.Duration // Cache expiry time.
+	CacheHost              string        // Cache server host.
+	CachePort              string        // Cache server port.
+	GoogleApiKey           string        // Google API key.
+	MailTrapHost           string        // MailTrap SMTP host.
+	MailTrapPort           int           // MailTrap SMTP port.
+	MailTrapUsername       string        // MailTrap SMTP username.
+	MailTrapPassword       string        // MailTrap SMTP password.
 }
 
 // Envs holds the loaded configuration values.
@@ -46,14 +46,14 @@ func initConfig() Config {
 		DBConnectionString:     getEnv("DB_CONNECTION_STRING"),
 		DBName:                 getEnv("DB_NAME"),
 		JWTSecret:              getEnv("JWT_SECRET"),
-		JWTExpirationInSeconds: time.Duration(getTimeEnv("JWT_EXPIRATION_IN_SECONDS")) * time.Second,
-		Blog_cache_db:          getEnv("BLOG_CACHE_DB"),
-		Blog_cache_expiry:      time.Duration(getTimeEnv("BLOG_CACHE_EXPIRY")) * time.Second,
-		Cache_host:             getEnv("CACHE_HOST"),
-		Cache_port:             getEnv("CACHE_PORT"),
-		Google_Api_Key:         getEnv("GOOGLE_API_KEY"),
+		JWTExpirationInSeconds: time.Duration(getIntEnv("JWT_EXPIRATION_IN_SECONDS")) * time.Second,
+		CacheDB:                getIntEnv("BLOG_CACHE_DB"),
+		CacheExpiry:            time.Duration(getIntEnv("BLOG_CACHE_EXPIRY")) * time.Second,
+		CacheHost:              getEnv("CACHE_HOST"),
+		CachePort:              getEnv("CACHE_PORT"),
+		GoogleApiKey:           getEnv("GOOGLE_API_KEY"),
 		MailTrapHost:           getEnv("MAILTRAP_HOST"),
-		MailTrapPort:           getEnv("MAILTRAP_PORT"),
+		MailTrapPort:           getIntEnv("MAILTRAP_PORT"),
 		MailTrapUsername:       getEnv("MAILTRAP_USERNAME"),
 		MailTrapPassword:       getEnv("MAILTRAP_PASSWORD"),
 	}
@@ -69,11 +69,11 @@ func getEnv(key string) string {
 	return value
 }
 
-// getTimeEnv retrieves the value of an environment variable as an integer.
+// getIntEnv retrieves the value of an environment variable as an integer.
 // If the key is not present or if there's an error in parsing, it logs a fatal error.
-func getTimeEnv(key string) int64 {
+func getIntEnv(key string) int {
 	valueStr := getEnv(key)
-	value, err := strconv.ParseInt(valueStr, 10, 64)
+	value, err := strconv.Atoi(valueStr)
 	if err != nil {
 		log.Fatalf("Error parsing environment variable %s as int: %v", key, err)
 	}

9d5d28f645f72060176613846c96cc444a635e34
makda <makdayosephzewge@gmail.com>
2024-08-23 03:58:32 -0400

    :adhesive_bandage: delete it!

--- mobile/AASTU-mobile-group-5/lib/features/chat/domain/use_case/delete_chat.dart
+++ mobile/AASTU-mobile-group-5/lib/features/chat/domain/use_case/delete_chat.dart
@@ -1,5 +1,16 @@
 
 
 
-xyyx;
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/failure/failure.dart';
+import '../repositories/chat_repository.dart';
+
+class DeleteChat{
+  final ChatRepository chatRepository;
+  DeleteChat(this.chatRepository);
+  Future<Either<Failure,void >>execute(String id){
+    return chatRepository.deleteChat(id);
+  }
+}
 

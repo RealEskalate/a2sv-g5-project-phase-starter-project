2cdf33107083a0b57a09d630dbdee9ede68ee240
NaolDaba <nahafile@gmail.com>
2024-08-24 01:17:42 +0300

    added profile managment functionality

--- backend/AAiT-backend-group-3/internal/delivery/controllers/user_controller.go
+++ backend/AAiT-backend-group-3/internal/delivery/controllers/user_controller.go
@@ -1,10 +1,14 @@
 package controllers
 
 import (
+	"AAIT-backend-group-3/internal/domain/dtos"
 	"AAIT-backend-group-3/internal/domain/models"
 	"AAIT-backend-group-3/internal/usecases"
 	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 type UserController struct {
@@ -16,6 +20,7 @@ type UserControllerInterface interface {
 	RefreshToken(c *gin.Context)
 	VerifyEmail(c *gin.Context)
 	Logout(c *gin.Context)
+	UpdateProfile(c *gin.Context)
 }
 
 func NewUserController(u usecases.UserUsecaseInterface) UserControllerInterface {
@@ -104,4 +109,40 @@ func (uc *UserController) Logout(c *gin.Context) {
 		return
 	}
 	c.JSON(200, gin.H{"message": "Successfully logged out"})
+}
+
+func (uc *UserController) UpdateProfile(c *gin.Context){
+	claims, _ := c.Get("claims")
+    jwtClaims, ok := claims.(jwt.MapClaims)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse claims"})
+        return
+    }
+    userID, ok := jwtClaims["userId"].(string)
+    if !ok {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse user ID"})
+        return
+    }
+
+	var updateData dtos.Profile
+    if username := c.PostForm("username"); username != "" {
+        updateData.UserName = username
+    }
+    if phoneNum := c.PostForm("phone_num"); phoneNum != "" {
+        updateData.PhoneNum = phoneNum
+    }
+    if bio := c.PostForm("bio"); bio != "" {
+        updateData.Bio = bio
+    }
+
+    image, _, err := c.Request.FormFile("profile_pic")
+    if err != nil && err != http.ErrMissingFile {
+        c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to process profile picture"})
+        return
+    }
+    if err := uc.user_usecase.UpdateProfile(userID, &updateData, image); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update profile"})
+        return
+    }
+    c.JSON(http.StatusOK, gin.H{"message": "Profile updated successfully"})
 }

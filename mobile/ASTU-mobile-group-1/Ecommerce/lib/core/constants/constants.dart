5fb6a0db85bd5989e525dcdb4bad8a9123a1b437
firaol <firaolkiya6@gmail.com>
2024-08-24 22:53:34 +0300

    update

--- mobile/ASTU-mobile-group-1/Ecommerce/lib/core/constants/constants.dart
+++ mobile/ASTU-mobile-group-1/Ecommerce/lib/core/constants/constants.dart
@@ -8,4 +8,10 @@ class Urls {
   static String getUser() => '$baseUrl/users/me';
   static String signIn() => '$baseUrl/auth/login';
   static String signUp() => '$baseUrl/auth/register';
+
+  static String myChat= 'https://g5-flutter-learning-path-be.onrender.com/api/v3/chats';
+  static String getChatById(String chatId)=>'$myChat/$chatId';
+  static String getChatMessages(String chatId)=>'$myChat/$chatId/messages';
+  static String initiateChat(String recieverId)=>'$myChat/$recieverId';
+
 }

297d1cfbfba08a1f5efd5e8fc9764ad9f09f1aa4
Gadisa16 <133113137+Gadisa16@users.noreply.github.com>
2024-08-24 14:38:01 +0300

    Added the card component and transaction table

--- web/AAiT-web-group-1/bankdash/app/page.tsx
+++ web/AAiT-web-group-1/bankdash/app/page.tsx
@@ -1,10 +1,8 @@
-import TransactionsTable from "./components/Transactions/transactions_table";
-import Card from "./components/Common/Card";
 
 export default function Home() {
   return (
     <main className=" relative">
-      <Card />
+
     </main>
   );
 }

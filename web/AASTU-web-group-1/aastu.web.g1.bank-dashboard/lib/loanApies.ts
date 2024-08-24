5f7ce9a9a3cdbc6c08b6466ef54f3d454b7e8994
manuel-heav <manuelheav32@gmail.com>
2024-08-24 15:24:48 +0300

    New changes

--- web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/lib/loanApies.ts
+++ web/AASTU-web-group-1/aastu.web.g1.bank-dashboard/lib/loanApies.ts
@@ -1,24 +1,28 @@
 import { getSession } from "next-auth/react";
 
-export  async function getLoansAll() {
-    try {
-        const session = await getSession();
-        const accessToken = session?.user.accessToken;
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/active-loans/all`, {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-            body: null,
-        }).then((res)=>(res.json()));
+export async function getLoansAll() {
+  try {
+    const session = await getSession();
+    const accessToken = session?.user.accessToken;
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/active-loans/all`,
+      {
+        cache: "force-cache",
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: null,
+      }
+    ).then((res) => res.json());
 
-        if (response.success){
-            // console.log(response.data);
-            return response.data;
-        }else{
-            throw new Error("failed to get data");
-        }
-    } catch (error) {
-      console.error("An error occurred on card:", error);
+    if (response.success) {
+      // console.log(response.data);
+      return response.data;
+    } else {
+      throw new Error("failed to get data");
     }
+  } catch (error) {
+    console.error("An error occurred on card:", error);
+  }
 }

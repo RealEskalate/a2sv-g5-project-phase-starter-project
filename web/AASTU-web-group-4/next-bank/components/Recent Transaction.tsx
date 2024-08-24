21716065e52c0e5852bd030832079744a6ee98a7
matiyasjosh <torbaw457@gmail.com>
2024-08-24 16:23:34 +0300

    modified:   web/AASTU-web-group-4/next-bank/app/(root)/accounts/page.tsx 	modified:   web/AASTU-web-group-4/next-bank/app/(root)/credit-card/page.tsx 	modified:   web/AASTU-web-group-4/next-bank/app/(root)/page.tsx 	modified:   web/AASTU-web-group-4/next-bank/app/(root)/transaction/page.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/AddNewCard.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/LineChart.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/QuickTransfer.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/Recent Transaction.tsx 	modified:   web/AASTU-web-group-4/next-bank/components/RecentTransaction.tsx

--- web/AASTU-web-group-4/next-bank/components/Recent Transaction.tsx
+++ web/AASTU-web-group-4/next-bank/components/Recent Transaction.tsx
@@ -59,8 +59,9 @@ const RecentTransaction = () => {
   if (status === "error") {
     return (
       <div className="flex flex-col items-center justify-center h-full text-red-500">
-        <TbFileSad
-          className={`text-gray-300 dark:text-white w-[400px] h-[70px]`}
+        <TbFileSad  
+          className={`text-gray-300 dark:text-[#993d4b] w-[400px] h-[70px] pb-2 block mx-auto`}
+          strokeWidth={1}
         />
         <div>Error fetching the recent transactions</div>
       </div>

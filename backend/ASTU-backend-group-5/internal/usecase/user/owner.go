c17f02c5b955682bdcab653c0fc9ce0ac1a5f426
Abdulwahid <Abdulwahidhussen10@gmail.com>
2024-08-24 04:52:56 -0700

    email sending when user promoted or demoted from adming

--- backend/ASTU-backend-group-5/internal/usecase/user/owner.go
+++ backend/ASTU-backend-group-5/internal/usecase/user/owner.go
@@ -1,12 +1,16 @@
 package user
 
 import (
+	"blogApp/internal/config"
+	"blogApp/pkg/email"
 	"context"
 	"errors"
 	"fmt"
+	"log"
 )
 
 func (u *UserUsecase) PromoteToAdmin(UserId string) error {
+	var emailSender email.EmailSender
 
 	user, err := u.repo.FindUserById(context.Background(), UserId)
 	fmt.Println(user)
@@ -21,11 +25,43 @@ func (u *UserUsecase) PromoteToAdmin(UserId string) error {
 	if err != nil {
 		return err
 	}
+
+	Config, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+		return nil
+	}
+	emailProvider := Config.EMAIL_PROVIDER
+
+	switch emailProvider {
+	case "simple":
+		emailSender = email.NewSimpleEmailSender(
+			Config.SMTP_HOST,
+			Config.EMAIL_PORT,
+			Config.EMAIL_SENDER_EMAIL,
+			Config.EMAIL_SENDER_PASSWORD,
+		)
+	default:
+		emailSender = email.NewSimpleEmailSender(
+			Config.SMTP_HOST,
+			Config.EMAIL_PORT,
+			Config.EMAIL_SENDER_EMAIL,
+			Config.EMAIL_SENDER_PASSWORD,
+		)
+	}
+
+	go func() {
+		err := emailSender.SendPromotionToAdminEmail(user.Email)
+		if err != nil {
+			log.Printf("Failed to send password reset email: %v", err)
+		}
+	}()
+
 	return nil
 }
 
 func (u *UserUsecase) DemoteFromAdmin(UserId string) error {
-
+	var emailSender email.EmailSender
 	user, err := u.repo.FindUserById(context.Background(), UserId)
 	if err != nil {
 		return err
@@ -38,5 +74,38 @@ func (u *UserUsecase) DemoteFromAdmin(UserId string) error {
 	if err != nil {
 		return err
 	}
+
+	Config, err := config.Load()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+		return nil
+	}
+
+	emailProvider := Config.EMAIL_PROVIDER
+
+	switch emailProvider {
+	case "simple":
+		emailSender = email.NewSimpleEmailSender(
+			Config.SMTP_HOST,
+			Config.EMAIL_PORT,
+			Config.EMAIL_SENDER_EMAIL,
+			Config.EMAIL_SENDER_PASSWORD,
+		)
+	default:
+		emailSender = email.NewSimpleEmailSender(
+			Config.SMTP_HOST,
+			Config.EMAIL_PORT,
+			Config.EMAIL_SENDER_EMAIL,
+			Config.EMAIL_SENDER_PASSWORD,
+		)
+	}
+
+	go func() {
+		err := emailSender.SendDemotionFromAdminEmail(user.Email)
+		if err != nil {
+			log.Printf("Failed to send password reset email: %v", err)
+		}
+	}()
+
 	return nil
 }

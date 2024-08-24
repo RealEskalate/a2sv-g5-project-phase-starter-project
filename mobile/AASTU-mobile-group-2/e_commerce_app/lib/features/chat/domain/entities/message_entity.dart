029b96411ac297ceaa23447ae17851911591c66b
Dina <dinaassefa04@gmail.com>
2024-08-24 15:40:38 +0300

    added a send message use case made changes on the message model deleted/commented messagecontent types

--- mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/domain/entities/message_entity.dart
+++ mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/domain/entities/message_entity.dart
@@ -2,31 +2,33 @@ import 'package:e_commerce_app/features/auth/domain/entities/user.dart';
 import 'package:e_commerce_app/features/chat/domain/entities/chat_entity.dart';
 import 'package:equatable/equatable.dart';
 
-class MessageContent {
-  final String image;
-  final String video;
-  final String audio;
-  final String file;
-  final String text;
+// class MessageContent {
+//   final String image;
+//   final String video;
+//   final String audio;
+//   final String file;
+//   final String text;
 
-  MessageContent(this.image, this.video, this.audio, this.file, this.text);
-}
+//   MessageContent(this.image, this.video, this.audio, this.file, this.text);
+// }
 
 class MessageEntity extends Equatable {
   final String chatId;
   final User sender;
   final ChatEntity chat;
-  final MessageContent content;
+  final String contentType;
+  final String message;
   final String messageId;
 
   MessageEntity({
+    required this.message,
       required this.messageId,
-      required this.content,
+      required this.contentType,
       required this.chatId,
       required this.chat,
       required this.sender,
   });
 
   @override
-  List<Object?> get props => [chat, content, sender, chatId, messageId];
+  List<Object?> get props => [message, chat, contentType, sender, chatId, messageId];
 }
--- mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/domain/repository/chat_repository.dart
+++ mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/domain/repository/chat_repository.dart
@@ -1,11 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:e_commerce_app/core/failure/failure.dart';
+import 'package:e_commerce_app/features/chat/domain/entities/message_entity.dart';
 
 import '../entities/chat_entity.dart';
 
 abstract class ChatRepository {
   Future<Either<Failure,ChatEntity>> getChatById(String chatId);
-  Future<Either<Failure,List<ChatEntity>>> getAllChats(String userId);
+  Future<Either<Failure,List<ChatEntity>>> getAllChats();
   Future<Either<Failure,ChatEntity>> createChatById(String sellerId);
   Future<Either<Failure,bool>> deleteChatById(String chatId);
+  Future<Either<Failure,MessageEntity>> sendMessage(String chatId, String message, String content);
 }

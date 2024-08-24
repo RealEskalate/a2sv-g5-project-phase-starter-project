c2719ff58dc8b09ad7f1162e4371b409597fa744
Dina <dinaassefa04@gmail.com>
2024-08-24 15:40:38 +0300

    added a send message use case made changes on the message model deleted/commented messagecontent types

--- mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/data/models/message_model.dart
+++ mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/data/models/message_model.dart
@@ -5,57 +5,36 @@ import 'package:e_commerce_app/features/chat/domain/entities/message_entity.dart
 class MessageModel extends MessageEntity {
   MessageModel({
     required String messageId,
-    required MessageContent content,
+    required String contentType,
     required String chatId,
     required ChatModel chat,
     required UserModel sender,
+    required String message,
   }) : super(
           messageId: messageId,
-          content: content,
+          contentType: contentType,
           chatId: chatId,
           chat: chat,
           sender: sender,
+          message: message,
         );
 
   factory MessageModel.fromJson(Map<String, dynamic> json) {
     return MessageModel(
       messageId: json['_id'],
-      content: MessageContent(
-        json['content']['image'] ?? '',
-        json['content']['video'] ?? '',
-        json['content']['audio'] ?? '',
-        json['content']['file'] ?? '',
-        json['content']['text'] ?? '',
-      ),
-      chatId: json['chat']['_id'],
+      contentType: json['type'], 
+      message: json['content'],
+      chatId: json['chat']['_id'], 
       chat: ChatModel.fromJson(json['chat']),
       sender: UserModel.fromJson(json['sender']),
     );
   }
 
-
-
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> contentMap = {};
-    if (content.text.isNotEmpty) {
-      contentMap['text'] = content.text;
-    }
-    if (content.image.isNotEmpty) {
-      contentMap['image'] = content.image;
-    }
-    if (content.video.isNotEmpty) {
-      contentMap['video'] = content.video;
-    }
-    if (content.audio.isNotEmpty) {
-      contentMap['audio'] = content.audio;
-    }
-    if (content.file.isNotEmpty) {
-      contentMap['file'] = content.file;
-    }
-
     return {
       '_id': messageId,
-      'content': contentMap,
+      'type': contentType, 
+      'content': message,
       'chat': (chat as ChatModel).toJson(),
       'sender': (sender as UserModel).toJson(),
       'chatId': chatId,

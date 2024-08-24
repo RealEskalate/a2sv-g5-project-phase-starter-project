17ebf7c23c7979505e249683d48df991a4225b95
makda <makdayosephzewge@gmail.com>
2024-08-24 08:15:14 -0400

    :clownface: message model

---
+++ mobile/AASTU-mobile-group-5/lib/features/chat/data/models/message_model.dart
@@ -0,0 +1,34 @@
+
+
+
+
+
+import '../../../user/data/models/user_model.dart';
+import '../../domain/entities/message_entity.dart';
+import 'chat_model.dart';
+
+class MessageModel extends MessageEntity{
+  MessageModel({
+    required super.messageId,
+    required super.sender,
+    required super.content,
+    required super.chat
+  });
+  factory MessageModel.fromJson(Map<String,dynamic>Json){
+    return MessageModel(
+      messageId: Json['data']['_id'],
+      sender: UserModel.fromJson(Json['data']['sender']),
+      chat:ChatModel.fromJson(Json['data']['chat']),
+      content: Json['data']['content'],
+    );
+  }
+  Map<String,dynamic>toJson(){
+    return{
+      '_id': messageId,
+      'sender':(sender as UserModel).toJson(),
+      'content': content,
+      'chat': (chat as ChatModel).toJson(),
+    };
+  }
+
+}

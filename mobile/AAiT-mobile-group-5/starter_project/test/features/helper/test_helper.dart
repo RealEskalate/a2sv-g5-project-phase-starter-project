c0d7c1f87edae2627983fe0f18c9aa77a52d4410
michael shimeles <105845333+michael-099@users.noreply.github.com>
2024-08-24 15:45:59 +0300

    test helper

---
+++ mobile/AAiT-mobile-group-5/starter_project/test/features/helper/test_helper.dart
@@ -0,0 +1,6 @@
+import 'package:starter_project/features/authentication/domain/repositories/user_repository.dart';
+import 'package:mockito/annotations.dart';
+@GenerateMocks([
+  UserRepository
+],)
+void main() {}

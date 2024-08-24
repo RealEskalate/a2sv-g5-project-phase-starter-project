8d1c3edf0526fd99ebf8d533458e88541f81185c
nanati45 <nansiasamnew@gmail.com>
2024-08-24 17:09:03 +0300

    skeletons

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/bank-dash/credit-card/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/bank-dash/credit-card/page.tsx
@@ -1,37 +1,37 @@
-import AddNewCard from '@/components/AddNewCard/AddNewCard';
-import CardList from '@/components/CardList/CardList';
-import CardSettings from '@/components/CardSettings/CardSettings';
-import CardExpenceStatistics from '@/components/Charts/CardExpenceStatistics';
-import StoreProvider from '@/providers/StoreProvider';
-import { Plus } from 'lucide-react';
-import React from 'react';
-import MyCardLists from '@/components/MyCard/MyCardLists';
+import AddNewCard from "@/components/AddNewCard/AddNewCard";
+import CardList from "@/components/CardList/CardList";
+import CardSettings from "@/components/CardSettings/CardSettings";
+import CardExpenceStatistics from "@/components/Charts/CardExpenceStatistics";
+import StoreProvider from "@/providers/StoreProvider";
+import { Plus } from "lucide-react";
+import React from "react";
+import MyCardLists from "@/components/MyCard/MyCardLists";
 
 export default function page() {
   return (
-    <div className='space-y-5 '>
+    <div className="space-y-5 ">
       <div>
-        <h1 className='text-[#333B69] pb-3 font-semibold'>My Cards</h1>
+        <h1 className="text-[#333B69] pb-3 font-semibold">My Cards</h1>
 
-        <div className='flex overflow-x-scroll space-x-2 scroll whitespace-nowrap scroll-smooth lg:flex lg:space-x-3  '>
+        <div className="flex overflow-x-scroll space-x-2 scroll whitespace-nowrap scroll-smooth lg:flex lg:space-x-3  ">
           <StoreProvider>
             <MyCardLists />
           </StoreProvider>
-          <div className='w-[295px] h-[175px] bg-gray-200 rounded-3xl justify-center items-center flex flex-shrink-0'>
+          <div className="w-[295px] h-[175px] bg-gray-200 rounded-3xl justify-center items-center flex flex-shrink-0">
             <Plus size={32} />
           </div>
         </div>
       </div>
 
-      <div className='space-y-5 lg:space-y-0 lg:flex lg:gap-6'>
-        <div className=' w-full lg:w-4/12'>
+      <div className="space-y-5 lg:space-y-0 lg:flex lg:gap-6">
+        <div className=" w-full lg:w-4/12">
           <CardExpenceStatistics />
         </div>
-        <div className='w-full lg:w-2/3  '>
+        <div className="w-full lg:w-2/3  ">
           <CardList />
         </div>
       </div>
-      <div className='space-y-5 md:space-y-0 md:flex md:gap-6'>
+      <div className="space-y-5 md:space-y-0 md:flex md:gap-6">
         <AddNewCard />
         <CardSettings />
       </div>
--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/MyCard/MyCardLists.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/MyCard/MyCardLists.tsx
@@ -1,12 +1,16 @@
-'use client';
-import { useGetAllCardsQuery } from '@/lib/redux/slices/cardSlice';
-import React from 'react';
-import MyCard from './MyCard';
-import { CardContentType } from '@/types/card.types';
+"use client";
+import { useGetAllCardsQuery } from "@/lib/redux/slices/cardSlice";
+import React from "react";
+import MyCard from "./MyCard";
+import { CardContentType } from "@/types/card.types";
+import CardSkeleton from "../AllSkeletons/CardSkeleton/CardSkeleton";
 
 export default function MyCardLists() {
   const { data, isLoading } = useGetAllCardsQuery({ page: 0, size: 5 });
   console.log(data?.content);
+  if (isLoading) {
+    return <CardSkeleton />;
+  }
   return (
     <>
       {data?.content.map((card: CardContentType, index: number) => (

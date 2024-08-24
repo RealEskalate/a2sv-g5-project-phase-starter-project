6ec83f257dde8d2530c5d562b7880e51f948c7f7
nuredinbedruyimer <nuredinbedru06@gmail.com>
2024-08-24 16:38:14 +0300

    Domain Layer And Models are Done

--- mobile/AAiT-mobile-group-5/starter_project/lib/features/shared/entities/user.dart
+++ mobile/AAiT-mobile-group-5/starter_project/lib/features/shared/entities/user.dart
@@ -3,14 +3,18 @@ import 'package:equatable/equatable.dart';
 class User extends Equatable {
   final String name;
   final String email;
-  final String password;
+  final String id;
 
   const User({
+    required this.id,
     required this.name,
     required this.email,
-    required this.password,
   });
 
   @override
-  List<Object?> get props => [name, email, password];
+  List<Object?> get props => [
+        id,
+        name,
+        email,
+      ];
 }

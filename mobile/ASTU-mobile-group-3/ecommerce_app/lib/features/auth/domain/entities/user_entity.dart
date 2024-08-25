89672768d6b23e2539058305fab7d2329884fc57
z-qine <zqine@localhost.localdomain>
2024-08-25 17:23:57 +0300

    added presentation layer

--- mobile/ASTU-mobile-group-3/ecommerce_app/lib/features/auth/domain/entities/user_entity.dart
+++ mobile/ASTU-mobile-group-3/ecommerce_app/lib/features/auth/domain/entities/user_entity.dart
@@ -1,13 +1,33 @@
 import 'package:equatable/equatable.dart';
 
 class UserEntity extends Equatable {
+  final String id;
   final String name;
   final String email;
-  final String password;
 
-  const UserEntity(
-      {required this.name, required this.email, required this.password});
+  const UserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+  });
+
+  // Method to convert UserEntity to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      '_id': id,
+      'name': name,
+      'email': email,
+    };
+  }
+
+  factory UserEntity.fromJson(Map<String, dynamic> json) {
+    return UserEntity(
+      id: json['_id'],
+      name: json['name'],
+      email: json['email'],
+    );
+  }
 
   @override
-  List<Object?> get props => [name, password, email];
+  List<Object?> get props => [id, name, email];
 }
--- mobile/ASTU-mobile-group-3/ecommerce_app/lib/features/chat/data/models/chat_model.dart
+++ mobile/ASTU-mobile-group-3/ecommerce_app/lib/features/chat/data/models/chat_model.dart
@@ -1,8 +1,30 @@
-
+import '../../../auth/data/model/user_model.dart';
 import '../../domain/entity/chat.dart';
 
 class ChatModel extends ChatEntity {
-  const ChatModel({required super.chatId, required super.user1, required super.user2});
-}
+  const ChatModel({
+    required String chatId,
+    required UserModel user1, // Use UserModel directly
+    required UserModel user2, // Use UserModel directly
+  }) : super(
+          chatId: chatId,
+          user1: user1,
+          user2: user2,
+        );
 
- 
+  factory ChatModel.fromJson(Map<String, dynamic> json) {
+    return ChatModel(
+      chatId: json['_id'],
+      user1: UserModel.fromJson(json['user1']),
+      user2: UserModel.fromJson(json['user2']),
+    );
+  }
+
+//   Map<String, dynamic> toJson() {
+//     return {
+//       '_id': chatId, // Adjusting key to match your JSON
+//       'user1': user1.toJson(), // Serialize UserModel directly
+//       'user2': user2.toJson(), // Serialize UserModel directly
+//     };
+//   }
+}

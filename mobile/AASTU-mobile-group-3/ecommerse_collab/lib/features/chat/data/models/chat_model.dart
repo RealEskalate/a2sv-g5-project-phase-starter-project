2698283bdb83877082cc592148450924683236c1
Mihretthe <mihretekalgn@gmail.com>
2024-08-25 10:57:43 +0300

    Correcting the data type of price

--- mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/data/models/chat_model.dart
+++ mobile/AASTU-mobile-group-3/ecommerse_collab/lib/features/chat/data/models/chat_model.dart
@@ -1,3 +1,31 @@
-class ChatModel{
+import '../../../authentication/domain/entity/user.dart';
+import '../../domain/entity/chat.dart';
+
+class ChatModel extends Chat{
+  final String id;
+  final User user1;
+  final User user2;
   
+
+  ChatModel({
+    required this.id,
+    required this.user1,  
+    required this.user2,  
+  }) : super(id: id, user1: user1, user2: user2);
+
+  factory ChatModel.fromJson(Map<String, dynamic> json) {
+    return ChatModel(
+      id: json['id'],
+      user1: json['user1'],
+      user2: json['user2'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'user1': user1,
+      'user2': user2,
+    };
+  }
 }

9bcdf0c024e6c48c3b24c4908b915de70ab65289
Natnael Worku <natiwe884@gmail.com>
2024-08-25 09:11:04 +0300

    fetched the data in the investments page

--- web/AASTU-web-group-4/next-bank/components/LineChartStraight.tsx
+++ web/AASTU-web-group-4/next-bank/components/LineChartStraight.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
-import { TrendingUp } from "lucide-react"
-import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+import { TrendingUp } from "lucide-react";
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
 import {
   Card,
@@ -10,13 +10,13 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart"
+} from "@/components/ui/chart";
 
 const chartData = [
   { month: "January", desktop: 186 },
@@ -25,26 +25,31 @@ const chartData = [
   { month: "April", desktop: 73 },
   { month: "May", desktop: 209 },
   { month: "June", desktop: 214 },
-]
+];
+interface chartData {
+  time: string;
+  value: number;
+}
 
+interface props {
+  yearlyData: chartData[];
+}
 const chartConfig = {
   desktop: {
     label: "Desktop",
     color: "#eab308",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
-export  default function LineChartStright() {
+export default function LineChartStright({ yearlyData }: props) {
   return (
     <Card className="rounded-2xl">
-      <CardHeader>
-
-      </CardHeader>
+      <CardHeader></CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
           <LineChart
             accessibilityLayer
-            data={chartData}
+            data={yearlyData}
             margin={{
               left: 12,
               right: 12,
@@ -52,7 +57,7 @@ export  default function LineChartStright() {
           >
             <CartesianGrid vertical={false} />
             <XAxis
-              dataKey="month"
+              dataKey="time"
               tickLine={false}
               axisLine={false}
               tickMargin={8}
@@ -63,7 +68,7 @@ export  default function LineChartStright() {
               content={<ChartTooltipContent hideLabel />}
             />
             <Line
-              dataKey="desktop"
+              dataKey="value"
               type="linear"
               stroke="var(--color-desktop)"
               strokeWidth={4}
@@ -73,5 +78,5 @@ export  default function LineChartStright() {
         </ChartContainer>
       </CardContent>
     </Card>
-  )
+  );
 }

569e70d4de1a543279b937497332adbfeea76fc7
naol86 <naol.kasinet@a2sv.org>
2024-08-26 01:30:43 +0300

    fix credit card page

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/types/investmentType.ts
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/types/investmentType.ts
@@ -1,20 +1,20 @@
+// import { MonthlyRevenue } from '@/components/Charts/MonthlyRevenue';
 export interface YearlyInvestment {
   time: string;
   value: number;
-};
+}
 
 export interface InvestmentType {
-  data:{
-      totalInvestment: number;
-      rateOfReturn: number;
-      yearlyTotalInvestment: YearlyInvestment[];
-  }
-};
+  data: {
+    totalInvestment: number;
+    rateOfReturn: number;
+    yearlyTotalInvestment: YearlyInvestment[];
+    monthlyRevenue: YearlyInvestment[];
+  };
+}
 
 export interface ApiResponse {
   success: boolean;
   message: string;
   data: InvestmentType;
-};
-
-
+}

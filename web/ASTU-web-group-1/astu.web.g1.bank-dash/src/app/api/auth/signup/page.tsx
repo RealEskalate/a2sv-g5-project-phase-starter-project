8a3b56b9d522b0a4b0e54052272dd85bb786c52d
kiya kebe <kiyakebe799@gmail.com>
2024-08-25 16:09:27 +0300

    added: toastify on login and signup

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/api/auth/signup/page.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/app/api/auth/signup/page.tsx
@@ -1,20 +1,20 @@
-import SignUpForm from '@/components/Form/AuthForm/SignUpForm';
-import Image from 'next/image';
+import SignUpForm from "@/components/Form/AuthForm/SignUpForm";
+import Image from "next/image";
 
 export default function page() {
   return (
-    <div className='flex justify-around items-center min-h-screen'>
-      <div className='hidden md:w-1/2 minrelative lg:flex items-center justify-center'>
+    <div className="flex justify-around items-center min-h-screen">
+      <div className="hidden md:w-1/2 minrelative lg:flex items-center justify-center">
         <Image
-          src='/assets/icons/signup.svg'
-          alt='hello'
+          src="/assets/icons/signup.svg"
+          alt="hello"
           width={300}
           height={300}
-          className='object-cover'
+          className="object-cover"
         />
       </div>
-      <div className='min-h-[100vh] lg:bg-slate-200 w-full lg:w-1/2 lg:p-6 flex items-center justify-center '>
-      <SignUpForm />
+      <div className="min-h-[100vh] lg:bg-slate-200 w-full lg:w-1/2 lg:p-6 flex items-center justify-center ">
+        <SignUpForm />
       </div>
     </div>
   );

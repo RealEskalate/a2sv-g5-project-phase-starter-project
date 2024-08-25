f921c378208dcdd0c303eaa76bc9256558e65927
firaol <firaolkiya6@gmail.com>
2024-08-25 18:47:31 +0300

    implmened presentation layer

--- mobile/ASTU-mobile-group-1/Ecommerce/lib/features/chat/presentation/widget/user_profile.dart
+++ mobile/ASTU-mobile-group-1/Ecommerce/lib/features/chat/presentation/widget/user_profile.dart
@@ -3,12 +3,17 @@ import '../../../../core/constants/colors.dart';
 import '../../../../core/constants/profile_photo.dart';
 import '../../../auth/presentation/pages/pages.dart';
 
-Widget showUser(){
-  return CircleAvatar(
-    minRadius: 30,
-    maxRadius: 40,
-    backgroundColor: ChatColors.getRandomColor(),
-  child: Image(
-    image: AssetImage(ChatUserProfilePhoto.getRandomPhoto())),
+Widget showUser({  VoidCallback? onClicked}){
+  return InkWell(
+    onTap: () {
+      onClicked ?? () {};
+    },
+    child: CircleAvatar(
+      minRadius: 30,
+      maxRadius: 40,
+      backgroundColor: ChatColors.getRandomColor(),
+    child: Image(
+      image: AssetImage(ChatUserProfilePhoto.getRandomPhoto())),
+    ),
   );
 }

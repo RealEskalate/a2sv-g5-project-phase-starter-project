10a68174f7ac496e220cd71f50edffa281ee517a
bisratberhanu <bisratbnegus@gmail.com>
2024-08-26 00:05:14 +0300

    implement on main function

--- backend/AAiT-backend-group-9/Blog_Starter/main.go
+++ backend/AAiT-backend-group-9/Blog_Starter/main.go
@@ -6,6 +6,11 @@ import (
 
 	route "Blog_Starter/api/router"
 	"Blog_Starter/config"
+	"Blog_Starter/repository"
+	"Blog_Starter/usecase"
+
+	"Blog_Starter/domain"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -17,11 +22,20 @@ func main() {
 	// Initialize MongoDB client
 	db := config.NewMongoDatabase(env)
 	defer config.CloseMongoDBConnection(db)
+	database := db.Database(env.DBName)
 
 	// Set up the Gin router
 
 	router := gin.Default()
 	router.Static("/upload", "./upload")
+
+
+	ur := repository.NewUserRepository(database, domain.CollectionUser)
+	timeout := time.Duration(env.ContextTimeout) * time.Second
+	userUsecase := usecase.NewUserUsecase(ur, timeout)
+	// Start the routine to clean up expired users every 24 hours
+	userUsecase.StartExpiredUserCleanup(24 * time.Hour)
+	
 
 	// Global CORS configuration
 	config := cors.DefaultConfig()
@@ -31,7 +45,6 @@ func main() {
 	router.Use(cors.New(config))
 
 	// Set up routes
-	timeout := time.Duration(env.ContextTimeout) * time.Second
 	route.Setup(env, timeout, db, router)
 
 	// Run the server

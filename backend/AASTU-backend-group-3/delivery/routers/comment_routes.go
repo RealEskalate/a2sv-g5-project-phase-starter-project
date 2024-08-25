a0c85d84a73bca629e1f5718582576cba71b8412
bereketsintayehu <bereketsintayehu32@gmail.com>
2024-08-26 01:46:12 +0300

    bug fix on comment custom error

--- backend/AASTU-backend-group-3/delivery/routers/comment_routes.go
+++ backend/AASTU-backend-group-3/delivery/routers/comment_routes.go
@@ -19,21 +19,21 @@ func SetUpComment(router *gin.Engine) {
 	comments := router.Group("/")
 	comments.Use(infrastracture.AuthMiddleware())
 	{
-		comments.POST("/comments",infrastracture.EligibilityMiddleware() ,commentController.CreateComment)
-		comments.PUT("/comments/:id",infrastracture.EligibilityMiddleware() ,commentController.UpdateComment)
-		comments.DELETE("/comments/:id", infrastracture.EligibilityMiddleware(),commentController.DeleteComment)
-		comments.GET("/comments/:id", infrastracture.EligibilityMiddleware(),commentController.GetCommentByID)
-		comments.GET("/posts/:postID/comments", infrastracture.EligibilityMiddleware(),commentController.GetComments)
+		comments.POST("/comments", commentController.CreateComment)
+		comments.PUT("/comments/:id", commentController.UpdateComment)
+		comments.DELETE("/comments/:id", commentController.DeleteComment)
+		comments.GET("/comments/:id", commentController.GetCommentByID)
+		comments.GET("/posts/:postID/comments", commentController.GetComments)
 
-		comments.POST("/replies", infrastracture.EligibilityMiddleware(),commentController.CreateReply)
-		comments.PUT("/replies/:id", infrastracture.EligibilityMiddleware(),commentController.UpdateReply)
-		comments.DELETE("/replies/:id", infrastracture.EligibilityMiddleware(),commentController.DeleteReply)
-		comments.GET("/replies/comments/:commentID", infrastracture.EligibilityMiddleware(),commentController.GetReplies)
+		comments.POST("/replies", commentController.CreateReply)
+		comments.PUT("/replies/:id", commentController.UpdateReply)
+		comments.DELETE("/replies/:id", commentController.DeleteReply)
+		comments.GET("/replies/comments/:commentID", commentController.GetReplies)
 
-		comments.POST("/comments/:id/like", infrastracture.EligibilityMiddleware(),commentController.LikeComment)
-		comments.POST("/comments/:id/unlike",infrastracture.EligibilityMiddleware() ,commentController.UnlikeComment)
-		comments.POST("/replies/:id/like", infrastracture.EligibilityMiddleware(),commentController.LikeReply)
-		comments.POST("/replies/:id/unlike", infrastracture.EligibilityMiddleware(),commentController.UnlikeReply)
+		comments.POST("/comments/:id/like", commentController.LikeComment)
+		comments.POST("/comments/:id/unlike" ,commentController.UnlikeComment)
+		comments.POST("/replies/:id/like", commentController.LikeReply)
+		comments.POST("/replies/:id/unlike", commentController.UnlikeReply)
 
 	}
 

a50a34241b093792251fb24659e233c9dc9000e0
ephyg <84809084+ephyg@users.noreply.github.com1>
2024-08-25 17:28:36 +0300

    chore: Update CardList component to fetch and display data from API

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/CardList/CardList.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/CardList/CardList.tsx
@@ -1,47 +1,28 @@
+"use client";
 import React from "react";
 import CardListCard from "./CardiListCard";
+import { useGetAllCardsQuery } from "@/lib/redux/slices/cardSlice";
 
 const CardList = () => {
+  const { data, isLoading } = useGetAllCardsQuery({ page: 1, size: 5 });
+  if (isLoading) return <div>Loading</div>;
+  const imageList = ["/assets/images/cardList.png"];
+  const bankList = ["CBE", "DBL Bank", "BRC Bank", "ABM Bank"];
   return (
     <>
       <div className="flex flex-col  w-full  ">
         <p className="text-[#333B69] pb-2 font-semibold">Card List</p>
         <div className="h-80 lg:h-[16.5rem] xl:h-80 overflow-y-scroll whitespace-nowrap scroll-smooth">
-          <CardListCard
-            cardType="Secondary"
-            bank="DBL Bank"
-            cardNumber="**** **** 5600"
-            imageUrl="/assets/images/cardList.png"
-            namainCard="William"
-          />
-          <CardListCard
-            cardType="Secondary"
-            bank="BRC Bank"
-            cardNumber="**** **** 4300"
-            imageUrl="/assets/images/cardList.png"
-            namainCard="Michael"
-          />
-          <CardListCard
-            cardType="Secondary"
-            bank="ABM Bank"
-            cardNumber="**** **** 7560"
-            imageUrl="/assets/images/cardList.png"
-            namainCard="Edward"
-          />
-          <CardListCard
-            cardType="Secondary"
-            bank="BRC Bank"
-            cardNumber="**** **** 4300"
-            imageUrl="/assets/images/cardList.png"
-            namainCard="Michael"
-          />
-          <CardListCard
-            cardType="Secondary"
-            bank="BRC Bank"
-            cardNumber="**** **** 4300"
-            imageUrl="/assets/images/cardList.png"
-            namainCard="Michael"
-          />
+          {data?.content?.map((card, index) => (
+            <CardListCard
+              key={card.id}
+              cardType={card.cardType}
+              bank={bankList[index % bankList.length]}
+              cardNumber={card.semiCardNumber}
+              imageUrl={imageList[index % imageList.length]}
+              namainCard={card.cardHolder}
+            />
+          ))}
         </div>
       </div>
     </>

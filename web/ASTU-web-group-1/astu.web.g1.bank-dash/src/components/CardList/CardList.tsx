eb2c10aabc3291f5bbc64e0eed2ad478efefcced
ephyg <84809084+ephyg@users.noreply.github.com1>
2024-08-25 17:42:22 +0300

    fix: Update CardList component to display loading skeleton when data is being fetched

--- web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/CardList/CardList.tsx
+++ web/ASTU-web-group-1/astu.web.g1.bank-dash/src/components/CardList/CardList.tsx
@@ -2,12 +2,26 @@
 import React from "react";
 import CardListCard from "./CardiListCard";
 import { useGetAllCardsQuery } from "@/lib/redux/slices/cardSlice";
+import CardListCardSkeleton from "../AllSkeletons/CardListSkeleton/CardListSkeleton";
 
 const CardList = () => {
   const { data, isLoading } = useGetAllCardsQuery({ page: 1, size: 5 });
-  if (isLoading) return <div>Loading</div>;
+  if (isLoading)
+    return (
+      <div>
+        {[...Array(5)].map((_, index) => (
+          <CardListCardSkeleton key={index} />
+        ))}
+      </div>
+    );
   const imageList = ["/assets/images/cardList.png"];
   const bankList = ["CBE", "DBL Bank", "BRC Bank", "ABM Bank"];
+  const formatCardNumber = (cardNumber: string) => {
+    // add space after every 4 digits
+
+    const temp = cardNumber.replace(/\d(?=\d{4})/g, "*");
+    return temp.slice(0, 4) + " " + temp.slice(4, 8);
+  };
   return (
     <>
       <div className="flex flex-col  w-full  ">
@@ -18,7 +32,7 @@ const CardList = () => {
               key={card.id}
               cardType={card.cardType}
               bank={bankList[index % bankList.length]}
-              cardNumber={card.semiCardNumber}
+              cardNumber={formatCardNumber(card.semiCardNumber)}
               imageUrl={imageList[index % imageList.length]}
               namainCard={card.cardHolder}
             />

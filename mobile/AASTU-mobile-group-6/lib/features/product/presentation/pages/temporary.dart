2b86856a9bf2f016a0a8d039e9bc3c4d6d901759
estif <kingestiff@gmail.com>
2024-08-25 10:56:28 +0300

    only home page darkmode

---
+++ mobile/AASTU-mobile-group-6/lib/features/product/presentation/pages/temporary.dart
@@ -0,0 +1,11 @@
+// Text(
+//   'Your Text Here',
+//   style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+//     fontSize: 18.0, // Your desired font size
+//     fontWeight: FontWeight.bold, // Your desired font weight
+//   ),
+// ),
+
+
+
+// Theme.of(context).colorScheme.onSurface,

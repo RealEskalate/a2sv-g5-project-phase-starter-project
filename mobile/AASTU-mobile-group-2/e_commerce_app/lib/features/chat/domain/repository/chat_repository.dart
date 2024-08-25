9035ca912d298883bd436c5d31e866e134b17c8b
Merbebt <merbebtsolomon@gmail.com>
2024-08-25 12:59:51 +0300

    repo

--- mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/domain/repository/chat_repository.dart
+++ mobile/AASTU-mobile-group-2/e_commerce_app/lib/features/chat/domain/repository/chat_repository.dart
@@ -4,8 +4,8 @@ import 'package:e_commerce_app/core/failure/failure.dart';
 import '../entities/chat_entity.dart';
 
 abstract class ChatRepository {
-  Future<Either<Failure,ChatEntity>> getChatById(String chatId);
-  Future<Either<Failure,List<ChatEntity>>> getAllChats(String userId);
+  Future<Either<Failure,List<ChatEntity>>> getAllChats();
   Future<Either<Failure,ChatEntity>> createChatById(String sellerId);
+  Future<Either<Failure,ChatEntity>> getChatById(String chatId);
   Future<Either<Failure,bool>> deleteChatById(String chatId);
 }

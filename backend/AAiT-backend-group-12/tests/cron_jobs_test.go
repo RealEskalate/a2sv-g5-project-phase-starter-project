2e21f18b8a2d3db04d3116092380c8d429da4231
Nathan M.S <natms24@gmail.com>
2024-08-25 23:52:16 +0300

    Add test:cron_jobs delete unverified logic test

--- backend/AAiT-backend-group-12/tests/cron_jobs_test.go
+++ backend/AAiT-backend-group-12/tests/cron_jobs_test.go
@@ -3,6 +3,7 @@ package tests
 import (
 	"blog_api/delivery/env"
 	"blog_api/domain"
+	cron_jobs "blog_api/infrastructure/cron"
 	initdb "blog_api/infrastructure/db"
 	"context"
 	"testing"
@@ -16,10 +17,10 @@ import (
 const LIFESPAN = 12
 
 var UserData = []domain.User{
-	{Username: "test1", CreatedAt: time.Now().Add(-LIFESPAN * time.Hour), IsVerified: false},
-	{Username: "test2", CreatedAt: time.Now().Add(-(LIFESPAN + 1) * time.Hour), IsVerified: false},
-	{Username: "test3", CreatedAt: time.Now(), IsVerified: false},
-	{Username: "test4", CreatedAt: time.Now().Add(-LIFESPAN * time.Hour), IsVerified: true},
+	{Username: "test1", Email: "test1@gmail.com", CreatedAt: time.Now().Add(-LIFESPAN * time.Hour), IsVerified: false},
+	{Username: "test2", Email: "test2@gmail.com", CreatedAt: time.Now().Add(-(LIFESPAN + 1) * time.Hour), IsVerified: false},
+	{Username: "test3", Email: "test3@gmail.com", CreatedAt: time.Now(), IsVerified: false},
+	{Username: "test4", Email: "test4@gmail.com", CreatedAt: time.Now().Add(-LIFESPAN * time.Hour), IsVerified: true},
 }
 
 type CronJobsTestSuite struct {
@@ -54,7 +55,30 @@ func (suite *CronJobsTestSuite) TearDownSuite() {
 }
 
 func (suite *CronJobsTestSuite) TestDeleteUnverifiedUsers() {
+	for _, user := range UserData {
+		_, err := suite.collection.InsertOne(context.Background(), user)
+		suite.Nil(err)
+	}
 
+	cron_jobs.DeleteUnverifiedUsers(suite.collection, time.Duration(LIFESPAN)*time.Hour)()
+
+	count, err := suite.collection.CountDocuments(context.Background(), bson.D{})
+	suite.Nil(err)
+	suite.Equal(count, int64(2))
+
+	res := suite.collection.FindOne(context.Background(), bson.D{{Key: "username", Value: UserData[0].Username}})
+	suite.NotNil(res.Err())
+	suite.Equal(res.Err(), mongo.ErrNoDocuments)
+
+	res = suite.collection.FindOne(context.Background(), bson.D{{Key: "username", Value: UserData[1].Username}})
+	suite.NotNil(res.Err())
+	suite.Equal(res.Err(), mongo.ErrNoDocuments)
+
+	res = suite.collection.FindOne(context.Background(), bson.D{{Key: "username", Value: UserData[2].Username}})
+	suite.Nil(res.Err())
+
+	res = suite.collection.FindOne(context.Background(), bson.D{{Key: "username", Value: UserData[3].Username}})
+	suite.Nil(res.Err())
 }
 
 func TestCronJobs(t *testing.T) {

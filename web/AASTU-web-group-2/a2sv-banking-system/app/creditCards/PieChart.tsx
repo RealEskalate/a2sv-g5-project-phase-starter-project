1bb1290e57b292b9942fcc660f3044790a355ab5
MTA2002 <mahfouz.teyib@aastustudent.edu.et>
2024-08-25 23:29:50 +0300

    Finalized Credit Cards and Settings Page

--- web/AASTU-web-group-2/a2sv-banking-system/app/creditCards/PieChart.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/creditCards/PieChart.tsx
@@ -75,7 +75,7 @@ export function PieChartPage() {
   const months = React.useMemo(() => desktopData.map((item) => item.month), []);
 
   return (
-    <div className="flex flex-col gap-5 bg-white rounded-2xl py-3 justify-center px-4 items-center shadow-sm h-full dark:bg-[#212c35] dark:border dark:border-[#333B69] dark:shadow-2xl">
+    <div className="flex flex-col gap-5 bg-white rounded-2xl py-3 justify-center px-4 items-center shadow-sm h-full dark:bg-[#050914] dark:border dark:border-[#333B69] dark:shadow-2xl">
       <Card
         data-chart={id}
         className="flex flex-col w-52 shadow-none bg-transparent border-none "

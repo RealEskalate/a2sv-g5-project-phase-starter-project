fe46d1e9cf253dc6156dd4702f9f38ebad6759f6
fasill <fasilhawultie19@gmail.com>
2024-08-25 20:30:39 +0300

    middleware modified

--- web/ASTU-web-group-2/banking-dashboard/middleware.ts
+++ web/ASTU-web-group-2/banking-dashboard/middleware.ts
@@ -7,6 +7,7 @@ export async function middleware(req: NextRequest) {
   const sessionToken = await getToken({ req, secret: process.env.AUTH_SECRET });
   const { pathname } = req.nextUrl;
 
+
   // Check if the token is expired
   if (sessionToken && isTokenExpired(sessionToken.accessToken)) {
     return NextResponse.redirect(new URL('/', req.url));
@@ -17,19 +18,9 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL('/dashboard', req.url));
   }
 
-  // Redirect unauthenticated users to `/signup` if they try to access routes other than `/`, `/signup`, or `/login`
-  if (!sessionToken && pathname !== '/' && pathname !== '/signup' && pathname !== '/login') {
+  // Redirect users trying to access `/login` to `/` if they are already authenticated
+  if (sessionToken && pathname === '/login') {
     return NextResponse.redirect(new URL('/', req.url));
-  }
-
-  // Redirect users trying to access `/login` to `/`
-  if (pathname === '/login') {
-    return NextResponse.redirect(new URL('/', req.url));
-  }
-
-  // Handle non-existent routes by redirecting to a 404 page
-  if (pathname !== '/' && pathname !== '/signup' && pathname !== '/login' && !pathname.startsWith('/api') && !pathname.startsWith('/static') && !pathname.startsWith('/favicon.ico')) {
-    return NextResponse.redirect(new URL('/404', req.url));
   }
 
   return NextResponse.next();

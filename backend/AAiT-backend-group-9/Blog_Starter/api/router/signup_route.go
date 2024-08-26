3ecab0fa828f2ecd1ce40df18215e6fec1365db3
Yani201 <yanetmekuria22@gmail.com>
2024-08-26 03:25:47 +0300

    implement signin with google

--- backend/AAiT-backend-group-9/Blog_Starter/api/router/signup_route.go
+++ backend/AAiT-backend-group-9/Blog_Starter/api/router/signup_route.go
@@ -8,20 +8,27 @@ import (
 	"Blog_Starter/domain"
 	"Blog_Starter/repository"
 	"Blog_Starter/usecase"
+	"Blog_Starter/utils/infrastructure"
+
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // NewSignupRouter sets up the signup routes.
 func NewSignupRouter(env *config.Env, timeout time.Duration, db *mongo.Client, group *gin.RouterGroup) {
-    database := db.Database(env.DBName) // Replace with your actual database name
-    or := repository.NewOtpRepository(database, domain.CollectionOTP)
-    ur := repository.NewUserRepository(database, domain.CollectionUser)
-    sc := controller.NewSignUpController(
-        usecase.NewSignUpUsecase(ur, timeout),
-        usecase.NewOtpUsecase(or, timeout),
-    )
-    group.POST("/signup", sc.SignUp)
-    group.PATCH("/verifyemail", sc.VerifyEmail)
-    group.PATCH("/resendotp", sc.ResendOTP)
+	database := db.Database(env.DBName) // Replace with your actual database name
+	or := repository.NewOtpRepository(database, domain.CollectionOTP)
+	ur := repository.NewUserRepository(database, domain.CollectionUser)
+
+	tm := &infrastructure.NewTokenManager{}
+	om := &infrastructure.NewOAuthManager{}
+
+	sc := controller.NewSignUpController(
+		usecase.NewSignUpUsecase(ur, tm, om, env, timeout),
+		usecase.NewOtpUsecase(or, timeout),
+	)
+	group.POST("/signup", sc.SignUp)
+	group.POST("/verifyemail", sc.VerifyEmail)
+	group.POST("/resendotp", sc.ResendOTP)
+	group.POST("/continuewithgoogle", sc.FederatedSignup)
 }

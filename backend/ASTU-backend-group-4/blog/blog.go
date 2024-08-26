4a822daf1b80d80608fef2e76ba6aa8d1169a5b4
Mohammed Ali <mohaali482@gmail.com>
2024-08-26 12:05:33 +0300

    feat(blog): change format

--- backend/ASTU-backend-group-4/blog/blog.go
+++ backend/ASTU-backend-group-4/blog/blog.go
@@ -89,23 +89,23 @@ type BlogSummary struct {
 
 type Comment struct {
 	ID        string    `json:"id,omitempty" bson:"_id,omitempty"`
-	BlogID    string    `json:"blog_id,omitempty"`
-	AuthorID  string    `json:"author_id,omitempty"`
-	Content   string    `json:"content,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	BlogID    string    `json:"blog_id" bson:"blog_id"`
+	AuthorID  string    `json:"author_id" bson:"author_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 }
 
 type Like struct {
 	ID     string `json:"id,omitempty" bson:"_id,omitempty"`
-	BlogID string `json:"blog_id,omitempty"`
-	UserID string `json:"user_id,omitempty"`
+	BlogID string `json:"blog_id,omitempty" bson:"blog_id"`
+	UserID string `json:"user_id,omitempty" bson:"user_id"`
 }
 
 type Dislike struct {
 	ID     string `json:"id,omitempty" bson:"_id,omitempty"`
-	BlogID string `json:"blog_id,omitempty"`
-	UserID string `json:"user_id,omitempty"`
+	BlogID string `json:"blog_id,omitempty" bson:"blog_id"`
+	UserID string `json:"user_id,omitempty" bson:"user_id"`
 }
 
 type FilterQuery struct {

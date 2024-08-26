5be2e402f57f5abcd782b480703b7e835c21271b
Samuel Keno <sgere195@gmail.com>
2024-08-26 11:39:03 +0300

    Sign up testing

--- web/ASTU-web-group-2/banking-dashboard/app/components/auth/signup/Preference.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/auth/signup/Preference.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import * as Yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import ToggleSwitch from './ToggleSwitch'; 
-import { MainData } from './Signup'; 
-import { useSignUpMutation } from '@/lib/service/TransactionService';
-import { useRouter } from 'next/navigation'; 
-import { signIn } from 'next-auth/react';
-import notify from '@/utils/notify';
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import ToggleSwitch from "./ToggleSwitch";
+import { MainData } from "./Signup";
+import { useSignUpMutation } from "@/lib/service/TransactionService";
+import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
+import notify from "@/utils/notify";
 const schema = Yup.object().shape({
-  currency: Yup.string().required('Currency is required'),
-  timeZone: Yup.string().required('TimeZone is required'),
+  currency: Yup.string().required("Currency is required"),
+  timeZone: Yup.string().required("TimeZone is required"),
   sentOrReceiveDigitalCurrency: Yup.boolean().required(),
   receiveMerchantOrder: Yup.boolean().required(),
   accountRecommendations: Yup.boolean().required(),
@@ -38,22 +38,32 @@ interface SignUpResponse {
   };
 }
 
-const YourFormComponent: React.FC<YourFormComponentProps> = ({ setMainData, mainData }) => {
-  const router = useRouter(); 
-  const { control, handleSubmit, register, formState: { errors } } = useForm<FormValues>({
+const YourFormComponent: React.FC<YourFormComponentProps> = ({
+  setMainData,
+  mainData,
+}) => {
+  const router = useRouter();
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormValues>({
     resolver: yupResolver(schema),
     defaultValues: {
-      currency: mainData.preference?.currency || '',
-      timeZone: mainData.preference?.timeZone || '',
-      sentOrReceiveDigitalCurrency: mainData.preference?.sentOrReceiveDigitalCurrency || false,
+      currency: mainData.preference?.currency || "",
+      timeZone: mainData.preference?.timeZone || "",
+      sentOrReceiveDigitalCurrency:
+        mainData.preference?.sentOrReceiveDigitalCurrency || false,
       receiveMerchantOrder: mainData.preference?.receiveMerchantOrder || false,
-      accountRecommendations: mainData.preference?.accountRecommendations || false,
+      accountRecommendations:
+        mainData.preference?.accountRecommendations || false,
     },
   });
-  const [signUp,{isLoading}] = useSignUpMutation();
-  const [posting,setIsPosting] = useState(false);
+  const [signUp, { isLoading }] = useSignUpMutation();
+  const [posting, setIsPosting] = useState(false);
   const onSubmit = async (data: FormValues) => {
-    setIsPosting(true)
+    setIsPosting(true);
     const formattedData = {
       ...mainData,
       preference: {
@@ -65,12 +75,12 @@ const YourFormComponent: React.FC<YourFormComponentProps> = ({ setMainData, main
         twoFactorAuthentication: true,
       },
     };
-  
+
     setMainData(formattedData);
-  
+
     const res = await signUp(formattedData);
-  
-    if ('data' in res) {
+
+    if ("data" in res) {
       const loginRes = await signIn("credentials", {
         userName: mainData.username,
         password: mainData.password,
@@ -80,85 +90,87 @@ const YourFormComponent: React.FC<YourFormComponentProps> = ({ setMainData, main
         notify.error("Invalid Credentials");
       } else {
         router.push("/dashboard");
-        notify.success('Signup successful');
-     
+        notify.success("Signup successful");
       }
-      
-    } else if ('error' in res) {
-      let errorMessage = 'Signup failed';
-      if (res.error && 'data' in res.error) {
+    } else if ("error" in res) {
+      let errorMessage = "Signup failed";
+      if (res.error && "data" in res.error) {
         const errorData = (res.error as { data?: { message: string } }).data;
-        errorMessage = errorData?.message || 'Signup failed';
+        errorMessage = errorData?.message || "Signup failed";
       }
       notify.error(errorMessage);
-      console.error('Signup failed', res.error);
+      console.error("Signup failed", res.error);
     }
-    setIsPosting(false)
+    setIsPosting(false);
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-        <div className='w-full'>
-          <label className='block mb-1 font-400 text-[16px] text-[#232323] capitalize'>
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="w-full">
+          <label className="block mb-1 font-400 text-[16px] text-[#232323] capitalize">
             Currency
           </label>
           <input
             placeholder={`USD`}
-            className='w-full p-2 border border-[#DFEAF2] rounded-[15px] focus:outline-none focus:ring-2 focus:ring-blue-200'
-            {...register('currency')}
+            className="w-full p-2 border border-[#DFEAF2] rounded-[15px] focus:outline-none focus:ring-2 focus:ring-blue-200"
+            {...register("currency")}
           />
-          {errors.currency && <p className='text-red-500'>{errors.currency.message}</p>}
+          {errors.currency && (
+            <p className="text-red-500">{errors.currency.message}</p>
+          )}
         </div>
-        <div className='w-full'>
-          <label className='block mb-1 font-400 text-[16px] text-[#232323] capitalize'>
+        <div className="w-full">
+          <label className="block mb-1 font-400 text-[16px] text-[#232323] capitalize">
             TimeZone
           </label>
           <input
             placeholder={`{GMT-12:00} International Data Line West`}
-            className='w-full p-2 border border-[#DFEAF2] rounded-[15px] focus:outline-none focus:ring-2 focus:ring-blue-200'
-            {...register('timeZone')}
+            className="w-full p-2 border border-[#DFEAF2] rounded-[15px] focus:outline-none focus:ring-2 focus:ring-blue-200"
+            {...register("timeZone")}
           />
-          {errors.timeZone && <p className='text-red-500'>{errors.timeZone.message}</p>}
+          {errors.timeZone && (
+            <p className="text-red-500">{errors.timeZone.message}</p>
+          )}
         </div>
       </div>
 
-      <div className='space-y-4'>
-        <label className='block mb-1 font-400 text-[16px] text-[#232323] capitalize'>
+      <div className="space-y-4">
+        <label className="block mb-1 font-400 text-[16px] text-[#232323] capitalize">
           Notification
         </label>
-        <div className='md:grid gap-3'>
+        <div className="md:grid gap-3">
           <Controller
-            name='sentOrReceiveDigitalCurrency'
+            name="sentOrReceiveDigitalCurrency"
             control={control}
             render={({ field }) => (
               <ToggleSwitch
-                id='sentOrReceiveDigitalCurrency'
-                label='I send or receive digital currency'
+                id="sentOrReceiveDigitalCurrency"
+                label="I send or receive digital currency"
                 checked={field.value}
                 onChange={field.onChange}
               />
             )}
           />
           <Controller
-            name='receiveMerchantOrder'
+            name="receiveMerchantOrder"
             control={control}
             render={({ field }) => (
               <ToggleSwitch
-                id='receiveMerchantOrder'
-                label='I receive merchant order'
+                id="receiveMerchantOrder"
+                label="I receive merchant order"
                 checked={field.value}
                 onChange={field.onChange}
               />
             )}
           />
           <Controller
-            name='accountRecommendations'
+            name="accountRecommendations"
             control={control}
             render={({ field }) => (
               <ToggleSwitch
-                id='accountRecommendations'
-                label='There are recommendations for my account'
+                id="accountRecommendations"
+                label="There are recommendations for my account"
                 checked={field.value}
                 onChange={field.onChange}
               />
@@ -166,13 +178,17 @@ const YourFormComponent: React.FC<YourFormComponentProps> = ({ setMainData, main
           />
         </div>
       </div>
-      <div className='flex justify-end'>
+      <div className="flex justify-end">
         <button
-          type='submit'
-          className='w-full md:w-auto px-4 py-2 bg-[#083E9E] text-white rounded-lg'
-          disabled = {posting}
-        >{posting?<div className="w-8 h-8 border-4 border-dashed rounded-full animate-spin [animation-duration:3s]  border-white mx-auto"></div>:"Register"}
-          
+          type="submit"
+          className="w-full md:w-auto px-4 py-2 bg-[#083E9E] text-white rounded-lg"
+          disabled={posting}
+        >
+          {posting ? (
+            <div className="w-8 h-8 border-4 border-dashed rounded-full animate-spin [animation-duration:3s]  border-white mx-auto"></div>
+          ) : (
+            "Register"
+          )}
         </button>
       </div>
     </form>

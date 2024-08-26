819f91da2055a256504a0ca7ac2d6299d9c399d5
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2024-08-26 12:33:12 +0300

    final updates

--- web/AASTU-web-group-2/a2sv-banking-system/app/dashboard/page.tsx
+++ web/AASTU-web-group-2/a2sv-banking-system/app/dashboard/page.tsx
@@ -54,21 +54,21 @@ type AllowedProperties = {
 type SessionDataType = {
   user: Data;
 };
-export const ListCardLoading = () => {
-  return (
-    <div className="flex gap-3 items-center rounded-2xl px-5 py-4 bg-white dark:bg-[#020817] w-[48%] md:w-[23%] animate-pulse">
-      <div className="text-3xl px-2 py-2 rounded-xl bg-gray-200 dark:bg-[#333B69]">
-        <div className="w-8 h-8 bg-gray-300 dark:bg-[#555B85] rounded-full"></div>
-      </div>
-      <div className="flex justify-between w-full flex-col">
-        <div>
-          <div className="h-4 bg-gray-300 dark:bg-[#555B85] rounded w-full mb-2"></div>
-          <div className="h-4 bg-gray-300 dark:bg-[#555B85] rounded w-full"></div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// export const ListCardLoading = () => {
+//   return (
+//     <div className="flex gap-3 items-center rounded-2xl px-5 py-4 bg-white dark:bg-[#020817] w-[48%] md:w-[23%] animate-pulse">
+//       <div className="text-3xl px-2 py-2 rounded-xl bg-gray-200 dark:bg-[#333B69]">
+//         <div className="w-8 h-8 bg-gray-300 dark:bg-[#555B85] rounded-full"></div>
+//       </div>
+//       <div className="flex justify-between w-full flex-col">
+//         <div>
+//           <div className="h-4 bg-gray-300 dark:bg-[#555B85] rounded w-full mb-2"></div>
+//           <div className="h-4 bg-gray-300 dark:bg-[#555B85] rounded w-full"></div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 export default function Home() {
   const [session, setSession] = useState<Data | null>(null);
   const [access_token, setAccess_token] = useState("");
@@ -203,7 +203,7 @@ export default function Home() {
             </div>
             <div className="flex space-x-6 overflow-x-auto [&::-webkit-scrollbar]:hidden">
               
-            {loading && <ListCardLoading/>}
+            {loading && <p>Loading ... </p>}
 
               {!loading && getCard &&
               getCard.map((items, index) => (

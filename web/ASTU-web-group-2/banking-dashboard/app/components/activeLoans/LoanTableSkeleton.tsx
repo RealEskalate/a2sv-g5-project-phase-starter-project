570d3d0705b95e20608c089e364f43b9ff4b75c8
wondmD <wondmenehdereje@gmail.com>
2024-08-26 12:02:52 +0300

    fixed issues with graph data

--- web/ASTU-web-group-2/banking-dashboard/app/components/activeLoans/LoanTableSkeleton.tsx
+++ web/ASTU-web-group-2/banking-dashboard/app/components/activeLoans/LoanTableSkeleton.tsx
@@ -4,7 +4,7 @@ const LoanTableSkeleton = () => {
       <div className="flex justify-center">
         <div className="w-full">
         
-          <div className="grid grid-cols-7 gap-4 mb-4">
+          <div className="grid md:grid-cols-7 grid-cols-3 gap-4 mb-4">
             <div className="hidden sm:block text-gray-500 text-[12px] md:text-[16px] font-medium">
               SL No
             </div>
@@ -30,14 +30,14 @@ const LoanTableSkeleton = () => {
     
           {/* Skeleton for table rows */}
           {Array.from({ length: 5 }).map((_, index) => (
-            <div className="grid grid-cols-7 gap-4 mb-2" key={index}>
+            <div className="grid md:grid-cols-7 grid-cols-3 gap-4 mb-2" key={index}>
               <div className="hidden sm:block bg-gray-400 h-[16px] rounded w-[60px]"></div>
               <div className="bg-gray-400 h-[16px] rounded w-[100px]"></div>
               <div className="bg-gray-400 h-[16px] rounded w-[100px]"></div>
               <div className="hidden sm:block bg-gray-400 h-[16px] rounded w-[80px]"></div>
               <div className="hidden sm:block bg-gray-400 h-[16px] rounded w-[100px]"></div>
               <div className="hidden sm:block bg-gray-400 h-[16px] rounded w-[100px]"></div>
-              <div className="bg-gray-400 h-[30px] rounded w-[70px]"></div>
+              <div className="bg-gray-400 h-[20px] rounded-lg w-[70px]"></div>
             </div>
           ))}
         </div>
